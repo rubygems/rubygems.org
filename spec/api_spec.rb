621864af3bc3483f126177839496ceb80fe6c62b
Nick Quaranto <nick@quaran.to>
2009-05-08 14:31:55 -0700

    Using Rack::Test since that's just better anyway

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe "Gemcutter API" do
   describe "on POST to /gems" do
     it "should accept a built gem" do
-      post '/gems', :gem => TestUploadedFile.new(File.join(File.dirname(__FILE__), *%w[gems test-0.0.0.gem]), 'application/rubygem', :binary)
+      post '/gems', :gem => Rack::Test::UploadedFile.new(File.join(File.dirname(__FILE__), *%w[gems test-0.0.0.gem]), 'application/rubygem', :binary)
       @response.status.should == 200
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,11 @@ $:.unshift File.join(File.dirname(__FILE__), '..')
 require 'rubygems'
 require 'spec'
 require 'gemcutter'
+
 require 'spec/interop/test'
 require 'sinatra/test'
+require 'rack/test'
+
 require 'rr'
 require 'webrat'
 
@@ -13,34 +16,4 @@ Test::Unit::TestCase.send :include, Sinatra::Test
 
 Spec::Runner.configure do |config|
   config.mock_with RR::Adapters::Rspec
-end
-
-# Ripped from actionpack.
-require 'tempfile'
-class TestUploadedFile
-  # The filename, *not* including the path, of the "uploaded" file
-  attr_reader :original_filename
-
-  # The content type of the "uploaded" file
-  attr_accessor :content_type
-
-  def initialize(path, content_type = Mime::TEXT, binary = false)
-    raise "#{path} file does not exist" unless File.exist?(path)
-    @content_type = content_type
-    @original_filename = path.sub(/^.*#{File::SEPARATOR}([^#{File::SEPARATOR}]+)$/) { $1 }
-    @tempfile = Tempfile.new(@original_filename)
-    @tempfile.set_encoding(Encoding::BINARY) if @tempfile.respond_to?(:set_encoding)
-    @tempfile.binmode if binary
-    FileUtils.copy_file(path, @tempfile.path)
-  end
-
-  def path #:nodoc:
-    @tempfile.path
-  end
-
-  alias local_path path
-
-  def method_missing(method_name, *args, &block) #:nodoc:
-    @tempfile.__send__(method_name, *args, &block)
-  end
 end
