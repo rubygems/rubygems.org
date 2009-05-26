cf529506aefd39f4091cc5ac1797a4d4d8f991ab
Nick Quaranto <nick@quaran.to>
2009-05-26 08:18:43 -0400

    Moving the index over to Rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title
+    "gemcutter"
+  end
+
+  def subtitle
+    "kickass gem hosting"
+  end
+
+  def page_title
+    "#{title}: #{subtitle}"
+  end
 end
