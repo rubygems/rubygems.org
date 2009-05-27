e6b22245d1ddf9b0f2ce0fc2cfa3b282393373a8
Nick Quaranto <nick@quaran.to>
2009-05-27 18:28:28 -0400

    Unvendoring test gems, and getting the features to work

---
+++ features/support/env.rb
@@ -0,0 +1,27 @@
+# Sets up the Rails environment for Cucumber
+ENV["RAILS_ENV"] ||= "cucumber"
+require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
+require 'cucumber/rails/world'
+require 'cucumber/formatter/unicode' # Comment out this line if you don't want Cucumber Unicode support
+Cucumber::Rails.use_transactional_fixtures
+Cucumber::Rails.bypass_rescue # Comment out this line if you want Rails own error handling 
+                              # (e.g. rescue_action_in_public / rescue_responses / rescue_from)
+require 'factory_girl'
+Dir[File.join(Rails.root, 'test', 'factories', '*.rb')].each { |f| require f }
+
+require 'test/unit/assertions'
+World(Test::Unit::Assertions)
+
+require 'webrat'
+Webrat.configure do |config|
+  config.mode = :rails
+end
+
+require 'webrat/core/matchers'
+require 'webrat/core/matchers/have_tag'
+
+#class Test::Unit::TestCase
+#  include RR::Adapters::TestUnit
+#end
+
+HOST = "localhost"
