1333c06e6043adbb5cf9ec98e2c404d81dd2ae1c
Nick Quaranto <nick@quaran.to>
2009-05-29 08:38:42 -0400

    Not exactly happy about this stubbing method, but I like defining the gemspec in code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -27,3 +27,24 @@ def regenerate_index
     "server/source_index"]
   Gemcutter.indexer.generate_index
 end
+
+def gem_spec
+  Gem::Specification.new do |s|
+    s.name = %q{test}
+    s.version = "0.0.0"
+
+    s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
+    s.authors = ["Joe User"]
+    s.date = %q{2009-05-22}
+    s.description = %q{This is my awesome gem.}
+    s.email = %q{joe@user.com}
+    s.files = [
+      "README.textile",
+      "Rakefile",
+      "VERSION.yml",
+      "lib/test.rb",
+      "test/test_test.rb"
+    ]
+    s.homepage = %q{http://user.com/test}
+  end
+end
