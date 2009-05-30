2e69e08e518cf8ccd877a573b83f5b015cfd7be8
Nick Quaranto <nick@quaran.to>
2009-05-30 00:01:55 -0400

    Naming the controller correctly and using to_param

--- test/functional/rubygems_controller_test.rb
+++ test/functional/rubygems_controller_test.rb
@@ -1,21 +1,21 @@
 require 'test_helper'
 
-class GemsControllerTest < ActionController::TestCase
+class RubygemsControllerTest < ActionController::TestCase
   context "On GET to index" do
     setup do
       @gems = (1..3).map { Factory(:rubygem) }
       get :index
     end
+
     should_respond_with :success
     should_render_template :index
     should_assign_to :gems
     should "render links" do
       @gems.each do |g|
         assert_contain g.name
-        assert_have_selector "a[href='#{gem_path(g)}']"
+        assert_have_selector "a[href='#{rubygem_path(g)}']"
       end
     end
   end
-
 end
 
