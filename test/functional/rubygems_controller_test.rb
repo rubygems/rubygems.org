76873866fb5278379e3a81e1d907e815f66a5224
Nick Quaranto <nick@quaran.to>
2009-05-30 00:32:34 -0400

    Moving the show view over

--- test/functional/rubygems_controller_test.rb
+++ test/functional/rubygems_controller_test.rb
@@ -17,5 +17,50 @@ class RubygemsControllerTest < ActionController::TestCase
       end
     end
   end
+
+  context "On GET to show" do
+    setup do
+      @gem = Factory(:rubygem)
+      @current_version = @gem.versions.first
+      get :show, :id => @gem.to_param
+    end
+
+    should_respond_with :success
+    should_render_template :show
+    should_assign_to :gem
+    should_assign_to :current_version
+    should "render info about the gem" do
+      assert_contain @gem.name
+      assert_contain @current_version.description
+      assert_contain @current_version.number
+      assert_contain @current_version.created_at.to_date.to_formatted_s(:long)
+      assert_not_contain "Versions"
+    end
+  end
+
+  context "On GET to show with a gem that has multiple versions" do
+    setup do
+      @gem = Factory(:rubygem)
+      @version = Factory(:version, :number => "1.0.0", :rubygem => @gem)
+      @gem.reload
+      @current_version = @gem.versions.first
+      get :show, :id => @gem.to_param
+    end
+
+    should_respond_with :success
+    should_render_template :show
+    should_assign_to :gem
+    should_assign_to :current_version
+    should "render info about the gem" do
+      assert_contain @gem.name
+      assert_contain @current_version.description
+      assert_contain @current_version.number
+      assert_contain @current_version.created_at.to_date.to_formatted_s(:long)
+
+      assert_contain "Versions"
+      assert_contain @gem.versions.last.number
+      assert_contain @gem.versions.last.created_at.to_date.to_formatted_s(:long)
+    end
+  end
 end
 
