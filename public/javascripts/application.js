5218a8b1ed113e5158a23749b811c0c367529c4c
Nick Quaranto <nick@quaran.to>
2009-05-30 14:53:54 -0400

    Removing prototype, adding jquery, and making the flash look decent

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+  divs = "#flash_success, #flash_notice, #flash_error"
+  $(divs).slideDown(function() {
+    timeout = setTimeout(function() {
+    $(divs).slideUp();
+    }, 10000);
+  });
+});
