b64c328a48b473518853ebe7240ae8ee6a12474c
Nick Quaranto <nick@quaran.to>
2009-05-29 22:15:02 -0400

    Removing the old hostess and pulling out the gemspec separately

--- apps/hostess/config.ru
+++
@@ -1,8 +0,0 @@
-$:.unshift File.expand_path(File.join(File.dirname(__FILE__)))
-
-require 'rubygems'
-require 'sinatra'
-require 'hostess'
-
-set :environment, :production
-run Gem::Hostess
--- test/factories/rubygem.rb
+++ test/factories/rubygem.rb
@@ -1,6 +1,7 @@
 Factory.define :rubygem do |rubygem|
   rubygem.name        { 'RGem' }
   rubygem.token       { 'asdf' }
-  rubygem.data        { gem_file("test-0.0.0.gem") }
   rubygem.association :user
+  rubygem.spec        { gem_spec }
+  rubygem.path        { gem_file.path }
 end
