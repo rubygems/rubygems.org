c184783a848be21099ca159412ff5ad0e39c1d5f
Nick Quaranto <nick@quaran.to>
2009-05-30 14:30:11 -0400

    Making the clearance given pages look decent, thanks to bostonrb and cssboy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :rubygems, :as => "gems"
+  map.resources :rubygems, :as => "gems", :collection => { :search => :get, :migrate => :get }
   map.root :controller => "home", :action => "index"
+  map.sign_up  'sign_up', :controller => 'clearance/users',    :action => 'new'
+  map.sign_in  'sign_in', :controller => 'clearance/sessions', :action => 'new'
+  map.sign_out 'sign_out',
+    :controller => 'clearance/sessions',
+    :action     => 'destroy',
+    :method     => :delete
 end
