112edd1ccb7665588f9963366fe4ba3670339dd8
Nick Quaranto <nick@quaran.to>
2009-05-31 01:13:31 -0400

    Getting ready to deploy

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+set :application, "gemcutter"
+set :server_ip, "gemcutter.org"
+set :deploy_to, "/home/rails/gemcutter"
+
+ssh_options[:port] = 1337
+set :port, 1337
+set :user, "rails"
+role :app, server_ip
+role :web, server_ip
+role :db,  server_ip, :primary => true
+
+set :scm, "git"
+set :repository,  "git://github.com/qrush/#{application}.git"
+set :deploy_via, :remote_cache
+set :ssh_options, { :forward_agent => true }
+
+namespace :deploy do
+  task :stop, :roles => [:app] do
+    puts "Use the deploy:restart task to restart the Rails application" 
+  end
+  task :start, :roles => [:app] do
+    puts "Use the deploy:restart task to restart the Rails application" 
+  end
+  task :restart, :roles => [:app] do
+    run "touch #{current_path}/tmp/restart.txt" 
+  end
+
+  task :setup do
+    run "mv #{release_path}/config/database.yml.tmp #{release_path}/config/database.yml"
+    run "rm -rf #{release_path}/server"
+    run "ln -s /home/rails/gems.gemcutter #{release_path}/server"
+    run "ln -s /home/rails/cache #{release_path}/cache"
+  end
+end
+
+after "deploy:finalize_update", "deploy:setup"
