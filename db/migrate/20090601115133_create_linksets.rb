50bc59040ae3876269e3311ee8ba064cc1471ff4
Nick Quaranto <nick@quaran.to>
2009-06-01 08:19:28 -0400

    Adding linksets

---
+++ db/migrate/20090601115133_create_linksets.rb
@@ -0,0 +1,19 @@
+class CreateLinksets < ActiveRecord::Migration
+  def self.up
+    create_table :linksets do |table|
+      table.integer :rubygem_id
+      table.string :home
+      table.string :wiki
+      table.string :docs
+      table.string :mail
+      table.string :code
+      table.string :bugs
+      table.timestamps
+    end
+
+  end
+
+  def self.down
+    drop_table :linksets
+  end
+end
