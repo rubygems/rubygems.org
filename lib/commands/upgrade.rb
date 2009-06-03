43952260f81740d939d712b480b3d6554c36f6d6
Nick Quaranto <nick@quaran.to>
2009-06-03 18:05:01 -0400

    Starting on the api keys

--- lib/commands/upgrade.rb
+++ lib/commands/upgrade.rb
@@ -1,6 +1,19 @@
+class Gem::StreamUI
+  def ask_for_password(message)
+    system "stty -echo"
+    password = ask(message)
+    system "stty echo"
+    password
+  end
+end
+
 class Gem::Commands::UpgradeCommand < Gem::Command
   def description
     'Upgrade your gem source to Gemcutter'
+  end
+
+  def ask_for_password(message)
+    ui.ask_for_password(message)
   end
 
   def initialize
@@ -8,14 +21,27 @@ class Gem::Commands::UpgradeCommand < Gem::Command
   end
 
   def execute
+    add_source
+    sign_in
+  end
+
+  def add_source
     if Gem.sources.include?(URL)
-      say("Gemcutter is already your primary gem source. Please use `gem downgrade` if you wish to no longer use Gemcutter.")
+      say "Gemcutter is already your primary gem source. Please use `gem downgrade` if you wish to no longer use Gemcutter."
     else
       say "Upgrading your primary gem source to gemcutter.org"
       Gem.sources.unshift URL
       Gem.configuration.write
     end
   end
+
+  def sign_in
+    say "Enter your Gemcutter credentials. Don't have an account yet? Create one at #{URL}/sign_up"
+
+    user = ask("Email: ")
+    password = ask_for_password("Password: ")
+  end
+
 end
 
 Gem::CommandManager.instance.register_command :upgrade
