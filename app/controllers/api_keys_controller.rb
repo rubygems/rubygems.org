a70558cf0cbffb62f0c5a125f3fabf39d9005c85
Nick Quaranto <nick@quaran.to>
2009-06-04 07:28:39 -0400

    Rendering api keys properly when requested

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -1,5 +1,10 @@
 class ApiKeysController < ApplicationController
   def show
-    render :text => "LOL"
+    authenticate_or_request_with_http_basic do |username, password|
+      @_current_user = User.authenticate(username, password)
+      if current_user.email_confirmed
+        render :text => current_user.api_key
+      end
+    end
   end
 end
