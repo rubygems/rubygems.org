ddaad6d5c6f010f0cbddd8f7d88bd1702c07ca07
Nick Quaranto <nick@quaran.to>
2009-06-04 07:28:39 -0400

    Rendering api keys properly when requested

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,4 +3,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 class UserTest < ActiveSupport::TestCase
   should_have_many :rubygems
 
+  should "create api key" do
+    user = Factory(:user)
+    assert_not_nil user.api_key
+  end
 end
