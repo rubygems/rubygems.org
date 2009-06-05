fd37c311adac31759645ab7eaba72aaa1b816180
Nick Quaranto <nick@quaran.to>
2009-06-05 08:25:39 -0400

    Making the features pass again by requesting the api key

--- features/step_definitions/gemcutter_steps.rb
+++ features/step_definitions/gemcutter_steps.rb
@@ -14,8 +14,23 @@ Given /^I have a gem "([^\"]*)" with version "([^\"]*)"$/ do |name, version|
 end
 
 When /^I push the gem "([^\"]*)" as "([^\"]*)"$/ do |name, creds|
-  path = File.join(TEST_DIR, name.split('-').first, "pkg", name)
   user, pass = creds.split('/')
   basic_auth(user, pass)
+end
+
+Given /^I have an api key for "([^\"]*)"$/ do |creds|
+  user, pass = creds.split('/')
+  basic_auth(user, pass)
+  visit api_key_path, :get
+  @api_key = response.body
+end
+
+When /^I push the gem "([^\"]*)" with my api key$/ do |name|
+  path = File.join(TEST_DIR, name.split('-').first, "pkg", name)
+  header("HTTP_AUTHORIZATION", @api_key)
   visit rubygems_path, :post, File.open(path).read
+end
+
+And /^I save and open the page$/ do
+  save_and_open_page
 end
