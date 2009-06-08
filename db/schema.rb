2844a7d1851b64d32ecd5540886bddcb783d5855
Nick Quaranto <nick@quaran.to>
2009-06-08 07:20:02 -0400

    Generating token on ownership instead of on rubygems

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090607011852) do
+ActiveRecord::Schema.define(:version => 20090608111256) do
 
   create_table "dependencies", :force => true do |t|
     t.string   "name"
