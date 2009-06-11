e49312e751cfa87034cdb614442f0a781c40e507
Nick Quaranto <nick@quaran.to>
2009-06-11 17:48:48 -0400

    Adding versions_count to Rubygems, making sure only gems with versions get on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     @count = Rubygem.count
-    @new = Rubygem.by_created_at(:desc).limited(5)
+    @new = Rubygem.with_versions.by_created_at(:desc).limited(5)
     @updated = Version.by_created_at(:desc).limited(5)
-    @downloaded = Rubygem.by_downloads(:desc).limited(5)
+    @downloaded = Rubygem.with_versions.by_downloads(:desc).limited(5)
   end
 end
---
+++ db/migrate/20090611123606_add_versions_count_to_rubygems.rb
@@ -0,0 +1,9 @@
+class AddVersionsCountToRubygems < ActiveRecord::Migration
+  def self.up
+    add_column :rubygems, :versions_count, :integer, :default => 0
+  end
+
+  def self.down
+    remove_column :rubygems, :versions_count
+  end
+end
