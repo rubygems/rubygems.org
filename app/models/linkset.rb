5d62b366bd81163dee91297bcbe905afd0049f15
Nick Quaranto <nick@quaran.to>
2009-06-11 21:07:18 -0400

    Don't show links unless there's actually something in there

--- app/models/linkset.rb
+++ app/models/linkset.rb
@@ -2,7 +2,13 @@ class Linkset < ActiveRecord::Base
   belongs_to :rubygem
   attr_protected :rubygem_id
 
-  %w(home wiki docs mail code bugs).each do |url|
+  LINKS = %w(home wiki docs mail code bugs)
+
+  LINKS.each do |url|
     validates_url_format_of url, :allow_nil => true, :allow_blank => true
+  end
+
+  def empty?
+    LINKS.map { |link| attributes[link] }.all?(&:blank?)
   end
 end
