3d6f853dfe6753c179932fd5680e066ec062cecc
Nick Quaranto <nick@quaran.to>
2009-06-11 21:07:18 -0400

    Don't show links unless there's actually something in there

--- test/unit/linkset_test.rb
+++ test/unit/linkset_test.rb
@@ -4,7 +4,24 @@ class LinksetTest < ActiveSupport::TestCase
   should_belong_to :rubygem
   should_not_allow_mass_assignment_of :rubygem_id
 
-  should "be valid with factory" do
-    assert_valid Factory.build(:linkset)
+  context "with a linkset" do
+    setup do
+      @linkset = Factory.build(:linkset)
+    end
+
+    should "be valid with factory" do
+      assert_valid @linkset
+    end
+
+    should "not be empty with some links filled out" do
+      assert !@linkset.empty?
+    end
+
+    should "be empty with no links filled out" do
+      Linkset::LINKS.each do |link|
+        @linkset.send("#{link}=", nil)
+      end
+      assert @linkset.empty?
+    end
   end
 end
