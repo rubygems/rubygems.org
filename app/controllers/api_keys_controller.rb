ffe3de1692f5872040cd49f021dccae39fa3897c
Nick Quaranto <nick@quaran.to>
2009-06-13 01:05:06 -0400

    Checking that the user was authenticated when handing out api keys

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -2,8 +2,10 @@ class ApiKeysController < ApplicationController
   def show
     authenticate_or_request_with_http_basic do |username, password|
       @_current_user = User.authenticate(username, password)
-      if current_user.email_confirmed
+      if current_user && current_user.email_confirmed
         render :text => current_user.api_key
+      else
+        false
       end
     end
   end
