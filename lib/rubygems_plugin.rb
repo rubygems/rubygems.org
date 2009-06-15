3fbf30ae507171d502111413a759e54a488c8d52
Nick Quaranto <nick@quaran.to>
2009-06-15 07:40:55 -0400

    Moving the signin process into the push command instead of in upgrade

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -4,4 +4,8 @@ require 'commands/downgrade'
 require 'commands/push'
 require 'commands/upgrade'
 
+Gem::CommandManager.instance.register_command :downgrade
+Gem::CommandManager.instance.register_command :push
+Gem::CommandManager.instance.register_command :upgrade
+
 URL = "http://gemcutter.org" unless defined?(URL)
