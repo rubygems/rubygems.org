cb1eaa0c7d4fa3cda9fe2ae6ee4631260e5ffb87
Nick Quaranto <nick@quaran.to>
2009-06-17 08:42:57 -0400

    Starting on properly testing processing incoming gems

--- lib/gemcutter.rb
+++ lib/gemcutter.rb
@@ -1,11 +1,39 @@
 class Gemcutter
-  def self.server_path(*more)
-    File.expand_path(File.join(File.dirname(__FILE__), '..', 'server', *more))
+  attr_reader :user, :data, :spec, :error_message, :error_code, :rubygem
+
+  def initialize(user, data)
+    @user = user
+    @data = data
   end
 
-  def self.indexer
-    indexer = Gem::Indexer.new(Gemcutter.server_path, :build_legacy => false)
-    def indexer.say(message) end
-    indexer
+  def process
+    pull_spec
+    find_rubygem if self.spec
+  end
+
+  def pull_spec
+    begin
+      format = Gem::Format.from_io(data)
+      @spec = format.spec
+    rescue Exception => e
+      @error_message = "Gemcutter cannot process this gem. Please try rebuilding it and installing it locally to make sure it's valid."
+      @error_code = 422
+    end
+  end
+
+  def find_rubygem
+    @rubygem = Rubygem.find_or_initialize_by_name(@spec.name)
+  end
+
+  class << self
+    def server_path(*more)
+      File.expand_path(File.join(File.dirname(__FILE__), '..', 'server', *more))
+    end
+
+    def indexer
+      indexer = Gem::Indexer.new(Gemcutter.server_path, :build_legacy => false)
+      def indexer.say(message) end
+      indexer
+    end
   end
 end
--- test/unit/gemcutter_test.rb
+++ test/unit/gemcutter_test.rb
@@ -22,4 +22,74 @@ class GemcutterTest < ActiveSupport::TestCase
     assert @indexer.respond_to?(:say)
     assert_nil @indexer.say("Should be quiet")
   end
+
+  context "creating a new gemcutter" do
+    setup do
+      @user = Factory(:email_confirmed_user)
+      @gem = gem_file
+      @cutter = Gemcutter.new(@user, @gem)
+    end
+
+    should "have some state" do
+      assert @cutter.respond_to?(:user)
+      assert @cutter.respond_to?(:data)
+      assert @cutter.respond_to?(:spec)
+      assert @cutter.respond_to?(:error_message)
+      assert @cutter.respond_to?(:error_code)
+      assert @cutter.respond_to?(:rubygem)
+
+      assert_equal @user, @cutter.user
+      assert_equal @gem, @cutter.data
+    end
+
+    context "processing incoming gems" do
+      should "work normally when things go well" do
+        mock(@cutter).pull_spec
+        stub(@cutter).spec { "ruby gem spec" }
+        mock(@cutter).find_rubygem
+        @cutter.process
+      end
+
+      should "not attempt to find rubygem if there's no spec" do
+        mock(@cutter).pull_spec
+        stub(@cutter).spec { nil }
+        mock(@cutter).find_rubygem.never
+        @cutter.process
+      end
+    end
+
+
+    context "pulling the spec " do
+      should "pull spec out of the given gem" do
+        @cutter.pull_spec
+        assert_not_nil @cutter.spec
+        assert @cutter.spec.is_a?(Gem::Specification)
+      end
+
+      should "not be able to pull spec from a bad path" do
+        stub(@cutter).data { "bad data" }
+        @cutter.pull_spec
+        assert_nil @cutter.spec
+        assert_equal @cutter.error_message, "Gemcutter cannot process this gem. Please try rebuilding it and installing it locally to make sure it's valid."
+        assert_equal @cutter.error_code, 422
+      end
+    end
+
+    context "finding rubygem" do
+      should "initialize new gem if one does not exist" do
+        @cutter.pull_spec
+        @cutter.find_rubygem
+        assert_not_nil @cutter.rubygem
+        assert @cutter.rubygem.new_record?
+      end
+
+      should "bring up existing gem with matching spec" do
+        @cutter.pull_spec
+        @rubygem = Factory(:rubygem, :spec => @cutter.spec, :name => @cutter.spec.name)
+
+        @cutter.find_rubygem
+        assert_equal @rubygem, @cutter.rubygem
+      end
+    end
+  end
 end
