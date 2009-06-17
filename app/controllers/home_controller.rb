af4902f9016a0e6f2893d4c662006140a665a8d7
Nick Quaranto <nick@quaran.to>
2009-06-17 07:57:22 -0400

    Making sure only gems with versions show up on the front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @count = Rubygem.count
+    @count = Rubygem.with_versions.count
     @new = Rubygem.with_versions.by_created_at(:desc).limited(5)
     @updated = Version.by_created_at(:desc).limited(5)
     @downloaded = Rubygem.with_versions.by_downloads(:desc).limited(5)
