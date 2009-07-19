24181aecb052b24f29b9f0bf89254e6bbe7cf19d
Jason Morrison <jason.p.morrison@gmail.com>
2009-07-19 19:45:09 -0400

    Adding validations for crappy version numbers
    Signed-off-by: Nick Quaranto <nick@quaran.to>

--- test/unit/version_test.rb
+++ test/unit/version_test.rb
@@ -4,6 +4,9 @@ class VersionTest < ActiveSupport::TestCase
   should_belong_to :rubygem
   should_have_many :requirements, :dependent => :destroy
   should_have_many :dependencies, :through => :requirements
+  should_not_allow_values_for :number, "#YAML<CEREALIZATION-FAIL>",
+                                       "1.2.3-\"[javalol]\"",
+                                       "0.8.45::Gem::PLATFORM::FAILBOAT"
 
   context "with a version" do
     setup do
