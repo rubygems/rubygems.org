a0c4ad72b95bc8e6e3869fe6a1d162bc9a6c299e
Jason Morrison <jason.p.morrison@gmail.com>
2009-07-19 19:45:09 -0400

    Adding validations for crappy version numbers
    Signed-off-by: Nick Quaranto <nick@quaran.to>

--- app/models/version.rb
+++ app/models/version.rb
@@ -3,6 +3,7 @@ class Version < ActiveRecord::Base
   belongs_to :rubygem, :counter_cache => true
   has_many :requirements, :dependent => :destroy
   has_many :dependencies, :through => :requirements, :dependent => :destroy
+  validates_format_of :number, :with => /^[\w\.\-_]+$/
 
   def to_s
     number
