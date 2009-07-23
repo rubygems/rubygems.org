e44dea124eeb647dd745c52bbbbed07ca2dae8f7
Nick Quaranto <nick@quaran.to>
2009-07-23 18:30:23 -0400

    Starting to spike out loading data completely into s3

--- lib/vault.rb
+++ lib/vault.rb
@@ -1,11 +1,51 @@
 module Vault
   module S3
+
     def store
-      puts "this works?"
+      write
+      update
     end
+
+    protected
+
+    def source_path
+      "source_index"
+    end
+
+    def source_index
+      if VaultObject.exists?(source_path)
+        @source_index ||= VaultObject.value(source_path)
+      else
+        @source_index ||= Gem::SourceIndex.new
+      end
+    end
+
+    def write
+      cache_path = "gems/#{spec.original_name}.gem"
+      data.rewind
+      VaultObject.store(cache_path, data, :authenticated => false, :access => :public_read)
+
+      quick_path = "quick/Marshal.#{Gem.marshal_version}/#{spec.original_name}.gemspec.rz"
+      Gemcutter.indexer.abbreviate spec
+      Gemcutter.indexer.sanitize spec
+      VaultObject.store(quick_path, Gem.deflate(Marshal.dump(spec)), :authenticated => false, :access => :public_read)
+    end
+
+    def update
+      source_index.add_spec spec, spec.original_name
+      VaultObject.store(source_path, source_index)
+    end
+
   end
 
   module FS
+    def store
+      write
+      update
+    end
+
+    protected
+
     def source_path
       Gemcutter.server_path("source_index")
     end
@@ -16,11 +56,6 @@ module Vault
       else
         @source_index ||= Gem::SourceIndex.new
       end
-    end
-
-    def store
-      write
-      update
     end
 
     def write
