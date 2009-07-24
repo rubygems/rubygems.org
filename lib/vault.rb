1cfe5e640b4034dfb2d775da56aa6817425c1ab6
Nick Quaranto <nick@quaran.to>
2009-07-23 23:59:06 -0400

    Actually serving gems through s3 now.

--- lib/vault.rb
+++ lib/vault.rb
@@ -1,5 +1,6 @@
 module Vault
   module S3
+    OPTIONS = {:authenticated => false, :access => :public_read}
 
     def store
       write
@@ -14,7 +15,7 @@ module Vault
 
     def source_index
       if VaultObject.exists?(source_path)
-        @source_index ||= VaultObject.value(Marshal.load(source_path))
+        @source_index ||= Marshal.load(VaultObject.value(source_path))
       else
         @source_index ||= Gem::SourceIndex.new
       end
@@ -23,25 +24,26 @@ module Vault
     def write
       cache_path = "gems/#{spec.original_name}.gem"
       data.rewind
-      VaultObject.store(cache_path, data, :authenticated => false, :access => :public_read)
+      VaultObject.store(cache_path, data, OPTIONS)
 
       quick_path = "quick/Marshal.#{Gem.marshal_version}/#{spec.original_name}.gemspec.rz"
       Gemcutter.indexer.abbreviate spec
       Gemcutter.indexer.sanitize spec
-      VaultObject.store(quick_path, Gem.deflate(Marshal.dump(spec)), :authenticated => false, :access => :public_read)
+      VaultObject.store(quick_path, Gem.deflate(Marshal.dump(spec)), OPTIONS)
     end
 
     def update
       source_index.add_spec spec, spec.original_name
-      VaultObject.store(source_path, Marshal.dump(source_index))
+      VaultObject.store(source_path, Marshal.dump(source_index), OPTIONS)
 
       specs_index = "specs.#{Gem.marshal_version}.gz"
-      latest_specs_index = "lastest_specs.#{Gem.marshal_version}.gz"
+      latest_specs_index = "latest_specs.#{Gem.marshal_version}.gz"
 
       [specs_index, latest_specs_index].each do |index|
         if VaultObject.exists?(index)
           binary = VaultObject.value(index)
-          loaded_index = Marshal.load(Gem.inflate(binary))
+          marshalled = Zlib::GzipReader.new(StringIO.new(binary)).read
+          loaded_index = Marshal.load(marshalled)
         else
           loaded_index = []
         end
@@ -54,7 +56,12 @@ module Vault
 
         loaded_index = Gemcutter.indexer.compact_specs(loaded_index)
 
-        VaultObject.store(index, Gem.deflate(Marshal.dump(loaded_index)))
+        data = StringIO.new
+        gzip = Zlib::GzipWriter.new(data)
+        gzip.write(Marshal.dump(loaded_index))
+        gzip.close
+
+        VaultObject.store(index, data.string, OPTIONS)
       end
     end
 
@@ -83,7 +90,7 @@ module Vault
     def write
       cache_path = Gemcutter.server_path('gems', "#{spec.original_name}.gem")
       File.open(cache_path, "wb") do |f|
-        f.write data
+        f.write data.read
       end
       File.chmod 0644, cache_path
 
