68b900c5f2997d279c67082ba0fc90ecd4c19c86
Nick Quaranto <nick@quaran.to>
2009-07-25 20:07:29 -0400

    Including the platform in the version number

--- lib/gemcutter.rb
+++ lib/gemcutter.rb
@@ -44,13 +44,19 @@ class Gemcutter
 
   def build
     rubygem.build_name(spec.name)
+    if spec.platform.to_s == "ruby"
+      number = spec.version.to_s
+    else
+      number = "#{spec.version}-#{spec.platform}"
+    end
+
     rubygem.build_version(
       :authors           => spec.authors.join(", "),
       :description       => spec.description,
       :summary           => spec.summary,
       :rubyforge_project => spec.rubyforge_project,
       :created_at        => spec.date,
-      :number            => spec.original_name.gsub(/^#{spec.name}-/, ""))
+      :number            => number)
     rubygem.build_dependencies(spec.dependencies)
     rubygem.build_links(spec.homepage)
     rubygem.build_ownership(user) if user
