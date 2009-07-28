dad8e7dc42c90b91959c4dfc9d24256f75144e97
Nick Quaranto <nick@quaran.to>
2009-07-28 19:45:04 -0400

    Removing the upgrade/downgrade commands in favor of gem tumble

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -2,12 +2,10 @@ $:.unshift File.dirname(__FILE__)     # For use/testing when no gem is installed
 
 require 'rubygems/command_manager'
 
-require 'commands/downgrade'
 require 'commands/push'
-require 'commands/upgrade'
+require 'commands/tumble'
 
-Gem::CommandManager.instance.register_command :downgrade
 Gem::CommandManager.instance.register_command :push
-Gem::CommandManager.instance.register_command :upgrade
+Gem::CommandManager.instance.register_command :tumble
 
 URL = "http://gemcutter.org" unless defined?(URL)
