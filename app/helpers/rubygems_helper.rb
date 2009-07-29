ec643ae24600678b54c5bc1c953a672e9608baae
Nick Quaranto <nick@quaran.to>
2009-07-29 18:23:31 -0400

    Adding a directory-style index to /gems so it's actually useful. Closes #20

--- app/helpers/rubygems_helper.rb
+++ app/helpers/rubygems_helper.rb
@@ -1,5 +1,10 @@
 module RubygemsHelper
+
   def link_to_page(text, url)
     link_to text, url unless url.blank?
+  end
+
+  def link_to_directory
+    ("A".."Z").map { |letter| link_to(letter, rubygems_path(:letter => letter)) }.join
   end
 end
