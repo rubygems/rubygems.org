560729e7fdb84a5993b6bef517b8837ce3cb4b78
Brian Cardarella <bcardarella@gmail.com>
2009-07-29 22:30:20 +0800

    Moved S3 Initialization code from the config/initilizers/s3.rb file to config/environments/production.rb Now we don't need a AWS key for Development and Test
    Signed-off-by: Nick Quaranto <nick@quaran.to>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,3 +28,13 @@ config.action_view.cache_template_loading            = true
 # config.threadsafe!
 
 HOST = "gemcutter.org"
+
+
+AWS::S3::Base.establish_connection!(
+  :access_key_id     => ENV['S3_KEY'],
+  :secret_access_key => ENV['S3_SECRET']
+)
+
+class VaultObject < AWS::S3::S3Object
+  set_current_bucket_to "gemcutter_production"
+end
--- config/initializers/s3.rb
+++
@@ -1,8 +0,0 @@
-AWS::S3::Base.establish_connection!(
-  :access_key_id     => ENV['S3_KEY'],
-  :secret_access_key => ENV['S3_SECRET']
-)
-
-class VaultObject < AWS::S3::S3Object
-  set_current_bucket_to "gemcutter_production"
-end
