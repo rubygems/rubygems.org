e6d1f16680d81d75e5ccf040775ab55e428464b6
Nick Quaranto <nick@quaran.to>
2009-08-05 23:44:03 -0400

    Making really sure dup versions don't get created

--- test/unit/version_test.rb
+++ test/unit/version_test.rb
@@ -4,15 +4,18 @@ class VersionTest < ActiveSupport::TestCase
   should_belong_to :rubygem
   should_have_many :requirements, :dependent => :destroy
   should_have_many :dependencies, :through => :requirements
-  should_not_allow_values_for :number, "#YAML<CEREALIZATION-FAIL>",
-                                       "1.2.3-\"[javalol]\"",
-                                       "0.8.45::Gem::PLATFORM::FAILBOAT"
 
   context "with a version" do
     setup do
       @version = Factory(:version)
       @info = "some info"
     end
+    subject { @version }
+
+    should_not_allow_values_for :number, "#YAML<CEREALIZATION-FAIL>",
+                                         "1.2.3-\"[javalol]\"",
+                                         "0.8.45::Gem::PLATFORM::FAILBOAT"
+    should_validate_uniqueness_of :number
 
     should "give number for #to_s" do
       assert_equal @version.number, @version.to_s
