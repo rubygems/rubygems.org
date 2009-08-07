aba909e3738308833b9c43b3f268fc3013d2f203
Nick Quaranto <nick@quaran.to>
2009-08-07 18:33:28 -0400

    Basic create action for migrations

--- app/controllers/migrations_controller.rb
+++ app/controllers/migrations_controller.rb
@@ -1,6 +1,17 @@
 class MigrationsController < ApplicationController
   before_filter :authenticate_with_api_key, :only => :create
+  rescue_from ActiveRecord::RecordNotFound, :with => lambda {
+    render :text => "This gem could not be found.", :status => :not_found
+  }
 
   def create
+    @rubygem = Rubygem.find(params[:rubygem_id])
+
+    if @rubygem.unowned?
+      ownership = @rubygem.ownerships.create(:user => current_user)
+      render :text => ownership.token
+    else
+      render :text => "This gem has already been migrated by another user.", :status => :forbidden
+    end
   end
 end
