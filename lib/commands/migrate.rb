8bc8d38c87fe71dcb7e297279f56f4042caa16a6
Nick Quaranto <nick@quaran.to>
2009-08-10 18:32:17 -0400

    Starting on the migrate command

--- lib/commands/migrate.rb
+++ lib/commands/migrate.rb
@@ -8,5 +8,36 @@ class Gem::Commands::MigrateCommand < Gem::AbstractCommand
   end
 
   def execute
+    setup
+    migrate
+  end
+
+  def migrate
+    name = get_one_gem_name
+    say "Starting migration of #{name} from RubyForge..."
+    token = get_token(name)
+    #upload_token(token)
+    #check_for_approval(name)
+  end
+
+  def get_token(name)
+
+    #rubygem_migrate PUT    /gems/:rubygem_id/migrate(.:format)        {:controller=>"migrations", :action=>"update"}
+    url = URI.parse("#{gemcutter_url}/gems/#{name}/migrate")
+
+    http = proxy_class.new(url.host, url.port)
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    http.use_ssl = (url.scheme == 'https')
+    request = proxy_class::Post.new(url.path)
+    request.add_field("Authorization", api_key)
+    response = http.request(request)
+
+    case response
+    when Net::HTTPSuccess
+      response.body
+    else
+      say response.body
+      terminate_interaction
+    end
   end
 end
