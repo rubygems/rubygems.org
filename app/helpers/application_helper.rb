4e8127b1f7d24b1599af7e2df1cee5a19c17d59e
Nick Quaranto <nick@quaran.to>
2009-08-19 00:34:18 -0400

    Renaming section to main and moving it into the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # Remove once 2.3.4 comes out!
+  # https://rails.lighthouseapp.com/projects/8994/tickets/1311-add-content_forname-helper
+  def content_for?(name)
+    instance_variable_get("@content_for_#{name}").present?
+  end
+
   def title
     "gemcutter"
   end
