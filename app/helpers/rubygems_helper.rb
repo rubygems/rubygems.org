7d18bcf3ecbdd4dd61ee3a6ab4c11f5c3cec6d27
Nick Quaranto <nick@quaran.to>
2009-08-20 08:41:59 -0400

    Starting on the gems index

--- app/helpers/rubygems_helper.rb
+++ app/helpers/rubygems_helper.rb
@@ -3,8 +3,4 @@ module RubygemsHelper
   def link_to_page(text, url)
     link_to text, url unless url.blank?
   end
-
-  def link_to_directory
-    ("A".."Z").map { |letter| link_to(letter, rubygems_path(:letter => letter)) }.join
-  end
 end
