efb739dfa300bf3fe58cd5f36f46fb6bcfc855f2
David Dollar <ddollar@gmail.com>
2009-08-25 19:24:20 -0400

    add /dashboard/mine.atom and /dashboard/subscribed.atom

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,13 +1,30 @@
 class DashboardsController < ApplicationController
 
+  before_filter :authenticate_with_api_key
   before_filter :redirect_to_root, :unless => :signed_in?
 
   def mine
-    @gems = current_user.rubygems
+    respond_to do |format|
+      format.html do
+        @gems = current_user.rubygems
+      end
+      format.atom do
+        @versions = Version.owned_by(current_user).published(20)
+        render 'versions/feed'
+      end
+    end
   end
 
   def subscribed
-    @gems = current_user.subscribed_gems
+    respond_to do |format|
+      format.html do
+        @gems = current_user.subscribed_gems
+      end
+      format.atom do
+        @versions = Version.subscribed_to_by(current_user).published(20)
+        render 'versions/feed'
+      end
+    end
   end
 
 end
