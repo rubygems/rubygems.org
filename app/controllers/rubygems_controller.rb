7375f8a0ac179fd82c3f9e7b7ceaadeadf7c2099
David Dollar <ddollar@gmail.com>
2009-08-25 19:23:39 -0400

    move versions feed into a more common place so it can be used on the dashboard

--- app/controllers/rubygems_controller.rb
+++ app/controllers/rubygems_controller.rb
@@ -14,6 +14,7 @@ class RubygemsController < ApplicationController
       end
       format.atom do
         @versions = Version.published(20)
+        render 'versions/feed'
       end
     end
   end
