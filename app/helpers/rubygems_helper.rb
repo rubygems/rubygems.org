1cffa7c67a348d637fea68bf28a8874f7dccca50
David Dollar <ddollar@gmail.com>
2009-08-25 16:01:33 -0400

    add subscribe/unsubscribe ability

--- app/helpers/rubygems_helper.rb
+++ app/helpers/rubygems_helper.rb
@@ -7,4 +7,25 @@ module RubygemsHelper
   def link_to_directory
     ("A".."Z").map { |letter| link_to(letter, rubygems_path(:letter => letter)) }.join
   end
+
+  def subscribe_link(gem)
+    subscribe = link_to_remote 'Subscribe',
+      :url    => rubygem_subscription_path(gem),
+      :method => :post,
+      :class  => :toggler,
+      :html   => {
+        :class  => :toggler,
+        :style  => gem.subscribers.find(:first, current_user) ? 'display:none' : 'display:block'
+      }
+  end
+
+  def unsubscribe_link(gem)
+    unsubscribe = link_to_remote 'Unsubscribe',
+      :url    => rubygem_subscription_path(gem),
+      :method => :delete,
+      :html   => {
+        :class  => :toggler,
+        :style  => gem.subscribers.find(:first, current_user) ? 'display:block' : 'display:none'
+      }
+  end
 end
