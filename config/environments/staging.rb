a5e10c86ff216759913ab5b4091679714536ed9b
Nick Quaranto <nick@quaran.to>
2009-09-01 01:12:53 -0400

    staging needed to be fixed too

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -35,8 +35,9 @@ config.after_initialize do
     :access_key_id     => ENV['S3_KEY'],
     :secret_access_key => ENV['S3_SECRET']
   )
+
+  class ::VaultObject < AWS::S3::S3Object
+    set_current_bucket_to "gemcutter_staging"
+  end
 end
 
-class ::VaultObject < AWS::S3::S3Object
-  set_current_bucket_to "gemcutter_staging"
-end
