5b7fd70aa7c2c3eb4adf7581d38b5ce27b858347
David Dollar <ddollar@gmail.com>
2009-09-03 15:35:16 -0400

    don't need jeweler any more

--- vendor/gems/jeweler-1.2.1/lib/jeweler/templates/minitest/flunking.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class <%= constant_name %>Test < Mini::Test::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
-end
