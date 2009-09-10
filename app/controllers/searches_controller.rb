6f3cae8258475e892d396f95cbac0b7194e9ce07
Nick Quaranto <nick@quaran.to>
2009-09-10 12:11:49 -0400

    Making sure search results have versions

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,7 @@ class SearchesController < ApplicationController
 
   def new
     if params[:query]
-      @gems = Rubygem.search(params[:query]).paginate(:page => params[:page])
+      @gems = Rubygem.search(params[:query]).with_versions.paginate(:page => params[:page])
     end
   end
 
