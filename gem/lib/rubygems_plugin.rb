ae99b825baf783013d715b848e50ae6de848bed4
Hallison Batista <email@hallisonbatista.com>
2009-09-15 16:30:33 -0400

    HTTP Proxy passed by environment variables
    * Added check for proxy environment variable.
    * Core requires added in abstract_command.

--- gem/lib/rubygems_plugin.rb
+++ gem/lib/rubygems_plugin.rb
@@ -2,6 +2,8 @@ $:.unshift File.dirname(__FILE__)     # For use/testing when no gem is installed
 
 require 'rubygems/command_manager'
 require 'commands/abstract_command'
+require 'net/http'
+require 'net/https'
 
 %w[migrate owner push tumble].each do |command|
   require "commands/#{command}"
