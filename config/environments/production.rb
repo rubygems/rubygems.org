3a365bdf9050bdba274908cfb3d07c80ebe6c2cf
Nick Quaranto <nick@quaran.to>
2009-09-23 22:31:14 -0400

    Moving memcache setup outside of the after_initialize block

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,11 +49,11 @@ config.after_initialize do
   #  require 'rack/cache'
   #  CACHE = ::MemCache.new(ENV['MEMCACHE_SERVERS'].split(','), :namespace => ENV['MEMCACHE_NAMESPACE'])
   #end
-
-  #if ENV['MEMCACHE_SERVERS']
-  #  memcache_config = ENV['MEMCACHE_SERVERS'].split(',')
-  #  memcache_config << {:namespace => ENV['MEMCACHE_NAMESPACE']}
-  #  config.cache_store = :mem_cache_store, memcache_config
-  #end
-
 end
+
+if ENV['MEMCACHE_SERVERS']
+  memcache_config = ENV['MEMCACHE_SERVERS'].split(',')
+  memcache_config << {:namespace => ENV['MEMCACHE_NAMESPACE']}
+  config.cache_store = :mem_cache_store, memcache_config
+end
+
