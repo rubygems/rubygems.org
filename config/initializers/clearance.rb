300dff22490857414842b4447f67e0784b88586d
Nick Quaranto <nick@quaran.to>
2009-10-02 15:46:27 -0400

    Bundle this up

--- config/initializers/clearance.rb
+++
@@ -1,5 +0,0 @@
-# Really not sure why this is necessary.
-require 'vendor/gems/thoughtbot-clearance-0.8.2/app/controllers/clearance/confirmations_controller'
-require 'vendor/gems/thoughtbot-clearance-0.8.2/app/controllers/clearance/passwords_controller'
-require 'vendor/gems/thoughtbot-clearance-0.8.2/app/controllers/clearance/sessions_controller'
-require 'vendor/gems/thoughtbot-clearance-0.8.2/app/controllers/clearance/users_controller'
