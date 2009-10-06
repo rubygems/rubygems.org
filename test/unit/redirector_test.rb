168ea477cf5b4647b00379513d93868d41fe924e
Nick Quaranto <nick@quaran.to>
2009-10-06 15:34:24 -0400

    Adding the REDIRECTOR to handle docs.rubygems.org redirects once the move happens

---
+++ test/unit/redirector_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class RedirectorTest < ActiveSupport::TestCase
+  def app
+    Redirector.new
+  end
+
+  %w[/book
+     /book/42
+     /chapter/58
+     /read/book/2
+     /export
+     /page/500
+     /shelf/9000
+     /syndicate.xml].each do |uri|
+    should "redirect to docs.rubygems.org when #{uri} is hit" do
+      get uri
+      assert_equal 301, last_response.status
+      assert_equal "http://docs.rubygems.org#{uri}", last_response.headers["Location"]
+    end
+  end
+end
