fbea0b8fbd4e89a3240c6dbb0e99a031a64ebcf7
Nick Quaranto <nick@quaran.to>
2009-10-07 18:22:31 -0400

    Tracking downloads properly for platform gems, required refactoring for how versions are looked up and handled

--- test/factories/version.rb
+++ test/factories/version.rb
@@ -1,10 +1,11 @@
 Factory.define :version do |version|
-  version.authors     { 'Joe User' }
-  version.description { 'Some awesome gem' }
-  version.number      { Factory.next(:version_number) }
-  version.built_at    { 1.day.ago }
+  version.authors           { 'Joe User' }
+  version.description       { 'Some awesome gem' }
+  version.number            { Factory.next(:version_number) }
+  version.built_at          { 1.day.ago }
+  version.platform          { "ruby" }
   version.rubyforge_project { 'awesome' }
-  version.association :rubygem
+  version.association       :rubygem
 end
 
 Factory.sequence :version_number do |n|
