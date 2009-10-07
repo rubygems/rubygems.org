b04474bd072d1c02c4946554df00dd0b90adf94d
Nick Quaranto <nick@quaran.to>
2009-10-07 16:56:44 -0400

    Stuff on the dashboard needs versions

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,9 +6,9 @@ class DashboardsController < ApplicationController
   def show
     respond_to do |format|
       format.html do
-        @my_gems = current_user.rubygems
+        @my_gems = current_user.rubygems.with_versions
         @latest_updates = Version.subscribed_to_by(current_user).published(20)
-        @subscribed_gems = current_user.subscribed_gems
+        @subscribed_gems = current_user.subscribed_gems.with_versions
       end
       format.atom do
         @versions = Version.subscribed_to_by(current_user).published(20)
