af01525a4a434b70fdb98ccb84a81708b05f980d
Nick Quaranto <nick@quaran.to>
2009-10-08 09:30:18 -0400

    Adding timestamps to users, why weren't they here before?

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091007183410) do
+ActiveRecord::Schema.define(:version => 20091008132519) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -109,6 +109,8 @@ ActiveRecord::Schema.define(:version => 20091007183410) do
     t.string   "api_key"
     t.string   "confirmation_token", :limit => 128
     t.string   "remember_token",     :limit => 128
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "users", ["confirmation_token", "id"], :name => "index_users_on_id_and_confirmation_token"
