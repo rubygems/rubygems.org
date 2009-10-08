d9352f6a1d06c7b6442b9196436e99038bcf90a8
Nick Quaranto <nick@quaran.to>
2009-10-08 09:30:18 -0400

    Adding timestamps to users, why weren't they here before?

---
+++ db/migrate/20091008132519_add_time_stamps_to_users.rb
@@ -0,0 +1,11 @@
+class AddTimeStampsToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :created_at, :datetime
+    add_column :users, :updated_at, :datetime
+  end
+
+  def self.down
+    remove_column :users, :created_at
+    remove_column :users, :updated_at
+  end
+end
