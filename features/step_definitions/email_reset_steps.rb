7992a9978fee38059e59b7ce0cba0c35ac694a03
Luke Redpath <contact@lukeredpath.co.uk>
2009-10-09 12:17:39 +0100

    Failing features for resetting email.

---
+++ features/step_definitions/email_reset_steps.rb
@@ -0,0 +1,11 @@
+Then /^I should receive an email entitled "([^\"]*)"$/ do |arg1|
+  pending
+end
+
+Given /^I have reset my email address to "([^\"]*)"$/ do |arg1|
+  pending
+end
+
+Then /^I should see the message "([^\"]*)"$/ do |arg1|
+  pending
+end
