f1f294f4abb9547407bf355abdd4caeeaacaca58
Nick Quaranto <nick@quaran.to>
2009-10-09 17:37:29 -0400

    Reordering versions again, in a migration

---
+++ db/migrate/20091009213456_reorder_versions_again.rb
@@ -0,0 +1,8 @@
+class ReorderVersionsAgain < ActiveRecord::Migration
+  def self.up
+    Rubygem.all.each &:reorder_versions
+  end
+
+  def self.down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091008132519) do
+ActiveRecord::Schema.define(:version => 20091009213456) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
