824ca94acc638ef410d114169a747e0778c3ebf4
David Dollar <ddollar@gmail.com>
2009-10-21 17:02:11 -0400

    use a boolean to designate "latest" versions

---
+++ db/migrate/20091021203534_add_latest_to_version.rb
@@ -0,0 +1,11 @@
+class AddLatestToVersion < ActiveRecord::Migration
+  def self.up
+    add_column :versions, :latest, :boolean
+
+    Rubygem.all.each &:reorder_versions
+  end
+
+  def self.down
+    remove_column :versions, :latest
+  end
+end
