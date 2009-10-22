b1f35f006786ec055d2db2f1ce222af94437c2e5
Nick Quaranto <nick@quaran.to>
2009-10-22 02:08:06 -0400

    Adding more junk into Version.with_indexed to handle the reverse ordering

--- lib/gemcutter.rb
+++ lib/gemcutter.rb
@@ -90,7 +90,7 @@ class Gemcutter
   end
 
   def specs_index
-    Version.reverse.with_indexed.map(&:to_index)
+    Version.with_indexed(true).map(&:to_index)
   end
 
   def latest_index
@@ -98,7 +98,7 @@ class Gemcutter
   end
 
   def prerelease_index
-    Version.prerelease.with_indexed.map(&:to_index)
+    Version.prerelease.with_indexed(true).map(&:to_index)
   end
 
   def perform
