62124aca5f581e4f5ff96a58cab5c1dc2e96b6d4
David A. Black <dblack@rubypal.com>
2009-10-26 21:05:14 -0400

    Cucumber features and steps for RubyForge transfer

---
+++ features/step_definitions/rubyforge_steps.rb
@@ -0,0 +1,26 @@
+Given /^I am a RubyForge user with an email of "([^\"]*)"$/ do |email|
+  @rf = Rubyforger.create(:email => "email@person.com",
+:encrypted_password => Digest::MD5.hexdigest("password"))
+end
+
+Given /^a RubyForge user signs in with "([^\"]*)\/(.*)"$/ do |email,password|
+  Given %{I am a RubyForge user with an email of "#{email}"}
+  And %{my RubyForge password is "#{password}"}
+  And %{I sign in as "#{email}/#{password}"}
+end
+
+Then /^my RubyForge password is "([^\"]*)"$/ do |password|
+  @rf.encrypted_password == Digest::MD5.hexdigest(password)
+end
+
+Then /^a confirmed user with an email of "([^\"]*)" exists$/ do |email|
+  assert(User.find_by_email(email))  
+end
+
+Then /^no RubyForge user exists with an email of "([^\"]*)"$/ do |email|
+  assert(!Rubyforger.find_by_email(email))
+end
+
+
+
+
