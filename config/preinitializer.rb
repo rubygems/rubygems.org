e9fbc6805127903d36882f64a227c9fd58707371
David Dollar <ddollar@gmail.com>
2009-11-01 17:36:09 -0500

    fix boot order for bundler

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,19 +1,1 @@
 require "#{File.dirname(__FILE__)}/../vendor/bundler_gems/environment"
-
-class Rails::Boot
-  def run
-    load_initializer
-    extend_environment
-    Rails::Initializer.run(:set_load_path)
-  end
-
-  def extend_environment
-    Rails::Initializer.class_eval do
-      old_load = instance_method(:load_environment)
-      define_method(:load_environment) do
-        Bundler.require_env RAILS_ENV
-        old_load.bind(self).call
-      end
-    end
-  end
-end
