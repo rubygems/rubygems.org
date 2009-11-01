a151f671f9ebf30169cffe9d2bdb34922b79706a
David Dollar <ddollar@gmail.com>
2009-11-01 17:36:09 -0500

    fix boot order for bundler

--- config/boot.rb
+++ config/boot.rb
@@ -109,5 +109,24 @@ module Rails
   end
 end
 
+# for bundler
+class Rails::Boot
+  def run
+    load_initializer
+    extend_environment
+    Rails::Initializer.run(:set_load_path)
+  end
+
+  def extend_environment
+    Rails::Initializer.class_eval do
+      old_load = instance_method(:load_environment)
+      define_method(:load_environment) do
+        Bundler.require_env RAILS_ENV
+        old_load.bind(self).call
+      end
+    end
+  end
+end
+
 # All that for this:
 Rails.boot!
