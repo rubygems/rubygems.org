ac435589a4efd3c46b4da2eb6d6f8161bb1d5c67
Nick Quaranto <nick@quaran.to>
2009-11-09 19:24:54 -0500

    Moving fixing full names into rake task

--- db/migrate/20091109203935_add_full_name_to_versions.rb
+++ db/migrate/20091109203935_add_full_name_to_versions.rb
@@ -1,13 +1,6 @@
 class AddFullNameToVersions < ActiveRecord::Migration
   def self.up
     add_column :versions, :full_name, :string
-
-    Version.without_any_callbacks do
-      Version.all(:include => :rubygem).each do |version|
-        version.full_nameify!
-      end
-    end
-
     add_index 'versions', 'full_name'
   end
 
