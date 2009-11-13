53366cb05bd651a62f5d8734691be61dd9485ff5
Tom Copeland <tom@infoether.com>
2009-11-13 00:25:33 -0500

    Using -e env vs the bare environment name

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,17 +55,17 @@ end
 namespace :delayed_job do
   desc "Start delayed_job process" 
   task :start, :roles => :app do
-    run "cd #{current_path}; script/delayed_job start #{rails_env}" 
+    run "cd #{current_path}; script/delayed_job -e #{rails_env} start" 
   end
 
   desc "Stop delayed_job process" 
   task :stop, :roles => :app do
-    run "cd #{current_path}; script/delayed_job stop #{rails_env}" 
+    run "cd #{current_path}; script/delayed_job -e #{rails_env} stop" 
   end
 
   desc "Restart delayed_job process" 
   task :restart, :roles => :app do
-    run "cd #{current_path}; script/delayed_job restart #{rails_env}" 
+    run "cd #{current_path}; script/delayed_job -e #{rails_env} restart" 
   end
 end
 
