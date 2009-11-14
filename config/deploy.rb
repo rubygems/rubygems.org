3328f546d40a99ba3405ff0211c75ff0744ebd79
Tom Copeland <tom@infoether.com>
2009-11-14 11:37:07 -0500

    Using monit to manage delayed_job

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,17 +67,17 @@ end
 namespace :delayed_job do
   desc "Start delayed_job process" 
   task :start, :roles => :app do
-    run "cd #{current_path}; script/delayed_job -e #{rails_env} start" 
+    run "sudo monit start delayed_job_#{rails_env}" 
   end
 
   desc "Stop delayed_job process" 
   task :stop, :roles => :app do
-    run "cd #{current_path}; script/delayed_job -e #{rails_env} stop" 
+    run "sudo monit stop delayed_job_#{rails_env}" 
   end
 
   desc "Restart delayed_job process" 
   task :restart, :roles => :app do
-    run "cd #{current_path}; script/delayed_job -e #{rails_env} restart" 
+    run "sudo monit restart delayed_job_#{rails_env}" 
   end
 end
 
