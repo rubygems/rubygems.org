82f8ea8618495c871a0e8450273a84b5ff18c338
Nick Quaranto <nick@quaran.to>
2009-11-21 10:47:55 -0800

    Moved POST to /gems out

--- test/functional/api/v1/rubygems_controller_test.rb
+++ test/functional/api/v1/rubygems_controller_test.rb
@@ -1,11 +1,16 @@
 require 'test_helper'
 
 class Api::V1::RubygemsControllerTest < ActionController::TestCase
+  should_forbid_access_when("pushing a gem") { post :create }
 
   should "route old paths to new controller" do
     get_route = {:controller => 'api/v1/rubygems', :action => 'show', :id => "rails", :format => "json"}
     assert_recognizes(get_route, '/gems/rails.json')
     assert_recognizes(get_route, '/api/v1/gems/rails.json')
+
+    post_route = {:controller => 'api/v1/rubygems', :action => 'create'}
+    assert_recognizes(post_route, :path => '/gems', :method => :post)
+    assert_recognizes(post_route, :path => '/api/v1/gems', :method => :post)
   end
 
   context "When logged in" do
@@ -67,6 +72,76 @@ class Api::V1::RubygemsControllerTest < ActionController::TestCase
       should_respond_with :not_found
       should "say the rubygem was not found" do
         assert_match /not be found/, @response.body
+      end
+    end
+  end
+
+  context "with a confirmed user authenticated" do
+    setup do
+      @user = Factory(:email_confirmed_user)
+      @request.env["HTTP_AUTHORIZATION"] = @user.api_key
+    end
+
+    context "On POST to create for new gem" do
+      setup do
+        @request.env["RAW_POST_DATA"] = gem_file.read
+        post :create
+      end
+      should_respond_with :success
+      should_assign_to(:_current_user) { @user }
+      should_change("the rubygem count") { Rubygem.count }
+      should "register new gem" do
+        assert_equal @user, Rubygem.last.ownerships.first.user
+        assert_equal "Successfully registered gem: test (0.0.0)", @response.body
+      end
+    end
+
+    context "On POST to create for existing gem" do
+      setup do
+        rubygem = Factory(:rubygem, :name => "test")
+        rubygem.ownerships.create(:user => @user, :approved => true)
+        rubygem.versions.create(:number => "0.0.0", :updated_at => 1.year.ago, :created_at => 1.year.ago)
+        @request.env["RAW_POST_DATA"] = gem_file("test-1.0.0.gem").read
+        post :create
+      end
+      should_respond_with :success
+      should_assign_to(:_current_user) { @user }
+      should "register new version" do
+        assert_equal @user, Rubygem.last.ownerships.first.user
+        assert_equal 1, Rubygem.last.ownerships.size
+        assert_equal 2, Rubygem.last.versions.size
+        assert_equal "Successfully registered gem: test (1.0.0)", @response.body
+      end
+    end
+
+    context "On POST to create with bad gem" do
+      setup do
+        @request.env["RAW_POST_DATA"] = "really bad gem"
+        post :create
+      end
+      should_respond_with :unprocessable_entity
+      should_not_change("the rubygem count") { Rubygem.count }
+      should "not register gem" do
+        assert_match /Gemcutter cannot process this gem/, @response.body
+      end
+    end
+
+    context "On POST to create for someone else's gem" do
+      setup do
+        @other_user = Factory(:email_confirmed_user)
+        create_gem(@other_user, :name => "test")
+        @rubygem.reload
+
+        @request.env["RAW_POST_DATA"] = gem_file("test-1.0.0.gem").read
+        post :create
+      end
+      should_respond_with 403
+      should_assign_to(:_current_user) { @user }
+      should "not allow new version to be saved" do
+        assert_equal 1, @rubygem.ownerships.size
+        assert_equal @other_user, @rubygem.ownerships.first.user
+        assert_equal 1, @rubygem.versions.size
+        assert_equal "You do not have permission to push to this gem.", @response.body
       end
     end
   end
