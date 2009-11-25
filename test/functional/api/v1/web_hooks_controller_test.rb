0faa49f764895aa650050081c1335e8586ce1ce3
Chad Fowler <chad@chadfowler.com>
2009-11-25 14:34:20 -0700

    404 when referring to a nonexistent gem, and return 'success' to be more consistent with other gemcutter commands

--- test/functional/api/v1/web_hooks_controller_test.rb
+++ test/functional/api/v1/web_hooks_controller_test.rb
@@ -19,6 +19,15 @@ class Api::V1::WebHooksControllerTest < ActionController::TestCase
        should_assign_to(:web_hook)
        should_respond_with 201
      end
+     
+     context "On POST to create a hook for a gem that doesn't exist here" do
+       setup do
+         post :create, :web_hook => {:gem_name => "a gem that doesnt' exist", :url => "http://example.org"}
+       end
+       
+       should_not_assign_to(:web_hook)
+       should_respond_with 404
+     end
    end
 end
 
