c1a3dbffce3dd135d776b6ab4497a15159412113
Chad Fowler <chad@chadfowler.com>
2009-11-26 09:41:40 -0700

    remove stub

--- app/controllers/api/v1/web_hooks_controller.rb
+++ app/controllers/api/v1/web_hooks_controller.rb
@@ -1,13 +1,6 @@
 class Api::V1::WebHooksController < ApplicationController
   before_filter :authenticate_with_api_key, :only => :create
   before_filter :verify_authenticated_user, :only => :create
-  ## HACK
-  class WebHook
-    def self.create(*args)
-     "not nil" 
-    end
-  end
-  ## HACK
   
   def create
     unless Rubygem.find_by_name(params[:web_hook][:gem_name])
