c1a87d7b85a2fa36248da82a61b4e3eeb920c375
John Pignata <john.pignata@gmail.com>
2009-11-27 16:38:16 -0500

    Output platform in Version on dashboard
    Signed-off-by: Nick Quaranto <nick@quaran.to>

--- features/step_definitions/factory_girl_steps.rb
+++ features/step_definitions/factory_girl_steps.rb
@@ -14,8 +14,19 @@ Given /^a version exists for the "([^\"]*)" rubygem with a description of "([^\"
   Factory(:version, :rubygem => rubygem, :description => version_description)
 end
 
+Given /^a version exists for the "([^\"]*)" rubygem with a platform of "([^\"]*)"$/ do |rubygem_name, version_platform|
+  rubygem = Rubygem.find_by_name!(rubygem_name)
+  Factory(:version, :rubygem => rubygem, :platform => version_platform, :created_at => 1.hour.ago)
+end
+
 Given /^the "([^\"]*)" rubygem is owned by "([^\"]*)"/ do |rubygem_name, owner_email|
   rubygem = Rubygem.find_by_name!(rubygem_name)
   owner   = User.find_by_email(owner_email)
   Factory(:ownership, :rubygem => rubygem, :user => owner, :approved => true)
+end
+
+Given /^a subscription by "([^\"]*)" to the gem "([^\"]*)"$/ do |user_email, rubygem_name|
+  rubygem = Rubygem.find_by_name!(rubygem_name)
+  user    = User.find_by_email(user_email)
+  Factory(:subscription, :rubygem => rubygem, :user => user)
 end
