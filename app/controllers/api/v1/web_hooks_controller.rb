e9e0df2e0d3aa67eba0e677b16cae5e8f48386c3
Ben Brinckerhoff <ben@devver.net>
2009-11-29 09:54:55 -0700

    Disallowed registering the same URL for the same gem name.

--- app/controllers/api/v1/web_hooks_controller.rb
+++ app/controllers/api/v1/web_hooks_controller.rb
@@ -6,10 +6,16 @@ class Api::V1::WebHooksController < ApplicationController
   before_filter :verify_authenticated_user, :only => :create
   
   def create
-    unless Rubygem.find_by_name(params[:gem_name])
+    url = params[:url]
+    gem_name = params[:gem_name]
+    unless Rubygem.find_by_name(gem_name)
       return render(:text => "Gem Not Found", :status => 404)
     end
-    @web_hook = WebHook.create(:url => params[:url], :gem_name => params[:gem_name])
-    render :text => 'success', :status => :created
+    if WebHook.find(:all, :conditions => {:url => url, :gem_name => gem_name}).empty?
+      @web_hook = WebHook.create(:url => url, :gem_name => gem_name)
+      render :text => 'success', :status => :created
+    else
+      render(:text => "WebHook '#{url}' has alredy been registered for Gem '#{gem_name}'", :status => 409)
+    end
   end
 end
--- test/functional/api/v1/web_hooks_controller_test.rb
+++ test/functional/api/v1/web_hooks_controller_test.rb
@@ -10,6 +10,7 @@ class Api::V1::WebHooksControllerTest < ActionController::TestCase
      end
 
      context "On POST to create hook for a gem that's hosted" do
+
        setup do
          @rubygem = Factory(:rubygem)
          Factory(:version, :rubygem => @rubygem)
@@ -18,8 +19,19 @@ class Api::V1::WebHooksControllerTest < ActionController::TestCase
 
        should_assign_to(:web_hook)
        should_respond_with 201
+
+      context "on POST to create hook that already exists" do
+        
+        setup do
+          post :create, {:gem_name => @rubygem.name, :url => "http://example.org"}
+        end
+
+        should("be only 1 web hook"){ assert_equal 1, WebHook.count }
+        should_respond_with 409
+      end
+
      end
-     
+
      context "On POST to create a hook for a gem that doesn't exist here" do
        setup do
          post :create, {:gem_name => "a gem that doesnt' exist", :url => "http://example.org"}
