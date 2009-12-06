58682527f9801a4778346bebe6f6266240dbff04
Nick Quaranto <nick@quaran.to>
2009-12-06 00:51:43 -0500

    Monster refactoring of the Hostess.

--- test/unit/hostess_test.rb
+++ test/unit/hostess_test.rb
@@ -5,62 +5,74 @@ class HostessTest < ActiveSupport::TestCase
     Hostess.new
   end
 
-  def touch(path)
-    path = Gemcutter.server_path(path)
-    FileUtils.mkdir_p(File.dirname(path))
-    FileUtils.touch(path)
+  def touch(path, local = true)
+    Hostess.local = local
+    if local
+      path = Gemcutter.server_path(path)
+      FileUtils.mkdir_p(File.dirname(path))
+      FileUtils.touch(path)
+    else
+      net_resp = FakeWeb::Responder.new(:get, "/", {}, 1).response
+      s3_resp = AWS::S3::S3Object::Response.new(net_resp)
+      stub(VaultObject).value(path, anything) { s3_resp }
+    end
   end
 
-  def remove(path)
-    path = Gemcutter.server_path(path)
-    FileUtils.rm(path) if File.exists?(path)
-  end
-
-  ["/prerelease_specs.4.8.gz",
-   "/latest_specs.4.8.gz",
-   "/specs.4.8.gz",
-   "/Marshal.4.8.Z"].each do |index|
-    should "serve up #{index}" do
-      touch index
+  def self.serve_up(index, local)
+    should "serve up #{index} #{local ? 'locally' : 'remotely'}" do
+      touch index, local
       get index
-      assert_not_nil last_response.headers["Cache-Control"]
       assert_equal 200, last_response.status
     end
   end
 
-  ["/yaml",
-   "/Marshal.4.8",
-   "/specs.4.8",
-   "/latest_specs.4.8",
-   "/prerelease_specs.4.8"].each do |old_index|
-    should "serve up #{old_index}" do
-      touch old_index
-      get old_index
-      assert_not_nil last_response.headers["Cache-Control"]
-      assert_equal 200, last_response.status
-    end
+  setup do
+    Hostess.local = false
   end
 
-  should "return quick gemspec" do
-    file = "/quick/Marshal.4.8/test-0.0.0.gemspec.rz"
-    touch file
-    get file
-    assert_not_nil last_response.headers["Cache-Control"]
-    assert_equal 200, last_response.status
-    assert_equal "application/x-deflate", last_response.content_type
+  %w[/prerelease_specs.4.8.gz
+     /latest_specs.4.8.gz
+     /specs.4.8.gz
+     /Marshal.4.8.Z
+     /yaml
+     /Marshal.4.8
+     /specs.4.8
+     /latest_specs.4.8
+     /prerelease_specs.4.8
+     /quick/Marshal.4.8/test-0.0.0.gemspec.rz
+  ].each do |index|
+    serve_up index, false
+    serve_up index, true
   end
 
-  should "serve up gem" do
+  should "serve up gem remotely" do
     download_count = Download.count
     file = "/gems/test-0.0.0.gem"
-    FileUtils.cp gem_file.path, Gemcutter.server_path("gems")
     rubygem = Factory(:rubygem, :name => "test")
     version = Factory(:version, :rubygem => rubygem, :number => "0.0.0")
 
     get file
     Delayed::Job.work_off
 
-    assert_equal 200, last_response.status
+    assert_equal 302, last_response.status
+    assert_equal download_count + 1, Download.count
+    assert_equal 1, rubygem.reload.downloads
+    assert_equal 1, version.reload.downloads_count
+  end
+
+  should "serve up gem locally" do
+    Hostess.local = false
+    download_count = Download.count
+    file = "/gems/test-0.0.0.gem"
+    FileUtils.cp gem_file.path, Gemcutter.server_path("gems")
+    
+    rubygem = Factory(:rubygem, :name => "test")
+    version = Factory(:version, :rubygem => rubygem, :number => "0.0.0")
+
+    get file
+    Delayed::Job.work_off
+
+    assert_equal 302, last_response.status
     assert_equal download_count + 1, Download.count
     assert_equal 1, rubygem.reload.downloads
     assert_equal 1, version.reload.downloads_count
@@ -68,14 +80,10 @@ class HostessTest < ActiveSupport::TestCase
 
   should "not be able to find non existant gemspec" do
     file = "/quick/Marshal.4.8/test-0.0.0.gemspec.rz"
-    remove file
-    get file
-    assert_equal 404, last_response.status
-  end
+    stub(VaultObject).value(anything, anything) do
+      raise AWS::S3::NoSuchKey.new("No such key!", VaultObject.current_bucket)
+    end
 
-  should "not be able to find non existant gem" do
-    file = "/gems/test-0.0.0.gem"
-    remove file
     get file
     assert_equal 404, last_response.status
   end
