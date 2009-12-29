89ddc42f3fb7f7392dc5d02856294506b19a42cf
Nick Quaranto <nick@quaran.to>
2009-12-29 14:56:52 -0500

    Giving the gemcutter gem tests an extreme makeover

--- gem/test/command_helper.rb
+++ gem/test/command_helper.rb
@@ -1,19 +1,18 @@
 require 'rubygems'
 require 'test/unit'
 
-require "#{File.dirname(__FILE__)}/../../vendor/bundler_gems/environment"
-
 require 'shoulda'
+require 'active_support'
+require 'active_support/test_case'
+require 'webmock'
+require 'rr'
+
 begin
   require 'redgreen'
 rescue LoadError
 end
-require 'active_support'
-require 'active_support/test_case'
-require 'fakeweb'
-require 'rr'
 
-FakeWeb.allow_net_connect = false
+WebMock.disable_net_connect!
 
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__), ".."))
 
