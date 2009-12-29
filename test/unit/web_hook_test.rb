3e05084a10f2529a782317bc0bd90db8cedd6136
Nick Quaranto <nick@quaran.to>
2009-12-29 02:08:17 -0500

    Web hook payloads recognize specific versions and not just the latest

--- test/unit/web_hook_test.rb
+++ test/unit/web_hook_test.rb
@@ -94,20 +94,19 @@ class WebHookTest < ActiveSupport::TestCase
 
   context "with a rubygem and version" do
     setup do
+      @rubygem = Factory(:rubygem,
+                         :name      => "foogem",
+                         :downloads => 42)
       @version = Factory(:version, 
+                         :rubygem           => @rubygem,
                          :number            => "3.2.1", 
                          :rubyforge_project => "foogem-rf",
                          :authors           => "AUTHORS",
                          :description       => "DESC")
-      @rubygem = Factory(:rubygem,
-                         :name      => "foogem",
-                         :versions  => [ @version ],
-                         :downloads => 42)
       @hook    = Factory(:web_hook,
                          :rubygem        => @rubygem,
-                         :host_with_port => 'localhost:1234')
-      @version.rubygem = @rubygem
-      @version.save
+                         :host_with_port => 'localhost:1234',
+                         :version        => @version)
     end
 
     should "have gem properties encoded in JSON" do
@@ -121,16 +120,31 @@ class WebHookTest < ActiveSupport::TestCase
       assert_equal "http://localhost:1234/gems/foogem", payload['project_uri']
       assert_equal "http://localhost:1234/gems/foogem-3.2.1.gem", payload['gem_uri']
     end
+
+    should "send the right version out even for older gems" do
+      new_version = Factory(:version, :number => "2.0.0", :rubygem => @rubygem)
+      new_hook    = Factory(:web_hook,
+                            :rubygem        => @rubygem,
+                            :host_with_port => 'localhost:1234',
+                            :version        => new_version)
+      payload = ActiveSupport::JSON.decode(new_hook.payload)
+      
+      assert_equal "foogem", payload['name']
+      assert_equal "2.0.0",  payload['version']
+      assert_equal "http://localhost:1234/gems/foogem", payload['project_uri']
+      assert_equal "http://localhost:1234/gems/foogem-2.0.0.gem", payload['gem_uri']
+    end
   end
 
   context "with a non-global hook job" do
     setup do 
       @url     = 'http://example.com/gemcutter'
-      @rubygem = Factory(:rubygem, 
-                         :versions => [Factory(:version)])
+      @rubygem = Factory(:rubygem)
+      @version = Factory(:version, :rubygem => @rubygem)
       @hook    = Factory(:web_hook,
                          :rubygem => @rubygem,
-                         :url     => @url)
+                         :url     => @url,
+                         :version => @version)
       WebMock.stub_request(:post, @url)
       @hook.perform
     end
@@ -154,10 +168,12 @@ class WebHookTest < ActiveSupport::TestCase
     setup do
       @url     = 'http://someinvaliddomain.com'
       @user    = Factory(:email_confirmed_user)
-      @rubygem = Factory(:rubygem, :versions => [Factory(:version)])
+      @rubygem = Factory(:rubygem)
+      @version = Factory(:version, :rubygem => @rubygem)
       @hook    = Factory(:web_hook, :url     => @url,
                                     :rubygem => @rubygem,
-                                    :user    => @user)
+                                    :user    => @user,
+                                    :version => @version)
       @hook.host_with_port = 'example.org'
     end
     
