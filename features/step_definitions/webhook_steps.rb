3336b63ee327d39eeb6381b83ccee71804a73c2a
Nick Quaranto <nick@quaran.to>
2009-12-29 15:21:57 -0500

    Confirming that the payload dumps the right version numbers in the features too

--- features/step_definitions/webhook_steps.rb
+++ features/step_definitions/webhook_steps.rb
@@ -1,3 +1,9 @@
-Then /^the webhook "([^\"]*)" should receive a POST$/ do |web_hook_url|
+Then /^the webhook "([^\"]*)" should receive a POST with gem "([^\"]*)" at version "([^\"]*)"$/ do |web_hook_url, gem_name, version_number|
   WebMock.assert_requested(:post, web_hook_url, :times => 1)
+
+  request = WebMock::RequestRegistry.instance.requested_signatures.hash.keys.first
+  json = ActiveSupport::JSON.decode(request.body)
+
+  assert_equal gem_name, json["name"]
+  assert_equal version_number, json["version"]
 end
