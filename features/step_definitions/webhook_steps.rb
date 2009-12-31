29710b5554c7ccf88e49a593a530845fc68a292e
Nick Quaranto <nick@quaran.to>
2009-12-31 16:57:54 -0500

    Test firing hooks

--- features/step_definitions/webhook_steps.rb
+++ features/step_definitions/webhook_steps.rb
@@ -1,12 +1,12 @@
 When /^I have added a webhook for "([^\"]*)" to gem "([^\"]*)" with my api key$/ do |web_hook_url, gem_name|
+  api_key_header
   WebMock.stub_request(:post, web_hook_url)
-  header("Authorization", @api_key)
   visit api_v1_web_hooks_path, :post, :gem_name => gem_name, :url => web_hook_url
 end
 
 When /^I have added a global webhook for "([^\"]*)" with my api key$/ do |web_hook_url|
+  api_key_header
   WebMock.stub_request(:post, web_hook_url)
-  header("Authorization", @api_key)
   visit api_v1_web_hooks_path,
         :post,
         :gem_name => WebHook::GLOBAL_PATTERN,
@@ -14,7 +14,7 @@ When /^I have added a global webhook for "([^\"]*)" with my api key$/ do |web_ho
 end
 
 When /^I list the webhooks with my api key$/ do
-  header("Authorization", @api_key)
+  api_key_header
   visit api_v1_web_hooks_path, :get, :format => "json"
 end
 
@@ -35,7 +35,7 @@ Then /^I should see "([^\"]*)" under "([^\"]*)"$/ do |web_hook_url, gem_name|
 end
 
 When /^I have removed a webhook for "([^\"]*)" from gem "([^\"]*)" with my api key$/ do |web_hook_url, gem_name|
-  header("Authorization", @api_key)
+  api_key_header
   visit remove_api_v1_web_hooks_path,
         :delete,
         :gem_name => gem_name,
@@ -43,11 +43,21 @@ When /^I have removed a webhook for "([^\"]*)" from gem "([^\"]*)" with my api k
 end
 
 When /^I have removed the global webhook for "([^\"]*)"$/ do |web_hook_url|
-  header("Authorization", @api_key)
+  api_key_header
   visit remove_api_v1_web_hooks_path,
         :delete,
         :gem_name => WebHook::GLOBAL_PATTERN,
         :url      => web_hook_url
+end
+
+When /^I have fired hooks for the "([^\"]*)" gem with my api key$/ do |gem_name|
+  api_key_header
+  visit fire_api_v1_web_hooks_path, :post, :gem_name => gem_name
+end
+
+When /^I have fired global hooks with my api key$/ do
+  api_key_header
+  visit fire_api_v1_web_hooks_path, :post, :gem_name => WebHook::GLOBAL_PATTERN
 end
 
 Then /^the webhook "([^\"]*)" should not receive a POST$/ do |web_hook_url|
