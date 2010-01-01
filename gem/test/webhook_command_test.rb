40cb0528a16576a361d51f5a6a62f601c45a3c69
Nick Quaranto <nick@quaran.to>
2010-01-01 16:43:44 -0500

    Some gem cleanup/standardization before starting on more webhooks

--- gem/test/webhook_command_test.rb
+++ gem/test/webhook_command_test.rb
@@ -9,18 +9,18 @@ class WebhookCommandTest < CommandTest
 
     should "setup and post hook" do
       stub(@command).setup
-      stub(@command).post_webhook
+      stub(@command).add_webhook
       @command.execute
       assert_received(@command) { |command| command.setup }
-      assert_received(@command) { |command| command.post_webhook }
+      assert_received(@command) { |command| command.add_webhook }
     end
-    
+
     should "raise an error with no arguments" do
       assert_raise Gem::CommandLineError do
-        @command.post_webhook
+        @command.add_webhook
       end
     end
-    
+
     context "adding a hook" do
       setup do
         @url = "https://gemcutter.org/api/v1/web_hooks"
@@ -28,8 +28,8 @@ class WebhookCommandTest < CommandTest
         stub(@command).options { {:args => ["#{@gem} -a http://example.org/gem_hooks"]} }
         stub_config({ :rubygems_api_key => "key" })
         WebMock.stub_request(:post, @url).to_return(:body => "Success!")
-      
-        @command.post_webhook
+
+        @command.add_webhook
       end
 
       should "say hook was added" do
