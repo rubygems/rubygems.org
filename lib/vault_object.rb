24201c3dab2a3a1a67731005e1f58303888e48e9
Nick Quaranto <nick@quaran.to>
2010-01-07 21:24:40 -0500

    Moving gem redirects to cloudfront url and cleaning up environment files

--- lib/vault_object.rb
+++ lib/vault_object.rb
@@ -1,8 +1,7 @@
-AWS::S3::Base.establish_connection!(
-  :access_key_id     => ENV['S3_KEY'],
-  :secret_access_key => ENV['S3_SECRET']
-)
+class VaultObject < AWS::S3::S3Object
+  set_current_bucket_to "#{Rails.env.downcase}.s3.rubygems.org"
 
-class ::VaultObject < AWS::S3::S3Object
-  set_current_bucket_to "gemcutter_#{(ENV['RACK_ENV'] || RAILS_ENV).downcase}"
+  def self.distribution_for(path)
+    "http://#{Rails.env.downcase}.cf.rubygems.org#{path}"
+  end
 end
