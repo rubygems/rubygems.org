7956ac331cd47c666c2c760227a6087ce14f6fd9
Nick Quaranto <nick@quaran.to>
2010-01-08 08:57:53 -0500

    Removing old mail config

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,3 +2,5 @@ require 'clearance/confirmations_controller'
 require 'clearance/passwords_controller'
 require 'clearance/sessions_controller'
 require 'clearance/users_controller'
+
+ClearanceMailer::DO_NOT_REPLY = "donotreply@gemcutter.org
