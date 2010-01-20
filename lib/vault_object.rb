89cd46c0f181f0f9c786e5d6189896593689205c
Nick Quaranto <nick@quaran.to>
2010-01-19 22:58:36 -0500

    Redirecting to all indexes like a good canonical RubyGems index should

--- lib/vault_object.rb
+++ lib/vault_object.rb
@@ -1,7 +1,11 @@
 class VaultObject < AWS::S3::S3Object
   set_current_bucket_to "#{Rails.env.downcase}.s3.rubygems.org"
 
-  def self.distribution_for(path)
+  def self.cf_url_for(path)
     "http://#{Rails.env.downcase}.cf.rubygems.org#{path}"
+  end
+
+  def self.s3_url_for(path)
+    "http://#{Rails.env.downcase}.s3.rubygems.org#{path}"
   end
 end
