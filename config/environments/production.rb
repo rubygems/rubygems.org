ae40d17e738afe4cdc4d3f2323024286fd496bf9
Nick Quaranto <nick@quaran.to>
2010-01-19 23:01:15 -0500

    Removing memcached, rack/cache, and smtp-tls since they're all not needed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,9 +7,3 @@ config.action_view.cache_template_loading            = true
 require Rails.root.join("config", "secret") if Rails.root.join("config", "secret.rb").file?
 
 HOST = "gemcutter.org"
-
-memcache_store = MemCache.new("localhost:11211", {:namespace => "_gemcutter_production"})
-config.middleware.insert_after(::Rack::Lock, ::Rack::Cache,
-  :verbose     => true,
-  :metastore   => memcache_store,
-  :entitystore => memcache_store)
