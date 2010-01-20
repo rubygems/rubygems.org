fd9cf51ce830197c5ab7be7ed2fc27d73b7f3422
Nick Quaranto <nick@quaran.to>
2010-01-19 23:01:15 -0500

    Removing memcached, rack/cache, and smtp-tls since they're all not needed

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -8,9 +8,3 @@ config.action_view.cache_template_loading            = true
 require Rails.root.join("config", "secret") if Rails.root.join("config", "secret.rb").file?
 
 HOST = "staging.rubygems.org"
-
-memcache_store = MemCache.new("localhost:11211", {:namespace => "_gemcutter_staging"})
-config.middleware.insert_after(::Rack::Lock, ::Rack::Cache,
-  :verbose     => true,
-  :metastore   => memcache_store,
-  :entitystore => memcache_store)
