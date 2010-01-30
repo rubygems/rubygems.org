e09649e2d2ca76a1bb5161815e56d4e5dbf12094
Nick Quaranto <nick@quaran.to>
2010-01-30 14:48:23 -0500

    Adding XML to the api/v1 search endpoint

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -3,7 +3,11 @@ class Api::V1::SearchesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def show
-    render :json => Rubygem.search(params[:query]).with_versions.paginate(:page => params[:page])
+    @gems = Rubygem.search(params[:query]).with_versions.paginate(:page => params[:page])
+    respond_to do |wants|
+      wants.json { render :json => @gems }
+      wants.xml  { render :xml  => @gems }
+    end
   end
 
 end
