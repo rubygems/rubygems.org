4af9253b58c0ee613d16999c1b095fdf32e7d59d
Tom Copeland <tom@infoether.com>
2010-02-02 20:24:40 -0500

    Using anonymous clone for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,6 +3,7 @@ role :web, "staging.gemcutter.org"
 role :db,  "staging.gemcutter.org", :primary => true
 
 set :deploy_to, "/var/www/rubycentral/staging.gemcutter.org/"
+set :repository, "git://github.com/qrush/gemcutter.git"
 
 namespace :deploy do
   desc "For the staging environment, move in a robots.txt that blocks robots from the entire site"
