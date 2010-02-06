41b587449e53d898d248b367346655b337650533
Wyatt Greene <techiferous@gmail.com>
2010-02-06 15:24:50 -0500

    When a ruby gem is destroyed, its subscriptions are destroyed, too.
    Signed-off-by: Nick Quaranto <nick@quaran.to>

--- app/models/rubygem.rb
+++ app/models/rubygem.rb
@@ -4,7 +4,7 @@ class Rubygem < ActiveRecord::Base
   has_many :owners, :through => :ownerships, :source => :user
   has_many :ownerships, :dependent => :destroy
   has_many :subscribers, :through => :subscriptions, :source => :user
-  has_many :subscriptions
+  has_many :subscriptions, :dependent => :destroy
   has_many :versions, :dependent => :destroy do
     def latest
       # try to find a ruby platform in the latest version
