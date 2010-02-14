7dc7913754cec08792724938df0d7a550fdbe80c
Nick Quaranto <nick@quaran.to>
2010-02-13 19:17:56 -0500

    Ditching rack-force_domain since /api requests need to hit the application for all domains

--- app/metal/redirector.rb
+++
@@ -1,9 +0,0 @@
-class Redirector < Sinatra::Default
-  %w[book chapter export read shelf syndicate].each do |resource|
-    get "/#{resource}*" do
-      status 301
-      response['Location'] = "http://docs.rubygems.org#{request.path}"
-      halt
-    end
-  end
-end
--- test/unit/redirector_test.rb
+++ test/unit/redirector_test.rb
@@ -2,7 +2,38 @@ require 'test_helper'
 
 class RedirectorTest < ActiveSupport::TestCase
   def app
-    Redirector.new
+    Rack::Builder.new do
+      use Redirector
+      run lambda {|env| [200, {"Content-Type" => "text/html"}, []]}
+    end
+  end
+
+  should "forward requests that don't match" do
+    get "/specs.4.8.gz", {}, {"HTTP_HOST" => HOST}
+    assert last_response.ok?
+  end
+
+  should "redirect requests from a non-HOST domain" do
+    path = "/gems/rails"
+    get path, {}, {"HTTP_HOST" => "gems.rubyforge.org"}
+
+    assert_equal 301, last_response.status
+    assert_equal "http://#{HOST}#{path}", last_response.headers["Location"]
+  end
+
+  should "redirect requests from a non-HOST domain with query string" do
+    path = "/search"
+    get path, {"query" => "rush"}, {"HTTP_HOST" => "gems.rubyforge.org"}
+
+    assert_equal 301, last_response.status
+    assert_equal "http://#{HOST}#{path}?query=rush", last_response.headers["Location"]
+  end
+
+  should "not redirect requests to the API from a non-HOST domain" do
+    path = "/api/v1/gems"
+    post path, {}, {"HTTP_HOST" => "gems.rubyforge.org"}
+
+    assert last_response.ok?
   end
 
   %w[/book
@@ -13,7 +44,8 @@ class RedirectorTest < ActiveSupport::TestCase
      /shelf/9000
      /syndicate.xml].each do |uri|
     should "redirect to docs.rubygems.org when #{uri} is hit" do
-      get uri
+      get uri, {}, {"HTTP_HOST" => HOST}
+
       assert_equal 301, last_response.status
       assert_equal "http://docs.rubygems.org#{uri}", last_response.headers["Location"]
     end
