11a1da5bf0213ed8405131c1d3163c298652ade0
John Trupiano <jtrupiano@gmail.com>
2010-03-01 21:27:04 -0500

    gem yank --undo for the gem

--- gem/test/yank_command_test.rb
+++ gem/test/yank_command_test.rb
@@ -22,23 +22,61 @@ class YankCommandTest < CommandTest
       setup do
         stub_api_key("key")
         stub_request(:delete, @api).to_return(:body => "Successfully yanked")
-
         @command.handle_options([@gem, "-v", @version])
-        @command.execute
       end
 
       should 'say gem was yanked' do
+        @command.execute
         assert_received(@command) do |command|
           command.say("Yanking gem from Gemcutter...")
           command.say("Successfully yanked")
         end
       end
+      
+      should 'invoke yank_gem' do
+        stub(@command).yank_gem(@version)
+        @command.execute
+        assert_received(@command) do |command|
+          command.yank_gem(@version)
+        end
+      end
 
       should 'delete to api' do
+        @command.execute
         assert_requested(:delete, @api,
                          :times => 1)
         assert_requested(:delete, @api,
                          :headers => { 'Authorization' => 'key' })
+      end
+    end
+    
+    context 'unyanking a gem' do
+      setup do
+        stub_api_key("key")
+        stub_request(:put, @api).to_return(:body => "Successfully unyanked")
+        @command.handle_options([@gem, "-v", @version, "--undo"])
+      end
+      
+      should 'say gem was unyanked' do
+        @command.execute
+        assert_received(@command) do |command|
+          command.say("Re-indexing gem")
+          command.say("Successfully unyanked")
+        end
+      end
+
+      should 'invoke unyank_gem' do
+        stub(@command).unyank_gem(@version)
+        @command.execute
+        assert_received(@command) do |command|
+          command.unyank_gem(@version)
+        end
+      end
+      
+      should 'put to api' do
+        @command.execute
+        assert_requested(:put, @api, :times => 1)
+        assert_requested(:put, @api, :headers => { 'Authorization' => 'key' })
       end
     end
   end
