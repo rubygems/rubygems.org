685e25d8ff5828d22878161b6f73bbfbc8733287
Nick Quaranto <nick@quaran.to>
2010-03-31 17:51:28 -0400

    Rolling over download history from the daily zset to hashes

--- test/unit/download_test.rb
+++ test/unit/download_test.rb
@@ -30,4 +30,27 @@ class DownloadTest < ActiveSupport::TestCase
     assert_equal 1, Download.today(version)
     assert_equal 0, Download.today(other_platform_version)
   end
+
+  should "roll over downloads into history hashes" do
+    rubygem = Factory(:rubygem)
+    version1 = Factory(:version, :rubygem => rubygem, :number => "1.0.0")
+    version2 = Factory(:version, :rubygem => rubygem, :number => "2.0.0")
+
+    Download.incr(version1)
+    Download.incr(version2)
+    Download.incr(version2)
+
+    Download.rollover
+    assert ! $redis.exists(Download::TODAY_KEY)
+    assert $redis.exists(Download::YESTERDAY_KEY)
+
+    rubygem_history = $redis.hgetall(Download.history_key(rubygem))
+    version1_history = $redis.hgetall(Download.history_key(version1))
+    version2_history = $redis.hgetall(Download.history_key(version2))
+
+    yesterday = 1.day.ago.to_date.to_s
+    assert_equal 3, rubygem_history[yesterday].to_i
+    assert_equal 1, version1_history[yesterday].to_i
+    assert_equal 2, version2_history[yesterday].to_i
+  end
 end
