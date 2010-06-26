2508f4a3b3927367a8aa8dcb6f31e5083315c498
Nick Quaranto <nick@quaran.to>
2010-06-26 00:13:42 -0400

    Keeping as much code inside of clearance's engine as possible

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < Clearance::SessionsController
+  include RubyforgeTransfer
+
+  before_filter :rf_check, :only => :create
+
+  def create
+    @user = User.authenticate(params[:session][:email],
+                              params[:session][:password])
+    if @user.nil?
+      flash_failure_after_create
+      render :template => 'sessions/new', :status => :unauthorized
+    else
+      if @user.email_confirmed? && !@user.email_changed?
+        sign_in(@user)
+        flash_success_after_create
+        redirect_back_or(url_after_create)
+      else
+        if @user.email_changed?
+          Mailer.deliver_email_reset(@user)
+        else
+          ClearanceMailer.deliver_confirmation(@user)
+        end
+        flash_notice_after_create
+        redirect_to(new_session_url)
+      end
+    end
+  end
+
+  private
+
+  def url_after_create
+    dashboard_url
+  end
+end
