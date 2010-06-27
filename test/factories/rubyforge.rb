d52665776f5493699ea98da051822dc9024d4cf6
Nick Quaranto <nick@quaran.to>
2010-06-27 19:37:02 -0400

    Refactoring rubyforge sign in logic out of vendor

--- test/factories/rubyforge.rb
+++ test/factories/rubyforge.rb
@@ -1,7 +1,3 @@
-Factory.sequence :email do |n|
-  "user#{n}@example.com"
-end
-
 Factory.define :rubyforger do |rf|
   rf.email                 { Factory.next :email }
   rf.encrypted_password    { Digest::SHA1.hexdigest("password") }
