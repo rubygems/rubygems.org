dd0ae5dcdb3d1d8c41b54f013e7adda8b4847379
Nick Quaranto <nick@quaran.to>
2010-07-21 21:10:26 -0400

    Schwing! Bringing back maintenance mode
    * Pushing some gems that dont play nice outside of the maintenance env
    * Hostess doesn't like referencing itself
    * Requiring specific railties so maintenance mode works
    * Redirector should go after ActionDispatch::ParamsParser instead
    * Gemifying validates_url_format_of
    * Removing rails_upgrade plugin
    * Updating maintenance mode video
    * VaultObject redirects to production bucket in maintenance mode

--- config/environments/maintenance.rb
+++ config/environments/maintenance.rb
@@ -1,6 +1,4 @@
 Gemcutter::Application.configure do
-  config.frameworks -= [:active_record]
-
   config.middleware.use ::Rack::Static,
     :urls => ["/index.html",
               "/favicon.ico",
@@ -9,8 +7,9 @@ Gemcutter::Application.configure do
     :root => "public/maintenance"
   config.middleware.use ::Rack::Maintenance,
     :file => File.join('public', 'maintenance', 'index.html')
+  config.plugins = []
 end
 
-require Rails.root.join("config", "secret") if Rails.root.join("config", "secret.rb").file?
+#require Rails.root.join("config", "secret") if Rails.root.join("config", "secret.rb").file?
 
 HOST = "rubygems.org"
--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,4 +1,4 @@
-silence_warnings do
+unless Rails.env.maintenance?
   Delayed::Job.const_set('MAX_ATTEMPTS', 3)
   Delayed::Job.const_set('MAX_RUN_TIME', 5.minutes)
 end
