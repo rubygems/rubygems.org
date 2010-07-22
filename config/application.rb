3a1bc6e46277186e2f6ff7082620b6c52709120d
Nick Quaranto <nick@quaran.to>
2010-07-21 21:10:26 -0400

    Schwing! Bringing back maintenance mode
    * Pushing some gems that dont play nice outside of the maintenance env
    * Hostess doesn't like referencing itself
    * Requiring specific railties so maintenance mode works
    * Redirector should go after ActionDispatch::ParamsParser instead
    * Gemifying validates_url_format_of
    * Removing rails_upgrade plugin
    * Updating maintenance mode video
    * VaultObject redirects to production bucket in maintenance mode

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,25 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require 'rails'
+require 'action_controller/railtie'
+
+unless Rails.env.maintenance?
+  require 'rails/test_unit/railtie'
+  require 'action_mailer/railtie'
+  require 'active_record/railtie'
+end
 
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Gemcutter
   class Application < Rails::Application
     config.time_zone = 'UTC'
-    config.action_mailer.delivery_method = :sendmail
-    config.encoding = "utf-8"
-    config.filter_parameters += [:password]
-    config.active_record.include_root_in_json = false
+    config.encoding  = "utf-8"
     config.middleware.use "Hostess"
+
+    unless Rails.env.maintenance?
+      config.action_mailer.delivery_method = :sendmail
+      config.active_record.include_root_in_json = false
+    end
   end
 end
