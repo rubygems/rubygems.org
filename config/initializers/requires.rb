c1b8f4887c1afd2d1fb818f4b3789b47f684cd85
Nick Quaranto <nick@quaran.to>
2010-07-21 22:35:41 -0400

    Remove vendored rubygems codebase, we can browse it on github now!

--- config/initializers/requires.rb
+++ config/initializers/requires.rb
@@ -1,4 +1,4 @@
-require 'lib/rubygems/format'
-require 'lib/rubygems/indexer'
+require 'rubygems/format'
+require 'rubygems/indexer'
 require 'rdoc/markup/simple_markup'
 require 'rdoc/markup/simple_markup/to_html'
