c5eebad3eef5339d4366cd2dea2e87c6088229e8
Joshua Nichols <josh@technicalpickles.com>
2010-07-31 19:16:35 -0400

    Fix pushes failing when the gemspec's author is very long. See http://help.rubygems.org/discussions/problems/233-all-gem-pushes-fail

--- test/unit/version_test.rb
+++ test/unit/version_test.rb
@@ -187,6 +187,13 @@ class VersionTest < ActiveSupport::TestCase
     end
   end
 
+  context "with a very long authors string." do
+    should "create without error" do
+
+      Factory.create(:version, :authors => ["Fbdoorman: David Pelaez", "MiniFB:Appoxy", "Dan Croak", "Mike Burns", "Jason Morrison", "Joe Ferris", "Eugene Bolshakov", "Nick Quaranto", "Josh Nichols", "Mike Breen", "Marcel G\303\266rner", "Bence Nagy", "Ben Mabey", "Eloy Duran", "Tim Pope", "Mihai Anca", "Mark Cornick", "Shay Arnett", "Jon Yurek", "Chad Pytel"])
+    end
+  end
+
   context "when indexing" do
     setup do
       @rubygem = Factory(:rubygem)
