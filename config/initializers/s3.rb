2203bde0fa9ef3d471d9a8541410c3263db196d3
Nick Quaranto <nick@quaran.to>
2010-08-18 16:18:52 -0400

    mock fog unless in production or staging

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,11 @@
 if ENV['S3_KEY'] && ENV['S3_SECRET']
-  Fog.mock! unless Rails.env == 'production'
+  if !Rails.env.production? && !Rails.env.staging?
+    Fog.mock!
+  end
+
   $fog = Fog::AWS::S3.new(
-    :aws_access_key_id      => ENV['S3_KEY'],
-    :aws_secret_access_key  => ENV['S3_SECRET']
+    :aws_access_key_id     => ENV['S3_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET']
   )
   $fog.directories.create(:key => "#{Rails.env.downcase}.s3.rubygems.org")
 end
