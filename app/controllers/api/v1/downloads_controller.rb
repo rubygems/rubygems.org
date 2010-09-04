862b1baac929cdb1da5a80bc8ce2b5792099858c
Nathan Sutton <nate@sevenwire.com>
2010-09-03 23:46:39 -0500

    Allow the retrieval of download counts for specific gems via the API

--- app/controllers/api/v1/downloads_controller.rb
+++ app/controllers/api/v1/downloads_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::DownloadsController < ApplicationController
+
+  before_filter :find_gem, :only => [:show]
+
   def index
     render :json => {
       "total" => Download.count
     }
   end
+
+  def show
+    render :json => {
+      "total_downloads" => @rubygem.downloads,
+      "latest_version_downloads" => @rubygem.versions.most_recent.downloads_count
+    }
+  end
+
 end
--- test/functional/api/v1/downloads_controller_test.rb
+++ test/functional/api/v1/downloads_controller_test.rb
@@ -12,4 +12,26 @@ class Api::V1::DownloadsControllerTest < ActionController::TestCase
       assert_equal @count, JSON.parse(@response.body)['total']
     end
   end
+
+  context "on GET to show" do
+    setup do
+      gem = Factory(:rubygem_with_downloads)
+      version1 = Factory(:version, :rubygem => gem, :number => '1.0.0')
+      version2 = Factory(:version, :rubygem => gem, :number => '2.0.0')
+
+      Download.incr(gem.name, version1.full_name)
+      Download.incr(gem.name, version2.full_name)
+      Download.incr(gem.name, version2.full_name)
+
+      get :show, :id => gem.name
+    end
+
+    should "have some json with the total downloads for the gem" do
+      assert_equal 3, JSON.parse(@response.body)['total_downloads']
+    end
+
+    should "have some json with the downloads for the most recent version of the gem" do
+      assert_equal 2, JSON.parse(@response.body)['latest_version_downloads']
+    end
+  end
 end
