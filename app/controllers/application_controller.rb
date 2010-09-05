93436f72285af699b32d4d3bdc4f3786e35b6fa3
Nathan Sutton <nate@sevenwire.com>
2010-09-05 17:41:31 -0500

    Restrict all actions to SSL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   include Clearance::Authentication
   protect_from_forgery :only => [:create, :update, :destroy]
 
+  ssl_required
+
   def authenticate_with_api_key
     api_key = request.headers["Authorization"] || params[:api_key]
     self.current_user = User.find_by_api_key(api_key)
