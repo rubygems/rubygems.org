d0124c9186f7e3c5a75bf796c94d6d280e03aef1
Fred Yates <fred@fred-yatess-macbook-pro.local>
2010-10-15 15:19:55 -0400

    sweet new flash message styles

--- db/schema.rb
+++ db/schema.rb
@@ -114,13 +114,13 @@ ActiveRecord::Schema.define(:version => 20101013135725) do
     t.string   "remember_token",     :limit => 128
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "handle"
     t.boolean  "email_reset"
+    t.string   "handle"
   end
 
-  add_index "users", ["confirmation_token", "id"], :name => "index_users_on_id_and_confirmation_token"
   add_index "users", ["email"], :name => "index_users_on_email"
   add_index "users", ["handle"], :name => "index_users_on_handle"
+  add_index "users", ["id", "confirmation_token"], :name => "index_users_on_id_and_confirmation_token"
   add_index "users", ["id", "token"], :name => "index_users_on_id_and_token"
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
   add_index "users", ["token"], :name => "index_users_on_token"
