a98f123765f9590d3fdc37f29f81ea732f7d8204
Nick Quaranto <nick@quaran.to>
2010-10-16 10:30:19 -0400

    Updating to paul_revere 0.1.5 with flash markup

--- public/javascripts/announcements.js
+++ public/javascripts/announcements.js
@@ -1,6 +1,6 @@
 function hideAnnouncement(announcement_created_at) {
   createCookie(announcement_created_at, 'hidden', 1);
-  $("#announcement").slideUp();
+  $("#announcement").parent().slideUp();
 }
 
 // http://www.quirksmode.org/js/cookies.html
