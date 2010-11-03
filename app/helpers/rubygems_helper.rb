alloy/gemcutter

Show links to gem overviews for all owners of a gem.

app/helpers/rubygems_helper.rb
@@ -51,4 +51,10 @@ module RubygemsHelper
   def documentation_path(version)
     "http://rubydoc.info/gems/#{version.rubygem.name}/#{version.number}/frames"
   end
+
+  def links_to_owners(rubygem)
+    rubygem.owners.sort_by(&:handle).map do |owner|
+      link_to owner.handle, user_rubygems_path(owner)
+    end.join(", ").html_safe
+  end
 end

test/functional/rubygems_controller_test.rb
@@ -8,6 +8,23 @@ class RubygemsControllerTest < ActionController::TestCase
       sign_in_as(@user)
     end
 
+    context "On GET to show for any gem" do
+      setup do
+        @owners = [@user, Factory(:email_confirmed_user)]
+        create_gem(*@owners)
+        get :show, :id => @rubygem.to_param
+      end
+
+      should respond_with :success
+      should render_template :show
+      should assign_to :rubygem
+      should "renders owner gems overview links" do
+        @owners.each do |owner|
+          assert_have_selector "a[href='#{user_rubygems_path(owner)}']"
+        end
+      end
+    end
+
     context "On GET to show for another user's gem" do
       setup do
         @rubygem = Factory(:rubygem)

test/unit/helpers/rubygems_helper_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require File.expand_path("../../../test_helper", __FILE__)
 
 class RubygemsHelperTest < ActionView::TestCase
   include Rails.application.routes.url_helpers
@@ -48,5 +48,13 @@ class RubygemsHelperTest < ActionView::TestCase
     should "not create link for code" do
       assert_nil link_to_page("Code", @linkset.code)
     end
+  end
+
+  should "create links to owners gem overviews" do
+    users = Array.new(2) { Factory(:email_confirmed_user) }
+    create_gem(*users)
+    expected_links = users.map { |u| link_to u.handle, user_rubygems_path(u) }.join(", ")
+    assert_equal expected_links, links_to_owners(@rubygem)
+    assert links_to_owners(@rubygem).html_safe?
   end
 end

rubygems/rubygems.org

app/helpers/rubygems_helper.rb
@@ -59,4 +59,10 @@ module RubygemsHelper
       end
     ]
   end
+
+  def links_to_owners(rubygem)
+    rubygem.owners.sort_by(&:handle).map do |owner|
+      link_to owner.handle, profile_path(owner)
+    end.join(", ").html_safe
+  end
 end

test/functional/rubygems_controller_test.rb
@@ -7,6 +7,23 @@ class RubygemsControllerTest < ActionController::TestCase
       sign_in_as(@user)
     end
 
+    context "On GET to show for any gem" do
+      setup do
+        @owners = [@user, Factory(:email_confirmed_user)]
+        create_gem(*@owners)
+        get :show, :id => @rubygem.to_param
+      end
+
+      should respond_with :success
+      should render_template :show
+      should assign_to :rubygem
+      should "renders owner gems overview links" do
+        @owners.each do |owner|
+          assert_have_selector "a[href='#{profile_path(owner)}']"
+        end
+      end
+    end
+
     context "On GET to show for another user's gem" do
       setup do
         @rubygem = Factory(:rubygem)

test/unit/helpers/rubygems_helper_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require File.expand_path("../../../test_helper", __FILE__)
 
 class RubygemsHelperTest < ActionView::TestCase
   include Rails.application.routes.url_helpers
@@ -68,5 +68,13 @@ class RubygemsHelperTest < ActionView::TestCase
         assert_equal [version.slug, rubygem_version_stats_path(@rubygem, version.slug)], links[index]
       end
     end
+  end
+
+  should "create links to owners gem overviews" do
+    users = Array.new(2) { Factory(:email_confirmed_user) }
+    create_gem(*users)
+    expected_links = users.map { |u| link_to u.handle, profile_path(u) }.join(", ")
+    assert_equal expected_links, links_to_owners(@rubygem)
+    assert links_to_owners(@rubygem).html_safe?
   end
 end