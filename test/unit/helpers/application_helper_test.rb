2b6319d60978199ac693be5a22bccdf36fd1f907
Nick Quaranto <nick@quaran.to>
2010-11-03 00:49:31 -0400

    Showing gravatars on rubygems#show, a bit of cleanup

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -1,11 +1,9 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-  
   should "produce ssl urls" do
-    expected = new_session_url(:protocol   => 'https')
+    expected = new_session_url(:protocol => 'https')
     actual = ssl_url_for :controller => 'clearance/sessions', :action => 'new'
     assert_equal expected, actual
   end
-  
 end
