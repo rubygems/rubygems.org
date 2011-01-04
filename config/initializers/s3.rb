5c42597a299656ff309876a27fd3b71a02476e59
Nick Quaranto <nick@quaran.to>
2011-01-04 13:43:49 -0500

    Create the s3 directory from the rubygems config

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -8,5 +8,5 @@ if ENV['S3_KEY'] && ENV['S3_SECRET']
     :aws_access_key_id      => ENV['S3_KEY'],
     :aws_secret_access_key  => ENV['S3_SECRET']
   )
-  $fog.directories.create(:key => "#{Rails.env.downcase}.s3.rubygems.org")
+  $fog.directories.create(:key => $rubygems_config[:s3_bucket])
 end
