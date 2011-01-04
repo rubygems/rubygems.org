3c7c8113487e5f4c8acd7b94116a1b2ccb04e16a
Nick Quaranto <nick@quaran.to>
2011-01-04 13:29:20 -0500

    Bring in qrush/heroku_asset_cacher, enable if set in config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,7 +4,7 @@ Gemcutter::Application.configure do
   config.action_controller.perform_caching = true
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
   config.active_support.deprecation = :notify
-  config.serve_static_assets = false
+  config.serve_static_assets = $rubygems_config[:asset_cacher]
   config.i18n.fallbacks = true
 end
 
