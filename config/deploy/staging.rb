e34c32fdda689bbad7413bacc5b5ef0c2f907045
Nick Quaranto <nick@quaran.to>
2011-01-31 22:52:49 -0500

    robots is in config dir too

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,7 +8,7 @@ set :deploy_to,  "/var/www/rubycentral/staging.gemcutter.org/"
 namespace :deploy do
   desc "For the staging environment, move in a robots.txt that blocks robots from the entire site"
   task :move_in_staging_robots_txt, :roles => :app do
-    run "cp #{deploy_to}/shared/system/robots.txt #{current_path}/public/robots.txt"
+    run "cp #{deploy_to}/shared/config/robots.txt #{current_path}/public/robots.txt"
   end
 end
 
