78a29a0c6f5b6124e3d510b7f1fbbbd31679d69f
Evan Light <evan@tripledogdare.net>
2011-02-06 11:34:06 -0500

    Yay! Prettier (good one, tundal45)

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -10,7 +10,9 @@ class Api::V1::StatsController < Api::BaseController
 
   def search
     full_name = params[:id].chomp(".json")
-    start, stop = [params[:from], params[:to]].map { |d| Date.parse(d) }
+    start, stop = [params[:from], params[:to]].map do |d| 
+      Date.parse(d)
+    end
     if version = Version.find_by_full_name(full_name)
       render :json => Download.counts_by_day_for_version_in_date_range(version, start, stop) 
     else
