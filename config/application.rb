9ccd86d7b7765ab4abe9ed77e4fbd3811525f7f0
Nick Quaranto <nick@quaran.to>
2011-02-09 20:20:07 -0500

    update to RG 1.5.1

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 $rubygems_config = YAML.load_file("config/rubygems.yml")[Rails.env].symbolize_keys
 HOST             = $rubygems_config[:host]
-RUBYGEMS_VERSION = "1.5.0"
+RUBYGEMS_VERSION = "1.5.1"
 
 module Gemcutter
   class Application < Rails::Application
