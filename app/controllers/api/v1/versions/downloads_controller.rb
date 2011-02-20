6856fc0c9d691daa87ca9a54d9a9496c6f10a089
Evan Light <evan@tripledogdare.net>
2011-02-19 23:51:57 -0500

    Added documentation for the API enhancements

--- app/controllers/api/v1/versions/downloads_controller.rb
+++ app/controllers/api/v1/versions/downloads_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::Versions::DownloadsController < Api::BaseController
   before_filter :parse_version_id
 
+  # Returns a JSON object containing the number of downloads by day
+  # for a particular gem version for 90 days of data.  Output resembles:
+  # { 
+  #   "2011-02-17":0,
+  #   "2011-02-18":0,
+  #   "2011-02-19":2,
+  #   "2011-02-20":1
+  #   ....
+  # }
   def index
     if version = Version.find_by_full_name(@name)
       render :json => Download.counts_by_day_for_version(version)
@@ -9,6 +18,15 @@ class Api::V1::Versions::DownloadsController < Api::BaseController
     end
   end
 
+  # Returns a JSON object containing the number of downloads by day
+  # for a particular gem version for an arbitrary date range. Output resembles:
+  # { 
+  #   "2011-02-17":0,
+  #   "2011-02-18":0,
+  #   "2011-02-19":2,
+  #   "2011-02-20":1
+  #   ....
+  # }
   def search
     start, stop = [params[:from], params[:to]].map do |d| 
       Date.parse(d)
