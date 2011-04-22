79ffb160db28576cec215fec4f796bbf89600745
Nick Quaranto <nick@quaran.to>
2011-04-22 16:07:27 -0500

    Order versions by position for api/v1/versions#show

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::VersionsController < Api::BaseController
   def show
     gem_name = params[:id].try(:chomp, ".json")
     if rubygem = Rubygem.find_by_name(gem_name)
-      render :json => rubygem.versions.indexed
+      render :json => rubygem.public_versions
     else
       render :text => "This rubygem could not be found.", :status => 404
     end
