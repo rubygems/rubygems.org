8f1f7e10421f61ce9b056fed5f65d21c0d8c6e51
Nick Quaranto <nick@quaran.to>
2011-04-22 15:43:57 -0500

    Deny yanked versions from new versions and downloads api

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::VersionsController < Api::BaseController
   def show
     gem_name = params[:id].try(:chomp, ".json")
     if rubygem = Rubygem.find_by_name(gem_name)
-      render :json => rubygem.versions
+      render :json => rubygem.versions.indexed
     else
       render :text => "This rubygem could not be found.", :status => 404
     end
