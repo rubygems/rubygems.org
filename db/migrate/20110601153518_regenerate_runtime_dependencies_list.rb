9dccfaadf3f0e1c8b32fa029ec0828098ea2e6e6
Nick Quaranto <nick@thoughtbot.com>
2011-06-05 23:02:47 -0400

    really remove dev deps in a migration

--- db/migrate/20110601153518_regenerate_runtime_dependencies_list.rb
+++ db/migrate/20110601153518_regenerate_runtime_dependencies_list.rb
@@ -1,15 +1,5 @@
 class RegenerateRuntimeDependenciesList < ActiveRecord::Migration
   def self.up
-    count = 0
-    batch_size = 10000
-    Dependency.find_each(:batch_size => batch_size, :include => :version) do |dep|
-      puts count if count % batch_size == 0
-      if dep.version
-        $redis.del(Dependency.runtime_key(dep.version.full_name))
-        dep.send(:push_on_to_list)
-      end
-      count += 1
-    end
   end
 
   def self.down
