6b7122fd653f4a0b12a9f55a5b422e5ffaa52df1
Nick Quaranto <nick@thoughtbot.com>
2011-06-05 23:02:47 -0400

    really remove dev deps in a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110601153518) do
+ActiveRecord::Schema.define(:version => 20110601153519) do
 
   create_table "announcements", :force => true do |t|
     t.text     "body"
