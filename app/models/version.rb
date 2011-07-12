f730865ea0b09b91e099a1560c0af66a325ad453
Nick Quaranto <nick@thoughtbot.com>
2011-07-11 23:03:21 -0400

    use rails 3 validation style

--- app/models/version.rb
+++ app/models/version.rb
@@ -7,8 +7,9 @@ class Version < ActiveRecord::Base
   after_create     :full_nameify!
   after_save       :reorder_versions
 
-  validates_format_of :number,   :with => /\A#{Gem::Version::VERSION_PATTERN}\z/
-  validates_format_of :platform, :with => Rubygem::NAME_PATTERN
+  validates :number,   :format => {:with => /\A#{Gem::Version::VERSION_PATTERN}\z/}
+  validates :platform, :format => {:with => Rubygem::NAME_PATTERN}
+
   validate :platform_and_number_are_unique, :on => :create
   validate :authors_format, :on => :create
 
