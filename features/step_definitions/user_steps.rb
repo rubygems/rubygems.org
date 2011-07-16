dbe5e78164d0957228580a0c2641f7afb2f34d58
Erik Michaels-Ober <sferik@gmail.com>
2011-07-15 20:08:06 -0700

    Cleanup EOL whitespace

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,7 +2,7 @@ Then /^I should see my new "(.*)"$/ do |humanized_attribute|
   attribute = humanized_attribute.downcase.gsub(/\s/, '_')
   previous_value = @me.send(attribute)
   new_value = @me.reload.send(attribute)
-  assert_not_equal previous_value, new_value, 
+  assert_not_equal previous_value, new_value,
     "New value for #{humanized_attribute} expected but it hasn't changed!"
   assert_match new_value, response.body
 end
--- gem/test/yank_command_test.rb
+++ gem/test/yank_command_test.rb
@@ -34,7 +34,7 @@ class YankCommandTest < CommandTest
           command.say("Successfully yanked")
         end
       end
-      
+
       should 'invoke yank_gem' do
         stub(@command).yank_gem(@version, @platform)
         @command.execute
@@ -50,7 +50,7 @@ class YankCommandTest < CommandTest
         assert_requested(:delete, @api,
                          :headers => { 'Authorization' => 'key' })
       end
-      
+
       context 'with a platform specified' do
         setup do
           stub_api_key("key")
@@ -59,7 +59,7 @@ class YankCommandTest < CommandTest
           stub_request(:delete, @api).to_return(:body => "Successfully yanked")
           @command.handle_options([@gem, "-v", @version, "-p", @platform])
         end
-        
+
         should 'say gem was yanked' do
           @command.execute
           assert_received(@command) do |command|
@@ -67,7 +67,7 @@ class YankCommandTest < CommandTest
             command.say("Successfully yanked")
           end
         end
-        
+
         should 'invoke yank_gem' do
           stub(@command).yank_gem(@version, @platform)
           @command.execute
@@ -76,9 +76,9 @@ class YankCommandTest < CommandTest
           end
         end
       end
-      
+
     end
-    
+
     context 'unyanking a gem' do
       setup do
         stub_api_key("key")
@@ -86,7 +86,7 @@ class YankCommandTest < CommandTest
         stub_request(:put, @api).to_return(:body => "Successfully unyanked")
         @command.handle_options([@gem, "-v", @version, "--undo"])
       end
-      
+
       should 'say gem was unyanked' do
         @command.execute
         assert_received(@command) do |command|
@@ -102,15 +102,15 @@ class YankCommandTest < CommandTest
           command.unyank_gem(@version, @platform)
         end
       end
-      
+
       should 'put to api' do
         @command.execute
         assert_requested(:put, @api, :times => 1)
         assert_requested(:put, @api, :headers => { 'Authorization' => 'key' })
       end
     end
-    
-    
+
+
     context 'with a platform specified' do
       setup do
         stub_api_key("key")
@@ -119,7 +119,7 @@ class YankCommandTest < CommandTest
         stub_request(:put, @api).to_return(:body => "Successfully unyanked")
         @command.handle_options([@gem, "-v", @version, "-p", @platform, "--undo"])
       end
-      
+
       should 'say gem was unyanked' do
         @command.execute
         assert_received(@command) do |command|
@@ -127,7 +127,7 @@ class YankCommandTest < CommandTest
           command.say("Successfully unyanked")
         end
       end
-      
+
       should 'invoke unyank_gem' do
         stub(@command).unyank_gem(@version, @platform)
         @command.execute
@@ -136,7 +136,7 @@ class YankCommandTest < CommandTest
         end
       end
     end
-    
+
   end
 
 end
