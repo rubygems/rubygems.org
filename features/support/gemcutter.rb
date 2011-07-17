9f47f602f9c8bd019a56cd93a9a986ecffbbe4b3
Erik Michaels-Ober <sferik@gmail.com>
2011-07-17 11:53:42 -0700

    Downgrade to cucumber-rails 0.3.2 and webrat 0.5.3
    I spent a day trying to get the cukes passing with the latest version of
    these gems and failed miserably. Perhaps someone else can try again in
    the future. I've created an issue so it doesn't get forgotten: #314

--- features/support/gemcutter.rb
+++ features/support/gemcutter.rb
@@ -1,15 +1,25 @@
+WebMock.disable_net_connect!
+
 Hostess.local = true
 
 TEST_DIR = File.join('/', 'tmp', 'gemcutter')
 
+DatabaseCleaner.clean_with :truncation
+DatabaseCleaner.strategy = :transaction
+
 Before do
-  WebMock.reset!
+  WebMock.reset_webmock
+  DatabaseCleaner.start
+
   FileUtils.mkdir(TEST_DIR)
   Dir.chdir(TEST_DIR)
+
   $fog.directories.create(:key => $rubygems_config[:s3_bucket], :public => true)
 end
 
 After do
+  DatabaseCleaner.clean
+
   FileUtils.rm_rf(TEST_DIR)
   $redis.flushdb
 end
