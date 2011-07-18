c22f295da9fef312669c60325b330bced47f3144
Erik Michaels-Ober <sferik@gmail.com>
2011-07-18 00:15:11 -0700

    Add YAML response format for API search

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -4,9 +4,12 @@ class Api::V1::SearchesController < Api::BaseController
 
   def show
     @gems = Rubygem.search(params[:query]).with_versions.paginate(:page => params[:page])
-    respond_to do |wants|
-      wants.json { render :json => @gems }
-      wants.xml  { render :xml  => @gems }
+    respond_to do |format|
+      format.json { render :json => @gems }
+      format.xml  { render :xml  => @gems }
+      # Convert object to JSON and back before converting to YAML in order to
+      # strip the object type (e.g. !ruby/ActiveRecord:Rubygem) from response
+      format.yaml { render :text => JSON.load(@gems.to_json).to_yaml}
     end
   end
 
