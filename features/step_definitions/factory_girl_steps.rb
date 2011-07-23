d9e385ab03198b4098b54117a559cd45c1729f87
Nick Quaranto <nick@thoughtbot.com>
2011-07-23 13:56:47 -0400

    consolidate application factories into one file

--- features/step_definitions/factory_girl_steps.rb
+++ features/step_definitions/factory_girl_steps.rb
@@ -1,9 +1,3 @@
-Factory.factories.each do |name, factory|
-  Given /^an? #{name} exists with an? (.*) of "([^"]*)"$/ do |attr, value|
-    Factory(name, attr.gsub(' ', '_') => value)
-  end
-end
-
 Given /^a version exists for the "([^\"]*)" rubygem with a number of "([^\"]*)"$/ do |rubygem_name, version_number|
   rubygem = Rubygem.find_by_name!(rubygem_name)
   Factory(:version, :rubygem => rubygem, :number => version_number)
@@ -29,12 +23,4 @@ Given /^a subscription by "([^\"]*)" to the gem "([^\"]*)"$/ do |user_email, rub
   rubygem = Rubygem.find_by_name!(rubygem_name)
   user    = User.find_by_email(user_email)
   Factory(:subscription, :rubygem => rubygem, :user => user)
-end
-
-Given "the following versions exist:" do |table|
-  table.hashes.each do |row|
-    Factory(:version, :rubygem  => Rubygem.find_by_name!(row["Rubygem"]),
-                      :number   => row["Number"],
-                      :platform => row["Platform"])
-  end
 end
--- test/factories/rubyforge.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :rubyforger do
-    email
-    encrypted_password Digest::SHA1.hexdigest("password")
-  end
-end
