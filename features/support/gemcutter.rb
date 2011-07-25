0df61789a59a8d1a838d4dbf65b05e9e029ed21b
Erik Michaels-Ober <sferik@gmail.com>
2011-07-25 09:30:58 -0700

    Fix duplicate transaction warnings

--- features/support/gemcutter.rb
+++ features/support/gemcutter.rb
@@ -5,24 +5,16 @@ Capybara.app_host = "https://gemcutter.local"
 
 TEST_DIR = File.join('/', 'tmp', 'gemcutter')
 
-DatabaseCleaner.clean_with :truncation
-DatabaseCleaner.strategy = :transaction
-
 require 'factory_girl/step_definitions'
 
 Before do
   WebMock.reset!
-  DatabaseCleaner.start
-
   FileUtils.mkdir(TEST_DIR)
   Dir.chdir(TEST_DIR)
-
   $fog.directories.create(:key => $rubygems_config[:s3_bucket], :public => true)
 end
 
 After do
-  DatabaseCleaner.clean
-
   FileUtils.rm_rf(TEST_DIR)
   $redis.flushdb
 end
