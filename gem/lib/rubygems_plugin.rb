82c41f5b5c1f3ef0c43fa3fed64b570895a2835d
Nick Quaranto <nick@thoughtbot.com>
2011-07-29 16:53:13 -0400

    Update repo url, gem/ now lives at rubygems/gemcutter

--- gem/lib/rubygems_plugin.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems/command_manager'
-
-if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.3.6')
-  %w[migrate tumble webhook yank].each do |command|
-    Gem::CommandManager.instance.register_command command.to_sym
-  end
-end
