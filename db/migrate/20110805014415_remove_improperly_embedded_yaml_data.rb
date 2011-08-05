3df61940fccf21b7c55be2d719a60247d32c08be
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-08-04 22:15:34 -0400

    Add migration to clean up malformed requirements fields from yaml/syck issues.

---
+++ db/migrate/20110805014415_remove_improperly_embedded_yaml_data.rb
@@ -0,0 +1,12 @@
+class RemoveImproperlyEmbeddedYamlData < ActiveRecord::Migration
+  def self.up
+    Dependency.where("requirements like '%YAML::Syck::DefaultKey%'").each do |d|
+      d.requirements = d.clean_requirements
+      d.save(:validate => false)
+    end
+  end
+
+  def self.down
+    # Do nothing.
+  end
+end
