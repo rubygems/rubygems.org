edf51b2d1bfd7efd1930245bc680c045c6faa444
Gabriel Horner <gabriel.horner@gmail.com>
2011-08-28 15:01:46 -0400

    making more api tests consistent
    add YAML tests for versions/downloads#index

--- test/functional/api/v1/searches_controller_test.rb
+++ test/functional/api/v1/searches_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class Api::V1::SearchesControllerTest < ActionController::TestCase
-  context "with some gems" do
+  def self.should_respond_to(format)
+    context "and with #{format.to_s.upcase}" do
+      setup do
+        get :show, :query => "match", :format => format
+      end
+      should respond_with :success
+      should "return a hash" do
+        assert Hash, yield(@response.body).class
+      end
+      should "only include matching gems" do
+        gems = yield(@response.body)
+        assert_equal 1, gems.size
+        assert_equal "match", gems.first['name']
+      end
+    end
+  end
+
+  context "on GET to show with query=match" do
     setup do
       @match = Factory(:rubygem, :name => "match")
       @other = Factory(:rubygem, :name => "other")
@@ -9,49 +26,16 @@ class Api::V1::SearchesControllerTest < ActionController::TestCase
       Factory(:version, :rubygem => @other)
     end
 
-    context "On GET to show with query=match for json" do
-      setup do
-        get :show, :query => "match", :format => "json"
-      end
-      should respond_with :success
-      should "return a json hash" do
-        assert_not_nil JSON.parse(@response.body)
-      end
-      should "only include matching gems" do
-        gems = JSON.parse(@response.body).map { |g| g["name"] }
-        assert_contains         gems, "match"
-        assert_does_not_contain gems, "other"
-      end
+    should_respond_to(:json) do |body|
+      JSON.parse body
     end
 
-    context "On GET to show with query=match for xml" do
-      setup do
-        get :show, :query => "match", :format => "xml"
-      end
-      should respond_with :success
-      should "return xml" do
-        assert_not_nil Nokogiri.parse(@response.body).root
-      end
-      should "only include matching gems" do
-        gems = Nokogiri.parse(@response.body).css("name")
-        assert_equal 1, gems.size
-        assert_equal "match", gems.first.content
-      end
+    should_respond_to(:xml) do |body|
+      Hash.from_xml(Nokogiri.parse(body).to_xml)['rubygems']
     end
 
-    context "On GET to show with query=match for yaml" do
-      setup do
-        get :show, :query => "match", :format => "yaml"
-      end
-      should respond_with :success
-      should "return yaml" do
-        assert_not_nil YAML.load(@response.body)
-      end
-      should "only include matching gems" do
-        gems = YAML.load(@response.body)
-        assert_equal 1, gems.size
-        assert_equal "match", gems.first['name']
-      end
+    should_respond_to(:yaml) do |body|
+      YAML.load body
     end
   end
 end
--- test/functional/api/v1/versions/downloads_controller_test.rb
+++ test/functional/api/v1/versions/downloads_controller_test.rb
@@ -12,7 +12,25 @@ class Api::V1::Versions::DownloadsControllerTest < ActionController::TestCase
                  :to => to.to_date.to_s
   end
 
-  context "on GET to show" do
+
+  def self.should_respond_to(format)
+    context "with #{format.to_s.upcase}" do
+      should "have 90 attributes, one per day of gem version download counts" do
+        get_index(@version, format)
+        assert_equal 90, yield(@response.body).size
+      end
+
+      should "have an object with the download counts by day" do
+        get_index(@version, format)
+        hash = yield(@response.body)
+        assert_equal 42, hash[@eight_nine_days_ago]
+        assert_equal 2, hash[@one_day_ago]
+        assert_equal 1, hash[Time.zone.today.to_s]
+      end
+    end
+  end
+
+  context "on GET to index" do
     setup do
       @version = Factory(:version)
       @eight_nine_days_ago = 89.days.ago.to_date.to_s
@@ -23,35 +41,12 @@ class Api::V1::Versions::DownloadsControllerTest < ActionController::TestCase
       Download.incr(@version.rubygem.name, @version.full_name)
     end
 
-
-    context "with JSON" do
-      should "have a JSON object with 90 attributes, one per day of gem version download counts" do
-        get_index(@version)
-        assert_equal 90, JSON.parse(@response.body).count
-      end
-
-      should "have a JSON object with the download counts by day" do
-        get_index(@version)
-        json = JSON.parse(@response.body)
-        assert_equal 42, json[@eight_nine_days_ago]
-        assert_equal 2, json[@one_day_ago]
-        assert_equal 1, json[Time.zone.today.to_s]
-      end
+    should_respond_to(:json) do |body|
+      JSON.parse body
     end
 
-    context "with YAML" do
-      should "have a YAML object with 90 attributes, one per day of gem version download counts" do
-        get_index(@version, 'yaml')
-        assert_equal 90, YAML.load(@response.body).count
-      end
-
-      should "have a JSON object with the download counts by day" do
-        get_index(@version, 'yaml')
-        yaml = YAML.load(@response.body)
-        assert_equal 42, yaml[@eight_nine_days_ago]
-        assert_equal 2, yaml[@one_day_ago]
-        assert_equal 1, yaml[Time.zone.today.to_s]
-      end
+    should_respond_to(:yaml) do |body|
+      YAML.load body
     end
 
   end
@@ -100,6 +95,37 @@ class Api::V1::Versions::DownloadsControllerTest < ActionController::TestCase
     end
   end
 
+  def self.should_respond_to(format)
+    context "with #{format.to_s.upcase}" do
+      should "return download stats for the days specified for at most 90 days" do
+        get_search(@version, @one_hundred_eighty_nine_days_ago, @one_hundred_days_ago, format)
+        hash = yield(@response.body)
+
+        assert_equal 90, hash.size
+        assert_equal 42, hash[@one_hundred_eighty_nine_days_ago]
+        assert_equal 1764, hash[@one_hundred_days_ago]
+        assert_nil hash[@one_hundred_ninety_days_ago]
+      end
+
+      should "return download stats for the days specified for ranges smaller than 90 days" do
+        get_search(@version, @one_hundred_one_days_ago, @one_hundred_days_ago, format)
+        hash = yield(@response.body)
+
+        assert_equal 2, hash.size
+        assert_equal 0, hash[@one_hundred_one_days_ago]
+        assert_equal 1764, hash[@one_hundred_days_ago]
+      end
+
+      should "be able to return stats for a single day" do
+        get_search(@version, @one_hundred_days_ago, @one_hundred_days_ago, format)
+        hash = yield(@response.body)
+
+        assert_equal 1, hash.size
+        assert_equal 1764, hash[@one_hundred_days_ago]
+      end
+    end
+  end
+
   context "on GET to search" do
     setup do
       @one_hundred_days_ago = 100.days.ago.to_date.to_s
@@ -117,31 +143,12 @@ class Api::V1::Versions::DownloadsControllerTest < ActionController::TestCase
         $redis.hincrby Download.history_key(@version), @one_hundred_days_ago, 1764
       end
 
-      should "return download stats for the days specified for at most 90 days" do
-        get_search(@version, @one_hundred_eighty_nine_days_ago, @one_hundred_days_ago)
-        json = JSON.parse(@response.body)
-
-        assert_equal 90, json.size
-        assert_equal 42, json[@one_hundred_eighty_nine_days_ago]
-        assert_equal 1764, json[@one_hundred_days_ago]
-        assert_nil json[@one_hundred_ninety_days_ago]
+      should_respond_to(:json) do |body|
+        JSON.parse body
       end
 
-      should "return download stats for the days specified for ranges smaller than 90 days" do
-        get_search(@version, @one_hundred_one_days_ago, @one_hundred_days_ago)
-        json = JSON.parse(@response.body)
-
-        assert_equal 2, json.size
-        assert_equal 0, json[@one_hundred_one_days_ago]
-        assert_equal 1764, json[@one_hundred_days_ago]
-      end
-
-      should "be able to return stats for a single day" do
-        get_search(@version, @one_hundred_days_ago, @one_hundred_days_ago)
-        json = JSON.parse(@response.body)
-
-        assert_equal 1, json.size
-        assert_equal 1764, json[@one_hundred_days_ago]
+      should_respond_to(:yaml) do |body|
+        YAML.load body
       end
     end
 
