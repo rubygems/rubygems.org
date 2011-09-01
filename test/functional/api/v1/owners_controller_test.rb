ddb2ed5f30f58749caf5788a549a44f3297c9c2d
Gabriel Horner <gabriel.horner@gmail.com>
2011-08-31 21:49:33 -0400

    owners api controller - add tests, use respond_to

--- test/functional/api/v1/owners_controller_test.rb
+++ test/functional/api/v1/owners_controller_test.rb
@@ -1,31 +1,49 @@
 require 'test_helper'
 
 class Api::V1::OwnersControllerTest < ActionController::TestCase
-  should "route GET with JSON to new controller" do
-    route = {:controller => 'api/v1/owners',
-             :action     => 'show',
-             :rubygem_id => "rails",
-             :format     => "json"}
-    assert_recognizes(route, '/api/v1/gems/rails/owners.json')
+  def self.should_respond_to(format)
+    should "route GET show with #{format.to_s.upcase}" do
+      route = {:controller => 'api/v1/owners',
+               :action     => 'show',
+               :rubygem_id => "rails",
+               :format     => format.to_s}
+      assert_recognizes(route, "/api/v1/gems/rails/owners.#{format}")
+    end
+
+    context "on GET to show with #{format.to_s.upcase}" do
+      setup do
+        @rubygem = Factory(:rubygem)
+        @user = Factory(:email_confirmed_user)
+        @rubygem.ownerships.create(:user => @user, :approved => true)
+
+        @request.env["HTTP_AUTHORIZATION"] = @user.api_key
+        get :show, :rubygem_id => @rubygem.to_param, :format => format
+      end
+
+      should "return an array" do
+        response = yield(@response.body)
+        assert_kind_of Array, response
+      end
+
+      should "return correct owner email" do
+        assert_equal @user.email, yield(@response.body)[0]['email']
+      end
+    end
   end
 
-  should "route GET with XML to new controller" do
-    route = {:controller => 'api/v1/owners',
-             :action     => 'show',
-             :rubygem_id => "rails",
-             :format     => "xml"}
-    assert_recognizes(route, '/api/v1/gems/rails/owners.xml')
+  should_respond_to :xml do |body|
+    Hash.from_xml(Nokogiri.parse(body).to_xml)['users']
   end
 
-  should "route GET with YAML to new controller" do
-    route = {:controller => 'api/v1/owners',
-             :action     => 'show',
-             :rubygem_id => "rails",
-             :format     => "yaml"}
-    assert_recognizes(route, '/api/v1/gems/rails/owners.yaml')
+  should_respond_to :json do |body|
+    JSON.parse body
   end
 
-  should "route POST to new controller" do
+  should_respond_to :yaml do |body|
+    YAML.load body
+  end
+
+  should "route POST" do
     route = {:controller => 'api/v1/owners',
              :action     => 'create',
              :rubygem_id => "rails",
@@ -33,7 +51,7 @@ class Api::V1::OwnersControllerTest < ActionController::TestCase
     assert_recognizes(route, :path => '/api/v1/gems/rails/owners.json', :method => :post)
   end
 
-  should "route DELETE to new controller" do
+  should "route DELETE" do
     route = {:controller => 'api/v1/owners',
              :action     => 'destroy',
              :rubygem_id => "rails",
