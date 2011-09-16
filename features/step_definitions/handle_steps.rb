c87a66368545ac2959605d442798ad696ed09ceb
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-09-16 18:27:54 -0400

    Hard code the steps of singing in with a new password, since we only ever do it once in all of the scenarios.

--- features/step_definitions/handle_steps.rb
+++ features/step_definitions/handle_steps.rb
@@ -5,3 +5,10 @@ end
 Given /^my handle is nil$/ do
   @me.update_attribute :handle, nil
 end
+
+When /^I sign in (?:with|as) "(.*)" with "(.*)"$/ do |email, password|
+  When %{I go to the sign in page}
+  And %{I fill in "Email" with "#{email}"}
+  And %{I fill in "Password" with "#{password}"}
+  And %{I press "Sign in"}
+end
