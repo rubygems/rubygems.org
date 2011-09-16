317a09c6a34d05f31cba77400801c361cbae05c8
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-09-16 19:20:06 -0400

    Update stock clearance matcher for email field to match our email or username fields.

--- features/step_definitions/clearance/clearance_steps.rb
+++ features/step_definitions/clearance/clearance_steps.rb
@@ -10,9 +10,9 @@ end
 
 Then /^I should see an email field$/ do
   if page.respond_to?(:should)
-    page.should have_css?("input[type='email']")
+    page.should have_css?("input[id='session_who']")
   else
-    assert page.has_css?("input[type='email']")
+    assert page.has_css?("input[id='session_who']")
   end
 end
 
