eeabd140dcd0450fa614e65e766ae4a75d2d5712
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-09-20 12:37:53 -0400

    Don't validate the versions on save of the rubygem; rails 3.1 raises on invalid records through associations where rails 3.0 did not.  This behavior is required since we first create invalid version records, and save the gem and then update the versions to be valid.

--- app/models/rubygem.rb
+++ app/models/rubygem.rb
@@ -5,7 +5,7 @@ class Rubygem < ActiveRecord::Base
   has_many :ownerships, :dependent => :destroy
   has_many :subscribers, :through => :subscriptions, :source => :user
   has_many :subscriptions, :dependent => :destroy
-  has_many :versions, :dependent => :destroy
+  has_many :versions, :dependent => :destroy, :validate => false
   has_many :web_hooks, :dependent => :destroy
   has_one :linkset, :dependent => :destroy
 
