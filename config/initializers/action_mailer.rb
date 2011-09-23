b894f8df4f1f95f5973c44b3ac35180e4664738d
Nick Quaranto <nick@thoughtbot.com>
2011-09-23 17:39:18 -0400

    use the right rails application config variable, wtf

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,4 +1,4 @@
-if Rails.configuration.action_mailer.delivery_method == :smtp
+if Rails.application.config.action_mailer.delivery_method == :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "25",
