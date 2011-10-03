f8cfbe3669dda0d08aec84f4c98aa8cdde9d5070
Erik Michaels-Ober <sferik@gmail.com>
2011-10-03 12:00:43 -0700

    Cleanup whitespace

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Gemcutter::Application.configure do
   config.active_support.deprecation = :notify
   config.serve_static_assets = $rubygems_config[:asset_cacher]
   config.i18n.fallbacks = true
-  
+
   config.action_dispatch.session = {
     :domain => ".rubygems.org",
     :secure => true
