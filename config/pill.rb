18c4f91f69f0ceb2b68f9b9f24fca26f8af18c45
Nick Quaranto <nick@quaran.to>
2011-11-24 11:43:05 -0500

    use rake jobs:work instead of script/delayed_job

--- config/pill.rb
+++ config/pill.rb
@@ -8,11 +8,10 @@ Bluepill.application("gemcutter", :log_file => log_file, :base_dir => base_dir) 
     process.start_grace_time    = 10.seconds
     process.stop_grace_time     = 10.seconds
     process.restart_grace_time  = 10.seconds
-    process.checks :mem_usage, :every => 10.seconds, :below => 400.megabytes, :times => 3
 
     process.environment = { 'RAILS_ENV' => RAILS_ENV }
-    process.start_command = "script/delayed_job start"
-    process.stop_command  = "script/delayed_job stop"
+    process.start_command = "bundle exec rake jobs:work"
+    process.daemonize = true
 
     process.pid_file = "#{RELEASE_PATH}/shared/pids/delayed_job.pid"
 
