cadeae8f029126cca759523f95079ee6662fe7c7
Nick Quaranto <nick@quaran.to>
2011-11-24 11:24:23 -0500

    it helps to actually run commands

--- lib/deploy/bluepill.rb
+++ lib/deploy/bluepill.rb
@@ -3,18 +3,18 @@ namespace :bluepill do
 
   desc "Stop processes that bluepill is monitoring and quit bluepill"
   task :quit, :roles => [:app] do
-    "bluepill stop #{bluepill_options}; true"
-    "bluepill quit #{bluepill_options}; true"
+    run "bluepill stop #{bluepill_options}; true"
+    run "bluepill quit #{bluepill_options}; true"
   end
 
   desc "Load bluepill configuration and start it"
   task :start, :roles => [:app] do
-    "bluepill load #{release_path}/config/pills/#{rails_env}.rb #{bluepill_options}"
+    run "bluepill load #{release_path}/config/pills/#{rails_env}.rb #{bluepill_options}"
   end
 
   desc "Prints bluepills monitored processes statuses"
   task :status, :roles => [:app] do
-    "bluepill status #{bluepill_options}"
+    run "bluepill status #{bluepill_options}"
   end
 end
 
