f7695491861d4d8dd880bf7ff60f16a1015d23a9
Nick Quaranto <nick@quaran.to>
2012-03-19 18:18:56 -0400

    no more joking around

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,15 +1,7 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-
 if Rails.env.maintenance? || Rails.env.staging? || Rails.env.production?
   secret_path = Rails.root.join("config", "secret.rb")
   if secret_path.file?
     require secret_path.to_s
+    Rails.application.config.secret_token = ENV['SECRET_TOKEN']
   end
 end
-
-Rails.application.config.secret_token = ENV['SECRET_TOKEN'] || '53336f5ffda880aa2f6d5fbb7603b6df4d975408afbf44642d342547c25f92265eb01c34b118ee7fa87475f69949b07b292d95276aae2f6bda029ea25d28dc28'
