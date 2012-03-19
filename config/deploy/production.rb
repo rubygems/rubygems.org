dd02718358baecb4e425ff14567d307e66c71b85
Evan Phoenix <evan@phx.io>
2012-03-19 11:22:17 -0700

    Change production deployment target to backend server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :app, "rubycentral.org"
-role :web, "rubycentral.org"
-role :db,  "rubycentral.org", :primary => true
+role :app, "b1.rubycentral.org"
+role :web, "b1.rubycentral.org"
+role :db,  "b1.rubycentral.org", :primary => true
 
 set :branch,    "production"
 set :deploy_to, "/var/www/rubycentral/gemcutter.org/"
