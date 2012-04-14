34cb30a50103ae979caaaa145700a2cd277ce652
Nick Quaranto <nick@quaran.to>
2012-04-14 11:22:56 -0400

    bump to RG 1.8.22

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ HOST             = $rubygems_config[:host]
 
 # DO NOT EDIT THIS LINE DIRECTLY
 # Instead, run: bundle exec rake gemcutter:rubygems:update VERSION=[version number] RAILS_ENV=[staging|production] S3_KEY=[key] S3_SECRET=[secret]
-RUBYGEMS_VERSION = "1.8.21"
+RUBYGEMS_VERSION = "1.8.22"
 
 module Gemcutter
   class Application < Rails::Application
