415ba285aa39cd772965d8ea7a4920e16b706f8e
Karel Minarik <karmi@karmi.cz>
2012-08-26 14:40:00 +0200

    [SEARCH] Added a more complex search query in the SearchesController#show method
    Previously, we have been searching gems based on their names only.
    
    With the new, more complex mapping defined in the preceding commit, we can add a more complex search query as well.
    
    We're using a boolean query, keeping the original match prefix query and adding the `query_string` query,
    which uses the Lucene query syntax (field specifation, boolean operators, wildcards,
    fuzzy search, range and proximity searches, grouping, etc).
    
    See:
    
    * http://www.elasticsearch.org/guide/reference/query-dsl/query-string-query.html
    * http://lucene.apache.org/core/3_6_1/queryparsersyntax.html

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,17 +1,24 @@
 class SearchesController < ApplicationController
 
   def show
-    if params[:query]
+    if params[:query].present?
       @gems = Rubygem.tire.search :page     => params[:page],
                                   :per_page => Rubygem.per_page,
                                   :load     => {:include => 'versions'} do |search|
 
         search.query do |s|
           s.filtered do |f|
-            f.query  { |q| q.match :name, params[:query], :type => 'phrase_prefix', :operator => 'and' }
+            f.query  do |q|
+              q.boolean do |it|
+                it.should { |q| q.match 'name.raw', params[:query], :boost => 500 }
+                it.should { |q| q.match :name, params[:query], :type => 'phrase_prefix', :operator => 'and', :boost => 100 }
+                it.should { |q| q.string params[:query], :default_operator => 'and' }
+              end
+            end
             f.filter :term, :indexed => true
           end
         end
+
         search.sort   do
           by 'downloads', :desc
           by 'name.raw',  :asc
