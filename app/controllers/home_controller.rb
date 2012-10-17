3fa608d05e1fd08f35a3e02b7fb510685cc050e6
Evan Phoenix <evan@fallingsnow.net>
2012-10-17 08:53:36 -0700

    Remove leaderboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @rubygems_count  = Rubygem.total_count
-    @downloads_count = Download.count
-    @latest          = Rubygem.latest
-    @downloaded      = Download.most_downloaded_today
-    @updated         = Version.just_updated
   end
 end
