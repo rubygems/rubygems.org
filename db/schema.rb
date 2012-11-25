240cb6c40c8bee33bbc7a032575e99b60c691a06
Erik Michaels-Ober <sferik@gmail.com>
2012-11-25 10:43:49 -0500

    Update delayed_job dependency to version 3.0.4

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120916165331) do
+ActiveRecord::Schema.define(:version => 20121124000000) do
 
   create_table "announcements", :force => true do |t|
     t.text     "body"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20120916165331) do
     t.string   "locked_by"
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
+    t.string   "queue"
   end
 
   create_table "dependencies", :force => true do |t|
