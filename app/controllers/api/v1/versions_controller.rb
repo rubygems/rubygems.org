29e89d0a31d80d2575a6cb654073ce2f4485f85d
Andreas Loupasakis <alup@aloop.org>
2013-03-03 21:43:45 +0200

    Reuse a helper method to dry the code

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::VersionsController < Api::BaseController
   respond_to :json, :xml, :yaml
+  before_filter :find_rubygem
 
   def show
-    if rubygem = Rubygem.find_by_name(params[:id]) and rubygem.public_versions.count.nonzero?
+    if @rubygem.public_versions.count.nonzero?
 
-      if stale?(rubygem)
-        respond_with(rubygem.public_versions, :yamlish => true)
+      if stale?(@rubygem)
+        respond_with(@rubygem.public_versions, :yamlish => true)
       end
     else
       render :text => "This rubygem could not be found.", :status => 404
