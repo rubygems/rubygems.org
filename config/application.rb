93e0fe5aa55b6653b863549bd2ad244f8127251a
Justin Leitgeb <justin@stackbuilders.com>
2013-03-06 08:10:27 -0500

    Bump to 2.0.2

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ HOST             = $rubygems_config[:host]
 
 # DO NOT EDIT THIS LINE DIRECTLY
 # Instead, run: bundle exec rake gemcutter:rubygems:update VERSION=[version number] RAILS_ENV=[staging|production] S3_KEY=[key] S3_SECRET=[secret]
-RUBYGEMS_VERSION = "2.0.1"
+RUBYGEMS_VERSION = "2.0.2"
 
 module Gemcutter
   class Application < Rails::Application
