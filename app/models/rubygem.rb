e05b30a37396457bea018698ccd67eeb8d49af60
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2013-03-25 22:08:04 -0700

    link changes that whit537 wanted.

--- app/models/rubygem.rb
+++ app/models/rubygem.rb
@@ -253,7 +253,7 @@ class Rubygem < ActiveRecord::Base
   end
 
   def gittip_url
-    'http://gittip.com/on/rubygems/gem/' + name
+    'https://gittip.com/on/rubygems/gem/' + name + '/'
   end
 
   def gittip_enabled?
