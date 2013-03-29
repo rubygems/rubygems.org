589fafc5f99f00e30698f015a8cce5bc35785e02
Jakub Suder <jakub.suder@gmail.com>
2013-03-29 16:55:52 +0100

    fix for logging in existing users
    clearance changed the default password strategy
    to bcrypt so it needs to be overridden here (might
    be a good idea to switch to bcrypt in future using
    Clearance::PasswordStrategies::BCryptMigrationFromSHA1)

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,5 +2,6 @@ unless Rails.env.maintenance?
   Clearance.configure do |config|
     config.mailer_sender = "donotreply@rubygems.org"
     config.secure_cookie = true
+    config.password_strategy = Clearance::PasswordStrategies::SHA1
   end
 end
