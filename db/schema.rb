4b262a0520f9574d291b202331b80b2dd8daa9af
Evan Phoenix <evan@phx.io>
2013-08-29 16:00:57 -0700

    Change requirements to a text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130110064832) do
+ActiveRecord::Schema.define(:version => 20130829225823) do
 
   create_table "announcements", :force => true do |t|
     t.text     "body"
@@ -145,9 +145,9 @@ ActiveRecord::Schema.define(:version => 20130110064832) do
     t.integer  "position"
     t.boolean  "latest"
     t.string   "full_name"
-    t.string   "licenses"
     t.integer  "size"
-    t.string   "requirements"
+    t.string   "licenses"
+    t.text     "requirements"
   end
 
   add_index "versions", ["built_at"], :name => "index_versions_on_built_at"
