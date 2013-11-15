19d7926a275a6c5c25db22886b4811af3d1df184
Mason Fischer <mason@thoughtbot.com>
2013-11-15 13:49:02 -0500

    Remove whiny_nils deprecation warings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 Gemcutter::Application.configure do
   config.cache_classes = false
-  config.whiny_nils = true
 
   config.consider_all_requests_local = true
   config.action_controller.perform_caching             = false
