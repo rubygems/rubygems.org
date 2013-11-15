3224c5ffe10d307903a487e3b29f14da82c4efe6
Mason Fischer <mason@thoughtbot.com>
2013-11-15 13:41:17 -0500

    Remove pending depreaction from tests

--- test/unit/helpers/rubygems_helper_test.rb
+++ test/unit/helpers/rubygems_helper_test.rb
@@ -82,13 +82,13 @@ class RubygemsHelperTest < ActionView::TestCase
     end
 
     should "show the overview link first" do
-      pending
+      skip
       overview = stats_options(@rubygem).first
       assert_equal ["Overview", rubygem_stats_path(@rubygem)], overview
     end
 
     should "have all the links for the rubygem" do
-      pending
+      skip
       _, *links = stats_options(@rubygem)
 
       @versions.sort.reverse.each_with_index do |version, index|
