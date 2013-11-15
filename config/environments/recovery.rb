e8db52d4c4351c3862bad112931485a7f3f3a758
Mason Fischer <mason@thoughtbot.com>
2013-11-15 13:49:02 -0500

    Remove whiny_nils deprecation warings

--- config/environments/recovery.rb
+++ config/environments/recovery.rb
@@ -2,7 +2,6 @@ Gemcutter::Application.configure do
   # for testing recovery mode on your local machine, LOCAL=1 rails s -e recovery
   if ENV["LOCAL"]
     config.cache_classes = false
-    config.whiny_nils = true
 
     config.consider_all_requests_local = true
     config.action_controller.perform_caching             = false
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,5 @@
 Gemcutter::Application.configure do
   config.cache_classes = true
-  config.whiny_nils = true
 
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
