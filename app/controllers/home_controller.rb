7032ac6583b49540583ecd06d189a9c937852243
Evan Phoenix <evan@fallingsnow.net>
2014-01-03 16:40:16 -0800

    Pull the rubygems version from the gem db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,19 @@
 class HomeController < ApplicationController
   def index
+    @version = current_rubygems_release
     @rubygems_count  = Rubygem.total_count
     @downloads_count = Download.count
+  end
+
+  def current_rubygems_release
+    if g = Rubygem.find_by_name("rubygems-update")
+      if v = g.versions.release.indexed.by_created_at.first
+        v.number
+      else
+        "0.0.0"
+      end
+    else
+      "0.0.0"
+    end
   end
 end
