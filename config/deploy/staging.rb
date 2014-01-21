a074dea3ef142241acc276c3eba02f0e994e2079
David Radcliffe <radcliffe.david@gmail.com>
2014-01-21 14:58:32 -0500

    deploy via capistrano!

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,15 +1,12 @@
-role :app, "staging.gemcutter.org"
-role :web, "staging.gemcutter.org"
-role :db,  "staging.gemcutter.org", :primary => true
+raise "No staging server setup yet!"
+# server ".us-west-2.compute.amazonaws.com", :app, :db, :primary => true
+# set :branch,    "staging"
 
-set :branch,     "staging"
-set :deploy_to,  "/var/www/rubycentral/staging.gemcutter.org/"
+# namespace :deploy do
+#   desc "For the staging environment, move in a robots.txt that blocks robots from the entire site"
+#   task :move_in_staging_robots_txt, :roles => :app do
+#     run "cp #{deploy_to}/shared/config/robots.txt #{current_path}/public/robots.txt"
+#   end
+# end
 
-namespace :deploy do
-  desc "For the staging environment, move in a robots.txt that blocks robots from the entire site"
-  task :move_in_staging_robots_txt, :roles => :app do
-    run "cp #{deploy_to}/shared/config/robots.txt #{current_path}/public/robots.txt"
-  end
-end
-
-after "deploy:symlink", "deploy:move_in_staging_robots_txt"
+# after "deploy:symlink", "deploy:move_in_staging_robots_txt"
