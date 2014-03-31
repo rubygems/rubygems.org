a0c09e8a10d4ca2f89e23b9d26e435fc7f66d7d5
Weston Platter <westonplatter@gmail.com>
2014-03-30 23:18:21 -0600

    require test gems. without modules not defined.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'factory_girl_rails'
+require 'webmock'
 require 'rr'
 
 class ActiveSupport::TestCase
