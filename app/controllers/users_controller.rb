2362c14eb869ecd32ab84c3ad0f330413664fce3
Weston Platter <westonplatter@gmail.com>
2014-04-06 22:03:40 -0600

    implement strong params for the UsersController within Clearance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < Clearance::UsersController
-
   ssl_required
-
+  
+  private 
+    def user_from_params
+      user_params = params[:user] || Hash.new
+      email       = user_params.delete(:email)
+      password    = user_params.delete(:password)
+      handle      = user_params.delete(:handle)
+    
+      Clearance.configuration.user_model.new(user_params).tap do |user|
+        user.handle   = handle
+        user.email    = email
+        user.password = password
+      end
+    end
 end
