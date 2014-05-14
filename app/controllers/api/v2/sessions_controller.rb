475582155bea69e806653cffc0850e61bf04dc1d
Arthur Neves <arthurnn@gmail.com>
2014-05-14 10:04:02 -0400

    Update clearance to 1.2.x
    Even after removing the references to assign current_user we still get WARNINGS:
    'Assigning the current_user this way has been deprecated. You should instead use the sign_in method.'
    
    clearance 1.2.0 includes
    https://github.com/thoughtbot/clearance/pull/376 which fixes the
    `current_user=` on `sign_in_as`.

--- app/controllers/api/v2/sessions_controller.rb
+++ app/controllers/api/v2/sessions_controller.rb
@@ -2,15 +2,15 @@ class SessionsController < Clearance::SessionsController
   ssl_required
 
   def create
-
     @user = User.authenticate(params[:session][:who],
                               params[:session][:password])
-    if @user.nil?
-      flash_failure_after_create
-      render :template => 'sessions/new', :status => :unauthorized
-    else
-      sign_in(@user)
-      redirect_back_or(url_after_create)
+    sign_in(@user) do |status|
+      if status.success?
+        redirect_back_or(url_after_create)
+      else
+        flash.now.notice = status.failure_message
+        render :template => 'sessions/new', :status => :unauthorized
+      end
     end
   end
 
