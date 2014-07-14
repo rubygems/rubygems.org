0439e43bde5e6015594e24899d901c676007375a
David Radcliffe <radcliffe.david@gmail.com>
2014-07-14 08:54:57 -0400

    production deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server 'deploy@app02-aws.rubygems.org', :app, :db, :primary => true
-role :restart, 'app02-aws.rubygems.org', :no_release => true
+server 'deploy@app02.production.rubygems.org', :app, :db, :primary => true
+role :restart, 'app02.production.rubygems.org', :no_release => true
 set :branch, 'master'
