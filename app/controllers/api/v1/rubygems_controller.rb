8be2684f7c3ab31bf8ada64f59992deae904d2a7
David Radcliffe <radcliffe.david@gmail.com>
2014-11-20 23:21:09 -0500

    start adding custom metrics

--- app/controllers/api/v1/rubygems_controller.rb
+++ app/controllers/api/v1/rubygems_controller.rb
@@ -31,8 +31,10 @@ class Api::V1::RubygemsController < Api::BaseController
   def yank
     if @version.indexed?
       @version.yank!
+      Librato.increment 'yank.success'
       render :text => "Successfully yanked gem: #{@version.to_title}"
     else
+      Librato.increment 'yank.failure'
       render :text => "The version #{params[:version]} has already been yanked.", :status => :unprocessable_entity
     end
   end
