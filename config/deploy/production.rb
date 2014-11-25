dd08c15e7b30f2899bfb0982db9dae444718156d
David Radcliffe <radcliffe.david@gmail.com>
2014-11-25 18:43:19 -0500

    update deploy config to reflect actual servers

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 server 'deploy@app02.production.rubygems.org', :app, :db, :primary => true
-role :restart, 'app02.production.rubygems.org', :no_release => true
-set :branch, 'master'
+server 'deploy@app03.production.rubygems.org', :app, :db
+role :restart, 'app02.production.rubygems.org', 'app03.production.rubygems.org', :no_release => true
+set :branch, 'production'
