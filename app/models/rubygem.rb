624ced82ec7495c04ce82a1585d71af22780d227
George Drummond <georgedrummond@gmail.com>
2014-11-26 23:03:34 +0000

    Don't use update_all on one record
    Conflicts:
    	app/models/rubygem.rb

--- app/models/rubygem.rb
+++ app/models/rubygem.rb
@@ -236,7 +236,7 @@ class Rubygem < ActiveRecord::Base
     numbers = self.reload.versions.sort.reverse.map(&:number).uniq
 
     self.versions.each do |version|
-      Version.update_all({:position => numbers.index(version.number)}, {:id => version.id})
+      Version.find(version.id).update_column(:position, numbers.index(version.number))
     end
 
     self.versions.update_all(:latest => false)
@@ -245,7 +245,7 @@ class Rubygem < ActiveRecord::Base
       platforms[version.platform] << version
       platforms
     end.each_value do |platforms|
-      Version.update_all({:latest => true}, {:id => platforms.sort.last.id})
+      Version.find(platforms.sort.last.id).update_column(:latest, true)
     end
   end
 
