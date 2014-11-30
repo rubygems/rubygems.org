30bf878b724dbc99ba7194e3f46a611d0d095674
Hsing-Hui Hsu <hsing-hui@carezone.com>
2014-11-29 16:15:02 -0800

    Improve version specification recommendation
    Previous Gemfile recommendations for rubygem version were too strict.
    This recommends the latest feature release version up to the latest version of
    the gem.
    
    Fixes #409

--- app/models/version.rb
+++ app/models/version.rb
@@ -237,7 +237,16 @@ class Version < ActiveRecord::Base
   end
 
   def to_bundler
-    %{gem '#{rubygem.name}', '~> #{number}'}
+    if number[0] == "0"
+      %{gem '#{rubygem.name}', '~> #{number}'}
+    else
+      release = feature_release(number)
+      if release == Gem::Version.new(number)
+        %{gem '#{rubygem.name}', '~> #{release}'}
+      else
+        %{gem '#{rubygem.name}', '~> #{release}', '>= #{number}'}
+      end
+    end
   end
 
   def to_gem_version
@@ -314,5 +323,10 @@ class Version < ActiveRecord::Base
                  :platform, platform)
 
     push
+  end
+
+  def feature_release(number)
+    feature_version = Gem::Version.new(number).segments[0,2].join('.')
+    Gem::Version.new(feature_version)
   end
 end
