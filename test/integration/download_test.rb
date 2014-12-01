f068418ea391aed6f60f7b7794ced6ad853afa5d
Nick Quaranto <nick@quaran.to>
2014-11-30 22:52:10 -0500

    Kill download.feature, effectively just tests platform gems can be downloaded alongside of normal gems

---
+++ test/integration/download_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class DownloadTest < ActionDispatch::IntegrationTest
+  test "downloading a gem" do
+    rubygem = create(:rubygem, name: "sandworm", number: "1.0.0")
+    create(:version, rubygem: rubygem, number: "1.0.0", platform: "java")
+
+    get rubygem_path("sandworm")
+    assert page.has_content? "Total downloads 0"
+
+    1.times do
+      get "/gems/sandworm-1.0.0.gem"
+    end
+
+    2.times do
+      get "/gems/sandworm-1.0.0-java.gem"
+    end
+
+    get rubygem_path("sandworm")
+    assert page.has_content? "Total downloads 3"
+
+    get rubygem_version_path("sandworm", "1.0.0")
+    assert page.has_content? "For this version 1"
+
+    get rubygem_version_path("sandworm", "1.0.0-java")
+    assert page.has_content? "For this version 2"
+  end
+end
