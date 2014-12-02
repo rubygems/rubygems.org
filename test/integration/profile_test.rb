b3d3691313e66ac66698623afdaa339b080a9a78
Nick Quaranto <nick@quaran.to>
2014-12-01 21:31:07 -0500

    Merge email change feature into profile test

--- test/integration/profile_test.rb
+++ test/integration/profile_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ProfileTest < ActionDispatch::IntegrationTest
   setup do
-    @user = create(:user, email: "nick@example.com", handle: "nick1")
+    @user = create(:user, email: "nick@example.com", password: "secret123", handle: "nick1")
     cookies[:remember_token] = @user.remember_token
   end
 
@@ -25,6 +25,21 @@ class ProfileTest < ActionDispatch::IntegrationTest
     put "profile", user: {handle: "nick2"}
     assert_response :success
     assert page.has_content? "Handle has already been taken"
+  end
+
+  test "changing email allows signing in with new email" do
+    put "profile", user: {email: "nick2@example.com"}
+    assert_response :redirect
+
+    delete sign_out_path
+    assert_response :redirect
+    assert_nil cookies[:remember_token]
+
+    post session_path, session: {who: "nick2@example.com", password: "secret123"}
+    assert_response :redirect
+
+    get root_path
+    assert page.has_content?("Sign out")
   end
 
   test "disabling email on profile" do
