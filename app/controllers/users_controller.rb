74ae67f10016cc3e1132fa1fe5ebc3a204cf6167
Arthur Neves <arthurnn@gmail.com>
2014-12-24 16:15:23 -0500

    Make signup use strong_parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,7 @@ class UsersController < Clearance::UsersController
 
   ssl_required
 
+  def user_params
+    params.require(:user).permit(:bio, :email, :handle, :hide_email, :location, :password, :website)
+  end
 end
