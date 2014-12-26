56259da237f53ca4ec4efd3bd8ff582c8dc64839
Arthur Neves <arthurnn@gmail.com>
2014-12-26 13:18:00 -0500

    Remove bundler assets group
    Rails 4+ dont build an app with assets group anymore, we can remove that right now under rails 3.2 too

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,9 @@ unless Rails.env.maintenance?
   require 'sprockets/railtie'
 end
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 $rubygems_config = YAML.load_file("config/rubygems.yml")[Rails.env].symbolize_keys
 HOST             = $rubygems_config[:host]
