61eb7019e14dd1857677827b11e8bd9efc38d954
Arthur Neves <arthurnn@gmail.com>
2014-12-26 18:14:30 -0500

    Dont use ssl cookies on tests or dev
    [related #680] - It doesnt fix the issue in production, as it is a
    clearance issue, but it resolves it in development and test.

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,7 +1,7 @@
 unless Rails.env.maintenance?
   Clearance.configure do |config|
     config.mailer_sender = "donotreply@rubygems.org"
-    config.secure_cookie = true
+    config.secure_cookie = true unless Rails.env.test? || Rails.env.development?
     config.password_strategy = Clearance::PasswordStrategies::BCryptMigrationFromSHA1
   end
 end
