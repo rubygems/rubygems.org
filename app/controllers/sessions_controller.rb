840b777b6349a7cbee7d7229425974ad3250983a
Arthur Neves <arthurnn@gmail.com>
2014-12-26 18:26:58 -0500

    No need for strong_params on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,10 @@ class SessionsController < Clearance::SessionsController
   ssl_required
 
   def create
-    @user = User.authenticate(params_who, params_password)
+    params.require(:session)
+    @user = User.authenticate(
+      params[:session][:who], params[:session][:password]
+    )
 
     sign_in(@user) do |status|
       if status.success?
@@ -26,13 +29,5 @@ class SessionsController < Clearance::SessionsController
 
   def url_after_create
     dashboard_url
-  end
-
-  def params_who
-    params.require(:session).permit(:who)[:who]
-  end
-
-  def params_password
-    params.require(:session).permit(:password)[:password]
   end
 end
