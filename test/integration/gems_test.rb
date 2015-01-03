9e02ade031e3705a9216ea010690a545ac97f412
Arthur Neves <arthurnn@gmail.com>
2015-01-03 13:12:00 -0500

    Add tests for json non-valid routes

--- test/integration/gems_test.rb
+++ test/integration/gems_test.rb
@@ -6,7 +6,7 @@ class GemsTest < ActionDispatch::IntegrationTest
     @rubygem = create(:rubygem, name: "sandworm", number: "1.0.0")
   end
 
-  test "gem page with a non valid format" do
+  test "gem page with a non valid HTTP_ACCEPT header" do
     get rubygem_path(@rubygem), nil, {'HTTP_ACCEPT' => 'application/mercurial-0.1'}
     assert page.has_content? "1.0.0"
   end
