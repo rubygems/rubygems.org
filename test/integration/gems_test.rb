620fa09f71bf7b19bea592fb0cd480dbf45d6d64
Arthur Neves <arthurnn@gmail.com>
2015-01-06 21:58:10 -0500

    Add subscribe integration tests

--- test/integration/gems_test.rb
+++ test/integration/gems_test.rb
@@ -17,4 +17,24 @@ class GemsTest < ActionDispatch::IntegrationTest
     assert_equal :atom, response.content_type.symbol
     assert page.has_content? "sandworm"
   end
+
+  test "subscribe to a gem" do
+    get rubygem_path(@rubygem, as: @user.id)
+    assert page.has_css?('a#subscribe')
+
+    post rubygem_subscription_path(@rubygem, as: @user.id), nil, {'HTTP_ACCEPT' => 'application/javascript'}
+
+    assert_match(/\("\.toggler"\)\.toggle\(\)/, @response.body)
+    assert_equal @user.subscribed_gems.first, @rubygem
+  end
+
+  test "unsubscribe to a gem" do
+    create(:subscription, rubygem: @rubygem, user: @user)
+
+    get rubygem_path(@rubygem, as: @user.id)
+    assert page.has_css?('a#unsubscribe')
+
+    delete rubygem_subscription_path(@rubygem, as: @user.id), nil, {'HTTP_ACCEPT' => 'application/javascript'}
+    assert_match(/\("\.toggler"\)\.toggle\(\)/, @response.body)
+  end
 end
