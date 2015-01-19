c487fe00c534b02ab35049b64291cc2d845edb8e
Arthur Neves <arthurnn@gmail.com>
2015-01-18 20:49:36 -0500

    Use strong_parameters to require params

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,17 +1,3 @@
 class Api::BaseController < ApplicationController
   skip_before_filter :require_ssl
-
-  private
-
-  def has_required_params?(*keys)
-    if keys.all? {|key| params[key] }
-      true
-    else
-      missing_params = keys.select {|key| !params[key] }
-      str = missing_params.size > 1 ? 'params' : 'param'
-      render :text => "Request is missing #{str} #{missing_params.map(&:inspect).to_sentence}",
-        :status => :bad_request
-      false
-    end
-  end
 end
