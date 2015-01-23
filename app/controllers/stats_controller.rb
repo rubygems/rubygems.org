cd88bfc54c1af62789e78c9dd2fb574b74633930
Fabian Rodriguez <fabianrbz@gmail.com>
2015-01-22 23:37:18 -0200

    Remove filter definition and invocations from the StatsController, given that #show is no longer defined.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,18 +1,9 @@
 class StatsController < ApplicationController
-  before_action :find_rubygem,  :only => :show
-  before_action :ensure_hosted, :only => :show
-
   def index
     @number_of_gems      = Rubygem.total_count
     @number_of_users     = User.count
     @number_of_downloads = Download.count
     @most_downloaded     = Rubygem.downloaded(10)
     @most_downloaded_count = @most_downloaded.first.downloads
-  end
-
-  private
-
-  def ensure_hosted
-    render :file => 'public/404', :status => :not_found if !@rubygem.hosted?
   end
 end
