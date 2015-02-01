b8be2088c8deb6d28b95f8cdcb19a383d9c5166c
David Radcliffe <radcliffe.david@gmail.com>
2015-01-31 21:05:37 -0500

    fix staging deploys

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,7 @@
 server 'deploy@app01.staging.rubygems.org', :app, :db, :primary => true
 role :restart, 'app01.staging.rubygems.org', :no_release => true
 set :branch, ENV['BRANCH'] || 'master'
+set :bundle_flags, ''
 
 namespace :deploy do
   desc "For the staging environment, symlink a robots.txt that blocks robots from the entire site"
