3362987146a7a65589c8b0893018f6c1d6c2448b
Fabian Rodriguez <fabianrbz@gmail.com>
2015-02-02 23:30:14 -0200

    Update the test to match the current behaviour and cleanup the controller and the test a bit.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @version = Rubygem.current_rubygems_release
-    @rubygems_count  = Rubygem.total_count
     @downloads_count = Download.count
   end
-
 end
--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -3,13 +3,7 @@ require 'test_helper'
 class HomeControllerTest < ActionController::TestCase
   context "on GET to index" do
     setup do
-      @rubygems_count = 1337
-      @downloads_count = 1_000_000
-      stub(Rubygem).total_count { @rubygems_count }
-      stub(Rubygem).latest { [] }
-      stub(Download).most_downloaded_today { [] }
-      stub(Version).just_updated { [] }
-      stub(Download).count { @downloads_count }
+      stub(Download).count { 1_000_000 }
       get :index
     end
 
@@ -17,15 +11,10 @@ class HomeControllerTest < ActionController::TestCase
     should render_template :index
 
     should "display counts" do
-      assert page.has_content?("1,337")
       assert page.has_content?("1,000,000")
     end
 
-    should "load up the total count, latest, and most downloaded gems" do
-      assert_received(Rubygem)  { |subject| subject.total_count }
-      assert_received(Rubygem)  { |subject| subject.latest }
-      assert_received(Download) { |subject| subject.most_downloaded_today }
-      assert_received(Version)  { |subject| subject.just_updated }
+    should "load up the downloaded gems count" do
       assert_received(Download) { |subject| subject.count }
     end
   end
