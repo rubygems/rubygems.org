5a0489f1512dc80e49bb884be3ce1210badeb116
Arthur Neves <arthurnn@gmail.com>
2015-02-14 12:59:39 -0500

    Ignore all errors on Honeybadger that rails handles

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,5 +1,7 @@
 Honeybadger.configure do |config|
   config.api_key = ENV['HONEYBADGER_API_KEY']
   config.unwrap_exceptions = false
-  config.ignore << "ActionDispatch::ParamsParser::ParseError"
+  ActionDispatch::ExceptionWrapper.rescue_responses.keys.each do |error|
+    config.ignore << error unless config.ignore.include? error
+  end
 end
