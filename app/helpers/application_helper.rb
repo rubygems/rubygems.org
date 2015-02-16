2a714b84878404e16d4dcda14aa5620b61a8a19c
Arthur Neves <arthurnn@gmail.com>
2015-02-15 23:22:50 -0500

    Fix helper html escaping for rails 4.2
    On Rails 4.2 sanitize and truncate will both escape the html, so we
    cannot call both methods as that would cause a double escaping.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,8 @@ module ApplicationHelper
   end
 
   def short_info(version)
-    escape_once(truncate(sanitize(version.info).strip, :length => 90)).html_safe
+    info = version.info.truncate(90)
+    escape_once(sanitize(info)).strip.html_safe
   end
 
   def gravatar(size, id = "gravatar", user = current_user)
