bbd74d853a53054d4ba58286d7e9f1bf253fa438
Arthur Neves <arthurnn@gmail.com>
2015-04-05 00:56:03 -0400

    Remove sinatra and simplify Hostess
    Hostess middleware will only be used on development and test, as ngnix is doing that in production

--- app/middleware/hostess.rb
+++ app/middleware/hostess.rb
@@ -1,96 +1,43 @@
-class Hostess < Sinatra::Base
-  enable :raise_errors
-  disable :show_exceptions
-
-  set :protection, { :except => [:json_csrf] }
-
-  cattr_accessor :local do
-    true
-  end
-
-  def serve
-    if self.class.local
-      send_file(Pusher.server_path(request.path_info))
-    else
-      yield
-    end
-  end
-
-  def serve_via_s3
-    serve do
-      redirect "http://#{Gemcutter.config['s3_domain']}#{request.path_info}"
-    end
-  end
-
-  def serve_via_cf
-    serve do
-      redirect "http://#{Gemcutter.config['cf_domain']}#{request.path_info}"
-    end
-  end
-
-  %w[/specs.4.8.gz
+class Hostess < Rack::Static
+  def initialize(app, options={})
+    options[:root] = Rails.root.join("server")
+    options[:urls] = %w[/specs.4.8.gz
      /latest_specs.4.8.gz
      /prerelease_specs.4.8.gz
-  ].each do |index|
-    get index do
-      content_type('application/x-gzip')
-      serve_via_s3
-    end
-  end
-
-  %w[/quick/rubygems-update-1.3.6.gemspec.rz
+     /quick/rubygems-update-1.3.6.gemspec.rz
      /yaml.Z
      /yaml.z
      /Marshal.4.8.Z
      /quick/index.rz
      /quick/latest_index.rz
-  ].each do |deflated_index|
-    get deflated_index do
-      content_type('application/x-deflate')
-      serve_via_s3
-    end
-  end
-
-  %w[/yaml
+     /yaml
      /Marshal.4.8
      /specs.4.8
      /latest_specs.4.8
      /prerelease_specs.4.8
      /quick/index
      /quick/latest_index
-  ].each do |old_index|
-    head old_index do
-      "Please upgrade your RubyGems, it's quite old: http://rubygems.org/pages/download"
-    end
+    ]
 
-    get old_index do
-      serve_via_s3
+    super(app, options)
+  end
+
+  def can_serve(path)
+    super(path) || gem_download_path(path) || path =~ /\/quick\/Marshal\.4\.8\/.*\.gemspec.rz/
+  end
+
+  def gem_download_path(path)
+    if path =~ /\/gems\/(.*)\.gem/
+      $1
     end
   end
 
-  get "/quick/Marshal.4.8/*.gemspec.rz" do
-    if Version.rubygem_name_for(full_name)
-      content_type('application/x-deflate')
-      serve_via_cf
-    else
-      error 404, "This gem does not currently live at RubyGems.org."
-    end
-  end
-
-  get "/gems/*.gem" do
-    if name = Version.rubygem_name_for(full_name)
+  def call(env)
+    path = env['PATH_INFO']
+    if (full_name = gem_download_path(path)) &&
+       name = Version.rubygem_name_for(full_name)
       Download.incr(name, full_name)
-      serve_via_cf
-    else
-      error 404, "This gem does not currently live at RubyGems.org."
     end
-  end
-
-  get "/downloads/*.gem" do
-    redirect "/gems/#{params[:splat].join}.gem"
-  end
-
-  def full_name
-    @full_name ||= params[:splat].join.chomp('.gem')
+    super
   end
 end
--- test/unit/hostess_test.rb
+++ test/unit/hostess_test.rb
@@ -2,20 +2,13 @@ require 'test_helper'
 
 class HostessTest < ActiveSupport::TestCase
   def app
-    Hostess.new
+    Hostess.new(proc { [200, {}, ''] })
   end
 
-  def touch(path, local = true)
-    Hostess.local = local
-    if local
-      path = Pusher.server_path(path)
-      FileUtils.mkdir_p(File.dirname(path))
-      FileUtils.touch(path)
-    end
-  end
-
-  setup do
-    Hostess.local = false
+  def touch(path)
+    path = Pusher.server_path(path)
+    FileUtils.mkdir_p(File.dirname(path))
+    FileUtils.touch(path)
   end
 
   %w[/prerelease_specs.4.8.gz
@@ -36,15 +29,9 @@ class HostessTest < ActiveSupport::TestCase
      /yaml.z
   ].each do |index|
     should "serve up #{index} locally" do
-      touch index, true
+      touch index
       get index
       assert_equal 200, last_response.status
-    end
-
-    should "serve up #{index} remotely" do
-      touch index, false
-      get index
-      assert_equal 302, last_response.status
     end
   end
 
@@ -63,13 +50,6 @@ class HostessTest < ActiveSupport::TestCase
       assert_equal 1, @rubygem.reload.downloads
       assert_equal 1, @version.reload.downloads_count
     end
-
-    should "redirect to cdn for a gem" do
-      get @file
-
-      assert_equal "http://#{Gemcutter.config['cf_domain']}#{@file}", last_response.headers["Location"]
-      assert_equal 302, last_response.status
-    end
   end
 
   should "not be able to find bad gem" do
@@ -77,14 +57,14 @@ class HostessTest < ActiveSupport::TestCase
     assert_equal 404, last_response.status
   end
 
-  should "find gemspec if loaded in redis" do
+  should "find gemspec" do
     rubygem = create(:rubygem, :name => "rails")
     version = create(:version, :number => "4.0.0", :rubygem => rubygem)
 
     path = "/quick/Marshal.4.8/#{version.full_name}.gemspec.rz"
-    touch path, false
+    touch path
     get path
-    assert_equal 302, last_response.status
+    assert_equal 200, last_response.status
   end
 
   should "not be able to find a bad gemspec" do
@@ -94,7 +74,6 @@ class HostessTest < ActiveSupport::TestCase
   end
 
   should "serve up gem locally" do
-    Hostess.local = true
     download_count = Download.count
     file = "/gems/test-0.0.0.gem"
     FileUtils.cp gem_file.path, Pusher.server_path("gems")
@@ -107,11 +86,5 @@ class HostessTest < ActiveSupport::TestCase
     assert_equal download_count + 1, Download.count
     assert_equal 1, rubygem.reload.downloads
     assert_equal 1, version.reload.downloads_count
-  end
-
-  should "redirect to /gems for /downloads" do
-    get "/downloads/rails-3.0.0.gem"
-    assert_equal "/gems/rails-3.0.0.gem", URI(last_response.headers["Location"]).path
-    assert_equal 302, last_response.status
   end
 end
