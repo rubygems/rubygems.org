c0aeeec8affcffe236cf53947d82aff2b7e3c77e
David Radcliffe <radcliffe.david@gmail.com>
2015-04-14 18:42:33 -0400

    move delayed_job logs to delayed_job.log

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,5 +1,6 @@
 require 'delayed_job'
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 
 PRIORITIES = {push: 1, download: 2, web_hook: 3 }
