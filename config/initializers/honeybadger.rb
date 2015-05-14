24156db8c2fb74299a067f723080e5a4a3e0da10
Erik Michaels-Ober <sferik@gmail.com>
2015-05-14 20:17:24 +0200

    Update honeybadger to version 2.0.12

--- config/initializers/honeybadger.rb
+++
@@ -1,7 +0,0 @@
-Honeybadger.configure do |config|
-  config.api_key = ENV['HONEYBADGER_API_KEY']
-  config.unwrap_exceptions = false
-  ActionDispatch::ExceptionWrapper.rescue_responses.keys.each do |error|
-    config.ignore << error unless config.ignore.include? error
-  end
-end
