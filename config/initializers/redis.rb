ae00cf50ea8741ec10b366e86344617a656ac2fb
David Radcliffe <radcliffe.david@gmail.com>
2015-07-02 09:54:38 -0400

    fix toxiproxy initializer
    toxiproxy isn’t loaded in production so we shouldn’t try to use it

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,14 +1,12 @@
-port = Toxiproxy.running? ? 22220 : 6379
-
-if Rails.env.test? && Toxiproxy.running?
-  Toxiproxy.populate([{
-    name: "redis",
-    listen: "127.0.0.1:#{port}",
-    upstream: "127.0.0.1:6379"
-  }])
-end
-
 if Rails.env.test?
+  port = Toxiproxy.running? ? 22220 : 6379
+  if Toxiproxy.running?
+    Toxiproxy.populate([{
+      name: "redis",
+      listen: "127.0.0.1:#{port}",
+      upstream: "127.0.0.1:6379"
+    }])
+  end
   Redis.current = Redis.new(db: 1, port: port)
 else
   Redis.current = Redis.new(url: ENV['REDISTOGO_URL'])
