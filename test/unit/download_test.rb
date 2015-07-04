265ee5d2b3f2141754deb734a5487ac0a0924d42
Faruk AYDIN <omerfaruk26@gmail.com>
2015-07-04 19:56:12 +0300

    remove Download model highest_rank method and highest_rank nl translation

--- test/unit/download_test.rb
+++ test/unit/download_test.rb
@@ -64,9 +64,6 @@ class DownloadTest < ActiveSupport::TestCase
     assert_equal 3, Download.rank(@version_1)
 
     assert_equal 3, Download.today([@version_1, @version_2])
-    assert_equal 2, Download.highest_rank([@version_1, @version_2])
-    assert_equal 1, Download.highest_rank([@version_3])
-    assert_equal 1, Download.highest_rank([@version_3, @version_4])
   end
 
   should "find most downloaded all time" do
@@ -225,10 +222,6 @@ class DownloadTest < ActiveSupport::TestCase
 
   should "return zero for rank if no downloads exist" do
     assert_equal 0, Download.rank(build(:version))
-  end
-
-  should "return zero for highest rank the given versions have zero downloads" do
-    assert_equal 0, Download.highest_rank([build(:version), build(:version)])
   end
 
   should "delete all old today keys except the current" do
