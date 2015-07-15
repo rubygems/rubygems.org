8fd3912bdad759131ea2470e22cde7f1342a98fc
Erik Michaels-Ober <sferik@gmail.com>
2015-07-15 10:19:45 +0200

    Always include spaces after comments

--- db/migrate/20091007183410_zero_out_version_downloads.rb
+++ db/migrate/20091007183410_zero_out_version_downloads.rb
@@ -1,9 +1,9 @@
 class ZeroOutVersionDownloads < ActiveRecord::Migration
   def self.up
-    #Version.update_all(:downloads_count => 0)
+    # Version.update_all(:downloads_count => 0)
   end
 
   def self.down
-    #Version.update_all(:downloads_count => nil)
+    # Version.update_all(:downloads_count => nil)
   end
 end
