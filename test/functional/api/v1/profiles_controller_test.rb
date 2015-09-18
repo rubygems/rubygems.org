e6d87bdd689b16f37d8f41e26fc1d9e9869a011f
Arthur Neves <arthurnn@gmail.com>
2015-09-17 23:10:48 -0400

    Fix test that was not running and Rubocop
    Turns out when I was trying to fix a Rubocop violation here, I found
    that we were using the same context name for testing the JSON and YAML
    responses. So Minitest was just running one of them.
    This fixes the problem and also the Rubocop violation.

--- test/functional/api/v1/profiles_controller_test.rb
+++ test/functional/api/v1/profiles_controller_test.rb
@@ -6,52 +6,59 @@ class Api::V1::ProfilesControllerTest < ActionController::TestCase
     sign_in_as(@user)
   end
 
-  def self.should_respond_to(format)
-    context "on GET to show with id" do
-      setup do
-        get :show, id: @user.id, format: format
-      end
-
-      should respond_with :success
-    end
-
-    context "on GET to show with handle" do
-      setup do
-        get :show, id: @user.handle, format: format
-      end
-
-      should respond_with :success
-      should "include the user email" do
-        response = yield @response.body
-        assert response.key?("email")
-        assert_equal @user.email, response["email"]
-      end
-    end
-
-    context "on GET to show when hide email" do
-      setup do
-        @user.update(hide_email: true)
-        get :show, id: @user.handle, format: format
-      end
-
-      should respond_with :success
-      should "hide the user email" do
-        response = yield @response.body
-        refute response.key?("email")
-      end
-
-      should "shows the handle" do
-        response = yield @response.body
-        assert_equal @user.handle, response["handle"]
-      end
-    end
-  end
-
-  should_respond_to :json do |body|
+  def to_json(body)
     JSON.parse body
   end
 
-  should_respond_to :yaml do |body|
+  def to_yaml(body)
     YAML.load body
+  end
+
+  def response_body
+    send("to_#{@format}", @response.body)
+  end
+
+  [:json, :yaml].each do |format|
+    context "when using #{format}" do
+      setup do
+        @format = format
+      end
+
+      context "on GET to show with id" do
+        setup do
+          get :show, id: @user.id, format: format
+        end
+
+        should respond_with :success
+      end
+
+      context "on GET to show with handle" do
+        setup do
+          get :show, id: @user.handle, format: format
+        end
+
+        should respond_with :success
+        should "include the user email" do
+          assert response_body.key?("email")
+          assert_equal @user.email, response_body["email"]
+        end
+      end
+
+      context "on GET to show when hide email" do
+        setup do
+          @user.update(hide_email: true)
+          get :show, id: @user.handle, format: format
+        end
+
+        should respond_with :success
+        should "hide the user email" do
+          refute response_body.key?("email")
+        end
+
+        should "shows the handle" do
+          assert_equal @user.handle, response_body["handle"]
+        end
+      end
+    end
   end
 end
