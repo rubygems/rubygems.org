7c2d3dccf35966e387a2f699b08aa66308759740
Nick Quaranto <nick@quaran.to>
2015-09-18 00:52:06 -0400

    Simplify doorkeeper config

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -1,22 +1,10 @@
 Doorkeeper.configure do
   orm :active_record
 
-  # This block will be called to check whether the resource owner is authenticated or not.
-  # called in the AuthorizedApplicationsController by `before_action :authenticate_resource_owner!`
   resource_owner_authenticator do
-    clearance_session = env[:clearance] # session = Clearance::Session.new(env)
-    user = clearance_session && clearance_session.current_user
-
-    if user
-      user
-    else
-      session[:return_to] = request.fullpath
-      redirect_to(sign_in_url)
-    end
+    env[:clearance].try(:current_user)
   end
 
-  # Restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
-  # called in the ApplicationsController by `before_action :authenticate_admin!`
   admin_authenticator do
     user = authenticate_resource_owner!
     if Gemcutter.admins.include? user.try(:email)
@@ -26,26 +14,18 @@ Doorkeeper.configure do
     end
   end
 
-  default_scopes  :public
-  # optional_scopes :write, :update
+  default_scopes :public
 
-  access_token_methods :from_bearer_authorization, :from_access_token_param, :from_bearer_param
+  access_token_methods :from_bearer_authorization
 
   native_redirect_uri 'urn:ietf:wg:oauth:2.0:oob'
 
-  # Forces the usage of the HTTPS protocol in non-native redirect uris.
-  # OAuth2 delegates security in communication to the HTTPS protocol so it is wise to keep this enabled.
   force_ssl_in_redirect_uri SimpleSSLRequirement::SSL_ENVIRONMENTS.include?(Rails.env)
 
-  # implicit and password grant flows have risks that you should understand before enabling:
-  #   http://tools.ietf.org/html/rfc6819#section-4.4.2
-  #   http://tools.ietf.org/html/rfc6819#section-4.4.3
-  #
   grant_flows %w(authorization_code)
 
-  # All applications are trusted, so auto-approve applications, and
-  # user skips authorization step.
+  # All applications are trusted!
   skip_authorization do |resource_owner, client|
-    true # We're only supporting trusted apps.
+    true
   end
 end
