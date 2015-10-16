e8952c1adc0f769f7a63b34b53c1eeea5e0f9940
Ross Kaffenberger <rosskaff@gmail.com>
2015-10-16 08:54:29 -0400

    Switch stats page rendering to redis on app config setting
    Switch stats page rendering to redis on app config setting
    
    stats controller rubocop fixes

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,7 +3,13 @@ class StatsController < ApplicationController
     @number_of_gems      = Rubygem.total_count
     @number_of_users     = User.count
     @number_of_downloads = Download.count
-    @most_downloaded     = Rubygem.downloaded(10).sort_by(&:downloads).reverse!
-    @most_downloaded_count = @most_downloaded.first.downloads
+
+    if Rails.application.config.stats_page_top_10_from_redis
+      @most_downloaded = Rubygem.downloaded(10)
+      _, @most_downloaded_count = @most_downloaded.first
+    else
+      @most_downloaded = Rubygem.downloaded(10).sort_by(&:downloads).reverse!
+      @most_downloaded_count = @most_downloaded.first.downloads
+    end
   end
 end
--- test/functional/stats_controller_test.rb
+++ test/functional/stats_controller_test.rb
@@ -58,4 +58,72 @@ class StatsControllerTest < ActionController::TestCase
       end
     end
   end
+
+  context "on GET to index from Redis" do
+    setup do
+      Rails.application.config.stubs(:stats_page_top_10_from_redis).returns true
+
+      @number_of_gems      = 1337
+      @number_of_users     = 101
+      @number_of_downloads = 42
+      @most_downloaded     = [[create(:rubygem), @number_of_downloads]]
+
+      Rubygem.stubs(:total_count).returns @number_of_gems
+      Download.stubs(:count).returns @number_of_downloads
+      Rubygem.stubs(:downloaded).returns @most_downloaded
+      User.stubs(:count).returns @number_of_users
+
+      get :index
+    end
+
+    should respond_with :success
+    should render_template :index
+
+    should "display number of gems" do
+      assert page.has_content?("1,337")
+    end
+
+    should "display number of users" do
+      assert page.has_content?("101")
+    end
+
+    should "display number of downloads" do
+      assert page.has_content?("42")
+    end
+
+    should "load up the number of gems, users, and downloads" do
+      assert_received(User, :count)
+      assert_received(Rubygem, :total_count)
+      assert_received(Download, :count)
+      assert_received(Rubygem, :downloaded) { |subject| subject.with(10) }
+    end
+  end
+
+  context "on GET to index with multiple gems" do
+    setup do
+      Rails.application.config.stubs(:stats_page_top_10_from_redis).returns true
+
+      rg1 = create(:rubygem, number: "1")
+      rg2 = create(:rubygem, number: "1")
+      rg3 = create(:rubygem, number: "1")
+      Rubygem.stubs(:downloaded).returns [[rg3, 30], [rg2, 20], [rg1, 10]]
+
+      get :index
+    end
+
+    should "display number of downloads" do
+      assert page.has_content?("30")
+      assert page.has_content?("20")
+      assert page.has_content?("10")
+    end
+
+    should "not have width greater than 100%" do
+      assert_select ".stats__graph__gem__meter" do |element|
+        element.map { |h| h[:style] }.each do |width|
+          width =~ /width\: (\d+[,.]\d+)%/
+          assert Regexp.last_match(1).to_f <= 100, "#{Regexp.last_match(1)} is greater than 100"
+        end
+      end
+    end
+  end
 end
