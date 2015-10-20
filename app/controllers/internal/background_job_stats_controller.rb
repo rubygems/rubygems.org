396d1996eed056691a7aaa3eb187399de7a1737c
David Radcliffe <radcliffe.david@gmail.com>
2015-10-19 22:39:12 -0400

    remove old background job stats controller

--- app/controllers/internal/background_job_stats_controller.rb
+++
@@ -1,7 +0,0 @@
-class Internal::BackgroundJobStatsController < ActionController::Metal
-  def stats
-    total = Delayed::Job.count
-    failed = Delayed::Job.where.not(failed_at: nil).count
-    self.response_body = "total=#{total} failed=#{failed}"
-  end
-end
