0c39fd9d5b2a532608ba32d32593d80bfea5b580
David Radcliffe <radcliffe.david@gmail.com>
2015-11-21 21:31:04 -0500

    add app01.production to deploy hosts

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server 'app01.production.rubygems.org', user: 'deploy', roles: %w(app)
 server 'app02.production.rubygems.org', user: 'deploy', roles: %w(app db)
 server 'app03.production.rubygems.org', user: 'deploy', roles: %w(app)
 server 'lb03.production.rubygems.org', user: 'deploy', roles: %w(lb), no_release: true
