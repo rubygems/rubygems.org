11599420902ca5fa959443536b2bd80762600c41
Luciano Sousa <ls@lucianosousa.net>
2015-11-28 23:28:42 -0200

    create a constant with pagination value. avoid duplicated values in the controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,15 +2,17 @@ class DashboardsController < ApplicationController
   before_action :authenticate_with_api_key, unless: :signed_in?
   before_action :redirect_to_root, unless: :signed_in?
 
+  DEFAULT_PAGINATION = 20
+
   def show
     respond_to do |format|
       format.html do
-        @my_gems = current_user.rubygems.with_versions.by_name
-        @latest_updates = Version.subscribed_to_by(current_user).published(20)
+        @my_gems         = current_user.rubygems.with_versions.by_name
+        @latest_updates  = Version.subscribed_to_by(current_user).published(DEFAULT_PAGINATION)
         @subscribed_gems = current_user.subscribed_gems.with_versions
       end
       format.atom do
-        @versions = Version.subscribed_to_by(current_user).published(20)
+        @versions = Version.subscribed_to_by(current_user).published(DEFAULT_PAGINATION)
         render 'versions/feed'
       end
     end
