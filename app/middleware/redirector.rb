ad49fdeb354ab3e38714f032c8cab69f2d3e83c9
Reed Loden <reed@reedloden.com>
2016-03-07 14:23:14 -0800

    Correct two URLs to use https:// and fix HTML error in stylesheet link href
    Change the `docs.rubygems.org` and `fonts.gstatic.com` usage to use
    https://. Also fix a "fragile syntax construct" by correcting `&`
    to be `&amp;` in the fonts stylesheet `<link href>`.
    
    (noticed since I was reading @nateberkopec's commits as a follow-up to his
    blog post)

--- app/middleware/redirector.rb
+++ app/middleware/redirector.rb
@@ -12,7 +12,7 @@ class Redirector
       fake_request = Rack::Request.new(env.merge("HTTP_HOST" => Gemcutter::HOST))
       redirect_to(fake_request.url)
     elsif request.path =~ %r{^/(book|chapter|export|read|shelf|syndicate)} && request.host !~ /docs/
-      redirect_to("http://docs.rubygems.org#{request.path}")
+      redirect_to("https://docs.rubygems.org#{request.path}")
     elsif request.path =~ %r{^/pages/docs$}
       redirect_to("http://guides.rubygems.org")
     elsif request.path =~ %r{^/pages/gem_docs$}
