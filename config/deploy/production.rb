92d193a32e8e55b8d7a23a8548ed76d115c3d903
David Radcliffe <radcliffe.david@gmail.com>
2016-03-27 23:00:22 -0400

    use branch from shipit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,3 @@ server 'app02.production.rubygems.org', user: 'deploy', roles: %w(app jobs db)
 server 'app03.production.rubygems.org', user: 'deploy', roles: %w(app jobs)
 server 'app04.production.rubygems.org', user: 'deploy', roles: %w(app)
 server 'lb03.production.rubygems.org', user: 'deploy', roles: %w(lb), no_release: true
-set :branch, 'master'
