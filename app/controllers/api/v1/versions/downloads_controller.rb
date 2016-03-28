d9338a5ce9b417b01261690722390240bd31594d
Arthur Neves <arthurnn@gmail.com>
2016-03-28 15:14:04 -0400

    Add message for 404 on downloads api

---
+++ app/controllers/api/v1/versions/downloads_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::Versions::DownloadsController < Api::BaseController
+  def index
+    render text: "This endpoint is not supported anymore", status: :not_found
+  end
+
+  def search
+    render text: "This endpoint is not supported anymore", status: :not_found
+  end
+end
