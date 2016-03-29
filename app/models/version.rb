16ac491b0311366e624e48f073d092bba3e9e622
Arthur Neves <arthurnn@gmail.com>
2016-03-28 21:29:44 -0400

    Fix rubocop

--- app/models/version.rb
+++ app/models/version.rb
@@ -3,7 +3,7 @@ require 'digest/sha2'
 class Version < ActiveRecord::Base
   belongs_to :rubygem, touch: true
   has_many :dependencies, -> { order('rubygems.name ASC').includes(:rubygem) }, dependent: :destroy
-  has_one :gem_download, Proc.new { |m| where(rubygem_id: m.rubygem_id) }
+  has_one :gem_download, proc { |m| where(rubygem_id: m.rubygem_id) }
 
   before_save :update_prerelease
   after_validation :join_authors
