d03dd3293ad5598d9c78600898d92aaee1bd72a2
Arthur Neves <arthurnn@gmail.com>
2016-03-29 21:36:26 -0400

    Call to_a to force one query only

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,7 +3,7 @@ class StatsController < ApplicationController
     @number_of_gems      = Rubygem.total_count
     @number_of_users     = User.count
     @number_of_downloads = GemDownload.total_count
-    @most_downloaded     = Rubygem.downloaded(10).includes(:gem_download)
-    @most_downloaded_count = @most_downloaded.first.downloads
+    @most_downloaded     = Rubygem.downloaded(10).includes(:gem_download).to_a
+    @most_downloaded_count = @most_downloaded.first.gem_download.count
   end
 end
