dbcfafc419112fdead46dfe2f8790f3d4ad53d38
sonalkr132 <aditya.prakash132@gmail.com>
2016-05-28 17:28:40 +0530

    Add test for CORS headers

--- app/controllers/api/v1/rubygems_controller.rb
+++ app/controllers/api/v1/rubygems_controller.rb
@@ -63,7 +63,7 @@ class Api::V1::RubygemsController < Api::BaseController
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'GET'
-    headers['Access-Control-Max-Age'] = "1728000"
+    headers['Access-Control-Max-Age'] = '1728000'
   end
 
   def cors_preflight_check
