9c6b551944ef76c6c8b8815419d8b93db54c8992
David Radcliffe <radcliffe.david@gmail.com>
2016-06-02 08:29:55 -0400

    app03 is just app role now

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server 'app01.production.rubygems.org', user: 'deploy', roles: %w(app)
 server 'app02.production.rubygems.org', user: 'deploy', roles: %w(app jobs db)
-server 'app03.production.rubygems.org', user: 'deploy', roles: %w(app jobs)
+server 'app03.production.rubygems.org', user: 'deploy', roles: %w(app)
 server 'app04.production.rubygems.org', user: 'deploy', roles: %w(app)
 server 'jobs01.production.rubygems.org', user: 'deploy', roles: %w(jobs)
 server 'lb03.production.rubygems.org', user: 'deploy', roles: %w(lb), no_release: true
