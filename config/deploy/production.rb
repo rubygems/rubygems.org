8cc251213e4fafea469555af2e6b97d708e04f74
David Radcliffe <radcliffe.david@gmail.com>
2016-06-03 13:00:27 -0400

    add app05

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,7 @@ server 'app01.production.rubygems.org', user: 'deploy', roles: %w(app)
 server 'app02.production.rubygems.org', user: 'deploy', roles: %w(app db)
 server 'app03.production.rubygems.org', user: 'deploy', roles: %w(app)
 server 'app04.production.rubygems.org', user: 'deploy', roles: %w(app)
+server 'app05.production.rubygems.org', user: 'deploy', roles: %w(app)
 server 'jobs01.production.rubygems.org', user: 'deploy', roles: %w(jobs)
 server 'jobs02.production.rubygems.org', user: 'deploy', roles: %w(jobs)
 server 'lb03.production.rubygems.org', user: 'deploy', roles: %w(lb), no_release: true
