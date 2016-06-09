31f49282f965d045ddc9d879e0cb0367f214892b
Aditya Prakash <aditya.prakash132@gmail.com>
2016-06-09 19:13:46 +0530

    Remove before destroy hook from ownership
    We want to delete the ownership record when we are deleting user.
    Gem with no ownership is orphaned.
    Ensuring that user can't remove the last user is done in owner controller.

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -3,17 +3,4 @@ class Ownership < ActiveRecord::Base
   belongs_to :user
 
   validates :user_id, uniqueness: { scope: :rubygem_id }
-
-  before_destroy :keep_last_owner
-
-  private
-
-  def keep_last_owner
-    if rubygem.owners.count == 1
-      errors[:base] << "Can't delete last owner of a gem."
-      false
-    else
-      true
-    end
-  end
 end
--- test/unit/ownership_test.rb
+++ test/unit/ownership_test.rb
@@ -20,24 +20,4 @@ class OwnershipTest < ActiveSupport::TestCase
 
     should validate_uniqueness_of(:user_id).scoped_to(:rubygem_id)
   end
-
-  context "with multiple ownerships on the same rubygem" do
-    setup do
-      @rubygem       = create(:rubygem)
-      @ownership_one = create(:ownership, rubygem: @rubygem)
-      @ownership_two = create(:ownership, rubygem: @rubygem)
-    end
-
-    should "allow deletion of one ownership" do
-      @ownership_one.destroy
-      assert_equal 1, @rubygem.owners.length
-    end
-
-    should "not allow deletion of both ownerships" do
-      @ownership_one.destroy
-      @ownership_two.destroy
-      assert_equal 1, @rubygem.owners.length
-      assert_equal "Can't delete last owner of a gem.", @ownership_two.errors[:base].first
-    end
-  end
 end
