495008e07d651617c571b9d1db109526c8803919
Andre Arko <andre@arko.net>
2016-06-26 00:24:14 -0700

    manually set strong ETags based on response body

--- app/controllers/api/compact_index_controller.rb
+++ app/controllers/api/compact_index_controller.rb
@@ -3,31 +3,29 @@ class Api::CompactIndexController < Api::BaseController
 
   def names
     names = Rubygem.ordered_names
-    response_body = CompactIndex.names(names)
-    requested_range_for(response_body)
+    render_range CompactIndex.names(names)
   end
 
   def versions
-    versions_file_location = Rails.application.config.rubygems['versions_file_location']
-    versions_file = CompactIndex::VersionsFile.new(versions_file_location)
+    versions_path = Rails.application.config.rubygems['versions_file_location']
+    versions_file = CompactIndex::VersionsFile.new(versions_path)
     from_date = versions_file.updated_at
-    extra_gems = Rubygem.compact_index_versions(from_date)
-    response_body = CompactIndex.versions(versions_file, extra_gems)
-    requested_range_for(response_body)
+    extra_gems = Rubygem.compact_index_versions(versions_file.updated_at)
+    render_range CompactIndex.versions(versions_file, extra_gems)
   end
 
   def info
     return unless stale?(@rubygem)
     info_params = @rubygem.compact_index_info
-    response_body = CompactIndex.info(info_params)
-    requested_range_for(response_body)
+    render_range CompactIndex.info(info_params)
   end
 
   private
 
-  def requested_range_for(response_body)
-    ranges = Rack::Utils.byte_ranges(env, response_body.bytesize)
+  def render_range(response_body)
+    headers['ETag'] = '"' << Digest::MD5.hexdigest(response_body) << '"'
 
+    ranges = Rack::Utils.byte_ranges(env, response_body.bytesize)
     if ranges
       response = ranges.map { |range| response_body.byteslice(range) }.join
       render status: 206, plain: response
@@ -35,4 +33,5 @@ class Api::CompactIndexController < Api::BaseController
       render status: 200, plain: response_body
     end
   end
+
 end
