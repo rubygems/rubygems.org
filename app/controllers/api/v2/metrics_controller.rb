2c698ac5641cabd4d6a12e7a69a674799f0c92df
Ashley Jean <ashh.jean@gmail.com>
2016-08-14 16:33:47 -0400

    Created API endpoint for Metrics

---
+++ app/controllers/api/v2/metrics_controller.rb
@@ -0,0 +1,30 @@
+class Api::V2::MetricsController < Api::BaseController
+  METRICS = %I[
+    bundler_version
+    ruby_version
+    ruby_platform
+  ]
+  def create
+    METRICS.each do |metric|
+      Metriks.counter("#{metric}/params[:metric]").increment
+    end
+  end
+end
+
+
+
+
+
+=begin
+
+bundler version
+- rubygems version
+- ruby version
+- ruby platform/host
+- bundler command being run
+- ruby engine and engine version (e.g. JRuby, Rubinius)
+- Bundler settings that have been set (keys only, not values)
+- CI information (is bundler running on a CI platform)
+
+
+=end
