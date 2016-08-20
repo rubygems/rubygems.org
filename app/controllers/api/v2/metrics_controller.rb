1351f17ac5170fb758210cd414272186df124574
Ashley Jean <ashh.jean@gmail.com>
2016-08-20 12:53:15 -0400

    Added more items for tracking

--- app/controllers/api/v2/metrics_controller.rb
+++ app/controllers/api/v2/metrics_controller.rb
@@ -15,21 +15,3 @@ class Api::V2::MetricsController < Api::BaseController
     end
   end
 end
-
-
-
-
-
-=begin
-
-bundler version
-- rubygems version
-- ruby version
-- ruby platform/host
-- bundler command being run
-- ruby engine and engine version (e.g. JRuby, Rubinius)
-- Bundler settings that have been set (keys only, not values)
-- CI information (is bundler running on a CI platform)
-
-
-=end
