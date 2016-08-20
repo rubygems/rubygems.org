27c37a9eee510868f08e5597e6db0ff783aabbdd
Ashley Jean <ashh.jean@gmail.com>
2016-08-20 12:46:06 -0400

    Added more items for tracking

--- app/controllers/api/v2/metrics_controller.rb
+++ app/controllers/api/v2/metrics_controller.rb
@@ -1,8 +1,13 @@
 class Api::V2::MetricsController < Api::BaseController
   METRICS = %I[
     bundler_version
+    rubygems_version
     ruby_version
     ruby_platform
+    bundler_command
+    ruby_engine
+    bundler_settings
+    ci_information
   ]
   def create
     METRICS.each do |metric|
