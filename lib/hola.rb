6feb90300ded3f88029f9edf53faed62e81df669
Thanatas Pongpanotkorn <xaxiclouddev@gmail.com>
2016-09-14 01:19:14 +0700

    Create hola.rb

---
+++ lib/hola.rb
@@ -0,0 +1,14 @@
+% cat hola.gemspec
+Gem::Specification.new do |s|
+  s.name        = 'hola'
+  s.version     = '0.0.0'
+  s.date        = '2010-04-28'
+  s.summary     = "Hola!"
+  s.description = "A simple hello world gem"
+  s.authors     = ["Nick Quaranto"]
+  s.email       = 'nick@quaran.to'
+  s.files       = ["lib/hola.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/hola'
+  s.license       = 'MIT'
+end
