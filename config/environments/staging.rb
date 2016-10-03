21502ffe6d5f870a7351d22dfb36add1fb71e8be
David Radcliffe <radcliffe.david@gmail.com>
2016-10-02 22:31:38 -0400

    use sendgrid to send emails

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_url_options = { host: Gemcutter::HOST,
                                                protocol: Gemcutter::PROTOCOL }
 
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,6 @@
 Clearance.configure do |config|
   config.allow_sign_up = (ENV['DISABLE_SIGNUP'].to_s == 'true') ? false : true
-  config.mailer_sender = "help@rubygems.org"
+  config.mailer_sender = "RubyGems.org <no-reply@mailer.rubygems.org>"
   config.secure_cookie = true unless Rails.env.test? || Rails.env.development?
   config.password_strategy = Clearance::PasswordStrategies::BCryptMigrationFromSHA1
 end
