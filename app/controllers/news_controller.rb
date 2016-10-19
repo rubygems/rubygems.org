2fc69bd6ce68fe269598f99e2d3d82305ff1fc5f
Olivier Lacan <hi@olivierlacan.com>
2016-10-19 13:22:19 +0200

    Base queries on versions instead of gems
    The popular action doesn’t work because of the issues discussed in
    66765d45a4472fb458bf9fa47a83f092941a5016#commitcomment-19480753
    
    I’d be curious if anyone has a way around that would allow me to
    sort versions by downloads for the popular action.
    
    I could simply query gems first to get the 100 most downloaded and then
    fetch recent versions for those.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,16 +2,20 @@ class NewsController < ApplicationController
   before_action :set_page, only: :index
 
   def show
-    @releases = Rubygem.joins(:latest_version)
-      .merge(Version.indexed.by_created_at.recent)
+    @versions = Version.joins(:rubygem)
+      .recent
+      .indexed
+      .by_created_at
       .paginate(page: @page, per_page: 10, total_entries: 100)
   end
 
   def popular
     @title = "New Releases — Popular Gems"
-    @releases = Rubygem.joins(:latest_version)
-      .by_downloads
-      .merge(Version.indexed.by_created_at.recent)
+    @versions = Version.joins(:rubygem)
+      .recent
+      .indexed
+      .by_created_at
+      .merge(Rubygem.by_downloads)
       .paginate(page: @page, per_page: 10, total_entries: 100)
 
     render :show
