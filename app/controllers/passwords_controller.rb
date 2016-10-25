4f3c9f66c987cfcc042f676b9a5e3995c0c6c5be
Aditya Prakash <aditya.prakash132@gmail.com>
2016-10-25 16:40:23 +0530

    Override find_user_for_create method of Clearance::PasswordsController
    This fixes:
    NoMethodError: undefined method `[]' for nil:NilClass
    
    origin:
    https://github.com/thoughtbot/clearance/blob/75e33bb0d0cd42aa97345d3496fd9e3e3ab73a05/app/controllers/clearance/passwords_controller.rb#L81

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,2 +1,12 @@
 class PasswordsController < Clearance::PasswordsController
+  def find_user_for_create
+    Clearance.configuration.user_model
+      .find_by_normalized_email password_params[:email]
+  end
+
+  private
+
+  def password_params
+    params.require(:password).permit(:email)
+  end
 end
