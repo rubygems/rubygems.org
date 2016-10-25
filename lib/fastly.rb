d1f3446f220a79a27dbe01449e9ad1a69c1381e0
David Radcliffe <radcliffe.david@gmail.com>
2016-10-24 22:54:30 -0400

    purge multiple domains

--- lib/fastly.rb
+++ lib/fastly.rb
@@ -8,17 +8,18 @@ end
 
 class Fastly
   def self.purge(path, soft = false)
-    return unless ENV['FASTLY_DOMAIN']
-    domain = "https://#{ENV['FASTLY_DOMAIN']}/"
-    headers = soft ? { 'Fastly-Soft-Purge' => 1 } : {}
+    return unless ENV['FASTLY_DOMAINS']
+    ENV['FASTLY_DOMAINS'].split(",").each do |domain|
+      url = "https://#{domain}/#{path}"
+      headers = soft ? { 'Fastly-Soft-Purge' => 1 } : {}
 
-    response = RestClient::Request.execute(method: :purge,
-                                           url: domain + path,
-                                           timeout: 10,
-                                           headers: headers)
-    json = JSON.parse(response)
-    Rails.logger.debug "Fastly purge url=#{domain + path} status=#{json['status']} id=#{json['id']}"
-    json
+      response = RestClient::Request.execute(method: :purge,
+                                             url: url,
+                                             timeout: 10,
+                                             headers: headers)
+      json = JSON.parse(response)
+      Rails.logger.debug "Fastly purge url=#{url} status=#{json['status']} id=#{json['id']}"
+    end
   end
 
   def self.purge_key(key, soft = false)
