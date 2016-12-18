2cd4e199a428a008107e533e23fd968998aaacdf
Laurent Arnoud <laurent@spkdev.net>
2016-12-18 18:24:39 +0100

    Remove duplicate @latest_version load

--- app/controllers/reverse_dependencies_controller.rb
+++ app/controllers/reverse_dependencies_controller.rb
@@ -5,7 +5,6 @@ class ReverseDependenciesController < ApplicationController
   before_action :set_page, only: [:index]
 
   def index
-    @latest_version = @rubygem.versions.most_recent
     @reverse_dependencies = @rubygem.reverse_dependencies.by_downloads
     if params[:rdeps_query] && params[:rdeps_query].is_a?(String)
       @reverse_dependencies = @reverse_dependencies.search(params[:rdeps_query])
