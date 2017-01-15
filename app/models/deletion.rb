e705c21a73a951405b30bd22f9793c1adf5a79f8
Aditya Prakash <aditya.prakash132@gmail.com>
2017-01-15 16:10:35 +0530

    Update yanked_info_checksum before deleting info cache
    Deleting info cache before updating yanked_info_checksum is not safe.
    Cache will be expired with `expire_cache` in `after_commit`.

--- app/models/deletion.rb
+++ app/models/deletion.rb
@@ -50,10 +50,8 @@ class Deletion < ActiveRecord::Base
   end
 
   def set_yanked_info_checksum
-    # expire info cache of last version
-    Rails.cache.delete("info/#{rubygem}")
-    gem_info = GemInfo.new(version.rubygem.name)
-    checksum = Digest::MD5.hexdigest(CompactIndex.info(gem_info.compact_index_info))
+    compact_index_info = GemInfo.new(version.rubygem.name).compute_compact_index_info
+    checksum = Digest::MD5.hexdigest(CompactIndex.info(compact_index_info))
     version.update_attribute :yanked_info_checksum, checksum
   end
 end
