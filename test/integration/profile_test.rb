dwradcliffe/rubygems.org-1

Add dependent destroy to user associations and tests

test/integration/profile_test.rb
@@ -63,4 +63,16 @@ class ProfileTest < SystemTest
     visit profile_path("nick1")
     refute page.has_content?("Email Me")
   end
+
+  test "deleting profile" do
+    sign_in
+    visit profile_path("nick1")
+    click_link "Edit Profile"
+
+    click_button "Delete"
+    fill_in "Password", with: "secret123"
+    click_button "Confirm"
+
+    assert page.has_content? "Your account has been successfully deleted."
+  end
 end

rubygems/rubygems.org

test/integration/profile_test.rb
@@ -102,4 +102,16 @@ class ProfileTest < SystemTest
 
     assert page.has_link?("@nick1", href: "https://twitter.com/nick1")
   end
+
+  test "deleting profile" do
+    sign_in
+    visit profile_path("nick1")
+    click_link "Edit Profile"
+
+    click_button "Delete"
+    fill_in "Password", with: "password12345"
+    click_button "Confirm"
+
+    assert page.has_content? "Your account has been successfully deleted."
+  end
 end