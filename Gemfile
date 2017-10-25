2a2271f00e6e74f3edc07cfb8da272e2fa440f4f
Colby Swandale <colby@taplaboratories.com>
2017-10-25 22:59:03 +1100

    use bootsnap in development to speed up rails load times

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ group :development, :test do
 end
 
 group :development do
+  gem 'bootsnap', require: false
   gem 'rails-erd'
 end
 
--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,8 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+env = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || ENV['ENV']
+dev_mode = ['', nil, 'development'].include? env
+
+require 'bootsnap/setup' if dev_mode && !ENV['NO_BOOTSNAP']
