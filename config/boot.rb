137bbd99b533c76a99c19dc5507599db71b096cc
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2018-08-24 13:26:59 +0000

    merge back config/boot.rb changes

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,8 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+env = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || ENV['ENV']
+dev_mode = ['', nil, 'development'].include? env
+
+require 'bootsnap/setup' if dev_mode && !ENV['NO_BOOTSNAP']
