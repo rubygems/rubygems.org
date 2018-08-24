de7aa09a9e705efd41b76100cc331b15dd7254a5
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2018-08-24 13:55:40 +0000

    comment out load_defaults for now

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 
 module Gemcutter
   class Application < Rails::Application
-    config.load_defaults 5.0
+    #config.load_defaults 5.0
 
     config.rubygems = Application.config_for :rubygems
 
