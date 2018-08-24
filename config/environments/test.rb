abd1aaf2942782bd443449eb163e1fc847626b3e
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2018-08-24 13:47:42 +0000

    merge test environment changes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,10 +37,21 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: Gemcutter::HOST,
+                                               protocol: Gemcutter::PROTOCOL }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  require 'clearance_backdoor'
+  config.middleware.use ClearanceBackdoor
+
+  config.active_support.test_order = :random
+
+  config.cache_store = :dalli_store
+
+  config.active_job.queue_adapter = :test
 end
