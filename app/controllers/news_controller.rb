c3b5bd82067ac2a8f49318fc475343b34a6fe1ca
Aditya Prakash <aditya.prakash132@gmail.com>
2018-10-28 23:00:59 +0530

    Limit news and popular gem's total entries
    Kaminari doesn't have option to set max_pages or max_entries per request

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,14 +1,27 @@
 class NewsController < ApplicationController
   before_action :set_page
+  before_action -> { limit_page Gemcutter::NEWS_MAX_PAGES }
 
   def show
-    @rubygems = Rubygem.news(7.days).page(@page).per(10).limit(100)
+    @rubygems = Rubygem.news(Gemcutter::NEWS_DAYS_LIMIT).page(@page).per(Gemcutter::NEWS_PER_PAGE)
+    limit_total_count
   end
 
   def popular
     @title = t(".title")
-    @rubygems = Rubygem.by_downloads.news(70.days).page(@page).per(10).limit(100)
+    @rubygems = Rubygem.by_downloads.news(Gemcutter::POPULAR_DAYS_LIMIT).page(@page).per(Gemcutter::NEWS_PER_PAGE)
+    limit_total_count
 
     render :show
+  end
+
+  private
+
+  def limit_total_count
+    class << @rubygems
+      def total_count
+        Gemcutter::NEWS_MAX_PAGES * Gemcutter::NEWS_PER_PAGE
+      end
+    end
   end
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,12 +1,11 @@
 class SearchesController < ApplicationController
-  before_action :set_page, :limit_page, only: :show
-  # Limit max page as ES result window is upper bounded by 10_000 records
-  MAX_PAGE = 100
+  before_action :set_page, only: :show
+  before_action -> { limit_page Gemcutter::SEARCH_MAX_PAGES }, only: :show
 
   def show
     return unless params[:query]&.is_a?(String)
     @error_msg, @gems = Rubygem.search(params[:query], elasticsearch: es_enabled?, page: @page)
-    limit_total_count if @gems.total_count > MAX_PAGE * Rubygem.default_per_page
+    limit_total_count if @gems.total_count > Gemcutter::SEARCH_MAX_PAGES * Rubygem.default_per_page
 
     @exact_match = Rubygem.name_is(params[:query]).with_versions.first
     redirect_to rubygem_path(@exact_match) if @exact_match && @gems.size == 1
@@ -17,14 +16,10 @@ class SearchesController < ApplicationController
 
   private
 
-  def limit_page
-    render_404 if @page > MAX_PAGE
-  end
-
   def limit_total_count
     class << @gems
       def total_count
-        MAX_PAGE * Rubygem.default_per_page
+        Gemcutter::SEARCH_MAX_PAGES * Rubygem.default_per_page
       end
     end
   end
