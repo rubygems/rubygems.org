9183bdab5c601bbb085dc44aa2ee687ceffe71b2
Kadu Ribeiro <mail@carlosribeiro.me>
2018-11-21 15:27:45 -0200

    Extract dynamic_form's error_messages_for to avoid a wrong i18n scope
    Closes #1834
    
    The problem occurs because we use `form.error_messages` in the form and
    dynamic_form adds `activerecord.errors.template.{header/body}` i18n key
    when it has error. The problem is that `rails-i18n` gem doesn't have
    this key. It has `errors.template.{header.body}` instead. To fix this,
    we did extract the method to our helper and removed this wrong scope.
    
    We also have removed the dynamic_form dependency with this change.

---
+++ app/helpers/dynamic_errors_helper.rb
@@ -0,0 +1,30 @@
+module DynamicErrorsHelper
+  # This code was extracted from https://github.com/joelmoss/dynamic_form/blob/master/lib/action_view/helpers/dynamic_form.rb#L186
+  # with a modification to fix https://github.com/rubygems/rubygems.org/issues/1834 issue. We also removed the dynamic_form
+  # dependency with this.
+  def error_messages_for(object)
+    count = object.errors.count
+    return '' if count.zero?
+
+    object_name = object.class.model_name.human.downcase
+    html = {}
+    %i[id class].each do |key|
+      html[key] = 'errorExplanation'
+    end
+
+    I18n.with_options scope: %i[errors template] do |locale|
+      header_message = locale.t(:header, count: count, model: object_name.to_s.tr('_', ' '))
+      message = locale.t(:body)
+      # binding.pry
+      error_messages = safe_join(object.errors.full_messages.map do |msg|
+        content_tag(:li, msg)
+      end)
+      contents = []
+      contents << content_tag(:h2, header_message) if header_message.present?
+      contents << content_tag(:p, message) if message.present?
+      contents << content_tag(:ul, error_messages)
+
+      content_tag(:div, safe_join(contents), html)
+    end
+  end
+end
