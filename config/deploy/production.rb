ba350e4e5fc875a7059b94980eabad055bde211c
David Radcliffe <radcliffe.david@gmail.com>
2018-12-31 08:24:22 -0500

    deploy production jobs workers to k8s

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,4 @@ server 'app02.production.rubygems.org', user: 'deploy', roles: %w[app db]
 server 'app03.production.rubygems.org', user: 'deploy', roles: %w[app]
 server 'app04.production.rubygems.org', user: 'deploy', roles: %w[app]
 server 'jobs01.production.rubygems.org', user: 'deploy', roles: %w[jobs]
-server 'jobs02.production.rubygems.org', user: 'deploy', roles: %w[jobs]
 server 'lb03.production.rubygems.org', user: 'deploy', roles: %w[lb], no_release: true
