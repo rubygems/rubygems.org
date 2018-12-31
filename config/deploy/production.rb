ea76f31f16fd8c5fff62b323de453eb143e19c2b
David Radcliffe <radcliffe.david@gmail.com>
2018-12-31 09:29:37 -0500

    deploy production shoryuken workers to k8s

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,4 @@ server 'app01.production.rubygems.org', user: 'deploy', roles: %w[app], primary:
 server 'app02.production.rubygems.org', user: 'deploy', roles: %w[app db]
 server 'app03.production.rubygems.org', user: 'deploy', roles: %w[app]
 server 'app04.production.rubygems.org', user: 'deploy', roles: %w[app]
-server 'jobs01.production.rubygems.org', user: 'deploy', roles: %w[jobs]
 server 'lb03.production.rubygems.org', user: 'deploy', roles: %w[lb], no_release: true
