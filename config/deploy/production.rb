2dc5d2058a4b62666afb64e8199990c7b3e12e1d
David Radcliffe <radcliffe.david@gmail.com>
2019-01-01 09:25:07 -0500

    remove old app servers from deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,3 @@
 server 'app01.production.rubygems.org', user: 'deploy', roles: %w[app], primary: true
 server 'app02.production.rubygems.org', user: 'deploy', roles: %w[app db]
-server 'app03.production.rubygems.org', user: 'deploy', roles: %w[app]
-server 'app04.production.rubygems.org', user: 'deploy', roles: %w[app]
 server 'lb03.production.rubygems.org', user: 'deploy', roles: %w[lb], no_release: true
