9d5e7a2c9252d09006b20c2a355d53ae304e206d
Aditya Prakash <aditya.prakash132@gmail.com>
2019-03-12 23:26:37 +0530

    Fix ArgumentError: string contains null byte for nested params
    Fixes: https://app.honeybadger.io/projects/40972/faults/38619383

--- test/integration/null_char_param_test.rb
+++ test/integration/null_char_param_test.rb
@@ -5,4 +5,9 @@ class NullCharParamTest < ActionDispatch::IntegrationTest
     get "/search?utf8=%E2%9C%93&query=php://input%00.&search_submit=%E2%8C%95"
     assert_response :bad_request
   end
+
+  test "nested params with null character respond with bad request" do
+    get "/search?utf8=%E2%9C%93&query[some]=php://input%00.&search_submit=%E2%8C%95"
+    assert_response :bad_request
+  end
 end
