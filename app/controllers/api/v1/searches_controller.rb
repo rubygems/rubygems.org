c99580a60b31a904dcc64f0afa540c46c06d23d4
Aditya Prakash <aditya.prakash132@gmail.com>
2019-03-13 00:00:02 +0530

    Fix NoMethodError: undefined method 'strip' for [""]:Array
    Fixes: https://app.honeybadger.io/projects/40972/faults/46242277

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::SearchesController < Api::BaseController
   skip_before_action :verify_authenticity_token
   before_action :set_page, only: :show
+  before_action :verify_query_string, only: :show
 
   def show
     @rubygems = ElasticSearcher.new(query_params, api: true, page: @page).search
@@ -11,6 +12,10 @@ class Api::V1::SearchesController < Api::BaseController
   end
 
   private
+
+  def verify_query_string
+    render plain: "bad request", status: :bad_request unless query_params.is_a?(String)
+  end
 
   def query_params
     params.require(:query)
---
+++ test/integration/api/v1/search_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class Api::V1::Search < ActionDispatch::IntegrationTest
+  test "request with non-string query shows bad request" do
+    get "/api/v1/search.json?query[]="
+    assert_response :bad_request
+  end
+end
