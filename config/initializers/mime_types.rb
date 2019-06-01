c244d5b1f24f04c93226e863b791e868613491a8
Aditya Prakash <aditya.prakash132@gmail.com>
2019-06-01 18:35:28 +0530

    Revert changes made to assets.rb and mime_types.rb

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,6 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+# Default Content-Type for api/v1/dependencies
+Mime::Type.register "application/octet-stream", :marshal
