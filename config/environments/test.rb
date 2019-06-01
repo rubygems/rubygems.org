8cb5d0b4abac2329b6976b9d44f7971dba32a9f1
Aditya Prakash <aditya.prakash132@gmail.com>
2019-06-01 18:43:23 +0530

    Revert changes to test.rb
    Kept small fixes in comments.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,7 +5,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
@@ -22,7 +22,6 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.cache_store = :null_store
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
@@ -36,10 +35,21 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: Gemcutter::HOST,
+                                               protocol: Gemcutter::PROTOCOL }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  require 'clearance_backdoor'
+  config.middleware.use ClearanceBackdoor
+
+  config.active_support.test_order = :random
+
+  config.cache_store = :mem_cache_store
+
+  config.active_job.queue_adapter = :test
 end
