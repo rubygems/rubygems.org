43c22555222a3a8c242f7ff78fdc1bcdaa50c6c6
Aditya Prakash <aditya.prakash132@gmail.com>
2019-06-24 11:45:41 +0530

    Add custom cop to complain on use of try
    links.rb is excluded. linkset&.send(short) fails as:
    NoMethodError: undefined method 'changelog' for `#<Linkset:0x00007fa32202a680>`

---
+++ lib/safe_navigation_cop.rb
@@ -0,0 +1,18 @@
+module RuboCop::Cop
+  class Style::CustomSafeNavigationCop < Cop
+    MSG = "Use ruby safe navigation opetator (&.) instead of try".freeze
+
+    def_node_matcher :try_call?, <<-PATTERN
+      (send (...) :try (...))
+    PATTERN
+
+    def_node_matcher :try_bang_call?, <<-PATTERN
+      (send (...) :try! (...))
+    PATTERN
+
+    def on_send(node)
+      return unless try_call?(node) || try_bang_call?(node)
+      add_offense(node)
+    end
+  end
+end
