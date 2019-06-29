2d3db49faea81dca7ad7bbad17533f522bde3aa5
Shunji Nishitani <shun_nishitani@me.com>
2019-06-29 16:02:59 +0900

    implement ajax in home page

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,7 +1,23 @@
+/*
 $(function(){
   $('#home_query').autocomplete({
     source:  function(request, response) {
     $.getJSON("/search/autocomplete", { query: request.term }, response);
     }
   });
+});
+*/
+
+
+$(document).on('keyup', '#home_query', function(e){
+  e.preventDefault();
+  var input = $.trim($(this).val());
+  $.ajax({
+    url: '/search/autocomplete',
+    type: 'GET',
+    data: ('query=' + input),
+    processData: false,
+    contentType: false,
+    dataType: 'json'
+  })
 });
