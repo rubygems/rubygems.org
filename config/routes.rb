dd9ab75be7528da903f6e516255cbe721cec2590
Shunji Nishitani <shun_nishitani@me.com>
2019-06-29 14:21:13 +0900

    change routing format of autocomplete from json to html

--- config/routes.rb
+++ config/routes.rb
@@ -126,10 +126,9 @@ Rails.application.routes.draw do
 
   ################################################################################
   # UI
-  get 'search/autocomplete', to: 'searches#autocomplete', defaults: { format: 'json' }
   scope constraints: { format: :html }, defaults: { format: 'html' } do
     resource :search, only: :show do
-      get :advanced
+      get :advanced, :autocomplete
     end
     resource :dashboard, only: :show, constraints: { format: /html|atom/ }
     resources :profiles, only: :show
