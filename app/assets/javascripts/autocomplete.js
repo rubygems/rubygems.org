dffc8e2888fee357cfa275ecac9935a017f7e40b
Shunji Nishitani <shun_nishitani@me.com>
2019-07-01 00:51:13 +0900

    ajax doesn't run if input is less than 3 characters

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -12,12 +12,18 @@ $(function(){
 $(document).on('keyup', '#home_query', function(e){
   e.preventDefault();
   var input = $.trim($(this).val());
-  $.ajax({
-    url: '/search/autocomplete',
-    type: 'GET',
-    data: ('query=' + input),
-    processData: false,
-    contentType: false,
-    dataType: 'json'
-  })
+  if (input.length >=2){
+    $.ajax({
+      url: '/search/autocomplete',
+      type: 'GET',
+      data: ('query=' + input),
+      processData: false,
+      contentType: false,
+      dataType: 'json'
+    }).done({
+      
+    })
+  }else{
+    data = nil;
+  }
 });
