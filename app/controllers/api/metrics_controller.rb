fffb26696fe1d89a19a21b0222e8e0525cd7ab9f
Or <orshadad@gmail.com>
2019-07-18 00:23:30 +0300

    instrument all low cardinality metrics

--- app/controllers/api/metrics_controller.rb
+++ app/controllers/api/metrics_controller.rb
@@ -1,5 +1,33 @@
 class Api::MetricsController < Api::BaseController
+  METRIC_KEYS = %i[
+    host
+    ruby_version
+    bundler_version
+    rubygems_version
+    ruby_engine
+    command
+    options
+    git_version
+    rvm_version
+    rbenv_version
+    chruby_version
+    ci
+    extra_ua
+  ].freeze
+
   def create
-    render json: params
+    head :ok && return if known_id?(params[:request_id])
+
+    METRIC_KEYS.each do |metric|
+      StatsD.increment(params[metric], tags=[metric]) if params[metric]
+    end
+
+  end
+
+  def known_id?(id)
+    return true if Rails.cache.read(id)
+
+    Rails.cache.write(id, true, expires_in: 120)
+    false
   end
 end
