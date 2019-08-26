fc2d8e63f677e6772257e2020f5f4942157ed9f4
Aditya Prakash <aditya.prakash132@gmail.com>
2019-08-26 22:38:49 +0530

    Revert "Disable email sent for gem push"
    This reverts commit e24f52343d09028a9ffdf2dae58fca05547664d1.

--- app/models/pusher.rb
+++ app/models/pusher.rb
@@ -99,10 +99,9 @@ class Pusher
 
   def after_write
     @version_id = version.id
-    # disabled until we upgrade our sendgrid account
-    # version.rubygem.notifiable_owners.each do |notified_user|
-    #   Mailer.delay.gem_pushed(user.id, @version_id, notified_user.id)
-    # end
+    version.rubygem.notifiable_owners.each do |notified_user|
+      Mailer.delay.gem_pushed(user.id, @version_id, notified_user.id)
+    end
     Delayed::Job.enqueue Indexer.new, priority: PRIORITIES[:push]
     rubygem.delay.index_document
     GemCachePurger.call(rubygem.name)
