4b1cc3e940312f1ef3e06776e2711ceb9e41708e
John McCann <johnfrancismccann@gmail.com>
2020-02-28 19:11:05 -0800

    Catch incorrect number of news items in test

--- test/functional/news_controller_test.rb
+++ test/functional/news_controller_test.rb
@@ -5,6 +5,7 @@ class NewsControllerTest < ActionController::TestCase
     @rubygem1 = create(:rubygem, downloads: 10)
     @rubygem2 = create(:rubygem, downloads: 20)
     @rubygem3 = create(:rubygem, downloads: 30)
+    10.times { |i| create(:version, rubygem: @rubygem2, created_at: 5.days.ago, platform: "platform#{i}") }
     create(:version, rubygem: @rubygem2, created_at: 5.days.ago)
     create(:version, rubygem: @rubygem3, created_at: 6.days.ago)
     create(:version, rubygem: @rubygem1, created_at: 59.days.ago)
@@ -20,10 +21,11 @@ class NewsControllerTest < ActionController::TestCase
     end
 
     should "order by created_at of gem version" do
-      expected_order = [@rubygem2, @rubygem3]
+      expected_order = [@rubygem2, @rubygem3].map(&:name)
+      actual_order = assert_select("h2.gems__gem__name").map(&:text)
 
-      assert_select("a.gems__gem > span > h2").each_with_index do |element, index|
-        assert_match(/#{expected_order[index].name}/, element.text)
+      expected_order.each_with_index do |expected_gem_name, i|
+        assert_match(/#{expected_gem_name}/, actual_order[i])
       end
     end
 
@@ -44,10 +46,11 @@ class NewsControllerTest < ActionController::TestCase
     end
 
     should "order by gem downloads" do
-      expected_order = [@rubygem3, @rubygem2, @rubygem1]
+      expected_order = [@rubygem3, @rubygem2, @rubygem1].map(&:name)
+      actual_order = assert_select("h2.gems__gem__name").map(&:text)
 
-      assert_select("a.gems__gem > span > h2").each_with_index do |element, index|
-        assert_match(/#{expected_order[index].name}/, element.text)
+      expected_order.each_with_index do |expected_gem_name, i|
+        assert_match(/#{expected_gem_name}/, actual_order[i])
       end
     end
 
