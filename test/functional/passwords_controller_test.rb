28539e3ffbf45bafe736e85595d028aa89ab2860
Colby Swandale <me@colby.fyi>
2020-03-15 22:05:26 +1100

    remove test checking if correct param is given in password reset
    Clearance no longer explicitly checks the given set of parameters or
    renders an error in the view.
    
    See: https://github.com/thoughtbot/clearance/commit/b6b1a51711767de6b43963b5b8921bdefbc16907#diff-b25407c495af3eeed79d7f2615f8a764

--- test/functional/passwords_controller_test.rb
+++ test/functional/passwords_controller_test.rb
@@ -2,14 +2,6 @@ require "test_helper"
 
 class PasswordsControllerTest < ActionController::TestCase
   context "on POST to create" do
-    context "when missing a parameter" do
-      should "raises parameter missing" do
-        post :create
-        assert_response :bad_request
-        assert page.has_content?("Request is missing param 'password'")
-      end
-    end
-
     context "with valid params" do
       setup do
         @user = create(:user)
