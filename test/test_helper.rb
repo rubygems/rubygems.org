3fd4663241faf93bb91b96225fa56b2a76e7538d
Colby Swandale <me@colby.fyi>
2020-03-19 21:26:28 +1100

    fix mocha require

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 
 require "rails/test_help"
-require "mocha/mini_test"
+require "mocha/minitest"
 require "capybara/rails"
 require "capybara/minitest"
 require "clearance/test_unit"
