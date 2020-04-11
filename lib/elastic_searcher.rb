9215b1829f98ca2c924777a290bb2e26bff2b8be
Aditya Prakash <aditya.prakash132@gmail.com>
2020-04-11 20:30:40 +0530

    Add unanalyzed field to prefix query and reduce boost of summary
    docs with multiple field match (name, summary and description) had
    better score than docs with prefix name match. To illustrate,
    https://web.archive.org/web/20200406210021/https://rubygems.org/search?utf8=%E2%9C%93&query=jekyll-the
    
    jekyll-assest is second result on above page even tho it has fewer
    downloads because of multiple mentions of `jekyll` in the summary.

--- lib/elastic_searcher.rb
+++ lib/elastic_searcher.rb
@@ -29,8 +29,15 @@ class ElasticSearcher
               should do
                 query_string do
                   query query_str
-                  fields ["name^5", "summary^3", "description"]
+                  fields ["name^5", "summary^2", "description"]
                   default_operator "and"
+                end
+              end
+
+              should do
+                prefix "name.unanalyzed" do
+                  value query_str
+                  boost 7
                 end
               end
 
