71d1ff2aaa5914ef638cd20f365e9ad6e61ad08f
David Radcliffe <radcliffe.david@gmail.com>
2020-04-16 10:03:30 -0400

    fix honeybadger config

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,8 +1,6 @@
-# return true to ignore or false to send
-Honeybadger.exception_filter do |notice|
-  ActionDispatch::ExceptionWrapper.rescue_responses.key? notice[:error_class]
-end
-
 Honeybadger.configure do |config|
   config.report_data = false if Rails.env.development?
+  config.before_notify do |notice|
+    notice.halt! if ActionDispatch::ExceptionWrapper.rescue_responses.key?(notice.error_class)
+  end
 end
