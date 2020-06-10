5e6822826dfecf3c0535b7ea650b5033376ada64
Frank Rietta <frank@rietta.com>
2020-06-10 15:18:29 -0400

    Better explanation for any exact match test

--- test/unit/gem_typo_test.rb
+++ test/unit/gem_typo_test.rb
@@ -12,7 +12,7 @@ class GemTypoTest < ActiveSupport::TestCase
     assert_equal false, gem_typo.protected_typo?
   end
 
-  should "return false for any exact match" do
+  should "return false for any exact match so that wner of the existing delayed_job_active_record Gem can push an update even though there is an existing typo squat delayed-job-active-record that would otherwise block the update" do
     existing_typo = build(:rubygem, name: "delayed-job-active-record")
     existing_typo.save(validate: false)
     existing_typo.versions.create(number: "1.0.1", platform: "ruby")
