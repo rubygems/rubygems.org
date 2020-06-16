5150e14eb9f7fa13c8b179df8873501cd5fb6f72
Davisc0801 <davisc1014@gmail.com>
2020-06-16 09:57:38 -0600

    Update schema version to match current master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_005140) do
+ActiveRecord::Schema.define(version: 2020_05_08_234114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
