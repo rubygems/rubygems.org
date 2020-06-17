5ae50017babba276f7ac784124176929c97616e4
Colby Swandale <me@colby.fyi>
2020-06-17 21:50:53 +1000

    move #find_gem_by_name with webhook scope into WebhooksController

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,12 +1,13 @@
 class Api::BaseController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  private
+  def gem_name
+    params[:gem_name] || params[:rubygem_name]
+  end
 
   def find_rubygem_by_name
-    @gem_name = params[:gem_name] || params[:rubygem_name]
-    @rubygem  = Rubygem.find_by_name(@gem_name)
-    return if @rubygem || @gem_name == WebHook::GLOBAL_PATTERN
+    @rubygem  = Rubygem.find_by name: gem_name
+    return if @rubygem
     render plain: "This gem could not be found", status: :not_found
   end
 
