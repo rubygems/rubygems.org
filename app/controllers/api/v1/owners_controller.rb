d2a6f53c3f145469b8987d2c28064a5a3fe017e5
Espartaco Palma <esparta@gmail.com>
2020-06-18 00:20:21 -0700

    Remove unreachable branch in api/v1/owners/gem endpoing
    The gem method is currently using User.find_by_slug! to find the user from
    the :handle params. #find_by_slug will trigger a AR::NotFound exception if
    is not able to find by id nor handle. Since is an exception the now-removed
    branch is not reacheable, ergo not needed.
    
    Added some functional test to cover this case.

--- app/controllers/api/v1/owners_controller.rb
+++ app/controllers/api/v1/owners_controller.rb
@@ -37,14 +37,10 @@ class Api::V1::OwnersController < Api::BaseController
 
   def gems
     user = User.find_by_slug!(params[:handle])
-    if user
-      rubygems = user.rubygems.with_versions
-      respond_to do |format|
-        format.json { render json: rubygems }
-        format.yaml { render yaml: rubygems }
-      end
-    else
-      render plain: "Owner could not be found.", status: :not_found
+    rubygems = user.rubygems.with_versions
+    respond_to do |format|
+      format.json { render json: rubygems }
+      format.yaml { render yaml: rubygems }
     end
   end
 
