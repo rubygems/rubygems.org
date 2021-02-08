a304450b57be530e64e7a49e7e4fb8fd8786113b
Aditya Prakash <aditya.prakash132@gmail.com>
2021-02-08 13:56:54 +0530

    Enable safe-site lax cookie policy
    Report form:
    https://securityheaders.com/?q=https%3A%2F%2Frubygems.org&followRedirects=on

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -23,7 +23,7 @@
 #
 # This change is not backwards compatible with earlier Rails versions.
 # It's best enabled when your entire app is migrated and stable on 6.1.
-# Rails.application.config.action_dispatch.cookies_same_site_protection = :lax
+Rails.application.config.action_dispatch.cookies_same_site_protection = :lax
 
 # Generate CSRF tokens that are encoded in URL-safe Base64.
 #
