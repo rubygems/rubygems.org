29d30b0777c83aaf01a3441d94d6c4ca63b08ba3
jsrn <jsrn@hey.com>
2021-03-12 22:01:33 +0000

    Set lang attr from current locale

---
+++ test/integration/locale_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class LocaleTest < SystemTest
+  test "html lang attribute is set from locale" do
+    I18n.available_locales.each do |locale|
+      visit root_path(locale: locale)
+      assert_equal locale.to_s, page.find("html")[:lang]
+    end
+  end
+
+  test "locale is switched via locale menu" do
+    visit root_path
+    assert_equal I18n.default_locale.to_s, page.find("html")[:lang]
+
+    click_link "Deutsch"
+    assert_equal "de", page.find("html")[:lang]
+  end
+
+  teardown { reset_session! }
+end
