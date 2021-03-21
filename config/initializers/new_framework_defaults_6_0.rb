bed24ab3af9cca9d8606e2187fe9ad0b63deeea2
Aditya Prakash <aditya.prakash132@gmail.com>
2021-03-21 16:18:03 +0530

    Enable use_cookies_with_metadata, rails 6.0 default

--- config/initializers/new_framework_defaults_6_0.rb
+++ config/initializers/new_framework_defaults_6_0.rb
@@ -14,7 +14,7 @@ Rails.application.config.action_view.default_enforce_utf8 = false
 #
 # This option is not backwards compatible with earlier Rails versions.
 # It's best enabled when your entire app is migrated and stable on 6.0.
-# Rails.application.config.action_dispatch.use_cookies_with_metadata = true
+Rails.application.config.action_dispatch.use_cookies_with_metadata = true
 
 # Change the return value of `ActionDispatch::Response#content_type` to Content-Type header without modification.
 Rails.application.config.action_dispatch.return_only_media_type_on_content_type = false
