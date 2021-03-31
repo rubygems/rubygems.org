dce29c3c799e6ac033e1bc2e2b711a74f180643b
Arthur Neves <arthurnn@gmail.com>
2021-03-30 21:43:57 -0400

    Add github invalidation api
    so secret scanning can talk to it, and invalid tokens that were exposed

---
+++ app/controllers/api/v1/github_secret_scanning_controller.rb
@@ -0,0 +1,55 @@
+class Api::V1::GithubSecretScanningController < Api::BaseController
+  include ApiKeyable
+
+  # API called by GitHub Secret Scanning tool
+  # see docs https://docs.github.com/en/developers/overview/secret-scanning
+  # Sample message:
+  #
+  # POST / HTTP/1.1
+  # Host: HOST
+  # Accept: */*
+  # content-type: application/json
+  # GITHUB-PUBLIC-KEY-IDENTIFIER: 90a421169f0a406205f1563a953312f0be898d3c7b6c06b681aa86a874555f4a
+  # GITHUB-PUBLIC-KEY-SIGNATURE: MEUCICxTWEpKo7BorLKutFZDS6ie+YFg6ecU7kEA6rUUSJqsAiEA9bK0Iy6vk2QpZOOg2IpBhZ3JRVdwXx1zmgmNAR7Izpc=
+  # Content-Length: 0000
+  #
+  # [{"token": "some_token", "type": "some_type", "url": "some_url"}]
+  #
+  def revoke
+    key_id = request.headers.fetch("GITHUB-PUBLIC-KEY-IDENTIFIER", "")
+    signature = request.headers.fetch("GITHUB-PUBLIC-KEY-SIGNATURE", "")
+
+    return render plain: "Missing GitHub Signature", status: :unauthorized if key_id.blank? || signature.blank?
+    key = secret_scanning_key(key_id)
+    return render plain: "Can't fetch public key from GitHub", status: :unauthorized if key.empty_public_key?
+    return render plain: "Invalid GitHub Signature", status: :unauthorized unless key.valid_github_signature?(signature, request.body.read.chomp)
+
+    tokens = params.require(:_json).map { |t| t.permit(:token, :type, :url) }
+    resp = []
+    tokens.each do |t|
+      api_key = ApiKey.find_by(hashed_key: t[:token])
+      label = if api_key&.destroy
+                Mailer.delay.api_key_revoked(api_key, t[:url])
+                "true_positive"
+              else
+                "false_positive"
+              end
+
+      resp << {
+        token_raw: t[:token],
+        token_type: t[:type],
+        label: label
+      }
+    end
+
+    respond_to do |format|
+      format.json { render json: resp }
+    end
+  end
+
+  private
+
+  def secret_scanning_key(key_id)
+    GitHubSecretScanning.new(key_id)
+  end
+end
---
+++ test/integration/api/v1/github_secret_scanning_test.rb
@@ -0,0 +1,135 @@
+require "test_helper"
+
+class Api::V1::GithubSecretScanningTest < ActionDispatch::IntegrationTest
+  HEADER_KEYID = "GITHUB-PUBLIC-KEY-IDENTIFIER".freeze
+  HEADER_SIGNATURE = "GITHUB-PUBLIC-KEY-SIGNATURE".freeze
+
+  KEYS_RESPONSE_BODY =
+    { "public_keys" => [
+      {
+        "key_identifier" => "test_key_id",
+        "is_current" => true
+      }
+    ] }.freeze
+
+  context "on POST to revoke" do
+    setup do
+      key = OpenSSL::PKey::EC.new("secp256k1").generate_key
+      @private_key_pem = key.to_pem
+      pkey = OpenSSL::PKey::EC.new(key.public_key.group)
+      pkey.public_key = key.public_key
+      @public_key_pem = pkey.to_pem
+
+      h = KEYS_RESPONSE_BODY.dup
+      h["public_keys"][0]["key"] = @public_key_pem
+      GitHubSecretScanning.stubs(:get_keys).returns(JSON.dump(h))
+
+      @tokens = [
+        { "token" => "some_token", "type" => "some_type", "url" => "some_url" }
+      ]
+
+      @user = create(:user)
+    end
+
+    context "with no key_id" do
+      setup do
+        post revoke_api_v1_api_key_path(@rubygem),
+          params: {},
+          headers: { HEADER_SIGNATURE => "bar" }
+      end
+
+      should "deny access" do
+        assert_response 401
+        assert_match "Missing GitHub Signature", @response.body
+      end
+    end
+
+    context "with no signature" do
+      setup do
+        post revoke_api_v1_api_key_path(@rubygem),
+          params: {},
+          headers: { HEADER_KEYID => "foo" }
+      end
+
+      should "deny access" do
+        assert_response 401
+        assert_match "Missing GitHub Signature", @response.body
+      end
+    end
+
+    context "with invalid key_id" do
+      setup do
+        post revoke_api_v1_api_key_path(@rubygem),
+          params: {},
+          headers: { HEADER_KEYID => "foo", HEADER_SIGNATURE => "bar" }
+      end
+
+      should "deny access" do
+        assert_response 401
+        assert_match "Can't fetch public key from GitHub", @response.body
+      end
+    end
+
+    context "with invalid signature" do
+      setup do
+        signature = sign_body("Hello world!")
+        post revoke_api_v1_api_key_path(@rubygem),
+          params: {},
+          headers: { HEADER_KEYID => "test_key_id", HEADER_SIGNATURE => Base64.encode64(signature) }
+      end
+
+      should "deny access" do
+        assert_response 401
+        assert_match "Invalid GitHub Signature", @response.body
+      end
+    end
+
+    context "without a valid token" do
+      setup do
+        signature = sign_body(JSON.dump(@tokens))
+        post revoke_api_v1_api_key_path(@rubygem),
+          params: @tokens,
+          headers: { HEADER_KEYID => "test_key_id", HEADER_SIGNATURE => Base64.encode64(signature) },
+          as: :json
+      end
+
+      should "returns success" do
+        assert_response :success
+        json = JSON.parse(@response.body)[0]
+        assert_equal "false_positive", json["label"]
+        assert_equal @tokens[0]["type"], json["token_type"]
+        assert_equal @tokens[0]["token"], json["token_raw"]
+      end
+    end
+
+    context "with a valid token" do
+      setup do
+        @api_key = create(:api_key)
+        @tokens << { "token" => @api_key.hashed_key, "type" => "rubygems", "url" => "some_url" }
+        signature = sign_body(JSON.dump(@tokens))
+
+        post revoke_api_v1_api_key_path(@rubygem),
+          params: @tokens,
+          headers: { HEADER_KEYID => "test_key_id", HEADER_SIGNATURE => Base64.encode64(signature) },
+          as: :json
+      end
+
+      should "returns success and remove the token" do
+        assert_response :success
+
+        json = JSON.parse(@response.body)
+        assert_equal "true_positive", json.last["label"]
+        assert_equal @tokens.last["token"], json.last["token_raw"]
+
+        assert_raises(ActiveRecord::RecordNotFound) { @api_key.reload }
+      end
+    end
+  end
+
+  private
+
+  def sign_body(body)
+    private_key = OpenSSL::PKey::EC.new(@private_key_pem)
+    private_key.sign(OpenSSL::Digest.new("SHA256"), body)
+  end
+end
