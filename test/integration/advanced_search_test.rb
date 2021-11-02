866b1ebc01288bd508a2d1dabdbd0aa4d83795ed
Rahul Zhade <rzhade3@users.noreply.github.com>
2021-11-02 06:43:55 +0000

    Add integration tests for advanced search

--- test/integration/advanced_search_test.rb
+++ test/integration/advanced_search_test.rb
@@ -10,12 +10,35 @@ class AdvancedSearchTest < SystemTest
     visit advanced_search_path
   end
 
+  test "searches for a gem while scoping advanced attributes" do
+    rubygem = create(:rubygem, name: "LDAP", number: "1.0.0", downloads: 3)
+    create(:version, summary: "some summary", description: "Hello World", rubygem: rubygem)
+
+    import_and_refresh
+
+    fill_in "Search Gems…", with: "downloads: <5"
+    click_button "advanced_search_submit"
+
+    assert current_path == search_path
+    assert has_content? "LDAP"
+  end
+
   test "enter inside any field will submit form" do
     ["#name", "#summary", "#description", "#downloads", "#updated"].each do |el|
       visit advanced_search_path
       find(el).send_keys(:return)
       assert current_path == search_path
     end
+  end
+
+  test "forms search query out of advanced attributes" do
+    fill_in "name", with: "hello"
+    fill_in "summary", with: "world"
+    fill_in "description", with: "foo"
+    fill_in "downloads", with: ">69"
+    fill_in "updated", with: ">2021-05-05"
+
+    page.find("#home_query").assert_text "name: hello summary: world description: foo downloads: >69 updated: >2021-05-05"
   end
 
   teardown do
