5ab97a8a038fe49d95e212cccb1d24bef6145139
Ashley Ellis Pierce <anellis12@gmail.com>
2022-01-18 12:55:30 -0500

    Rename helper methods

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,25 +1,32 @@
 class Api::V1::ProfilesController < Api::BaseController
-  before_action :set_user, only: [:show]
 
   def show
-    respond_to do |format|
-      format.json { render json: @user }
-      format.yaml { render yaml: @user }
+    if params[:id]
+      show_public_profile
+    else
+      show_private_profile
     end
   end
 
   private
 
-  def set_user
-    @user =
-      if params[:id]
-        User.find_by_slug!(params[:id])
-      else
-        authenticate_or_request_with_http_basic do |username, password|
-          if (user = User.authenticate(username.strip, password))
-            user.private_payload
-          end
-        end
+  def show_public_profile
+    user = User.find_by_slug!(params[:id])
+    respond_with(user)
+  end
+
+  def show_private_profile
+    authenticate_or_request_with_http_basic do |username, password|
+      if (user = User.authenticate(username.strip, password))
+        respond_with(user.private_payload)
       end
+    end
+  end
+
+  def respond_with(response)
+    respond_to do |format|
+      format.json { render json: response }
+      format.yaml { render yaml: response }
+    end
   end
 end
