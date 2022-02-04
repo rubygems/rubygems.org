936a41f2aae9a409c43eaf886f042f3a27ae62b8
Ashley Ellis Pierce <anellis12@gmail.com>
2022-02-04 09:17:43 -1000

    inline authentication

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::ProfilesController < Api::BaseController
-  before_action :authenticate_user, only: [:me]
 
   def show
     @user = User.find_by_slug!(params[:id])
@@ -10,17 +9,14 @@ class Api::V1::ProfilesController < Api::BaseController
   end
 
   def me
-    respond_to do |format|
-      format.json { render json: @user }
-      format.yaml { render yaml: @user }
-    end
-  end
-
-  private
-
-  def authenticate_user
     authenticate_or_request_with_http_basic do |username, password|
-      @user = AuthenticatedUser.authenticate(username.strip, password)
+      user = AuthenticatedUser.authenticate(username.strip, password)
+      if user
+        respond_to do |format|
+          format.json { render json: user }
+          format.yaml { render yaml: user }
+        end
+      end
     end
   end
 end
