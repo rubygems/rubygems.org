a82f711d6fb8d942bb9dd52c23ab244f04ae9250
Ashley Ellis Pierce <anellis12@gmail.com>
2022-02-18 10:07:50 -0500

    Rename AuthenticatedUser to User::WithPrivateFields

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::ProfilesController < Api::BaseController
 
   def me
     authenticate_or_request_with_http_basic do |username, password|
-      if (user = AuthenticatedUser.authenticate(username.strip, password))
+      if (user = User::WithPrivateFields.authenticate(username.strip, password))
         respond_to do |format|
           format.json { render json: user }
           format.yaml { render yaml: user }
--- app/models/user/with_private_fields.rb
+++ app/models/user/with_private_fields.rb
@@ -1,7 +1,7 @@
 # This enables us to return private fields, like mfa_level, to an authenticated user
 # so that they may retrieve this information about their own profile without it
 # being exposed to all users in their public profile
-class AuthenticatedUser < User
+class User::WithPrivateFields < User
   def payload
     super.merge({ "mfa" => mfa_level })
   end
