0e8f1706a656e4d10def1fc6bb62fe1554f7bf5e
Kevin Lin <kevin.lin@shopify.com>
2022-06-15 09:04:53 -0700

    gem_downloads->down

--- db/migrate/20220614221414_ownership_foreign_key.rb
+++ db/migrate/20220614221414_ownership_foreign_key.rb
@@ -3,7 +3,7 @@ class OwnershipForeignKey < ActiveRecord::Migration[7.0]
     add_foreign_key :ownerships, :users, on_delete: :cascade
   end
 
-  def gem_downloads
+  def down
     remove_foreign_key :ownerships, :users
   end
 end
