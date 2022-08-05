7e1fdd38a6e9fa14fe47e06a222220506b045305
bettymakes <makewithbetty@gmail.com>
2022-08-05 18:13:23 -0400

    Add comprehensive test suite for mailer
    Tests are flaky? They seem to fail when they're run together.
    When they're run in isolation, they're fine.

--- test/unit/mailer_test.rb
+++ test/unit/mailer_test.rb
@@ -20,12 +20,14 @@ class MailerTest < ActiveSupport::TestCase
       assert_equal [@user.email], email.to
       assert_equal ["no-reply@mailer.rubygems.org"], email.from
       assert_equal "Please enable multi-factor authentication on your RubyGems account", email.subject
-      assert_match "Thank you for making the RubyGems ecosystem more secure", email.text_part.body.to_s
+      assert_match "Recently, we've announced our security-focused ambitions to the community.", email.text_part.body.to_s
     end
   end
 
   context "sending mail for mfa required soon announcement" do
     should "send mail to users with with more than 180M+ downloads and have MFA disabled" do
+      ActionMailer::Base.deliveries.clear
+      
       @user = create(:user, mfa_level: "disabled")
       create(:rubygem, owners: [@user], downloads: MIN_DOWNLOADS_FOR_MFA_REQUIRED_POLICY)
 
@@ -33,12 +35,63 @@ class MailerTest < ActiveSupport::TestCase
       Rake::Task["mfa_policy:reminder_enable_mfa"].invoke
       Delayed::Worker.new.work_off
 
+      sleep 4
+
       refute_empty ActionMailer::Base.deliveries
       email = ActionMailer::Base.deliveries.last
       assert_equal [@user.email], email.to
       assert_equal ["no-reply@mailer.rubygems.org"], email.from
       assert_equal "[Action Required] Enable multi-factor authentication on your RubyGems account by August 15", email.subject
-      assert_match "Thank you for making the RubyGems ecosystem more secure", email.text_part.body.to_s
+      assert_match "Recently, we've announced our security-focused ambitions to the community.", email.text_part.body.to_s
+    end
+
+    should "send mail to users with with more than 180M+ downloads and have weak MFA" do
+      ActionMailer::Base.deliveries.clear
+     
+      user = create(:user, mfa_level: "ui_only")
+      create(:rubygem, owners: [user], downloads: MIN_DOWNLOADS_FOR_MFA_REQUIRED_POLICY)
+
+      Gemcutter::Application.load_tasks
+      Rake::Task["mfa_policy:reminder_enable_mfa"].invoke
+      Delayed::Worker.new.work_off
+
+      sleep 4
+
+      refute_empty ActionMailer::Base.deliveries
+      email = ActionMailer::Base.deliveries.last
+      assert_equal [user.email], email.to
+      assert_equal ["no-reply@mailer.rubygems.org"], email.from
+      assert_equal "[Action Required] Enable multi-factor authentication on your RubyGems account by August 15", email.subject
+      assert_match "Recently, we've announced our security-focused ambitions to the community.", email.text_part.body.to_s
+    end
+
+    should "not send mail to users with with more than 180M+ downloads and have strong MFA" do
+      ActionMailer::Base.deliveries.clear
+
+      user = create(:user, mfa_level: "ui_and_api")
+      create(:rubygem, owners: [user], downloads: MIN_DOWNLOADS_FOR_MFA_REQUIRED_POLICY)
+
+      Gemcutter::Application.load_tasks
+      Rake::Task["mfa_policy:reminder_enable_mfa"].invoke
+      Delayed::Worker.new.work_off
+
+      sleep 4
+
+      assert_empty ActionMailer::Base.deliveries
+    end
+
+    should "not send mail to users with with less than 180M downloads" do
+      ActionMailer::Base.deliveries.clear
+      user = create(:user)
+      create(:rubygem, owners: [user], downloads: MIN_DOWNLOADS_FOR_MFA_REQUIRED_POLICY - 1)
+
+      Gemcutter::Application.load_tasks
+      Rake::Task["mfa_policy:reminder_enable_mfa"].invoke
+      Delayed::Worker.new.work_off
+
+      sleep 4
+
+      assert_empty ActionMailer::Base.deliveries
     end
   end
 
