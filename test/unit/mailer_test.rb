6428e6a19f6eb7ae263d882581d5d9b582c3ceaf
bettymakes <makewithbetty@gmail.com>
2022-08-07 01:16:24 -0400

    Add remaining reminder tests

--- test/unit/mailer_test.rb
+++ test/unit/mailer_test.rb
@@ -30,7 +30,6 @@ class MailerTest < ActionMailer::TestCase
       create(:rubygem, owners: [user], downloads: MIN_DOWNLOADS_FOR_MFA_REQUIRED_POLICY)
 
       @io_output, _error = capture_io { Rake::Task["mfa_policy:reminder_enable_mfa"].execute }
-
       Delayed::Worker.new.work_off
 
       refute_empty ActionMailer::Base.deliveries
@@ -40,6 +39,44 @@ class MailerTest < ActionMailer::TestCase
       assert_equal "[Action Required] Enable multi-factor authentication on your RubyGems account by August 15", email.subject
       assert_match "Thank you for making the RubyGems ecosystem more secure", email.text_part.body.to_s
       assert_match "Sending 1 MFA reminder email", @io_output
+    end
+
+    should "send mail to users with with more than 180M+ downloads and have weak MFA" do
+      user = create(:user, mfa_level: "ui_only")
+      create(:rubygem, owners: [user], downloads: MIN_DOWNLOADS_FOR_MFA_REQUIRED_POLICY)
+
+      @io_output, _error = capture_io { Rake::Task["mfa_policy:reminder_enable_mfa"].execute }
+      Delayed::Worker.new.work_off
+
+      refute_empty ActionMailer::Base.deliveries
+      email = ActionMailer::Base.deliveries.last
+      assert_equal [user.email], email.to
+      assert_equal ["no-reply@mailer.rubygems.org"], email.from
+      assert_equal "[Action Required] Enable multi-factor authentication on your RubyGems account by August 15", email.subject
+      assert_match "Recently, we've announced our security-focused ambitions to the community", email.text_part.body.to_s
+      assert_match "Sending 1 MFA reminder email", @io_output
+    end
+
+    should "not send mail to users with with more than 180M+ downloads and have strong MFA" do
+      user = create(:user, mfa_level: "ui_and_api")
+      create(:rubygem, owners: [user], downloads: MIN_DOWNLOADS_FOR_MFA_REQUIRED_POLICY)
+
+      @io_output, _error = capture_io { Rake::Task["mfa_policy:reminder_enable_mfa"].execute }
+      Delayed::Worker.new.work_off
+
+      assert_empty ActionMailer::Base.deliveries
+      assert_match "Sending 0 MFA reminder email", @io_output
+    end
+
+    should "not send mail to users with with less than 180M downloads" do
+      user = create(:user)
+      create(:rubygem, owners: [user], downloads: MIN_DOWNLOADS_FOR_MFA_REQUIRED_POLICY - 1)
+
+      @io_output, _error = capture_io { Rake::Task["mfa_policy:reminder_enable_mfa"].execute }
+      Delayed::Worker.new.work_off
+
+      assert_empty ActionMailer::Base.deliveries
+      assert_match "Sending 0 MFA reminder email", @io_output
     end
   end
 
