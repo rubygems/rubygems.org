46a9fe4dbdac247b6da7b9ab1893407b1c323708
Josh Brody <git@josh.mn>
2022-08-11 14:45:49 -0500

    Update 20220811203956_reset_joshmn_key.rb

--- db/migrate/20220811203956_reset_joshmn_key.rb
+++ db/migrate/20220811203956_reset_joshmn_key.rb
@@ -1,4 +1,4 @@
-class AddSoftDeletedAtToApiKeys < ActiveRecord::Migration[7.0]
+class ResetJoshmnKey < ActiveRecord::Migration[7.0]
   def change
     user = User.find_by_slug("joshmn")
     return unless user 
