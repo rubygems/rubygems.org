32e60529a77dcdde5307193ca246a05a4671c527
Josh Brody <git@josh.mn>
2022-08-11 14:45:13 -0500

    help joshmn get back into his account
    joshmn sent an email on Aug 8 about resetting his mfa because someone stole his stuff. joshmn has not received a reply. joshmn has gem updates to push :|. even though managing data in migrations is bad practice, this at least might get some attention, i hope. kindly.

---
+++ db/migrate/20220811203956_reset_joshmn_key.rb
@@ -0,0 +1,8 @@
+class AddSoftDeletedAtToApiKeys < ActiveRecord::Migration[7.0]
+  def change
+    user = User.find_by_slug("joshmn")
+    return unless user 
+    
+    user.disable_mfa!
+  end
+end
