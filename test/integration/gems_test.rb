6a41c490f554912757b66846e67832c0ef265778
Takuya Noguchi <takninnovationresearch@gmail.com>
2022-09-11 12:54:08 +0000

    Use alternate pages to avoid to confuse search engines
    Signed-off-by: Takuya Noguchi <takninnovationresearch@gmail.com>

--- test/integration/gems_test.rb
+++ test/integration/gems_test.rb
@@ -28,11 +28,25 @@ class GemsTest < ActionDispatch::IntegrationTest
     assert page.has_content? "sandworm"
   end
 
-  test "canonical url for gem points to most recent version" do
+  test "canonical/alternate urls for gem points to most recent version" do
+    base_url = "http://localhost/gems/sandworm/versions/1.1.1".freeze
     create(:version, rubygem: @rubygem, number: "1.1.1")
     get rubygem_path(@rubygem)
-    css = %(link[rel="canonical"][href="http://localhost/gems/sandworm/versions/1.1.1"])
+    css = %(link[rel="canonical"][href="#{base_url}"])
+    assert page.has_css?(css, visible: false)
+    css = %(link[rel="alternate"][hreflang])
+    alternates = page.all(:css, css, visible: false)
+    # I18n.available_locales.length + 1 (x-default)
+    assert_equal 10, alternates.length
+    exp = I18n.available_locales.map { "#{base_url}?locale=#{_1}" } << base_url
+    act = alternates.pluck(:href)
+    assert_same_elements exp, act
+  end
 
+  test "canonical locale urls for gem points to most recent version with locale" do
+    create(:version, rubygem: @rubygem, number: "1.1.1")
+    get rubygem_path(@rubygem, locale: "en")
+    css = %(link[rel="canonical"][href="http://localhost/gems/sandworm/versions/1.1.1?locale=en"])
     assert page.has_css?(css, visible: false)
   end
 
