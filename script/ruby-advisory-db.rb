e6af13e9b879cd8c1cf51f497fdd5cf2a2fd2011
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2022-10-19 23:39:23 +0200

    Only count cve if gem is affected

--- script/ruby-advisory-db.rb
+++ script/ruby-advisory-db.rb
@@ -4,9 +4,22 @@ vulnerabilities = Dir['/home/ylecuyer/Projects/ruby-advisory-db/gems/**/*.yml']
   .map { _1.split('/')[-2..] }
   .each_with_object({}) { |elt, hash| hash[elt[0]] ||= []; hash[elt[0]] << elt[1] }
 
-vulnerabilities.each do |k, v|
-  puts k
-  gem = Rubygem.where(name: k).first
+vulnerabilities.each do |gem_name, cves|
+  puts gem_name
+  gem = Rubygem.where(name: gem_name).first
   next unless gem
-  gem.versions.update_all(cve_count: v.count)
+
+  gem.versions.update_all(cve_count: 0)
+
+  cves.each do |cve|
+    patched_versions = YAML.load_file("#{PATH}/gems/#{gem_name}/#{cve}").dig("patched_versions", -1)&.split(',')
+
+    gem.versions.each do |version|
+      version.cve_count += 1 if patched_versions.nil? || !Gem::Requirement.new(patched_versions).satisfied_by?(Gem::Version.new(version.number))
+
+      version.save!(validate: false)
+    end
+  rescue Gem::Requirement::BadRequirementError => e
+    puts "Error #{e.class} #{e.message}"
+  end
 end
