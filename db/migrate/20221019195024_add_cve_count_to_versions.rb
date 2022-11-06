187e09b491c0480ee169eaf2d5121aacf3789cf4
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2022-11-06 15:18:41 +0100

    clean

--- db/migrate/20221019195024_add_cve_count_to_versions.rb
+++
@@ -1,5 +0,0 @@
-class AddCveCountToVersions < ActiveRecord::Migration[7.0]
-  def change
-    add_column :versions, :cve_count, :integer
-  end
-end
--- db/migrate/20221025202151_add_cves_to_version.rb
+++
@@ -1,5 +0,0 @@
-class AddCvesToVersion < ActiveRecord::Migration[7.0]
-  def change
-    add_column :versions, :cves, :string
-  end
-end
