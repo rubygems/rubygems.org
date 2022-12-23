f89fea1205d21800c1e41925faf69f79507279f8
Jacques Chester <jacques.chester@shopify.com>
2022-12-23 13:11:47 -0500

    Reorg logic into single if-elsif-else expression

--- app/controllers/api/v1/webauthn_verifications_controller.rb
+++ app/controllers/api/v1/webauthn_verifications_controller.rb
@@ -2,9 +2,8 @@ class Api::V1::WebauthnVerificationsController < Api::BaseController
   def create
     authenticate_or_request_with_http_basic do |username, password|
       user = User.authenticate(username.strip, password)
-      next false if user.nil?
 
-      if user.webauthn_credentials.present?
+      if user&.webauthn_credentials.present?
         verification = user.refresh_webauthn_verification
         webauthn_path = "example.com/webauthn/#{verification.path_token}"
         respond_to do |format|
@@ -12,8 +11,10 @@ class Api::V1::WebauthnVerificationsController < Api::BaseController
           format.yaml { render yaml: { path: webauthn_path, expiry: verification.path_token_expires_at.utc } }
           format.json { render json: { path: webauthn_path, expiry: verification.path_token_expires_at.utc } }
         end
-      else
+      elsif user # the user exists but hasn't configured any credentials
         render plain: t("settings.edit.no_webauthn_credentials"), status: :unprocessable_entity
+      else # invalid user
+        false
       end
     end
   end
