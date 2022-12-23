a442498b30ec02beccab3f76cdba361610bc3aa1
Jacques Chester <jacques.chester@shopify.com>
2022-12-23 10:12:03 -0500

    Add WebAuthn verification URL endpoint (#3305)
    * Add WebAuthn verification URL endpoint
    
    Hardcoded hostname to example.com, pending followup to use Rails config.
    
    - Add /api/v1/webauthn_verification
    - Add WebauthnVerificationController
    - Add WebauthnUserMethods#refresh_webauthn_verification
    
    Co-authored-by: Ashley Ellis Pierce <anellis12@gmail.com>

---
+++ app/controllers/api/v1/webauthn_verifications_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::WebauthnVerificationsController < Api::BaseController
+  def create
+    authenticate_or_request_with_http_basic do |username, password|
+      user = User.authenticate(username.strip, password)
+
+      if user
+        if user.webauthn_credentials.present?
+          verification = user.refresh_webauthn_verification
+          webauthn_path = "example.com/webauthn/#{verification.path_token}"
+          respond_to do |format|
+            format.any(:all) { render plain: webauthn_path }
+            format.yaml { render yaml: { path: webauthn_path, expiry: verification.path_token_expires_at.utc } }
+            format.json { render json: { path: webauthn_path, expiry: verification.path_token_expires_at.utc } }
+          end
+        else
+          render plain: t("settings.edit.no_webauthn_credentials"), status: :unprocessable_entity
+        end
+      else
+        false
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
         end
       end
       resource :multifactor_auth, only: :show
+      resource :webauthn_verification, only: :create
       resources :profiles, only: :show
       get "profile/me", to: "profiles#me"
       resources :downloads, only: :index do
