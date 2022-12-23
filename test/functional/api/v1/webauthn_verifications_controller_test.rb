eb1a45a3bcfde1d95da7ddcf9969d7e8612ab19b
Jacques Chester <jacques.chester@shopify.com>
2022-12-23 10:12:03 -0500

    Add WebAuthn verification URL endpoint (#3305)
    * Add WebAuthn verification URL endpoint
    
    Hardcoded hostname to example.com, pending followup to use Rails config.
    
    - Add /api/v1/webauthn_verification
    - Add WebauthnVerificationController
    - Add WebauthnUserMethods#refresh_webauthn_verification
    
    Co-authored-by: Ashley Ellis Pierce <anellis12@gmail.com>

---
+++ test/functional/api/v1/webauthn_verifications_controller_test.rb
@@ -0,0 +1,97 @@
+require "test_helper"
+
+class Api::V1::WebauthnVerificationsControllerTest < ActionController::TestCase
+  should "route new paths to new controller" do
+    route = { controller: "api/v1/webauthn_verifications", action: "create" }
+    assert_recognizes(route, { path: "/api/v1/webauthn_verification", method: :post })
+  end
+
+  def authorize_with(str)
+    @request.env["HTTP_AUTHORIZATION"] = "Basic #{Base64.encode64(str)}"
+  end
+
+  def self.should_respond_to_format(format)
+    context "when the request asks for format '#{format}'" do
+      setup do
+        @user = create(:user)
+        create(:webauthn_credential, user: @user)
+        authorize_with("#{@user.email}:#{@user.password}")
+
+        travel_to Time.utc(2023, 1, 1, 0, 0, 0) do
+          post :create, format: format
+        end
+
+        @token = @user.webauthn_verification.path_token
+      end
+
+      should respond_with :success
+
+      should "have a body" do
+        assert_not_nil @response.body
+      end
+
+      if format == :plain
+        should "return only the Webauthn verification URL with path token" do
+          assert_equal @response.body, "example.com/webauthn/#{@token}"
+        end
+      else
+        should "return a YAML or JSON document with path token" do
+          response = YAML.safe_load(@response.body)
+          assert_equal response["path"], "example.com/webauthn/#{@token}"
+        end
+
+        should "return a YAML or JSON document with path expiry" do
+          response = YAML.safe_load(@response.body)
+          assert_equal "2023-01-01T00:02:00.000Z", response["expiry"]
+        end
+      end
+    end
+  end
+
+  context "on POST to create" do
+    context "with no credentials" do
+      setup { post :create }
+      should "deny access" do
+        assert_response 401
+        assert_match "HTTP Basic: Access denied.", @response.body
+      end
+    end
+
+    context "with invalid credentials" do
+      setup do
+        @user = create(:user)
+        create(:webauthn_credential, user: @user)
+        authorize_with("bad\0:creds")
+        post :create
+      end
+
+      should "deny access" do
+        assert_response 401
+        assert_match "HTTP Basic: Access denied.", @response.body
+      end
+    end
+
+    context "user has enabled webauthn" do
+      should_respond_to_format :yaml
+      should_respond_to_format :json
+      should_respond_to_format :plain
+
+      should "not sign in user" do
+        refute_predicate @controller.request.env[:clearance], :signed_in?
+      end
+    end
+
+    context "user has not enabled webauthn" do
+      setup do
+        @user = create(:user)
+        authorize_with("#{@user.email}:#{@user.password}")
+        post :create
+      end
+
+      should respond_with :unprocessable_entity
+      should "tell the user they don't have a WebAuthn hardware token" do
+        assert_match "You don't have any security devices", response.body
+      end
+    end
+  end
+end
