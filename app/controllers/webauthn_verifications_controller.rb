2e4d0c693dd75233312f1dc2f25adc72d978faca
Jenny Shen <jenny.shen@shopify.com>
2023-01-06 11:09:44 -0500

    Set Webauthn authentication challenge in prompt page

--- app/controllers/webauthn_verifications_controller.rb
+++ app/controllers/webauthn_verifications_controller.rb
@@ -2,12 +2,13 @@ class WebauthnVerificationsController < ApplicationController
   before_action :set_user
 
   def prompt
-    redirect_to root_path, alert: t(".no_webauthn_devices") if @user.webauthn_credentials.blank?
+    redirect_to root_path, alert: t("webauthn_verifications.prompt.no_webauthn_devices") if @user.webauthn_credentials.blank?
 
     @webauthn_options = @user.webauthn_options_for_get
 
     session[:webauthn_authentication] = {
-      "challenge" => @webauthn_options.challenge
+      "challenge" => @webauthn_options.challenge,
+      "user" => @user.id
     }
   end
 
