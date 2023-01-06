d6f43b5bc73d8099a20f185e5a5c747fa2671abc
Jenny Shen <jenny.shen@shopify.com>
2023-01-06 11:09:44 -0500

    Set Webauthn authentication challenge in prompt page

--- test/functional/webauthn_verifications_controller_test.rb
+++ test/functional/webauthn_verifications_controller_test.rb
@@ -28,22 +28,44 @@ class WebauthnVerificationsControllerTest < ActionController::TestCase
     context "when given a valid webauthn token param" do
       setup do
         @user = create(:user)
-        create(:webauthn_credential, user: @user)
-        token = create(:webauthn_verification, user: @user).path_token
-        get :prompt, params: { webauthn_token: token }
+        @token = create(:webauthn_verification, user: @user).path_token
       end
 
-      should respond_with :success
-      should "render the verification page" do
-        assert page.has_content?("Authenticate with Security Device")
+      context "with webauthn devices enabled" do
+        setup do
+          create(:webauthn_credential, user: @user)
+          get :prompt, params: { webauthn_token: @token }
+        end
+
+        should respond_with :success
+        should "set webauthn authentication" do
+          assert_equal @user.id, session[:webauthn_authentication]["user"]
+          assert_not_nil session[:webauthn_authentication]["challenge"]
+        end
+
+        should "render the verification page" do
+          assert page.has_content?("Authenticate with Security Device")
+        end
+
+        should "set the user" do
+          assert page.has_content?(@user.name)
+        end
+
+        should "provide the verification button" do
+          assert page.has_button?("Authenticate")
+        end
       end
 
-      should "set the user" do
-        assert page.has_content?(@user.name)
-      end
+      context "with no webauthn devices enabled" do
+        setup do
+          get :prompt, params: { webauthn_token: @token }
+        end
 
-      should "provide the verification button" do
-        assert page.has_button?("Authenticate")
+        should respond_with :redirect
+        should redirect_to("the homepage") { root_url }
+        should "display error that user has no webauthn devices enabled" do
+          assert_equal "You don't have any security devices enabled", flash[:alert]
+        end
       end
     end
   end
