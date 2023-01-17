9551fcc6cd726b86cdcd39b7dfb2910f79a2df1c
Jacques Chester <jacques.chester@shopify.com>
2023-01-17 14:16:05 -0500

    Extract expire_path_token into WebauthnVerification model
    From the WebauthnVerificationsController

--- app/controllers/webauthn_verifications_controller.rb
+++ app/controllers/webauthn_verifications_controller.rb
@@ -24,7 +24,7 @@ class WebauthnVerificationsController < ApplicationController
     user_webauthn_credential.update!(sign_count: webauthn_credential.sign_count)
     # TODO: generate webauthn verification otp
 
-    expire_webauthn_verification
+    @verification.expire_path_token
 
     # TODO: render html with webauthn verification otp instead of json
     render json: { message: "success" }
@@ -47,11 +47,6 @@ class WebauthnVerificationsController < ApplicationController
 
   def set_user
     @user = @verification.user
-  end
-
-  def expire_webauthn_verification
-    @verification.path_token_expires_at = 1.second.ago
-    @verification.save!
   end
 
   def webauthn_credential
--- test/unit/webauthn_verification_test.rb
+++ test/unit/webauthn_verification_test.rb
@@ -9,4 +9,20 @@ class WebauthnVerificationTest < ActiveSupport::TestCase
   should validate_presence_of(:path_token)
   should validate_uniqueness_of(:path_token)
   should validate_presence_of(:path_token_expires_at)
+
+  context "#expire_path_token" do
+    setup do
+      travel_to Time.utc(2023, 1, 1, 0, 0, 0) do
+        user = create(:user)
+        @verification = create(:webauthn_verification, user: user)
+      end
+    end
+
+    should "set the path_token_expires_at to 1 second ago" do
+      travel_to Time.utc(2023, 1, 1, 0, 1, 0) do
+        @verification.expire_path_token
+        assert_equal Time.utc(2023, 1, 1, 0, 0, 59), @verification.path_token_expires_at
+      end
+    end
+  end
 end
