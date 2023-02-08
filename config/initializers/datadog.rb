a43e4936d98b6f1996019c3cbb2610b3a51a77ae
Samuel Giddins <segiddins@segiddins.me>
2023-02-08 15:38:40 -0800

    Add datadog runtime tracing

---
+++ config/initializers/datadog.rb
@@ -0,0 +1,30 @@
+require "app_revision"
+
+Datadog.configure do |c|
+  c.service = "rubygems.org"
+  c.version = AppRevision.version
+  c.env = Rails.env
+
+  c.runtime_metrics.enabled = Rails.env.production? || Rails.env.staging?
+
+  c.tracing.report_hostname = true
+  c.tracing.distributed_tracing.propagation_inject_style << 'tracecontext'
+  c.tracing.distributed_tracing.propagation_extract_style << 'tracecontext'
+
+  if Rails.env.test? || Rails.env.development?
+    c.tracing.transport_options = proc { |t|
+      # Set transport to no-op mode. Does not retain traces.
+      t.adapter :test
+    }
+    c.diagnostics.startup_logs.enabled = false
+  end
+
+  c.tracing.instrument :aws
+  c.tracing.instrument :dalli
+  c.tracing.instrument :delayed_job
+  c.tracing.instrument :faraday, split_by_domain: true
+  c.tracing.instrument :pg
+  c.tracing.instrument :rails
+  c.tracing.instrument :rest_client, split_by_domain: true
+  c.tracing.instrument :shoryuken
+end
