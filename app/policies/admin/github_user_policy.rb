bad26d2347d18cb5903e33e7c0099cb5ef3a8817
Samuel Giddins <segiddins@segiddins.me>
2023-02-08 10:43:19 -0800

    Fix GitHubUserPolicy scope also add tests for the pundit policy

--- app/policies/admin/github_user_policy.rb
+++ app/policies/admin/github_user_policy.rb
@@ -2,7 +2,7 @@ class Admin::GitHubUserPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      if user.belongs_to_team?("rubygems-org")
+      if belongs_to_team?("rubygems-org")
         scope.all
       else
         scope.where(id: user.id)
--- test/policies/admin/github_user_policy_test.rb
+++ test/policies/admin/github_user_policy_test.rb
@@ -1,18 +1,42 @@
 require "test_helper"
 
 class Admin::GitHubUserPolicyTest < ActiveSupport::TestCase
+  setup do
+    @user = FactoryBot.create(:admin_github_user)
+    @admin = FactoryBot.create(:admin_github_user, :is_admin)
+  end
+
   def test_scope
+    assert_equal [@user], Admin::GitHubUserPolicy::Scope.new(
+      @user,
+      Admin::GitHubUser.all
+    ).resolve.to_a
+
+    assert_equal [@user, @admin], Admin::GitHubUserPolicy::Scope.new(
+      @admin,
+      Admin::GitHubUser.all
+    ).resolve.to_a
   end
 
-  def test_show
+  def test_avo_show
+    assert_predicate Admin::GitHubUserPolicy.new(@admin, @user), :avo_show?
+    assert_predicate Admin::GitHubUserPolicy.new(@admin, @admin), :avo_show?
+    refute_predicate Admin::GitHubUserPolicy.new(@user, @user), :avo_show?
+    refute_predicate Admin::GitHubUserPolicy.new(@user, @admin), :avo_show?
   end
 
-  def test_create
+  def test_avo_create
+    refute_predicate Admin::GitHubUserPolicy.new(@user, @user), :avo_create?
+    refute_predicate Admin::GitHubUserPolicy.new(@admin, @admin), :avo_create?
   end
 
-  def test_update
+  def test_avo_update
+    refute_predicate Admin::GitHubUserPolicy.new(@user, @user), :avo_update?
+    refute_predicate Admin::GitHubUserPolicy.new(@admin, @admin), :avo_update?
   end
 
-  def test_destroy
+  def test_avo_destroy
+    refute_predicate Admin::GitHubUserPolicy.new(@user, @user), :avo_destroy?
+    refute_predicate Admin::GitHubUserPolicy.new(@admin, @admin), :avo_destroy?
   end
 end
