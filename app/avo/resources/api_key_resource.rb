1704697af950a8f4f72eec94601d0e6a1e492544
Samuel Giddins <segiddins@segiddins.me>
2023-02-13 11:37:30 -0800

    Add avo resources for ownerships & API keys
    Disallow index, since they only need to be reachable from other resources

---
+++ app/avo/resources/api_key_resource.rb
@@ -0,0 +1,29 @@
+class ApiKeyResource < Avo::BaseResource
+  self.title = :name
+  self.includes = []
+
+  field :id, as: :id, hide_on: :index
+
+  field :name, as: :text, link_to_resource: true
+  field :hashed_key, as: :text, visible: ->(_) { false }
+  field :user, as: :belongs_to
+  field :last_accessed_at, as: :date_time
+  field :soft_deleted_at, as: :date_time
+  field :soft_deleted_rubygem_name, as: :text
+
+  sidebar do
+    heading "Permissions"
+
+    field :index_rubygems, as: :boolean
+    field :push_rubygem, as: :boolean
+    field :yank_rubygem, as: :boolean
+    field :add_owner, as: :boolean
+    field :remove_owner, as: :boolean
+    field :access_webhooks, as: :boolean
+    field :show_dashboard, as: :boolean
+    field :mfa, as: :boolean
+  end
+
+  field :api_key_rubygem_scope, as: :has_one
+  field :ownership, as: :has_one
+end
