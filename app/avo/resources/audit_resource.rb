483e7c3c2feaa980f10e4e6a9e25cda8ccfc71d1
Samuel Giddins <segiddins@segiddins.me>
2023-02-13 21:59:14 -0800

    Make the audited changes the focus of the audit show component

--- app/avo/resources/audit_resource.rb
+++ app/avo/resources/audit_resource.rb
@@ -6,23 +6,31 @@ class AuditResource < Avo::BaseResource
   ]
 
   field :action, as: :text
-  field :auditable, as: :belongs_to,
-    polymorphic_as: :auditable,
-    types: [::User]
-  field :admin_github_user, as: :belongs_to
-  field :created_at, as: :date_time
-  field :comment, as: :text
 
-  field :audited_changes_arguments, as: :json_viewer, only_on: :show do |model|
-    model.audited_changes["arguments"]
-  end
-  field :audited_changes_fields, as: :json_viewer, only_on: :show do |model|
-    model.audited_changes["fields"]
-  end
-  field :audited_changes_models, as: :text, as_html: true, only_on: :show do
-    model.audited_changes["models"]
+  sidebar do
+    field :admin_github_user, as: :belongs_to
+    field :created_at, as: :date_time
+    field :comment, as: :text
+
+    field :auditable, as: :belongs_to,
+      polymorphic_as: :auditable,
+      types: [::User],
+      name: "Edited Record"
+
+    heading "Action Details"
+
+    field :audited_changes_arguments, as: :json_viewer, only_on: :show do |model|
+      model.audited_changes["arguments"]
+    end
+    field :audited_changes_fields, as: :json_viewer, only_on: :show do |model|
+      model.audited_changes["fields"]
+    end
+    field :audited_changes_models, as: :text, as_html: true, only_on: :show do
+      model.audited_changes["models"]
+    end
+
+    field :id, as: :id
   end
 
   field :audited_changes, as: :audited_changes
-  field :id, as: :id
 end
