67e0cacfd3c7e83ab96bddf8f67e9c318aaa9e13
Samuel Giddins <segiddins@segiddins.me>
2023-02-14 17:16:36 -0800

    Add policy tests

--- test/policies/ownership_policy_test.rb
+++ test/policies/ownership_policy_test.rb
@@ -1,18 +1,41 @@
 require "test_helper"
 
 class OwnershipPolicyTest < ActiveSupport::TestCase
+  setup do
+    @ownership = FactoryBot.create(:ownership)
+    @admin = FactoryBot.create(:admin_github_user, :is_admin)
+    @non_admin = FactoryBot.create(:admin_github_user)
+  end
+
   def test_scope
+    assert_equal [@ownership], Pundit.policy_scope!(
+      @admin,
+      Ownership
+    ).to_a
   end
 
-  def test_show
+  def test_avo_index
+    refute_predicate Pundit.policy!(@admin, Ownership), :avo_index?
+    refute_predicate Pundit.policy!(@non_admin, Ownership), :avo_index?
   end
 
-  def test_create
+  def test_avo_show
+    assert_predicate Pundit.policy!(@admin, @ownership), :avo_show?
+    refute_predicate Pundit.policy!(@non_admin, @ownership), :avo_show?
   end
 
-  def test_update
+  def test_avo_create
+    refute_predicate Pundit.policy!(@admin, Ownership), :avo_create?
+    refute_predicate Pundit.policy!(@non_admin, Ownership), :avo_create?
   end
 
-  def test_destroy
+  def test_avo_update
+    refute_predicate Pundit.policy!(@admin, @ownership), :avo_update?
+    refute_predicate Pundit.policy!(@non_admin, @ownership), :avo_update?
+  end
+
+  def test_avo_destroy
+    refute_predicate Pundit.policy!(@admin, @ownership), :avo_destroy?
+    refute_predicate Pundit.policy!(@non_admin, @ownership), :avo_destroy?
   end
 end
