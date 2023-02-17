add9ccb4e857b1feed3966b2c38acbdafe8a8d57
Jenny Shen <jenny.shen@shopify.com>
2023-02-17 10:07:07 -0500

    Update schema to include the correct version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_191823) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_03_041509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
