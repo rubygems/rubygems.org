46c123c9336cc1e33283397658b5545b6c12520f
Mercedes Bernard <mercedesrbernard@gmail.com>
2023-02-23 12:59:56 -0600

    add service class to handle hcaptcha verification

---
+++ lib/hcaptcha_verifier.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+module HcaptchaVerifier
+  class << self
+    VERIFY_ENDPOINT = "https://hcaptcha.com/siteverify"
+    LOGIN_CAPTCHA_THRESHOLD = 4
+
+    def should_verify_login?(user_display_id)
+      key = Rack::Attack::LOGIN_THROTTLE_PER_USER_KEY
+      period = Rack::Attack::LOGIN_LIMIT_PERIOD
+
+      keys = keys(key, period, user_display_id)
+      should_verify(keys, LOGIN_CAPTCHA_THRESHOLD)
+    end
+
+    def call(client_response, remote_ip)
+      return false unless client_response
+
+      payload = {
+        response: client_response,
+        remoteip: remote_ip,
+        secret: ENV.fetch("HCAPTCHA_SECRET", "0x0000000000000000000000000000000000000000"),
+        sitekey: ENV.fetch("HCAPTCHA_SITE_KEY", "10000000-ffff-ffff-ffff-000000000001")
+      }
+
+      response = RestClient.post VERIFY_ENDPOINT,
+        payload,
+        :timeout        => 5,
+        :open_timeout   => 5,
+        "Content-Type"  => "application/x-www-form-urlencoded"
+
+      response_json = JSON.parse(response)
+
+      Rails.logger.error("hCaptcha verification failed: #{response_json['error_codes'].join(';')}") if response_json["error_codes"]
+
+      response_json["success"]
+    end
+
+    private
+
+    def should_verify(keys, limit)
+      keys.any? do |key|
+        attempts = Rack::Attack.cache.store.read(key)
+        attempts && attempts >= limit
+      end
+    end
+
+    def keys(key, period, discriminator)
+      time_counter      = (Time.now.to_i / period).to_i
+      # counter may have incremented by 1 since the key was set, best to reset prev counter as well.
+      # pre time counter/window key is applicable for +1 second after the counter has changed
+      # see: https://github.com/kickstarter/rack-attack/pull/85
+      prev_time_counter = time_counter - 1
+      prefix            = Rack::Attack.cache.prefix
+      ["#{prefix}:#{time_counter}:#{key}:#{discriminator}",
+       "#{prefix}:#{prev_time_counter}:#{key}:#{discriminator}"]
+    end
+  end
+end
---
+++ test/unit/hcaptcha_verifier_test.rb
@@ -0,0 +1,104 @@
+require "test_helper"
+require "helpers/rate_limit_helpers"
+
+class HcaptchaVerifierTest < ActiveSupport::TestCase
+  include RateLimitHelpers
+
+  setup do
+    Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+    Rails.cache.clear
+
+    @ip = "1.2.3.4"
+    @user = create(:user, email: "elmo@example.com", password: PasswordHelpers::SECURE_TEST_PASSWORD,
+                  remember_token_expires_at: Gemcutter::REMEMBER_FOR.from_now)
+  end
+
+  context ".should_verify_login?" do
+    context "when the user has not yet tried to login in the past hour" do
+      should "returns false" do
+        refute HcaptchaVerifier.should_verify_login?(@user.email)
+      end
+    end
+
+    context "when the user has tried to login a couple times in the past hour" do
+      setup do
+        scope = Rack::Attack::LOGIN_THROTTLE_PER_USER_KEY
+        update_limit_for("#{scope}:#{@user.email}", 2, Rack::Attack::LOGIN_LIMIT_PERIOD)
+      end
+
+      should "returns false" do
+        refute HcaptchaVerifier.should_verify_login?(@user.email)
+      end
+    end
+
+    context "when the user has tried to login 4 or more times in the past hour" do
+      setup do
+        scope = Rack::Attack::LOGIN_THROTTLE_PER_USER_KEY
+        update_limit_for("#{scope}:#{@user.email}", 4, Rack::Attack::LOGIN_LIMIT_PERIOD)
+      end
+
+      should "returns true" do
+        assert HcaptchaVerifier.should_verify_login?(@user.email)
+      end
+    end
+  end
+
+  context ".call" do
+    setup do
+      @client_response_token = "10000000-aaaa-bbbb-cccc-000000000001"
+    end
+
+    context "when hcaptcha verifies the captcha response" do
+      should "return true" do
+        RestClient.expects(:post)
+          .with(anything,
+            has_entries(response: @client_response_token, remoteip: @ip),
+            has_entries("Content-Type" => "application/x-www-form-urlencoded"))
+          .returns({ success: true }.to_json)
+
+        assert HcaptchaVerifier.call(@client_response_token, @ip)
+      end
+    end
+
+    context "when hcaptcha cannot verify the captcha response due to bot activity" do
+      should "return false" do
+        RestClient.expects(:post)
+          .with(anything,
+            has_entries(response: @client_response_token, remoteip: @ip),
+            has_entries("Content-Type" => "application/x-www-form-urlencoded"))
+          .returns({ success: false }.to_json)
+        refute HcaptchaVerifier.call(@client_response_token, @ip)
+      end
+
+      should "not log an error" do
+        Rails.logger.stubs(:error).raises("Rails.logger.error was called!")
+        assert_nothing_raised do
+          HcaptchaVerifier.call(@client_response_token, @ip)
+        end
+      end
+    end
+
+    context "when hcaptcha cannot verify the captcha response due to API error" do
+      should "return false" do
+        RestClient.expects(:post)
+          .with(anything,
+            has_entries(response: @client_response_token, remoteip: @ip),
+            has_entries("Content-Type" => "application/x-www-form-urlencoded"))
+          .returns({ success: false, error_codes: ["invalid-or-already-seen-response"] }.to_json)
+
+        refute HcaptchaVerifier.call(@client_response_token, @ip)
+      end
+
+      should "log an error" do
+        RestClient.expects(:post)
+          .with(anything,
+            has_entries(response: @client_response_token, remoteip: @ip),
+            has_entries("Content-Type" => "application/x-www-form-urlencoded"))
+          .returns({ success: false, error_codes: ["invalid-or-already-seen-response"] }.to_json)
+        Rails.logger.expects(:error).with("hCaptcha verification failed: invalid-or-already-seen-response")
+
+        HcaptchaVerifier.call(@client_response_token, @ip)
+      end
+    end
+  end
+end
