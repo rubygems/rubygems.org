aeffdaed2e35fcfcbc0eb02c497b29dec47f02b6
Mercedes Bernard <mercedesrbernard@gmail.com>
2023-03-01 15:19:52 -0600

    creating privacy pass challenge and redeeming token working

---
+++ lib/privacy_pass_redeemer.rb
@@ -0,0 +1,110 @@
+class PrivacyPassRedeemer
+  # TEST_HEADER = "PrivateToken token=AALLcAly6EBWhQyLe7d2jQVMqj_w7v2hckQXu5eKu54mU80z0u0E7GYrFJZIgA9ThFzYXSj_ClklhsUFukcUko1h4Wnyhorwo9CdnHxm9RcdbiX5wzcNo47zc4Al24lDNrehyirOcQyXbw1MIki0dHfjP0w5IshPGULtqYXdY0dQBeu9r_94BTFWThH2RiX6saYBWyj8t-AZS8wI6ZgBVleng8t611TYgacqua_NBpQ4zzWy44fsqNM6j5HhpmZEUWnh0_DwS1i4CsutizdljoVrMkzmHPf-x2PaPVN5gUbFDGf49u0nrcg80HiEeRzNjeek79duhJLHCdVZ-W3pX64zcLhlRiA4HHGdnx4eCC1IqY6o-iXl68nd0lcopz8GReSsKw4tKlmhuj8pk5kTpqZ7qdLdW4x2a-TSfpidzwUv9VYMVFwaVxZXjMpzQlbaKSTvXC5dQ2EAWC-4S7k1GnMWdOJ_YMmnWRXKGfuc_UyJP5UJ8yDcGWLr3HIXS5dDmVIM4_d0ncxFd1GIcSXliHt3ih1zUE5x6f7u5M7D0RrY37eqsCBYM3azOVPqKvPnF5hryzdBlY8vjuBSu__2_XAzBgOfkn1gouXU4ebYeWW5A0t6lEDII6T3hv-YsdmB8oMC-_V0qDyGQjYBM3LeKTYqXD9ndufQXwMaHy6XQzZkhRYpUYHSI7BagNS_rK4ogYLUdo1mqczbQMBedMRAfSlINBF01FTW9aTC0RHpVD7auDEQ4YVBUaAN1dt9K1X59LTFOBvrN8e6Ahi8RU2wWMz-wfq6NRzNg7o391h9jHicTA==".freeze
+  HEADER_FORMAT = /^PrivateToken token=(?<token>.*)$/
+  # See https://www.ietf.org/archive/id/draft-ietf-privacypass-protocol-04.html#name-token-type
+  # See https://www.ietf.org/archive/id/draft-ietf-privacypass-protocol-04.html#section-6.3
+  # struct {
+  #   uint16_t token_type = 0x0002
+  #   uint8_t nonce[32];
+  #   uint8_t challenge_digest[32];
+  #   uint8_t token_key_id[32];
+  #   uint8_t authenticator[Nk];
+  # } Token;
+
+  TOKEN_TYPE_LENGTH = 1
+  NONCE_LENGTH = 32
+  CHALLENGE_DIGEST_LENGTH = 32
+  TOKEN_KEY_ID_LENGTH = 32
+  NK_AUTHENTICATOR_LENGTH = 512
+
+  def self.call(authorization_header, session_id)
+    new(authorization_header, session_id).redeem
+  end
+
+  def initialize(authorization_header, session_id)
+    matches = HEADER_FORMAT.match(authorization_header)
+    raise ArgumentError, "invalid Authorization header format" if matches.nil?
+
+    @token = matches.named_captures["token"]
+    decoded_token = Base64.urlsafe_decode64(@token)
+    @unpacked_token = decoded_token.unpack("nC#{NONCE_LENGTH}C#{CHALLENGE_DIGEST_LENGTH}C#{TOKEN_KEY_ID_LENGTH}C#{NK_AUTHENTICATOR_LENGTH}")
+    @token_type = @unpacked_token.slice(0, TOKEN_TYPE_LENGTH)
+
+    @session_id = session_id
+  end
+
+  def redeem
+    return false unless valid_token_type?
+    return false unless valid_token_length?
+    return false unless confirm_single_redemption?
+
+    public_key.verify(nil, authenticator_string, data_string)
+  end
+
+  private
+
+  attr_reader :token_type, :unpacked_token, :session_id
+
+  def valid_token_type?
+    token_type == [PrivacyPassTokenizer::TOKEN_TYPE]
+  end
+
+  def valid_token_length?
+    expected_token_length = TOKEN_TYPE_LENGTH + NONCE_LENGTH + CHALLENGE_DIGEST_LENGTH + TOKEN_KEY_ID_LENGTH + NK_AUTHENTICATOR_LENGTH
+    unpacked_token.length == expected_token_length
+  end
+
+  def confirm_single_redemption?
+    cache_key = "#{PrivacyPassTokenizer::CHALLENGE_DIGEST_CACHE_PREFIX}#{session_id}"
+    cached_challenge_digest = Rails.cache.fetch(cache_key)
+    exists = cached_challenge_digest && cached_challenge_digest == challenge_digest_hex
+    Rails.cache.delete(cache_key) if exists
+    exists
+  end
+
+  def token_type_bytes
+    start_index = 0
+    unpacked_token.slice(start_index, TOKEN_TYPE_LENGTH)
+  end
+
+  def nonce_bytes
+    start_index = TOKEN_TYPE_LENGTH
+    unpacked_token.slice(start_index, NONCE_LENGTH)
+  end
+
+  def challenge_digest_bytes
+    start_index = TOKEN_TYPE_LENGTH + NONCE_LENGTH
+    unpacked_token.slice(start_index, CHALLENGE_DIGEST_LENGTH)
+  end
+
+  def challenge_digest_hex
+    challenge_digest_bytes.pack("C*").unpack1("H*")
+  end
+
+  def token_key_id_bytes
+    start_index = TOKEN_TYPE_LENGTH + NONCE_LENGTH + CHALLENGE_DIGEST_LENGTH
+    unpacked_token.slice(start_index, TOKEN_KEY_ID_LENGTH)
+  end
+
+  def authenticator_bytes
+    start_index = TOKEN_TYPE_LENGTH + NONCE_LENGTH + CHALLENGE_DIGEST_LENGTH + TOKEN_KEY_ID_LENGTH
+    unpacked_token.slice(start_index, NK_AUTHENTICATOR_LENGTH)
+  end
+
+  def authenticator_string
+    authenticator_bytes.pack("C#{NK_AUTHENTICATOR_LENGTH}")
+  end
+
+  def data_string
+    verifiable_data = token_type_bytes + nonce_bytes + challenge_digest_bytes + token_key_id_bytes
+    verifiable_data.pack("nC#{NONCE_LENGTH}C#{CHALLENGE_DIGEST_LENGTH}C#{TOKEN_KEY_ID_LENGTH}")
+  end
+
+  def public_key
+    # gsubbed = issuer_public_key.tr("-", "+").tr("_", "/").gsub("\r\n", "").scan(/.{1,64}/).join("\n")
+    gsubbed = PrivacyPassTokenizer.issuer_public_key.tr("-", "+").tr("_", "/")
+    with_line_endings = gsubbed[0] + gsubbed[1..].scan(/.{1,64}/).join("\n")
+    pem = "-----BEGIN PUBLIC KEY-----\n#{with_line_endings}\n-----END PUBLIC KEY-----\n"
+    OpenSSL::PKey.read(pem)
+  end
+end
---
+++ lib/privacy_pass_tokenizer.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+# The TokenChallenge structure contains the type of token, the hostname of the issuer, an optional context to bind to your challenge, and the hostname of your server. iOS 16 and macOS Ventura support token type 2, which uses publicly verifiable RSA Blind Signatures.
+
+# struct {
+#     uint16_t token_type;               // 0x0002, in network-byte order
+#     uint16_t issuer_name_length;       // Issuer name length, in network-byte order
+#     char issuer_name[];                // Hostname of the token issuer
+#     uint8_t redemption_context_length; // Redemption context length (0 or 32)
+#     uint8_t redemption_context[];      // Redemption context, either 0 or 32 bytes
+#     uint16_t origin_info_length;       // Origin info length, in network-byte order
+#     char origin_info[];                // Hostname of your server
+# } TokenChallenge;
+
+class PrivacyPassTokenizer
+  TOKEN_TYPE = 2
+  CHALLENGE_DIGEST_CACHE_PREFIX = "privacy_pass_challenge_digest_"
+
+  # Base64 url encoded
+  def self.issuer_public_key
+    # fastly
+    ENV.fetch("PRIVACY_PASS_ISSUER_PUBLIC_KEY",
+    "MIICUjA9BgkqhkiG9w0BAQowMKANMAsGCWCGSAFlAwQCAqEaMBgGCSqGSIb3DQEBCDALBglghkgBZQMEAgKiAwIBMAOCAg8AMIICCgKCAgEAubzND7lvK-u5aIFHSt_NHIzsUFuHCiwVm88kvUvbvJbjj4be_IdNwpfdfXz-vq6NeEwlKEDLtD9oMVop_kEf5HEBkiipBrcn10h17XvYPuc6lA7Z4Y8IjvSjLVydXmcabmslwreMysD1ZINjzadjlJxiIKWCeS_3OJ4dxMTirt8HMUl8cRQafTjSOBJIZNtoNiT5rr5_5SYfEQW8v25RZcitkL0Lrg1mkSutRR00EE3zMelQBGR0sf8jDgBRiPQ1-sSgaIicIciZ2ysiPdkQZBIDpIFnvqed4yKY9gZCn-ASztoe1JhJvzWUwO7Nyz1Mb6pxAV0aQNfb0rDMTPNtyF4z7kgtFcsr5SXiJUa13bm0G2QvEdhl177poghh2RDaT5tcEmiRRvdQWBTeVgFyjrX3-29TZCT4z8VyvDxPPIUIi2rN8-9lN6mmsF0KhlyKe4jUuDwK7dc6Q0gZyjKiE3n-ygErrlbOYU16MGQiJQRV6Z8j38OSD4Lf5-SzsgkAa6Zbv95Zf8TZRS-fuagiXGZcmfdO9zXkuNlRlGguEUpx520SANRciPIBZxFBDXga6kcT9Hfha0n97Mhq57kFRe2Cox7qpq79x9TjUvHOZkWMSv2KmZpoF0wbI2NvlZngbW6Z1hC-X4C3evilX7O498r45Ct7PNjq11Qjso82DwMCAwEAAQ==")
+  end
+
+  def challenge_token
+    Base64.urlsafe_encode64(raw_challenge_token)
+  end
+
+  def register_challenge_for_redemption(session_id)
+    digest = Digest::SHA256.hexdigest(Base64.urlsafe_decode64(challenge_token))
+
+    Rails.cache.write("#{CHALLENGE_DIGEST_CACHE_PREFIX}#{session_id}", digest)
+  end
+
+  private
+
+  REDEMPTION_CONTEXT_LENGTH = 32
+
+  def raw_challenge_token
+    "#{token_type}#{issuer_name_length}#{issuer_name}#{redemption_context_length}#{redemption_context}#{origin_info_length}#{origin_info}"
+  end
+
+  def token_type
+    as_unsigned_16_bit([TOKEN_TYPE])
+  end
+
+  def issuer_name
+    ENV.fetch("PRIVACY_PASS_ISSUER_NAME", "demo-issuer.private-access-tokens.fastly.com")
+  end
+
+  def issuer_name_length
+    as_unsigned_16_bit([issuer_name.size])
+  end
+
+  def raw_redemption_context
+    @raw_redemption_context ||= SecureRandom.hex(REDEMPTION_CONTEXT_LENGTH / 2)
+  end
+
+  def redemption_context
+    as_unsigned_8_bit(raw_redemption_context.bytes) unless raw_redemption_context.empty?
+  end
+
+  def redemption_context_length
+    as_unsigned_8_bit([raw_redemption_context.size])
+  end
+
+  def origin_info
+    # "a55c-2600-1700-3040-74a0-240f-fda0-91b0-1cff.ngrok.io"
+    ""
+  end
+
+  def origin_info_length
+    as_unsigned_16_bit([origin_info.size])
+  end
+
+  def as_unsigned_16_bit(bytes, length = "*")
+    # 16-bit unsigned, network (big-endian) byte order
+    bytes.pack("n#{length}")
+  end
+
+  def as_unsigned_8_bit(bytes, length = "*")
+    # 8-bit unsigned (unsigned char)
+    bytes.pack("C#{length}")
+  end
+end
