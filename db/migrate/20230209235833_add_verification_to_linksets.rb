be245baba6f9720c2f14d313fee7409333a66bb5
Arlette Thibodeau <arlette.thibodeau@cloudcity.io>
2023-03-03 14:10:15 -0800

    Consolidate Linkset verification migrations

--- db/migrate/20230209235833_add_verification_to_linksets.rb
+++ db/migrate/20230209235833_add_verification_to_linksets.rb
@@ -1,7 +1,7 @@
 class AddVerificationToLinksets < ActiveRecord::Migration[7.0]
   def change
-    %w{home code docs wiki mail bugs}.each do |name|
-      add_column :linksets, "#{name}_verified", :boolean
+    %w[home code docs wiki mail bugs].each do |name|
+      add_column :linksets, "#{name}_verified_at", :date
     end
   end
 end
--- db/migrate/20230303002215_change_verified_boolean_to_date.rb
+++
@@ -1,8 +0,0 @@
-class ChangeVerifiedBooleanToDate < ActiveRecord::Migration[7.0]
-  def change
-    %w{home code docs wiki mail bugs}.each do |name|
-      remove_column(:linksets, "#{name}_verified", :boolean)
-      add_column :linksets, "#{name}_verified_at", :date
-    end
-  end
-end
