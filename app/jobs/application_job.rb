0fa437c15fe12f069dece49e198226915fb370a7
Samuel Giddins <segiddins@segiddins.me>
2023-03-06 09:31:36 -0800

    Add a default retry for application job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,8 @@
 class ApplicationJob < ActiveJob::Base
+  # Default to retrying errors a few times, so we don't get an alert for
+  # spurious errors
+  retry_on StandardError, wait: :exponentially_longer, attempts: 5
+
   # Automatically retry jobs that encountered a deadlock
   retry_on ActiveRecord::Deadlocked
 
