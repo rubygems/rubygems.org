ebf32fefc184f26d46d7503d0fdb5c5d96ed2c2a
Samuel Giddins <segiddins@segiddins.me>
2023-03-17 16:21:05 -0700

    Fix fastly soft purging (#3619)
    Add corresponding test

--- test/unit/fastly_test.rb
+++ test/unit/fastly_test.rb
@@ -23,6 +23,16 @@ class FastlyTest < ActiveSupport::TestCase
         .to_return(status: 200, body: "{}")
       Fastly.purge(path: "some-url")
     end
+
+    should "soft purge for each domain" do
+      stub_request(:purge, "https://domain1.example.com/some-url")
+        .with(headers: { "Fastly-Key" => "api-key", "Fastly-Soft-Purge" => "1" })
+        .to_return(status: 200, body: "{}")
+      stub_request(:purge, "https://domain2.example.com/some-url")
+        .with(headers: { "Fastly-Key" => "api-key", "Fastly-Soft-Purge" => "1" })
+        .to_return(status: 200, body: "{}")
+      Fastly.purge(path: "some-url", soft: true)
+    end
   end
 
   context ".purge_key" do
@@ -31,6 +41,12 @@ class FastlyTest < ActiveSupport::TestCase
         .with(headers: { "Fastly-Key" => "api-key" })
         .to_return(status: 200, body: "{}")
       Fastly.purge_key("some-key")
+    end
+    should "send a post request for soft purges" do
+      stub_request(:post, "https://api.fastly.com/service/service-id/purge/some-key")
+        .with(headers: { "Fastly-Key" => "api-key", "Fastly-Soft-Purge" => "1" })
+        .to_return(status: 200, body: "{}")
+      Fastly.purge_key("some-key", soft: true)
     end
   end
 end
