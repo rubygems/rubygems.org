eb539e50144ff2017be2e63f59858da26719b862
Jenny Shen <jenny.shen@shopify.com>
2023-04-03 16:29:45 -0400

    Replace dependency API route with search

--- test/integration/encoding_test.rb
+++ test/integration/encoding_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class EncodingTest < ActionDispatch::IntegrationTest
   test "invalid utf-8 characters should be sanitized" do
-    get "/api/v1/dependencies?gems=vagrant,vagrant-login,vagrant-share,vagrant%ADvbguest"
+    get "/api/v1/search.json?query=vagrant%ADvbguest"
 
     assert_response :success
   end
