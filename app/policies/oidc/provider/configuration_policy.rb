8e27b92e74723a9a934cf27710a514e5a9cd7c91
Samuel Giddins <segiddins@segiddins.me>
2023-04-16 12:14:36 -0700

    Remove unused file

--- app/policies/oidc/provider/configuration_policy.rb
+++
@@ -1,8 +0,0 @@
-class OIDC::Provider::ConfigurationPolicy < ApplicationPolicy
-  class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
-  end
-end
