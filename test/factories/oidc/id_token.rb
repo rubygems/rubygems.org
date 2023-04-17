f418950fb88ea5e58f2c2a27e50cacfb56fa701a
Samuel Giddins <segiddins@segiddins.me>
2023-04-16 18:57:38 -0700

    Better association factories

--- test/factories/oidc/id_token.rb
+++ test/factories/oidc/id_token.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :oidc_id_token, class: "OIDC::IdToken" do
-    api_key_role { build(:oidc_api_key_role) }
+    api_key_role factory: :oidc_api_key_role
     provider { api_key_role.provider }
-    api_key { build(:api_key, user: api_key_role.user) }
+    api_key { association :api_key, user: api_key_role.user, key: SecureRandom.hex(20) }
     jwt do
       {
         claims: {
