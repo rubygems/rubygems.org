830c78a84c85ebc44857896de9b2a68a86942c06
Samuel Giddins <segiddins@segiddins.me>
2023-04-19 23:48:19 +0200

    Fix oidc provider new view in avo

--- app/avo/resources/oidc_provider_resource.rb
+++ app/avo/resources/oidc_provider_resource.rb
@@ -11,8 +11,9 @@ class OIDCProviderResource < Avo::BaseResource
   # Fields generated from the model
   field :issuer, as: :text, link_to_resource: true
   field :configuration, as: :nested do
+    visible_on = %i[edit new]
     OIDC::Provider::Configuration.then { (_1.required_attributes + _1.optional_attributes) - fields.map(&:id) }.each do |k|
-      field k, as: (k.to_s.end_with?("s_supported") ? :tags : :text), visible: ->(_) { view == :edit || value.send(k).present? }
+      field k, as: (k.to_s.end_with?("s_supported") ? :tags : :text), visible: ->(_) { visible_on.include?(view) || value.send(k).present? }
     end
   end
   field :jwks, as: :array_of, field: :json_viewer, hide_on: :index
