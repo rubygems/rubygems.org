4c161ff545946b77fc6952c4296b5fbddaa249c6
Arun Agrawal <arunagw@gmail.com>
2023-04-20 12:39:35 +0530

    Fix code review comments and use model validation

--- app/avo/actions/change_email.rb
+++ app/avo/actions/change_email.rb
@@ -9,16 +9,13 @@ class ChangeEmail < BaseAction
   self.confirm_button_label = "Change Email"
 
   class ActionHandler < ActionHandler
-    set_callback :handle, :before do
-      error "Must enter Email" unless fields[:from_email].presence
-    end
-
     def handle_model(user)
       user.email = fields["from_email"]
       user.email_confirmed = false
       user.generate_confirmation_token
 
-      return unless user.save
+      return unless user.save!
+
       Mailer.email_confirmation(user).deliver_later
     end
   end
