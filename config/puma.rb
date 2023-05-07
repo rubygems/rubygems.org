a62fce61e5b22806efe3ac08e0e71b0f380abf99
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2023-05-08 04:16:19 +1000

    increase the number of puma threads from 1 to 5 (#3773)

--- config/puma.rb
+++ config/puma.rb
@@ -7,10 +7,9 @@
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
-# max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
-# min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-# threads min_threads_count, max_threads_count
-threads 1, 1 # TODO: switch to threaded after initial puma deploy
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
 
 require "concurrent"
 
