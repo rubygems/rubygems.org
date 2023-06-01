e7aab4551022b6027298dfd1faa460945bf8942d
Jenny Shen <jenny.shen@shopify.com>
2023-05-31 23:11:14 -0400

    Add webauthn_enabled?, webauthn_disabled?, count_webauthn_credentials helper methods
    Co-authored-by: Eric Herscovich <eric.herscovich@shopify.com>

--- test/models/concerns/user_webauthn_methods_test.rb
+++ test/models/concerns/user_webauthn_methods_test.rb
@@ -11,6 +11,43 @@ class UserWebauthnMethodsTest < ActiveSupport::TestCase
     end
   end
 
+  context "#webauthn_enabled?" do
+    should "return true if webauthn is enabled" do
+      create(:webauthn_credential, user: @user)
+
+      assert_predicate @user, :webauthn_enabled?
+    end
+
+    should "return false if webauthn is disabled" do
+      refute_predicate @user, :webauthn_enabled?
+    end
+  end
+
+  context "#webauthn_disabled?" do
+    should "return true if webauthn is disabled" do
+      assert_predicate @user, :webauthn_disabled?
+    end
+
+    should "return false if webauthn is enabled" do
+      create(:webauthn_credential, user: @user)
+
+      refute_predicate @user, :webauthn_disabled?
+    end
+  end
+
+  context "#count_webauthn_credentials" do
+    should "return 0 if webauthn is disabled" do
+      assert_equal 0, @user.count_webauthn_credentials
+    end
+
+    should "return number of webauthn credentials if webauthn is enabled" do
+      create(:webauthn_credential, user: @user)
+      create(:webauthn_credential, user: @user)
+
+      assert_equal 2, @user.count_webauthn_credentials
+    end
+  end
+
   context "#webauthn_options_for_create" do
     should "returns options with id, and name" do
       user_create_options = @user.webauthn_options_for_create.user
