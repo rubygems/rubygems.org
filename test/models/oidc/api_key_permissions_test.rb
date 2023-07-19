424d15885ed4e06d672c8a737410ee7e12d9a4f4
Samuel Giddins <segiddins@segiddins.me>
2023-07-18 18:20:23 -0700

    Test OIDC::ApiKeyPermissions

---
+++ test/models/oidc/api_key_permissions_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class OIDC::ApiKeyPermissionsTest < ActiveSupport::TestCase
+  make_my_diffs_pretty!
+
+  should validate_presence_of :scopes
+  should validate_presence_of :valid_for
+
+  test "validates scopes are known" do
+    permissions = OIDC::ApiKeyPermissions.new(scopes: ["unknown"])
+    permissions.validate
+
+    assert_equal ["unknown scope: unknown"], permissions.errors.messages[:"scopes[0]"]
+  end
+
+  test "validates scopes are unique" do
+    permissions = OIDC::ApiKeyPermissions.new(scopes: %w[openid openid])
+    permissions.validate
+
+    assert_equal ["must be unique"], permissions.errors.messages[:scopes]
+  end
+
+  test "validates gems has maximum length of 1" do
+    permissions = OIDC::ApiKeyPermissions.new(gems: %w[a b])
+    permissions.validate
+
+    assert_equal ["is too long (maximum is 1 character)"], permissions.errors.messages[:gems]
+  end
+end
