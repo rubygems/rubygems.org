ad16a83d3916d12b77d41f8deea994b8fffab3fa
Samuel Giddins <segiddins@segiddins.me>
2023-07-19 13:33:02 -0700

    Add role token to avo resource

--- app/avo/resources/oidc_api_key_role_resource.rb
+++ app/avo/resources/oidc_api_key_role_resource.rb
@@ -6,8 +6,10 @@ class OIDCApiKeyRoleResource < Avo::BaseResource
   #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   # end
 
-  field :id, as: :id, link_to_resource: true
+  field :token, as: :text, link_to_resource: true, readonly: true
+  field :id, as: :id, link_to_resource: true, hide_on: :index
   # Fields generated from the model
+  field :name, as: :text
   field :provider, as: :belongs_to
   field :user, as: :belongs_to
   field :api_key_permissions, as: :nested do
@@ -15,7 +17,6 @@ class OIDCApiKeyRoleResource < Avo::BaseResource
     field :scopes, as: :tags, suggestions: ApiKey::API_SCOPES.map { { label: _1, value: _1 } }
     field :gems, as: :tags, suggestions: -> { Rubygem.limit(10).pluck(:name).map { { value: _1, label: _1 } } }
   end
-  field :name, as: :text
   field :access_policy, as: :nested do
     field :statements, as: :array_of, field: :nested do
       field :effect, as: :select, options: { "Allow" => "allow" }, default: "Allow"
