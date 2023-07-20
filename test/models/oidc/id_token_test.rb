a2f3bb14ae43ecfd44ad3b9b922633c75429c341
Samuel Giddins <segiddins@segiddins.me>
2023-07-20 12:07:30 -0700

    RuboCop

--- test/models/oidc/id_token_test.rb
+++ test/models/oidc/id_token_test.rb
@@ -18,16 +18,19 @@ class OIDC::IdTokenTest < ActiveSupport::TestCase
 
   test "#to_json" do
     id_token = FactoryBot.create(:oidc_id_token)
+
     assert_equal id_token.payload.to_json, id_token.to_json
   end
 
   test "#to_xml" do
     id_token = FactoryBot.create(:oidc_id_token)
+
     assert_equal id_token.payload.to_xml(root: "oidc:id_token"), id_token.to_xml
   end
 
   test "#to_yaml" do
     id_token = FactoryBot.create(:oidc_id_token)
+
     assert_equal id_token.payload.to_yaml, id_token.to_yaml
   end
 end
