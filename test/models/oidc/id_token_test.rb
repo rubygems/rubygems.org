cb2157064f9188f379139fb8c86edd63b7ec2a54
Samuel Giddins <segiddins@segiddins.me>
2023-07-20 11:40:04 -0700

    Test OIDC::IdToken serialization methods

--- test/models/oidc/id_token_test.rb
+++ test/models/oidc/id_token_test.rb
@@ -15,4 +15,19 @@ class OIDC::IdTokenTest < ActiveSupport::TestCase
       pp FactoryBot.create(:oidc_id_token, provider: id_token.provider, jwt: id_token.jwt, api_key_role:)
     end
   end
+
+  test "#to_json" do
+    id_token = FactoryBot.create(:oidc_id_token)
+    assert_equal id_token.payload.to_json, id_token.to_json
+  end
+
+  test "#to_xml" do
+    id_token = FactoryBot.create(:oidc_id_token)
+    assert_equal id_token.payload.to_xml(root: "oidc:id_token"), id_token.to_xml
+  end
+
+  test "#to_yaml" do
+    id_token = FactoryBot.create(:oidc_id_token)
+    assert_equal id_token.payload.to_yaml, id_token.to_yaml
+  end
 end
