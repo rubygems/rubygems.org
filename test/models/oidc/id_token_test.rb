2134fd3d8e7c42a60234dfc2617b69e8993ca31d
Samuel Giddins <segiddins@segiddins.me>
2023-07-20 11:40:34 -0700

    remove debugging code

--- test/models/oidc/id_token_test.rb
+++ test/models/oidc/id_token_test.rb
@@ -12,7 +12,7 @@ class OIDC::IdTokenTest < ActiveSupport::TestCase
     api_key_role = FactoryBot.create(:oidc_api_key_role)
     id_token = FactoryBot.create(:oidc_id_token, api_key_role:)
     assert_raises(ActiveRecord::RecordInvalid) do
-      pp FactoryBot.create(:oidc_id_token, provider: id_token.provider, jwt: id_token.jwt, api_key_role:)
+      FactoryBot.create(:oidc_id_token, provider: id_token.provider, jwt: id_token.jwt, api_key_role:)
     end
   end
 
