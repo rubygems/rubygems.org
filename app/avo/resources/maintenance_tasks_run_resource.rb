156e605a5eefe3b3f5a918770b4798bf9dddb9d9
Samuel Giddins <segiddins@segiddins.me>
2023-07-30 13:46:28 -0700

    Add avo filter for maintenance task run status

--- app/avo/resources/maintenance_tasks_run_resource.rb
+++ app/avo/resources/maintenance_tasks_run_resource.rb
@@ -6,17 +6,20 @@ class MaintenanceTasksRunResource < Avo::BaseResource
   #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   # end
 
+  class StatusFilter < ScopeBooleanFilter; end
+  filter StatusFilter, arguments: { default: MaintenanceTasks::Run.statuses.transform_values { true } }
+
   field :id, as: :id
   # Fields generated from the model
   field :task_name, as: :text
-  field :started_at, as: :date_time
-  field :ended_at, as: :date_time
-  field :time_running, as: :number
+  field :started_at, as: :date_time, sortable: true
+  field :ended_at, as: :date_time, sortable: true
+  field :time_running, as: :number, sortable: true
   field :tick_count, as: :number
   field :tick_total, as: :number
   field :job_id, as: :text
   field :cursor, as: :number
-  field :status, as: :select, enum: ::MaintenanceTasks::Run.statuses
+  field :status, as: :select, enum: MaintenanceTasks::Run.statuses
   field :error_class, as: :text
   field :error_message, as: :text
   field :backtrace, as: :textarea
