a1f4aa99a8967b78e7f0d8d60cd2165f1a9a1435
Samuel Giddins <segiddins@segiddins.me>
2023-09-09 10:41:37 -0700

    Add tests that patterns match in linear time (#4045)
    * Add tests that patterns match in linear time
    
    Attempting to ensure we dont regress on ReDOS vulnerabilities
    
    ---------
    
    Co-authored-by: Josef Šimánek <josef.simanek@gmail.com>

---
+++ test/unit/patterns_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class PatternsTest < ActiveSupport::TestCase
+  test "JAVA_HTTP_USER_AGENT is linear" do
+    assert Regexp.linear_time?(Patterns::JAVA_HTTP_USER_AGENT)
+  end
+
+  test "ROUTE_PATTERN is linear" do
+    assert Regexp.linear_time?(Patterns::ROUTE_PATTERN)
+  end
+
+  test "LAZY_ROUTE_PATTERN is linear" do
+    assert Regexp.linear_time?(Patterns::LAZY_ROUTE_PATTERN)
+  end
+
+  test "NAME_PATTERN is linear" do
+    assert Regexp.linear_time?(Patterns::NAME_PATTERN)
+  end
+
+  test "URL_VALIDATION_REGEXP is linear" do
+    skip "regexp is not linear"
+
+    assert Regexp.linear_time?(Patterns::URL_VALIDATION_REGEXP)
+  end
+
+  test "VERSION_PATTERN is linear" do
+    skip "regexp is not linear"
+
+    assert Regexp.linear_time?(Patterns::VERSION_PATTERN)
+  end
+end
