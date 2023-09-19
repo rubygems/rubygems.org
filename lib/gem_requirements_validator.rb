6d26bb75f062c7b5a2ac99dcf0ecf62cc3baef9b
Samuel Giddins <segiddins@segiddins.me>
2023-09-19 10:41:04 -0700

    Avoid testing regexp against a potentially huge string

--- lib/gem_requirements_validator.rb
+++ lib/gem_requirements_validator.rb
@@ -4,7 +4,9 @@ class GemRequirementsValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    return if value.split(", ").all? { |requirement| Patterns::REQUIREMENT_PATTERN.match?(requirement) }
+    return if value.split(", ").all? do |requirement|
+                requirement.length < Gemcutter::MAX_FIELD_LENGTH && Patterns::REQUIREMENT_PATTERN.match?(requirement)
+              end
     record.errors.add(attribute, "must be list of valid requirements")
   end
 end
