f20b28562799d0b08e3a42510cdd7eb0a66bffae
Samuel Giddins <segiddins@segiddins.me>
2023-09-21 11:28:06 -0700

    Add backfill for spec_sha256 on versions (#4083)
    Followup to https://github.com/rubygems/rubygems.org/pull/4031

---
+++ test/tasks/maintenance/backfill_spec_sha256_task_test.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Maintenance::BackfillSpecSha256TaskTest < ActiveSupport::TestCase
+  context "#collection" do
+    should "return versions without spec_sha256" do
+      create(:version)
+      b = create(:version, spec_sha256: nil)
+      create(:version, spec_sha256: nil, indexed: false)
+
+      assert_equal [b], Maintenance::BackfillSpecSha256Task.collection.to_a
+    end
+  end
+
+  context "#process" do
+    setup do
+      @rubygem = create(:rubygem, name: "rubygem")
+    end
+
+    teardown do
+      RubygemFs.mock!
+    end
+
+    should "update the spec sha256" do
+      RubygemFs.instance.store("quick/Marshal.4.8/rubygem-1.gemspec.rz", "spec contents")
+
+      v = create(:version, rubygem: @rubygem, number: "1", platform: "ruby", spec_sha256: nil)
+      Maintenance::BackfillSpecSha256Task.process(v)
+
+      assert_equal "Ry6N90Xp7Or9qGoWziaaTotD1K7vOAonnRAAPjXCzic=", v.reload.spec_sha256
+    end
+
+    should "error if spec is missing" do
+      v = create(:version, rubygem: @rubygem, number: "1", platform: "ruby", spec_sha256: nil)
+      assert_raise { Maintenance::BackfillSpecSha256Task.process(v) }
+    end
+
+    should "not update the spec sha256 if it is already set" do
+      RubygemFs.instance.store("quick/Marshal.4.8/rubygem-1.gemspec.rz", "spec contents")
+
+      v = create(:version, rubygem: @rubygem, number: "1", platform: "ruby", spec_sha256: "Ry6N90Xp7Or9qGoWziaaTotD1K7vOAonnRAAPjXCzic=")
+      Maintenance::BackfillSpecSha256Task.process(v)
+
+      assert_equal "Ry6N90Xp7Or9qGoWziaaTotD1K7vOAonnRAAPjXCzic=", v.reload.spec_sha256
+    end
+
+    should "error if spec_sha256 is incorrect" do
+      RubygemFs.instance.store("quick/Marshal.4.8/rubygem-1.gemspec.rz", "spec contents 2")
+
+      v = create(:version, rubygem: @rubygem, number: "1", platform: "ruby", spec_sha256: "Ry6N90Xp7Or9qGoWziaaTotD1K7vOAonnRAAPjXCzic=")
+      assert_no_changes "v.reload.spec_sha256" do
+        assert_raise { Maintenance::BackfillSpecSha256Task.process(v) }
+      end
+    end
+  end
+end
