94586fe861e132abaa3e484073334727ace04fa9
Samuel Giddins <segiddins@segiddins.me>
2023-09-26 11:50:37 -0700

    Make version backfills more resilient (#4089)
    * Make version backfills more resilient
    
    - .present? doesnt work with binary data
    
    - there are old gems with conflicting full names with different cases, handle them in backfill
    
    - skip validating fields unless they themselves have changed (makes it easier to backfill unrelated fields without convoluted error handling)
    
    * Fix version validations to run on both create && when the validated attribute changes

--- app/tasks/maintenance/backfill_gem_platform_to_versions_task.rb
+++ app/tasks/maintenance/backfill_gem_platform_to_versions_task.rb
@@ -1,12 +1,24 @@
 # frozen_string_literal: true
 
 class Maintenance::BackfillGemPlatformToVersionsTask < MaintenanceTasks::Task
+  include SemanticLogger::Loggable
+
+  FULL_NAME_ATTRIBUTES = %i[full_name gem_full_name].freeze
+
   def collection
     Version.where(gem_platform: nil)
   end
 
-  def process(element)
-    platform = Gem::Platform.new(element.platform)
-    element.update!(gem_platform: platform.to_s)
+  def process(version)
+    platform = Gem::Platform.new(version.platform)
+    version.update!(gem_platform: platform.to_s)
+  rescue ActiveRecord::RecordInvalid => e
+    if e.record.errors.errors.all? { |error| FULL_NAME_ATTRIBUTES.include?(error.attribute) && error.type == :taken }
+      version.save!(validate: false)
+      logger.warn "Version #{version.full_name} failed validation setting gem_platform to #{platform.to_s.inspect} but was saved without validation",
+                  error: e
+    else
+      logger.error "Version #{version.full_name} failed validation setting gem_platform to #{platform.to_s.inspect}", error: e
+    end
   end
 end
