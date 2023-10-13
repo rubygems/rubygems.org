140eb073c9c3184db2f9de05ed7ddc0f91d35cc2
Samuel Giddins <segiddins@segiddins.me>
2023-10-13 12:48:13 -0700

    Allow administering gem typo exceptions thru admin interface
    Removes another support action that requires using the console

---
+++ app/avo/resources/gem_typo_exception_resource.rb
@@ -0,0 +1,15 @@
+class GemTypoExceptionResource < Avo::BaseResource
+  self.title = :name
+  self.includes = []
+  self.search_query = lambda {
+    scope.where("name ILIKE ?", "%#{params[:q]}%")
+  }
+
+  field :id, as: :id, hide_on: :index
+  # Fields generated from the model
+  field :name, as: :text, link_to_resource: true
+  field :info, as: :textarea
+  # add fields here
+  field :created_at, as: :date_time, sortable: true
+  field :updated_at, as: :date_time, sortable: true
+end
