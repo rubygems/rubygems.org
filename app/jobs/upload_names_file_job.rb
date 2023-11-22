e9311703ce78a066c58b3de2157e41f6c7473dc1
Samuel Giddins <segiddins@segiddins.me>
2023-11-21 23:43:27 -0800

    Upload compact index /names to S3
    For consistency with other compact index endpoints

---
+++ app/jobs/upload_names_file_job.rb
@@ -0,0 +1,42 @@
+class UploadNamesFileJob < ApplicationJob
+  queue_with_priority PRIORITIES.fetch(:push)
+
+  include GoodJob::ActiveJobExtensions::Concurrency
+  good_job_control_concurrency_with(
+    # Maximum number of jobs with the concurrency key to be
+    # concurrently enqueued (excludes performing jobs)
+    #
+    # Because the job only uses current state at time of perform,
+    # it makes no sense to enqueue more than one at a time
+    enqueue_limit: good_job_concurrency_enqueue_limit(default: 1),
+    perform_limit: good_job_concurrency_perform_limit(default: 1),
+    key: name
+  )
+
+  def perform
+    names = GemInfo.ordered_names
+    response_body = CompactIndex.names(names)
+
+    content_md5 = Digest::MD5.base64digest(response_body)
+    checksum_sha256 = Digest::SHA256.base64digest(response_body)
+
+    response = RubygemFs.compact_index.store(
+      "names", response_body,
+      public_acl: false, # the compact-index bucket does not have ACLs enabled
+      metadata: {
+        "surrogate-control" => "max-age=3600, stale-while-revalidate=1800",
+        "surrogate-key" => "names s3-compact-index s3-names",
+        "sha256" => checksum_sha256,
+        "md5" => content_md5
+      },
+      cache_control: "max-age=60, public",
+      content_type: "text/plain; charset=utf-8",
+      checksum_sha256:,
+      content_md5:
+    )
+
+    logger.info(message: "Uploading names file succeeded", response:)
+
+    FastlyPurgeJob.perform_later(key: "s3-names", soft: true)
+  end
+end
