294a444547a4222fbdeba699f8a53b1008d1d20b
Martin Emde <martin.emde@gmail.com>
2024-01-22 15:45:24 -0800

    Remove selenium patch now that rails#47117 is merged

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,7 +3,4 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include OauthHelpers
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
-
-  # TODO: remove once https://github.com/rails/rails/pull/47117 is released
-  Selenium::WebDriver.logger.ignore(:capabilities)
 end
