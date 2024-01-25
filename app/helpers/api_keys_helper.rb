f0d06f49a5620d2a2b9958118bec9178942a0e88
Martin Emde <martin.emde@gmail.com>
2024-01-25 15:39:55 -0800

    Add exclusive scope behavior to api key creation form

--- app/helpers/api_keys_helper.rb
+++ app/helpers/api_keys_helper.rb
@@ -5,6 +5,19 @@ module ApiKeysHelper
     api_key.rubygem ? api_key.rubygem.name : t("api_keys.all_gems")
   end
 
+  def api_key_scope_html_data(api_scope)
+    if ApiKey::EXCLUSIVE_SCOPES.include?(api_scope)
+      {
+        api_key_form_target: "exclusiveCheckbox"
+      }
+    else
+      {
+        api_key_form_target: "checkbox",
+        gemscope: ApiKey::APPLICABLE_GEM_API_SCOPES.include?(api_scope) ? api_scope : nil
+      }
+    end
+  end
+
   private
 
   def invalid_gem_tooltip(name)
--- app/javascript/controllers/api_key_form_controller.js
+++ app/javascript/controllers/api_key_form_controller.js
@@ -1,24 +1,38 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["gemCheckbox", "gemSelector"]
+  static targets = ["checkbox", "exclusiveCheckbox", "gemSelector"]
 
   connect() {
     this.toggleGemSelector();
   }
 
-  gemCheckboxTargetConnected(el) {
-    el.addEventListener("click", () => this.toggleGemSelector() );
+  checkboxTargetConnected(el) {
+    el.addEventListener("change", () => {
+      if (el.checked) { this.exclusiveCheckboxTarget.checked = false }
+      if (el.dataset.gemscope) { this.toggleGemSelector(); }
+    })
+  }
+
+  exclusiveCheckboxTargetConnected(el) {
+    el.addEventListener("change", () => {
+      if (el.checked) { this.checkboxTargets.forEach((checkbox) => checkbox.checked = false) }
+      this.toggleGemSelector();
+    });
   }
 
   toggleGemSelector() {
-    var isApplicableGemSelected = this.gemCheckboxTargets.find((target) => target.checked);
-    if (isApplicableGemSelected) {
+    // what type is checkboxTargets?
+    var selected = this.checkboxTargets.find(function(target) {
+      return target.dataset.gemscope && target.checked
+    })
+
+    if (selected) {
       this.gemSelectorTarget.disabled = false;
       this.removeHiddenRubygemField();
     } else {
       this.gemSelectorTarget.value = "";
-      this.gemSelectorTarget.disable = true;
+      this.gemSelectorTarget.disabled = true;
       this.addHiddenRubygemField();
     }
   }
