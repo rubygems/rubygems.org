fc9b3b29c9e048adf2dcb7958af5febe4b44b67b
Martin Emde <martin.emde@gmail.com>
2024-01-24 21:40:07 -0800

    Get rails ujs working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import * as $ from 'jquery';
 import "controllers"
+import Rails from "@rails/ujs"
+Rails.start()
