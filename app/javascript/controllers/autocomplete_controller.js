9ee6445b473514c43a0d149f84a5d916ccd3c65a
Martin Emde <martin.emde@gmail.com>
2024-01-24 20:47:07 -0800

    autocomplete converted and not erroring, but not tested fully

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -1,80 +1,74 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["query suggest"]
+
   connect() {
-    if ($('#home_query').length){
-      autocomplete($('#home_query'));
-      var suggest = $('#suggest-home');
-    } else {
-      autocomplete($('#query'));
-      var suggest = $('#suggest');
+    this.indexNumber = -1;
+    /* remove suggest drop down if clicked anywhere on page */
+    $('html').click(function(e) { $(this.suggestTarget).find('li').remove(); });
+  }
+
+  queryTargetConnected(search) {
+    search.addEventListener('input', (e) => {
+      var term = $.trim($(search).val());
+      if (term.length >= 2) {
+        console.log(term);
+        $.ajax({
+          url: '/api/v1/search/autocomplete',
+          type: 'GET',
+          data: ('query=' + term),
+          processData: false,
+          dataType: 'json'
+        }).done((data) => {
+          this.addToSuggestList(search, data);
+        });
+      } else {
+        $(this.suggestTarget).find('li').remove();
+      }
+    });
+
+    search.addEventListener('keydown', (e) => {
+      if (e.keyCode == 38) {
+        this.indexNumber--;
+        this.focusItem(search);
+      } else if (e.keyCode == 40) {
+        this.indexNumber++;
+        this.focusItem(search);
+      };
+    });
+  }
+
+  addToSuggestList(search, data) {
+    $(this.suggestTarget).find('li').remove();
+
+    for (var i = 0; i < data.length && i < 10; i++) {
+      var newItem = $('<li>').text(data[i]);
+      $(newItem).attr('class', 'menu-item');
+      $(this.suggestTarget).append(newItem);
+
+      /* submit the search form if li item was clicked */
+      newItem.click(function() {
+        $(search).val($(this).html());
+        $(search).parent().submit()
+      });
+
+      newItem.hover(function () {
+        $('li').removeClass('selected');
+        $(this).addClass("selected");
+      });
     }
 
-    var indexNumber = -1;
+    this.indexNumber = -1;
+  };
 
-    function autocomplete(search) {
-      search.bind('input', function(e) {
-        var term = $.trim($(search).val());
-        if (term.length >= 2) {
-          $.ajax({
-            url: '/api/v1/search/autocomplete',
-            type: 'GET',
-            data: ('query=' + term),
-            processData: false,
-            dataType: 'json'
-          }).done(function(data) {
-            addToSuggestList(search, data);
-          });
-        } else {
-          suggest.find('li').remove();
-        }
-      });
+  focusItem(search){
+    var suggestLength = $(this.suggestTarget).find('li').length;
+    if (this.indexNumber >= suggestLength) this.indexNumber = 0;
+    if (this.indexNumber < 0) this.indexNumber = suggestLength - 1;
 
-      search.keydown(function(e) {
-        if (e.keyCode == 38) {
-          indexNumber--;
-          focusItem(search);
-        } else if (e.keyCode == 40) {
-          indexNumber++;
-          focusItem(search);
-        };
-      });
-    };
-
-    function addToSuggestList(search, data) {
-      suggest.find('li').remove();
-
-      for (var i = 0; i < data.length && i < 10; i++) {
-        var newItem = $('<li>').text(data[i]);
-        $(newItem).attr('class', 'menu-item');
-        suggest.append(newItem);
-
-        /* submit the search form if li item was clicked */
-        newItem.click(function() {
-          search.val($(this).html());
-          search.parent().submit()
-        });
-
-        newItem.hover(function () {
-          $('li').removeClass('selected');
-          $(this).addClass("selected");
-        });
-      }
-
-      indexNumber = -1;
-    };
-
-    function focusItem(search){
-      var suggestLength = suggest.find('li').length;
-      if (indexNumber >= suggestLength) indexNumber = 0;
-      if (indexNumber < 0) indexNumber = suggestLength - 1;
-
-      $('li').removeClass('selected');
-      suggest.find('li').eq(indexNumber).addClass('selected');
-      search.val(suggest.find('.selected').text());
-    };
-
-    /* remove suggest drop down if clicked anywhere on page */
-    $('html').click(function(e) { suggest.find('li').remove(); });
+    $('li').removeClass('selected');
+    $(this.suggestTarget).find('li').eq(this.indexNumber).addClass('selected');
+    $(search).val($(this.suggestTarget).find('.selected').text());
   }
 }
