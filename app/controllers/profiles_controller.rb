f4ebbd3c77fe91dc512ceff36f7f3a1eae1f032c
Alvaro Sanchez <hello@alvarosanchez.dev>
2024-02-17 14:39:54 -0600

    Added form with select to sort gem list in user profile show page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,10 +8,14 @@ class ProfilesController < ApplicationController
   before_action :disable_cache, only: :edit
 
   def show
-    @user           = User.find_by_slug!(params[:id])
-    rubygems        = @user.rubygems_downloaded
-    @rubygems       = rubygems.slice!(0, 10)
-    @extra_rubygems = rubygems
+    @user = User.find_by_slug!(params[:id])
+    rubygems = @user.rubygems
+    @rubygems = case params[:sort_by]
+                when "name"
+                  rubygems.reorder(:name)
+                else
+                  rubygems.by_downloads
+                end
   end
 
   def me
