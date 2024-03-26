016132464c74d2f520e2283e0726a903f9b072a4
Samuel Giddins <segiddins@segiddins.me>
2024-03-25 19:38:16 -0600

    Migrate from sprockets to propshaft (#4123)

--- app/assets/config/manifest.js
+++
@@ -1,6 +0,0 @@
-//= link application.css
-//= link_tree ../../../vendor/assets/images
-//= link_tree ../builds
-//= link_tree ../../javascript .js
-//= link_tree ../../javascript/src .js
-//= link_tree ../../../vendor/javascript .js
