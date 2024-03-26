98b1b0fdf16b0e9bee6b3721ee9c2dd8cbb4d5e9
Martin Emde <martinemde@users.noreply.github.com>
2024-03-25 18:57:17 -0700

    Revert "Migrate from sprockets to propshaft (#4123)" (#4557)
    This reverts commit 016132464c74d2f520e2283e0726a903f9b072a4.

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link application.css
+//= link_tree ../../../vendor/assets/images
+//= link_tree ../builds
+//= link_tree ../../javascript .js
+//= link_tree ../../javascript/src .js
+//= link_tree ../../../vendor/javascript .js
