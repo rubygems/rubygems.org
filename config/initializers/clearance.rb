269ea95705559f81492371eed7234052cd491cbc
Josef Šimánek <josef.simanek@gmail.com>
2024-03-27 01:30:03 +0100

    Simplify mailers.
    - remove Cleareance from mailers
    - share configuration in base mailer

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,7 +2,7 @@ require_relative "../../lib/confirmed_user_guard"
 
 Clearance.configure do |config|
   config.allow_sign_up = ENV['DISABLE_SIGNUP'].to_s != 'true'
-  config.mailer_sender = "RubyGems.org <no-reply@mailer.rubygems.org>"
+  config.mailer_sender = Gemcutter::MAIL_SENDER
   config.secure_cookie = true unless Rails.env.local?
   config.password_strategy = Clearance::PasswordStrategies::BCrypt
   config.sign_in_guards = [ConfirmedUserGuard]
