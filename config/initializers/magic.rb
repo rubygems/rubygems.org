814f2f28fb7f6d01e3f5f4187c7a0edca9f73dfb
Samuel Giddins <segiddins@segiddins.me>
2024-06-13 18:17:43 +0200

    Add a more helpful error message when magic file is not found (#4794)

--- config/initializers/magic.rb
+++ config/initializers/magic.rb
@@ -1,13 +1,19 @@
 if ENV["MAGIC"].nil?
   # Be resilient to the gem being moved after installation, as is done in the docker image
-  ENV["MAGIC"] = Dir.glob(File.join(
-                            Gem.loaded_specs.fetch("ruby-magic").full_gem_path,
-                            "ports",
-                            "*",
-                            "libmagic",
-                            Magic.version_string,
-                            "share",
-                            "misc",
-                            "magic.mgc"
-                          )).sole
+  glob = File.join(
+    Gem.loaded_specs.fetch("ruby-magic").full_gem_path,
+    "ports",
+    "*",
+    "libmagic",
+    Magic.version_string,
+    "share",
+    "misc",
+    "magic.mgc"
+  )
+  ENV["MAGIC"] =
+    begin
+      Dir.glob(glob).sole
+    rescue Enumerable::SoleItemExpectedError
+      raise "Could not find magic.mgc in #{glob.inspect}\nTry running `bundle pristine ruby-magic` to fix this issue."
+    end
 end
