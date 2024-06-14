78508de3777f15afab0505c398ed028b8c1320a5
Josef Šimánek <josef.simanek@gmail.com>
2024-06-14 02:11:02 +0200

    Migrate to local_time gem and local_time JS.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import Rails from "@rails/ujs";
 Rails.start();
+
+import LocalTime from "local-time"
+LocalTime.start()
+
 import "controllers"
 
 import "src/clipboard_buttons";
