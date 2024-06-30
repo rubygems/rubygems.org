51bb0e139cbcc2afb30700900795fda848cb1e56
Andre Arko <andre@arko.net>
2024-06-29 21:22:38 -0700

    set up org relationships

--- app/models/org.rb
+++ app/models/org.rb
@@ -1,2 +1,6 @@
 class Org < ApplicationRecord
+  has_many :rubygems, through: :ownerships
+  has_many :memberships, -> { where.not(confirmed_at: nil) }, dependent: :destroy, inverse_of: :org
+  has_many :unconfirmed_memberships, -> { where(confirmed_at: nil) }, class_name: "Membership", dependent: :destroy, inverse_of: :org
+  has_many :users, through: :memberships
 end
--- test/models/org_test.rb
+++ test/models/org_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class OrgTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should have_many(:memberships).dependent(:destroy)
+  should have_many(:unconfirmed_memberships).dependent(:destroy)
+  should have_many(:users).through(:memberships)
 end
