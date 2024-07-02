2fdc8a071857fb76d3b5f1cebf1e92802c6b18b6
Andre Arko <andre@arko.net>
2024-07-02 16:24:49 -0700

    membership test

--- test/models/membership_test.rb
+++ test/models/membership_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class MembershipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to(:org)
+  should belong_to(:user)
 end
