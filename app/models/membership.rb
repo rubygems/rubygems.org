b42fd5bf3fa128c535ba6d1f2acbbbd7ba54353f
Andre Arko <andre@arko.net>
2024-07-02 16:40:58 -0700

    add and test membership confirmation

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,10 @@
 class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :org
+
+  scope :confirmed, -> { where.not(confirmed_at: nil) }
+
+  def confirmed?
+    !confirmed_at.nil?
+  end
 end
--- test/models/membership_test.rb
+++ test/models/membership_test.rb
@@ -3,4 +3,23 @@ require "test_helper"
 class MembershipTest < ActiveSupport::TestCase
   should belong_to(:org)
   should belong_to(:user)
+
+  setup do
+    @org = FactoryBot.create(:org)
+    @user = FactoryBot.create(:user)
+  end
+
+  should "be unconfirmed by default" do
+    membership = Membership.create!(org: @org, user: @user)
+
+    assert_not(membership.confirmed?)
+    assert_empty(Membership.confirmed)
+  end
+
+  should "be confirmed with confirmed_at" do
+    membership = Membership.create!(org: @org, user: @user, confirmed_at: Time.zone.now)
+
+    assert_predicate(membership, :confirmed?)
+    assert_equal(Membership.confirmed, [membership])
+  end
 end
