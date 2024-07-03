0629e55bea9af80a15e28313ef777f62ec8e610c
Andre Arko <andre@arko.net>
2024-07-02 17:27:10 -0700

    refine and test validations

--- app/models/org.rb
+++ app/models/org.rb
@@ -1,10 +1,14 @@
 class Org < ApplicationRecord
-  validates :handle, presence: true, uniqueness: true
-  validates :name, presence: true
+  validates :handle, presence: true, uniqueness: { case_sensitive: false }, length: { within: 2..40 }
+  validates :handle, format: {
+    with: /\A[A-Za-z][A-Za-z_\-0-9]*\z/,
+    message: "must start with a letter and can only contain letters, numbers, underscores, and dashes"
+  }, allow_nil: true
+  validates :name, presence: true, length: { within: 2..255 }
   validate :unique_with_user_handle
 
   def unique_with_user_handle
-    errors.add(:handle, "is not available") if User.exists?(handle: handle)
+    errors.add(:handle, "has already been taken") if handle && User.where("handle = lower(?)", handle.downcase).any?
   end
 
   has_many :rubygems, through: :ownerships
--- test/models/org_test.rb
+++ test/models/org_test.rb
@@ -4,4 +4,71 @@ class OrgTest < ActiveSupport::TestCase
   should have_many(:memberships).dependent(:destroy)
   should have_many(:unconfirmed_memberships).dependent(:destroy)
   should have_many(:users).through(:memberships)
+
+  # Waiting for Ownerships to be made polymorphic
+  #
+  # should have_many(:ownerships).dependent(:destroy)
+  # should have_many(:unconfirmed_ownerships).dependent(:destroy)
+  # should have_many(:rubygems).through(:ownerships)
+
+  context "validations" do
+    context "handle" do
+      should allow_value("CapsLOCK").for(:handle)
+      should_not allow_value(nil).for(:handle)
+      should_not allow_value("1abcde").for(:handle)
+      should_not allow_value("abc^%def").for(:handle)
+      should_not allow_value("abc\n<script>bad").for(:handle)
+
+      should "be between 2 and 40 characters" do
+        org = build(:org, handle: "a")
+
+        refute_predicate org, :valid?
+        assert_contains org.errors[:handle], "is too short (minimum is 2 characters)"
+
+        org.handle = "a" * 41
+
+        refute_predicate org, :valid?
+        assert_contains org.errors[:handle], "is too long (maximum is 40 characters)"
+
+        org.handle = "abcdef"
+        org.valid?
+
+        assert_nil org.errors[:handle].first
+      end
+
+      should "be invalid when an empty string" do
+        org = build(:org, handle: "")
+
+        refute_predicate org, :valid?
+      end
+
+      should "be invalid when nil" do
+        refute_predicate build(:org, handle: nil), :valid?
+      end
+
+      should "be invalid with duplicate handle on create" do
+        create(:org, handle: "test")
+        org = build(:org, handle: "Test")
+
+        refute_predicate org, :valid?
+      end
+
+      should "be invalid with duplicate handle on update" do
+        create(:org, handle: "test")
+        org = create(:org, handle: "test2")
+        org.update(handle: "Test")
+
+        assert_contains org.errors[:handle], "has already been taken"
+        refute_predicate org, :valid?
+      end
+
+      should "be invalid if user has handle already" do
+        create(:user, handle: "test")
+        org = build(:org, handle: "Test")
+
+        refute_predicate org, :valid?
+        assert_contains org.errors[:handle], "has already been taken"
+      end
+    end
+  end
 end
