2d56dea13859ffa1123fc1a0c805bc4f433262b1
Andre Arko <andre@arko.net>
2024-07-02 17:13:41 -0700

    add org validations

--- app/models/org.rb
+++ app/models/org.rb
@@ -1,4 +1,12 @@
 class Org < ApplicationRecord
+  validates :handle, presence: true, uniqueness: true
+  validates :name, presence: true
+  validate :unique_with_user_handle
+
+  def unique_with_user_handle
+    errors.add(:handle, "is not available") if User.exists?(handle: handle)
+  end
+
   has_many :rubygems, through: :ownerships
   has_many :memberships, -> { where.not(confirmed_at: nil) }, dependent: :destroy, inverse_of: :org
   has_many :unconfirmed_memberships, -> { where(confirmed_at: nil) }, class_name: "Membership", dependent: :destroy, inverse_of: :org
