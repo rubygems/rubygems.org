2d0ca96ac461356654be81ae0d853c5ef2cb7966
Andre Arko <andre@arko.net>
2024-07-09 20:20:09 -0700

    add org string limits

--- db/migrate/20240630025625_create_orgs.rb
+++ db/migrate/20240630025625_create_orgs.rb
@@ -1,8 +1,8 @@
 class CreateOrgs < ActiveRecord::Migration[7.1]
   def change
     create_table :orgs do |t|
-      t.string :handle
-      t.string :name
+      t.string :handle, limit: 40
+      t.string :name, limit: 255
       t.timestamp :deleted_at
 
       t.timestamps
