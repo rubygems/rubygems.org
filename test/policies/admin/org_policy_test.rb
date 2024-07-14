4dec52d6f8fca2c5298901f0ea0b546599efd4ba
Andre Arko <andre@arko.net>
2024-07-14 03:05:01 -0700

    fix the org policy test to test orgs

--- test/policies/admin/org_policy_test.rb
+++ test/policies/admin/org_policy_test.rb
@@ -1,29 +1,38 @@
 require "test_helper"
 
-class Admin::UserPolicyTest < AdminPolicyTestCase
+class Admin::OrgPolicyTest < AdminPolicyTestCase
   setup do
-    @user = FactoryBot.create(:user)
+    @org = FactoryBot.create(:org)
     @admin = FactoryBot.create(:admin_github_user, :is_admin)
     @non_admin = FactoryBot.create(:admin_github_user)
   end
 
   def test_scope
+    assert_equal [@org], policy_scope!(@admin, Org).to_a
   end
 
   def test_show
+    assert_authorizes @admin, @org, :avo_show?
+    refute_authorizes @non_admin, @org, :avo_show?
   end
 
   def test_create
+    refute_authorizes @admin, @org, :avo_create?
+    refute_authorizes @non_admin, @org, :avo_create?
   end
 
   def test_update
+    refute_authorizes @admin, @org, :avo_update?
+    refute_authorizes @non_admin, @org, :avo_update?
   end
 
   def test_destroy
+    refute_authorizes @admin, @org, :avo_destroy?
+    refute_authorizes @non_admin, @org, :avo_destroy?
   end
 
   def test_search
-    assert_authorizes @admin, @user, :avo_search?
-    refute_authorizes @non_admin, @user, :avo_search?
+    assert_authorizes @admin, @org, :avo_search?
+    refute_authorizes @non_admin, @org, :avo_search?
   end
 end
