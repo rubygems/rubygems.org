74ae33617b1f840365698c6d37b83f511acd86c5
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-07-18 18:48:35 +1000

    Add Access Level field to Ownership Avo Resource

--- lib/access.rb
+++ lib/access.rb
@@ -4,4 +4,29 @@ module Access
   GUEST = 0
   MAINTAINER = 60
   OWNER = 70
+
+  ROLES = {
+    guest: GUEST,
+    maintainer: MAINTAINER,
+    owner: OWNER
+  }
+
+  def self.label_for_role(role)
+    key = ROLES.fetch(role.to_sym) { nil }
+    return nil if key.nil?
+    I18n.t("access.roles.#{role}")
+  end
+
+  def self.label_for_role_flag(flag)
+    raise ArgumentError, "flag must be an integer" unless flag.is_a?(Integer)
+    role = ROLES.key(flag) { nil }
+    return nil if role.nil?
+    I18n.t("access.roles.#{role}")
+  end
+
+  def self.options
+    ROLES.map do |_, permission|
+      [self.label_for_role(role), permission]
+    end
+  end
 end
