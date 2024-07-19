6d0b86fdf5c7db4358348886696d9dd86a6eca94
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-07-19 13:39:05 +1000

    Remove guest

--- lib/access.rb
+++ lib/access.rb
@@ -1,12 +1,10 @@
 module Access
   AccessDeniedError = Class.new(StandardError)
 
-  GUEST = 0
   MAINTAINER = 60
   OWNER = 70
 
   ROLES = {
-    guest: GUEST,
     maintainer: MAINTAINER,
     owner: OWNER
   }
