7187e38c43c927ac778c5d4808a5c19e1e58fd89
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-08-07 15:51:32 +1000

    require ownership for managing ownership requests & trusted publishing

--- test/policies/rubygem_policy_test.rb
+++ test/policies/rubygem_policy_test.rb
@@ -45,6 +45,14 @@ class RubygemPolicyTest < PolicyTestCase
     end
   end
 
+  context "#close_ownership_requests" do
+    should "only allow the owner to close ownership requests" do
+      assert_authorized @owner, :close_ownership_requests?
+      refute_authorized @maintainer, :close_ownership_requests?
+      refute_authorized @user , :close_ownership_requests?
+    end
+  end
+
   context "#show_adoption?" do
     should "be true if the gem is owned by the user" do
       assert_authorized @owner, :show_adoption?
@@ -70,7 +78,7 @@ class RubygemPolicyTest < PolicyTestCase
   context "#configure_trusted_publishers?" do
     should "only allow the owner" do
       assert_authorized @owner, :configure_trusted_publishers?
-      assert_authorized @maintainer, :configure_trusted_publishers?
+      refute_authorized @maintainer, :configure_trusted_publishers?
       refute_authorized @user, :configure_trusted_publishers?
       refute_authorized nil, :configure_trusted_publishers?
     end
@@ -79,7 +87,7 @@ class RubygemPolicyTest < PolicyTestCase
   context "#show_unconfirmed_ownerships?" do
     should "only allow the owner" do
       assert_authorized @owner, :show_unconfirmed_ownerships?
-      assert_authorized @maintainer, :show_unconfirmed_ownerships?
+      refute_authorized @maintainer, :show_unconfirmed_ownerships?
       refute_authorized @user, :show_unconfirmed_ownerships?
       refute_authorized nil, :show_unconfirmed_ownerships?
     end
