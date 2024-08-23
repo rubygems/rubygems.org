971a0841cd416c2e0474727ce86e0bf345455647
Jônatas Davi Paganini <jonatasdp@gmail.com>
2024-08-23 20:12:14 -0300

    Fix model names

--- app/models/download_record.rb
+++
@@ -1,6 +0,0 @@
-# This model is used to connect to the downloads database
-class DownloadRecord < ApplicationRecord
-  self.abstract_class = true
-  extend Timescaledb::ActsAsHypertable
-  connects_to database: { writing: :downloads, reading: :downloads }
-end
