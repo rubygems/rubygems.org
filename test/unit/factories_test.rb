fb5f5d294bfef0778d74ecd5d9ac891f530a3f17
Josef Šimánek <josef.simanek@gmail.com>
2024-09-01 00:04:47 +0200

    Add missing assertions to test/unit.

--- test/unit/factories_test.rb
+++ test/unit/factories_test.rb
@@ -2,6 +2,6 @@ require "test_helper"
 
 class FactoriesTest < ActiveSupport::TestCase
   test "can create factories including traits" do
-    FactoryBot.lint(traits: true)
+    assert_nothing_raised { FactoryBot.lint(traits: true) }
   end
 end
