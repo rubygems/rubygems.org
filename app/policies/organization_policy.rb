645ea766360465cd61fe5f0c75d0e12f921eca6c
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-09-17 07:44:04 +0000

    Add organization policy object

---
+++ app/policies/organization_policy.rb
@@ -0,0 +1,48 @@
+class OrganizationPolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    organization_member_with_role?(user, minimum_required_role: Access::OWNER)
+  end
+
+  def create?
+    true
+  end
+
+  def add_gem?
+    organization_member_with_role?(user, minimum_required_role: Access::OWNER)
+  end
+
+  def remove_gem?
+    organization_member_with_role?(user, minimum_required_role: Access::OWNER)
+  end
+
+  def add_membership?
+    organization_member_with_role?(user, minimum_required_role: Access::OWNER)
+  end
+
+  def update_membership?
+    organization_member_with_role?(user, minimum_required_role: Access::OWNER)
+  end
+
+  def remove_membership?
+    organization_member_with_role?(user, minimum_required_role: Access::OWNER)
+  end
+
+  def show_membership?
+    organization_member_with_role?(user, minimum_required_role: Access::MAINTAINER)
+  end
+
+  def list_memberships?
+    organization_member_with_role?(user, minimum_required_role: Access::MAINTAINER)
+  end
+
+  def destroy?
+    false # For now organizations cannot be deleted
+  end
+end
