104b8570837d07af632abae4dbe23adaec22342d
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-09-17 06:24:15 +0000

    Fixup migration file name

--- db/migrate/20240802151324_add_role_to_ownership.rb
+++ db/migrate/20240802151324_add_role_to_ownership.rb
@@ -1,4 +1,4 @@
-class AddAccessLevelToOwnership < ActiveRecord::Migration[7.1]
+class AddRoleToOwnership < ActiveRecord::Migration[7.1]
   def change
     add_column :ownerships, :role, :integer, null: false, default: 70 # Access::OWNER
   end
