51e6828b63962cc06a06480d30cf4ef919b04b08
Samuel Giddins <segiddins@segiddins.me>
2024-09-23 09:58:54 -0700

    Use avo main for actionstorage fix
    Signed-off-by: Samuel Giddins <segiddins@segiddins.me>

--- Gemfile
+++ Gemfile
@@ -62,7 +62,8 @@ gem "faraday-multipart", "~> 1.0"
 gem "timescaledb", "~> 0.3"
 
 # Admin dashboard
-gem "avo", "~> 3.12"
+# github needed until https://github.com/avo-hq/avo/pull/3215 is released
+gem "avo", "~> 3.12", github: "avo-hq/avo"
 gem "pagy", "~> 8.4"
 gem "view_component", "~> 3.14"
 gem "pundit", "~> 2.4"
--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine" # TODO: comment out, needed until https://github.com/avo-hq/avo/pull/3215 is released
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 # require "action_mailbox/engine"
