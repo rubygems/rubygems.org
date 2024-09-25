332482f45badacb59d30af14c5ab51620a84b8df
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-09-25 08:41:58 +0000

    Add team member model

---
+++ app/avo/resources/team_member_resource.rb
@@ -0,0 +1,10 @@
+class TeamMemberResource < Avo::BaseResource
+  self.title = :id
+  self.includes = []
+  # self.search_query = -> do
+  #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+  # end
+
+  field :id, as: :id
+  # add fields here
+end
