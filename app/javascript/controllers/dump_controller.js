c28e3c8ba2d935365cd86d8dff71713f1f6bff38
Martin Emde <me@martinemde.com>
2024-09-27 18:41:33 -0700

    Convert data dumps to stimulus.js

---
+++ app/javascript/controllers/dump_controller.js
@@ -0,0 +1,66 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["list", "template"]
+  static values = { type: String }
+
+  connect() {
+    this.getDumpData(this.typeValue);
+  }
+
+  getDumpData(type) {
+    fetch('https://s3-us-west-2.amazonaws.com/rubygems-dumps/?prefix=production/public_' + type)
+      .then(response => response.text())
+      .then(data => {
+        const parser = new DOMParser();
+        const xml = parser.parseFromString(data, "application/xml");
+        const files = this.parseS3Listing(xml);
+        this.render(files);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  parseS3Listing(xml) {
+    const contents = Array.from(xml.getElementsByTagName('Contents'));
+    return contents.map(item => {
+      return {
+        Key: item.getElementsByTagName('Key')[0].textContent,
+        LastModified: item.getElementsByTagName('LastModified')[0].textContent,
+        Size: item.getElementsByTagName('Size')[0].textContent,
+        StorageClass: item.getElementsByTagName('StorageClass')[0].textContent
+      };
+    });
+  }
+
+  render(files) {
+    files
+      .filter(item => 'STANDARD' === item.StorageClass)
+      .sort((a, b) => Date.parse(b.LastModified) - Date.parse(a.LastModified))
+      .forEach(item => {
+        let text = `${item.LastModified.replace('.000Z', '')} (${this.bytesToSize(item.Size)})`;
+        let uri = `https://s3-us-west-2.amazonaws.com/rubygems-dumps/${item.Key}`;
+        this.appendItem(text, uri);
+      });
+  }
+
+  appendItem(text, uri) {
+    const clone = this.templateTarget.content.cloneNode(true);
+    const a = clone.querySelector('a')
+    a.textContent = text;
+    a.href = uri;
+    this.element.appendChild(clone)
+  }
+
+  bytesToSize(bytes) {
+    var i, k, sizes;
+    if (bytes === 0) {
+      return '0 Bytes';
+    }
+    k = 1024;
+    sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    i = Math.floor(Math.log(bytes) / Math.log(k));
+    return (bytes / Math.pow(k, i)).toPrecision(3) + " " + sizes[i];
+  }
+}
