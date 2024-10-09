ee996aaefd676d09e1ef6038312dfda75d8539b9
Martin Emde <me@martinemde.com>
2024-10-09 10:54:05 -0700

    Update importmap vendored javascript with new comment
    Add `rake importmap:pristine` to redownload and add comment.
    Fix importmap's incomplete semver regex that loses prereleases.
    The regex used is from semver.org.
    
    Change `rake importmap:verify` to discard the comment when comparing
    content of vendored javascript files.
    Add VERBOSE to importmap:verify to diagnose problems.

--- vendor/javascript/stimulus-rails-nested-form.js
+++ vendor/javascript/stimulus-rails-nested-form.js
@@ -1,2 +1,4 @@
+// stimulus-rails-nested-form@4.1.0 downloaded from https://ga.jspm.io/npm:stimulus-rails-nested-form@4.1.0/dist/stimulus-rails-nested-form.mjs
+
 import{Controller as e}from"@hotwired/stimulus";class r extends e{add(e){e.preventDefault();const t=this.templateTarget.innerHTML.replace(/NEW_RECORD/g,(new Date).getTime().toString());this.targetTarget.insertAdjacentHTML("beforebegin",t)}remove(e){e.preventDefault();const t=e.target.closest(this.wrapperSelectorValue);if("true"===t.dataset.newRecord)t.remove();else{t.style.display="none";const e=t.querySelector("input[name*='_destroy']");e.value="1"}}}r.targets=["target","template"];r.values={wrapperSelector:{type:String,default:".nested-form-wrapper"}};export{r as default};
 
