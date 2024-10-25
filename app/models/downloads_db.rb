f76d25011ad17f90400360a0a746614d794caf3b
Jônatas Davi Paganini <jonatasdp@gmail.com>
2024-10-25 12:28:57 -0300

    Refactor scopes

--- app/models/downloads_db.rb
+++ app/models/downloads_db.rb
@@ -2,7 +2,4 @@ class DownloadsDB < ApplicationRecord
   self.abstract_class = true
 
   connects_to database: { writing: :downloads }
-
-  extend Timescaledb::ActsAsHypertable
-  include Timescaledb::ContinuousAggregatesHelper
 end
