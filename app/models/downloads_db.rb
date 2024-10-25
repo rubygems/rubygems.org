b4fd14976de437e8eb5eabdb5c9861cd4c775db3
Jônatas Davi Paganini <jonatasdp@gmail.com>
2024-10-25 11:46:00 -0300

    Use continuous_aggregate macro

--- app/models/downloads_db.rb
+++ app/models/downloads_db.rb
@@ -2,4 +2,7 @@ class DownloadsDB < ApplicationRecord
   self.abstract_class = true
 
   connects_to database: { writing: :downloads }
+
+  extend Timescaledb::ActsAsHypertable
+  include Timescaledb::ContinuousAggregatesHelper
 end
