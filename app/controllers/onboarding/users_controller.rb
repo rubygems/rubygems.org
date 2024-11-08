0d9dfc6e4547d5102082f46b3c181703aa05b77d
Martin Emde <me@martinemde.com>
2024-11-07 19:12:34 -0800

    little updates

--- app/controllers/onboarding/users_controller.rb
+++ app/controllers/onboarding/users_controller.rb
@@ -2,23 +2,22 @@ class Onboarding::UsersController < Onboarding::BaseController
   layout "onboarding"
 
   def edit
-    initialize_roles
   end
 
   def update
     if @organization_onboarding.update(onboarding_user_params)
       redirect_to onboarding_confirm_path
     else
-      initialize_roles
       render :edit, status: :unprocessable_entity
     end
   end
 
   private
 
-  def initialize_roles
-    @roles = Membership.roles.map { |k, _| [Membership.human_attribute_name("role.#{k}"), k] }
+  def role_options
+    @role_options ||= Membership.roles.map { |k, _| [Membership.human_attribute_name("role.#{k}"), k] }
   end
+  helper_method :role_options
 
   def onboarding_user_params
     params.require(:organization_onboarding).permit(invites_attributes: %i[id role])
