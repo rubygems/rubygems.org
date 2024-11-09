d778bcb23c6c0ac117375b26c160a06ba6ff46aa
Martin Emde <me@martinemde.com>
2024-11-09 14:30:45 -0800

    Fix team model tests

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -7,19 +7,19 @@ class TeamTest < ActiveSupport::TestCase
   end
 
   context "validations" do
-    should "validate the presence of name and slug" do
-      team = build(:team, organization: @organization, name: nil, slug: nil)
+    should "validate the presence of name and handle" do
+      team = build(:team, organization: @organization, name: nil, handle: nil)
 
       assert_not team.valid?
       assert_includes team.errors.messages[:name], "can't be blank"
-      assert_includes team.errors.messages[:slug], "can't be blank"
+      assert_includes team.errors.messages[:handle], "can't be blank"
     end
 
-    should "validate a unique slug within an organization" do
-      team = build(:team, organization: @organization, slug: @team.slug)
+    should "validate a unique handle within an organization" do
+      team = build(:team, organization: @organization, handle: @team.handle)
 
       assert_not team.valid?
-      assert_includes team.errors.messages[:slug], "has already been taken"
+      assert_includes team.errors.messages[:handle], "has already been taken"
     end
   end
 end
