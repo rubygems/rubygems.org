4c905d022ac4e82b7b71ead3ef75ae84c368d2c1
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-11-13 23:35:44 +1100

    Merge null migration into previous migration

--- db/migrate/20241104065953_add_organization_onboarding_invites.rb
+++ db/migrate/20241104065953_add_organization_onboarding_invites.rb
@@ -3,7 +3,7 @@ class AddOrganizationOnboardingInvites < ActiveRecord::Migration[7.2]
     create_table :organization_onboarding_invites do |t|
       t.references :organization_onboarding, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      t.string :role, null: false
+      t.string :role, null: true
 
       t.timestamps
     end
--- db/migrate/20241108002409_allow_null_on_onboarding_role.rb
+++
@@ -1,5 +0,0 @@
-class AllowNullOnOnboardingRole < ActiveRecord::Migration[7.2]
-  def change
-    change_column_null :organization_onboarding_invites, :role, true
-  end
-end
