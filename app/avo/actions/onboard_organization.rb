f5c8811c5365182e49afd2e131551c84b82eedca
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-11-13 22:27:11 +1100

    Update Organization Onboard avo resources with fields and actions

---
+++ app/avo/actions/onboard_organization.rb
@@ -0,0 +1,18 @@
+class Avo::Actions::OnboardOrganization < Avo::Actions::ApplicationAction
+  self.name = "Onboard Organization"
+  self.visible = lambda {
+    current_user.team_member?("rubygems-org") && view == :show
+  }
+
+  self.message = lambda {
+    "Are you sure you would like to onboard this organization?"
+  }
+
+  self.confirm_button_label = "Onboard"
+
+  class ActionHandler < Avo::Actions::ActionHandler
+    def handle_record(organization)
+      organization.onboard!
+    end
+  end
+end
