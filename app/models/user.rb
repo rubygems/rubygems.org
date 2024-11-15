94f31e536b54ed3a42f866b54b891e1944e25ecb
Jacklyn Ma <29336370+jacklynhma@users.noreply.github.com>
2024-11-14 18:00:19 -0600

    passing tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -82,7 +82,7 @@ class User < ApplicationRecord
     message: "can only contain letters, numbers, and underscores"
   }, allow_nil: true, length: { within: 0..20 }
 
-  validates_formatting_of :homepage_url, using: :url, allow_nil: true
+  validates_formatting_of :homepage_url, using: :url, allow_blank: true
 
   validates :password,
     length: { minimum: 10 },
