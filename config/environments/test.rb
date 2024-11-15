ea0b2b601cfe96345ceeda41269a1ade87cad416
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2024-11-15 13:27:07 +1100

    Set Rails cache to in-memory rather than Memcache

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.cache_store = :mem_cache_store
+  config.cache_store = :memory_store
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = :none
