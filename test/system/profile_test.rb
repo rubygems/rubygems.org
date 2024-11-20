b56b1d0c2da94280d186f20c34b997fb8e91af6f
Jacklyn Ma <29336370+jacklynhma@users.noreply.github.com>
2024-11-19 19:57:10 -0500

    attempting to write system test

---
+++ test/system/profile_test.rb
@@ -0,0 +1,26 @@
+require "application_system_test_case"
+
+class ProfileTest < ApplicationSystemTestCase
+  setup do
+    @user = create(:user, email: "nick@example.com", password: PasswordHelpers::SECURE_TEST_PASSWORD, handle: "nick1", mail_fails: 1)
+  end
+
+  def sign_in
+    visit sign_in_path
+    fill_in "Email or Username", with: @user.reload.email
+    fill_in "Password", with: @user.password
+    click_button "Sign in"
+  end
+
+  test "adding X(formerly Twitter) username without filling in your password" do
+    sign_in
+    visit profile_path("nick1")
+
+    click_link "Edit Profile"
+    fill_in "user_twitter_username", with: "nick1twitter"
+    click_button "Update"
+
+    # assert_content("please fill in your password")
+    assert page.has_content?("nick1twitter")
+  end
+end
