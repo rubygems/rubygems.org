89cc35c60aaf5a14805853a332398ce13f8d28e7
Jacklyn Ma <29336370+jacklynhma@users.noreply.github.com>
2024-11-23 20:19:44 -0500

    new XXS tests for homepage url

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -175,6 +175,18 @@ class UserTest < ActiveSupport::TestCase
     context "homepage url" do
       should allow_value("https://www.mywebsite.com").for(:homepage_url)
       should_not allow_value("<html><body>hi</body><html>").for(:homepage_url)
+      should_not allow_value("javascript:alert('hello');").for(:homepage_url)
+      should_not allow_value("file:///etc/passwd").for(:homepage_url)
+      should_not allow_value("file://C:/Windows/System32/cmd.exe").for(:homepage_url)
+      should_not allow_value("data:text/html,<script>alert('XSS')</script>").for(:homepage_url)
+      should_not allow_value("data:text/html;base64,SGVsbG8sIFdvcmxkIQ==").for(:homepage_url)
+      should_not allow_value("data:text/html,<img src='x' onerror='alert(1)'>").for(:homepage_url)
+      should_not allow_value("data:text/html,<iframe src='javascript:alert(1)'></iframe>").for(:homepage_url)
+      # should_not allow_value("http://www.site.site?#redirect=www.fake-target.site").for(:homepage_url)
+      # should_not allow_value("http://www.site.com/redirect?url=http://www.malicious-site.com").for(:homepage_url)
+      # should_not allow_value("http://www.site.com/?next=http://www.malicious-site.com").for(:homepage_url)
+      # should_not allow_value("http://www.site.com/?url=http://www.malicious-site.com").for(:homepage_url)
+      # should_not allow_value("http://www.site.com/?redirect=http://www.malicious-site.com").for(:homepage_url)
     end
 
     context "password" do
