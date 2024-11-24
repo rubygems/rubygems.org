b05155147af7a0daac4d50441b421855107d8007
Jacklyn Ma <29336370+jacklynhma@users.noreply.github.com>
2024-11-23 22:27:37 -0500

    add rescue

--- lib/http_url_validator.rb
+++ lib/http_url_validator.rb
@@ -2,6 +2,7 @@ class HttpUrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     uri = URI::DEFAULT_PARSER.parse(value)
     record.errors.add attribute, "is not a valid URL" unless [URI::HTTP, URI::HTTPS].member?(uri.class)
-    record.errors.add attribute, "is not a valid URL"
+  rescue URI::InvalidURIError
+      record.errors.add attribute, "is not a valid URL"
   end
 end
