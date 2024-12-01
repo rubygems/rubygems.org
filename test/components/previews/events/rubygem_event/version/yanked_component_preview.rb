c7bbd32996b74e45fdadc42e7dc86da8a8b30074
Martin Emde <martinemde@users.noreply.github.com>
2024-12-01 13:47:32 -0800

    Update rubocop, replace custom cop with official safe navigator cop (#5292)
    * Fix rubocop problems in event previews

--- test/components/previews/events/rubygem_event/version/yanked_component_preview.rb
+++ test/components/previews/events/rubygem_event/version/yanked_component_preview.rb
@@ -1,19 +1,13 @@
 class Events::RubygemEvent::Version::YankedComponentPreview < Lookbook::Preview
-  def default(rubygem: Rubygem.first!, # rubocop:disable Metrics/ParameterLists
-    number: "0.0.1", platform: "ruby",
-    version_gid: rubygem.versions.where(number:, platform:).first&.to_gid,
-    yanked_by: "Yanker", actor_gid: version_gid&.find&.yanker&.to_gid)
-    event = FactoryBot.build(:events_rubygem_event, rubygem:, tag: Events::RubygemEvent::VERSION_YANKED, additional:
-    {
-      number:,
-      platform:,
-      yanked_by:,
+  def default(rubygem: Rubygem.first!, **additional)
+    additional[:number] ||= "0.0.1"
+    additional[:platform] ||= "ruby"
+    version = rubygem.versions.find_by(additional.slice(:number, :platform))
+    additional[:version_gid] ||= version&.to_gid
+    additional[:actor_gid] ||= version&.yanker&.to_gid
+    additional[:yanked_by] ||= "Yanker"
 
-      version_gid:,
-      actor_gid:
-    })
-    render Events::RubygemEvent::Version::YankedComponent.new(
-      event:
-    )
+    event = FactoryBot.build(:events_rubygem_event, rubygem:, tag: Events::RubygemEvent::VERSION_YANKED, additional:)
+    render Events::RubygemEvent::Version::YankedComponent.new(event:)
   end
 end
