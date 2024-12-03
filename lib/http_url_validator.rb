7308cc4a8bc49afe11fcbdbe569da94750551ba7
Jacklyn Ma <29336370+jacklynhma@users.noreply.github.com>
2024-12-03 13:45:34 -0500

    Fix indentation

--- lib/http_url_validator.rb
+++ lib/http_url_validator.rb
@@ -3,6 +3,6 @@ class HttpUrlValidator < ActiveModel::EachValidator
     uri = URI::DEFAULT_PARSER.parse(value)
     record.errors.add attribute, "is not a valid URL" unless [URI::HTTP, URI::HTTPS].member?(uri.class)
   rescue URI::InvalidURIError
-      record.errors.add attribute, "is not a valid URL"
+    record.errors.add attribute, "is not a valid URL"
   end
 end
