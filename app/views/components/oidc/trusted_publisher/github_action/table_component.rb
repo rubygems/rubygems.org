ab6d524aa6e735f0b52512827d592c8a7e8bb6d3
Martin Emde <me@martinemde.com>
2025-01-11 10:12:44 -0800

    fix CSS classname

--- app/views/components/oidc/trusted_publisher/github_action/table_component.rb
+++ app/views/components/oidc/trusted_publisher/github_action/table_component.rb
@@ -3,14 +3,14 @@ class OIDC::TrustedPublisher::GitHubAction::TableComponent < ApplicationComponen
 
   def view_template
     dl(class: "tw-flex tw-flex-col sm:tw-grid sm:tw-grid-cols-2 tw-items-baseline tw-gap-4 full-width overflow-wrap") do
-      dt(class: "adoption__heading ") { "GitHub Repository" }
+      dt(class: "orange_text ") { "GitHub Repository" }
       dd { code { github_action.repository } }
 
-      dt(class: "adoption__heading ") { "Workflow Filename" }
+      dt(class: "orange_text ") { "Workflow Filename" }
       dd { code { github_action.workflow_filename } }
 
       if github_action.environment?
-        dt(class: "adoption__heading") { "Environment" }
+        dt(class: "orange_text") { "Environment" }
         dd { code { github_action.environment } }
       end
     end
