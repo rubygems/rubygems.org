dd2c0645c4dac81d9c325c5d5eedcb165ae30995
Ali Kabeel <alik@bendingspoons.com>
2025-02-09 23:27:30 +0200

    fix: ensure COOP header is set correctly on all pages

---
+++ lib/gemcutter/middleware/security_headers_middleware.rb
@@ -0,0 +1,14 @@
+class SecurityHeadersMiddleware
+    def initialize(app)
+      @app = app
+    end
+  
+    def call(env)
+      status, headers, response = @app.call(env)
+  
+      # Ensure COOP is set for all responses (HTML, APIs, static files, errors)
+      headers["Cross-Origin-Opener-Policy"] = "same-origin"
+  
+      [status, headers, response]
+    end
+  end
