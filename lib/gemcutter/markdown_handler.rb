ef32f32763df1fb9719ed77318fdc2ac9f97911d
Samuel Giddins <segiddins@segiddins.me>
2025-03-11 20:56:07 -0700

    RuboCop autocorrect
    Signed-off-by: Samuel Giddins <segiddins@segiddins.me>

--- lib/gemcutter/markdown_handler.rb
+++ lib/gemcutter/markdown_handler.rb
@@ -1,11 +1,9 @@
-module Gemcutter
-  module MarkdownHandler
-    def self.erb
-      @erb ||= ActionView::Template.registered_template_handler(:erb)
-    end
+module Gemcutter::MarkdownHandler
+  def self.erb
+    @erb ||= ActionView::Template.registered_template_handler(:erb)
+  end
 
-    def self.call(template, source)
-      "@title = t('.title'); prose { Kramdown::Document.new(#{source.dump}).to_html.html_safe }"
-    end
+  def self.call(_template, source)
+    "@title = t('.title'); prose { Kramdown::Document.new(#{source.dump}).to_html.html_safe }"
   end
 end
