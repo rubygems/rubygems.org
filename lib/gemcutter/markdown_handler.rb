6bf5cecbdc53da5ebad015fd91b3e9211bfa50ca
Samuel Giddins <segiddins@segiddins.me>
2025-03-12 11:33:56 -0700

    Rewrite policy index in erb for automatic links
    Signed-off-by: Samuel Giddins <segiddins@segiddins.me>

--- lib/gemcutter/markdown_handler.rb
+++ lib/gemcutter/markdown_handler.rb
@@ -1,8 +1,4 @@
 module Gemcutter::MarkdownHandler
-  def self.erb
-    @erb ||= ActionView::Template.registered_template_handler(:erb)
-  end
-
   def self.call(_template, source)
     "@title = t('.title'); prose { Kramdown::Document.new(#{source.dump}).to_html.html_safe }"
   end
