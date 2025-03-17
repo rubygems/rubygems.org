8bd4f183579613a25b77bb97d88131dcca90360c
Josef Šimánek <josef.simanek@gmail.com>
2025-03-17 06:57:17 +0100

    Fix flaky encoding/search tests.

--- test/integration/api/v1/search_test.rb
+++ test/integration/api/v1/search_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class Api::V1::Search < ActionDispatch::IntegrationTest
+  include SearchKickHelper
+
   test "request with non-string query shows bad request" do
+    import_and_refresh
+
     get "/api/v1/search.json?query[]="
 
     assert_response :bad_request
