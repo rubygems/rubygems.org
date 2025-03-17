8033b1a1c6bd24846bd4b0c0d3c1323f3e751c22
Josef Šimánek <josef.simanek@gmail.com>
2025-03-17 04:25:31 +0100

    Fix WebAuthn warning.

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -1,11 +1,11 @@
 WebAuthn.configure do |config|
-  config.origin = [if Rails.env.development?
-                     ENV.fetch("WEBAUTHN_ORIGIN", "http://localhost:3000")
-                   elsif Rails.env.test?
-                     "#{Gemcutter::PROTOCOL}://#{Gemcutter::HOST}:31337"
-                   else
-                     "#{Gemcutter::PROTOCOL}://#{Gemcutter::HOST}"
-                   end]
+  config.allowed_origins = [if Rails.env.development?
+                              ENV.fetch("WEBAUTHN_ORIGIN", "http://localhost:3000")
+                            elsif Rails.env.test?
+                              "#{Gemcutter::PROTOCOL}://#{Gemcutter::HOST}:31337"
+                            else
+                              "#{Gemcutter::PROTOCOL}://#{Gemcutter::HOST}"
+                            end]
   config.rp_name = Gemcutter::HOST_DISPLAY
   # config.rp_id = Gemcutter::HOST
 end
