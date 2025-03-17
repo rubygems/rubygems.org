2761635391c1bcd0f2fa03fa7b1556e90c3e2baa
Josef Šimánek <josef.simanek@gmail.com>
2025-03-17 06:57:17 +0100

    Fix flaky encoding/search tests.

--- test/integration/encoding_test.rb
+++ test/integration/encoding_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class EncodingTest < ActionDispatch::IntegrationTest
+  include SearchKickHelper
+
   test "invalid utf-8 characters should be sanitized" do
+    import_and_refresh
+
     get "/api/v1/search.json?query=vagrant%ADvbguest"
 
     assert_response :success
