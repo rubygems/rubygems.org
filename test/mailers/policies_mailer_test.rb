973e70e33632bb86c509395bf25221a91c0c6ced
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2025-04-28 05:00:05 +0000

    clean up tests

--- test/mailers/policies_mailer_test.rb
+++ test/mailers/policies_mailer_test.rb
@@ -15,6 +15,20 @@ class PoliciesMailerTest < ActionMailer::TestCase
     assert_equal I18n.t("policies_mailer.policy_update_announcement.subject", host: Gemcutter::HOST_DISPLAY), email.subject
   end
 
+  test "send policy announcement to user with blocked email" do
+    @user.email = "blocked@rubygems.org"
+    @user.blocked_email = "original-email@example.com"
+    @user.save!
+
+    PoliciesMailer.policy_update_announcement(@user).deliver_now
+
+    refute_empty ActionMailer::Base.deliveries
+    email = ActionMailer::Base.deliveries.last
+
+    assert_equal [@user.blocked_email], email.to
+    assert_equal I18n.t("policies_mailer.policy_update_announcement.subject", host: Gemcutter::HOST_DISPLAY), email.subject
+  end
+
   test "send policy update review closed" do
     email = PoliciesMailer.policy_update_review_closed(@user)
     assert_emails 1 do
@@ -22,6 +36,20 @@ class PoliciesMailerTest < ActionMailer::TestCase
     end
 
     assert_equal [@user.email], email.to
+    assert_equal I18n.t("policies_mailer.policy_update_review_closed.subject", host: Gemcutter::HOST_DISPLAY), email.subject
+  end
+
+  test "send policy review closed to user with blocked email" do
+    @user.email = "blocked@rubygems.org"
+    @user.blocked_email = "original-email@example.com"
+    @user.save!
+
+    PoliciesMailer.policy_update_review_closed(@user).deliver_now
+
+    refute_empty ActionMailer::Base.deliveries
+    email = ActionMailer::Base.deliveries.last
+
+    assert_equal [@user.blocked_email], email.to
     assert_equal I18n.t("policies_mailer.policy_update_review_closed.subject", host: Gemcutter::HOST_DISPLAY), email.subject
   end
 end
