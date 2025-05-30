4e6d5f3d20e2b114a2a0e1461df7bb9984faadb9
Colby Swandale <996377+colby-swandale@users.noreply.github.com>
2025-05-30 02:35:12 +0000

    test the task scope for email policy

--- test/tasks/maintenance/policy_announcement_email_task_test.rb
+++ test/tasks/maintenance/policy_announcement_email_task_test.rb
@@ -9,6 +9,12 @@ class Maintenance::PolicyAnnouncementEmailTaskTest < ActiveSupport::TestCase
     @user = create(:user)
   end
 
+  test "scoping the task to all users" do
+    task = Maintenance::PolicyAnnouncementEmailTask.new
+
+    assert_equal User.all, task.collection
+  end
+
   test "places the background task in the correct queue" do
     assert_enqueued_with(job: ActionMailer::MailDeliveryJob, queue: :within_24_hours) do
       Maintenance::PolicyAnnouncementEmailTask.process(@user)
