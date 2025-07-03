d9b6bd7b93e50c7a59444f5f2a885a4aeddef7ff
Landon Grindheim <landon.grindheim@gmail.com>
2025-07-03 09:40:50 -0400

    😅 Rectify LaunchDarkly state (#5782)
    * Re-introduce StoreVersionContentsJob calling
    
    This had been enabled in LaunchDarkly before the gem was removed.
    
    * Remove previously-gated StatsD reporting
    
    These had been configured to be disabled in LaunchDarkly, but the gem
    was removed. Removing the calls now.
    
    * Prevent job from running in test env
    
    This is a fast followup re-introducing behavior that was configured in a
    service, but removed.
    
    * Remove assertions around dead code
    
    These have been disabled in production for some time. Since we're not
    using them, let's get rid of them. We can always add them back when
    needed.
    
    * Always run StoreVersionContentsJob
    
    This had been enabled in production, but that was not reflected in
    tests. I've stubbed StoreVersionContentsJob in VersionsSystemTest, which
    I don't love. I'll look into related test setup and see what needs to be
    done to actually exercise the code.

--- app/jobs/good_job_statsd_job.rb
+++ app/jobs/good_job_statsd_job.rb
@@ -11,46 +11,11 @@ class GoodJobStatsDJob < ApplicationJob
 
   def perform
     filter = Filter.new({})
-    now = Time.now.utc
 
     state_counts = filter.states
     gauge "count", state_counts
 
-    state_staleness = state_counts.each_key.index_with do |state|
-      case state
-      when "scheduled", "queued" # not started jobs don't have discrete execution entry yet
-        columns = %w[executions_good_jobs.scheduled_at executions_good_jobs.created_at]
-        relation = :executions
-      when "retried", "running", "succeeded", "discarded"
-        columns = %w[good_job_executions.scheduled_at good_job_executions.created_at]
-        relation = :discrete_executions
-      else raise "unknown GoodJob state '#{state}'"
-      end
-
-      staleness(now, filter.filtered_query(state:), columns, relation)
-    end
-    gauge "staleness", state_staleness
-
-    state_latest_execution = state_counts.each_key.index_with do |state|
-      columns = %w[good_job_executions.performed_at good_job_executions.finished_at good_job_executions.scheduled_at good_job_executions.created_at]
-      staleness(now, filter.filtered_query(state:), columns, :discrete_executions)
-    end
-    gauge "latest_execution", state_latest_execution
-
     nil
-  end
-
-  def staleness(now, filtered_query, columns, joined_relation)
-    filtered_query.joins(joined_relation).then do |rel|
-      rel.pluck(
-        *rel.group_values,
-        Arel::Nodes::Max.new(
-          [Arel::Nodes.build_quoted(now, rel.arel_table[:created_at]) -
-            rel.arel_table.coalesce(*rel.send(:arel_columns, columns))]
-        )
-      )
-        .to_h { |*a, v| [a, v] }
-    end
   end
 
   def gauge(key, values)
