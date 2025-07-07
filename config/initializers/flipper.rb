babfec92441b400f4eb7a810c9c75d849c2686c4
Landon Grindheim <landon.grindheim@gmail.com>
2025-07-07 12:51:25 -0400

    Make placeholder text self-documenting

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -7,7 +7,7 @@ end
 Flipper::UI.configure do |ui_config|
   ui_config.banner_text = "#{Rails.env.capitalize} Environment"
 
-  ui_config.add_actor_placeholder = "Enter User;handle OR Organization;handle"
+  ui_config.add_actor_placeholder = "Enter user:handle OR org:handle"
   ui_config.actor_names_source = lambda do |keys|
     # keys are like ["User;john_doe", "Organization;acme_corp"]
     keys.each_with_object({}) do |key, hash|
