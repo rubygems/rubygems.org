2a0df610704c26ed541414beaaa15b58ccbac587
Landon Grindheim <landon.grindheim@gmail.com>
2025-08-20 12:13:55 -0400

    Move method definition out of `prepended` block

--- app/controllers/concerns/maintenance_tasks_auditable.rb
+++ app/controllers/concerns/maintenance_tasks_auditable.rb
@@ -5,27 +5,27 @@ module MaintenanceTasksAuditable
     include Auditable
 
     around_action :audit_action
+  end
 
-    def audit_action(&)
-      return yield if params[:action].in?(%w[show index])
+  def audit_action(&)
+    return yield if params[:action].in?(%w[show index])
 
-      action = params.fetch(:action)
-      task_name = params.fetch(:task_id)
+    action = params.fetch(:action)
+    task_name = params.fetch(:task_id)
 
-      action_name = "Manual #{action} of #{task_name}"
+    action_name = "Manual #{action} of #{task_name}"
 
-      run = @run
+    run = @run
 
-      value, _audit = in_audited_transaction(
-        auditable: run || ->(changed_records:) { changed_records.keys.grep(MaintenanceTasks::Run).sole },
-        admin_github_user: admin_user,
-        action: action_name,
-        fields: params.slice(:comment).reverse_merge(comment: action_name),
-        arguments: params,
-        models: [run].compact,
-        &
-      )
-      value
-    end
+    value, _audit = in_audited_transaction(
+      auditable: run || ->(changed_records:) { changed_records.keys.grep(MaintenanceTasks::Run).sole },
+      admin_github_user: admin_user,
+      action: action_name,
+      fields: params.slice(:comment).reverse_merge(comment: action_name),
+      arguments: params,
+      models: [run].compact,
+      &
+    )
+    value
   end
 end
