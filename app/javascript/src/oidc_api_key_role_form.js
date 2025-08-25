2ef9ba0bf2be0caad6e11c056942130a5752315c
Martin Emde <me@martinemde.com>
2025-08-24 20:52:44 -0700

    Remove jquery

--- app/javascript/src/oidc_api_key_role_form.js
+++ app/javascript/src/oidc_api_key_role_form.js
@@ -1,34 +1,25 @@
-import $ from "jquery";
+document.addEventListener("DOMContentLoaded", () => {
+  // Remove nested fields
+  document.addEventListener("click", (e) => {
+    const removeButton = e.target.closest("button.form__remove_nested_button");
+    if (!removeButton) return;
+    e.preventDefault();
+    const nestedField = removeButton.closest(".form__nested_fields");
+    if (nestedField) nestedField.remove();
+  });
 
-$(function () {
-  function wire() {
-    var removeNestedButtons = $("button.form__remove_nested_button");
-
-    removeNestedButtons.off("click");
-    removeNestedButtons.click(function (e) {
-      e.preventDefault();
-      var button = $(this);
-      var nestedField = button.closest(".form__nested_fields");
-
-      nestedField.remove();
-    });
-
-    var addNestedButtons = $("button.form__add_nested_button");
-    addNestedButtons.off("click");
-    addNestedButtons.click(function (e) {
-      e.preventDefault();
-      var button = $(this);
-      var nestedFields = button.siblings("template.form__nested_fields");
-
-      var content = nestedFields
-        .html()
-        .replace(/NEW_OBJECT/g, new Date().getTime());
-
-      $(content).insertAfter(button.siblings().last());
-
-      wire();
-    });
-  }
-
-  wire();
+  // Add nested fields
+  document.addEventListener("click", (e) => {
+    const addButton = e.target.closest("button.form__add_nested_button");
+    if (!addButton) return;
+    e.preventDefault();
+    const parent = addButton.parentElement;
+    if (!parent) return;
+    const templateSibling = Array.from(parent.children).find((el) =>
+      el.matches("template.form__nested_fields"),
+    );
+    if (!templateSibling) return;
+    const html = templateSibling.innerHTML.replace(/NEW_OBJECT/g, Date.now());
+    parent.insertAdjacentHTML("beforeend", html);
+  });
 });
--- app/javascript/src/transitive_dependencies.js
+++ app/javascript/src/transitive_dependencies.js
@@ -1,24 +1,23 @@
-import $ from "jquery";
-
-$(document).on("click", ".deps_expanded-link", function () {
+document.addEventListener("click", function (event) {
+  const trigger = event.target.closest(".deps_expanded-link");
+  if (!trigger) return;
+  event.preventDefault();
   try {
-    var current = $(this);
-    var gem_id = this.dataset.gemId;
-    var version_id = this.dataset.version;
-    const url =
-      "/gems/" + gem_id + "/versions/" + version_id + "/dependencies.json";
-    $.ajax({
-      type: "get",
-      url: url,
-      success: function (resp) {
-        renderDependencies(resp, current);
-      },
-      error: function () {
-        var error_message =
+    const gemId = trigger.dataset.gemId;
+    const versionId = trigger.dataset.version;
+    const url = `/gems/${gemId}/versions/${versionId}/dependencies.json`;
+    fetch(url, { method: "GET" })
+      .then((response) => response.json())
+      .then((resp) => {
+        renderDependencies(resp, trigger);
+      })
+      .catch(() => {
+        const error_message =
           "<ul class='deps_item--error'>Request failed. please reload the page and try again</ul>";
-        current.parent().next().next().html(error_message);
-      },
-    });
+        const container =
+          trigger.parentElement.nextElementSibling.nextElementSibling;
+        if (container) container.innerHTML = error_message;
+      });
   } catch (e) {
     alert(e);
   }
@@ -31,29 +30,39 @@ function renderDependencies(resp, current) {
 }
 
 function arrow_toggler(current) {
-  var toggler =
+  const toggler =
     "<span class='deps_expanded arrow_toggle deps_expanded-down'></span>";
-  current.parent().html(toggler);
+  const parent = current.parentElement;
+  if (parent) parent.innerHTML = toggler;
 }
 
 function scope_display(current, deps, scope) {
-  if (deps.length != 0) {
-    var new_gems = current.parent().next().next();
-    if (scope == "development") {
-      new_gems = new_gems.next();
+  if (deps.length !== 0) {
+    let new_gems = current.parentElement.nextElementSibling.nextElementSibling;
+    if (scope === "development") {
+      new_gems = new_gems.nextElementSibling;
     }
-    new_gems.find(".deps_scope").append(deps);
+    const scopeContainer = new_gems.querySelector(".deps_scope");
+    if (scopeContainer) scopeContainer.insertAdjacentHTML("beforeend", deps);
   }
 }
 
-$(document).on("click", ".scope", function () {
-  $(this).toggleClass("scope--expanded");
-  $(this).next().toggleClass("deps_toggle");
+document.addEventListener("click", function (event) {
+  const el = event.target.closest(".scope");
+  if (!el) return;
+  el.classList.toggle("scope--expanded");
+  if (el.nextElementSibling)
+    el.nextElementSibling.classList.toggle("deps_toggle");
 });
 
-$(document).on("click", ".arrow_toggle", function () {
-  var runtime_div = $(this).parent().next().next();
-  runtime_div.toggleClass("deps_toggle");
-  runtime_div.next().toggleClass("deps_toggle");
-  $(this).toggleClass("deps_expanded-down");
+document.addEventListener("click", function (event) {
+  const el = event.target.closest(".arrow_toggle");
+  if (!el) return;
+  const runtime_div = el.parentElement.nextElementSibling.nextElementSibling;
+  if (runtime_div) {
+    runtime_div.classList.toggle("deps_toggle");
+    if (runtime_div.nextElementSibling)
+      runtime_div.nextElementSibling.classList.toggle("deps_toggle");
+  }
+  el.classList.toggle("deps_expanded-down");
 });
