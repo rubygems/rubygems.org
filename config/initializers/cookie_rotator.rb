041e9e625145d4869808e4941904499db126ea44
Landon Grindheim <landon.grindheim@gmail.com>
2025-10-09 15:12:46 -0400

    Prepare for (graceful) secret_key_base rotation
    By extending the existing cookie_rotator.rb (used to migrate to
    SHA-256), we can achieve just-in-time rotation for sessions. This should
    keep users from being logged out.
    
    I've introduced `PREVIOUS_SECRET_KEY_BASE`, and set it to the current
    value of secret_key_base so that we can make a minimal change when
    rotating. This _should_ allow us to rotate when needed going forward as
    well.

--- config/initializers/cookie_rotator.rb
+++ config/initializers/cookie_rotator.rb
@@ -1,14 +1,22 @@
 Rails.application.config.after_initialize do
   Rails.application.config.action_dispatch.cookies_rotations.tap do |cookies|
     salt = Rails.application.config.action_dispatch.authenticated_encrypted_cookie_salt
-    secret_key_base = Rails.application.secret_key_base
 
-    key_generator = ActiveSupport::KeyGenerator.new(
-      secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1
-    )
-    key_len = ActiveSupport::MessageEncryptor.key_len
-    secret = key_generator.generate_key(salt, key_len)
+    # Add rotation for previous secret_key_base (for graceful key rotation without user logout)
+    previous_secret_key_base = ENV['PREVIOUS_SECRET_KEY_BASE']
+    if previous_secret_key_base.present?
+      # Rotate encrypted cookies with old secret
+      old_key_generator = ActiveSupport::KeyGenerator.new(
+        previous_secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1
+      )
+      key_len = ActiveSupport::MessageEncryptor.key_len
+      old_secret = old_key_generator.generate_key(salt, key_len)
+      cookies.rotate :encrypted, old_secret
 
-    cookies.rotate :encrypted, secret
+      # Rotate session cookies with old secret
+      session_salt = Rails.application.config.action_dispatch.signed_cookie_salt
+      old_session_secret = old_key_generator.generate_key(session_salt, 64)
+      cookies.rotate :signed, old_session_secret
+    end
   end
 end
