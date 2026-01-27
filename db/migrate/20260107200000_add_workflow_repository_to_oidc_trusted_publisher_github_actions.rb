79429ef9e3e821180be50323350f8f17f9b83f61
Jenny Shen <jenny.shen@shopify.com>
2026-01-26 19:48:52 -0500

    Wrap safety_assured in AddWorkflowRepositoryToOIDCTrustedPublisherGitHubActions migration

--- db/migrate/20260107200000_add_workflow_repository_to_oidc_trusted_publisher_github_actions.rb
+++ db/migrate/20260107200000_add_workflow_repository_to_oidc_trusted_publisher_github_actions.rb
@@ -1,8 +1,10 @@
 class AddWorkflowRepositoryToOIDCTrustedPublisherGitHubActions < ActiveRecord::Migration[8.0]
   def change
-    change_table :oidc_trusted_publisher_github_actions, bulk: true do |t|
-      t.string :workflow_repository_owner
-      t.string :workflow_repository_name
+    safety_assured do
+      change_table :oidc_trusted_publisher_github_actions, bulk: true do |t|
+        t.string :workflow_repository_owner
+        t.string :workflow_repository_name
+      end
     end
   end
 end
