3f81507363bcd9d5be2239979934852c7ba2deeb
brian-penguin <tenggren@thoughtbot.com>
2026-06-03 13:43:01 -0400

    Updates for the Homepage!
    Why this change is being made:
    - This is kind of an all in one fell-swoop change to match the new "B"
      designs from [Figma
      prototype](https://www.figma.com/proto/glT62VyAlJak6eh9d0KRSb/%F0%9F%92%8E-RubyGems?node-id=4565-30141&viewport=13690%2C396%2C1.95&t=GIWjDCMSPBCPs9T6-0&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=4565%3A30141&show-proto-sidebar=1)
    
    What were the changes made to support this:
    - Add two new SVGs a health_cross and a favorite. These are used in the
      boxes below the hero section
    - Use the new hammy layout (NOTE: we will probably not want to show the
      breadcrumb lead on the homepage)
    - use i18n for the text in the new Donation callout
    
    Axe violation fix: Unique ID / tag for the search
    Why this change is being made:
    - Aria labels and id elements must be Unique on the page. This is the
      only page which has both the nav gem search and the middle page gem
      search.
    
    What were the changes made to support this:
    - Add a unique tag for the search element in the middle of the page
    
    - Update the specs to reflect homepage changes

--- test/system/autocompletes_test.rb
+++ test/system/autocompletes_test.rb
@@ -12,12 +12,14 @@ class AutocompletesTest < ApplicationSystemTestCase
     create(:version, :reindex, rubygem: rubygem, indexed: true)
 
     visit root_path
-    @fill_field = find_by_id "query"
+    @fill_field = find_by_id "homepage_gem_query"
+    @form = @fill_field.ancestor("form")
     @fill_field.set "rubo"
-    page.find(".autocomplete-done", wait: Capybara.default_max_wait_time)
+    # Wait for the autocomplete request to populate the listbox before each test.
+    @form.assert_selector "[role='option']", minimum: 1
   end
 
-  test "search field" do
+  test "submitting the field runs a search" do
     @fill_field.set "rubocop"
     click_on class: "home__search__icon"
 
@@ -83,8 +85,96 @@ class AutocompletesTest < ApplicationSystemTestCase
 
   test "mouse click a suggestion item to submit" do
     first("li", text: "rubocop").click
+    @form.click_button
 
     assert_current_path search_path, ignore_query: true
     assert_text "rubocop"
+  end
+
+  test "only one suggestion is selected when hovering" do
+    suggestion_options.each(&:hover)
+
+    assert_single_active_suggestion
+  end
+
+  test "only one suggestion is selected when using arrow keys" do
+    @fill_field.send_keys :down
+    @form.assert_selector "#{SUGGESTIONS}[aria-activedescendant]"
+    @fill_field.send_keys :down
+
+    assert_single_active_suggestion
+  end
+
+  test "suggestions don't appear when the gem does not exist" do
+    @fill_field.set "ruxyz"
+
+    @form.assert_no_selector "[role='option']"
+  end
+
+  test "suggestions don't appear unless the search field is focused" do
+    find("h1").click
+
+    @form.assert_no_selector "[role='option']"
+  end
+
+  test "down arrow key fills the field with a suggestion" do
+    @fill_field.send_keys :down
+
+    assert_no_field "homepage_gem_query", with: "rubo"
+  end
+
+  test "up arrow key fills the field with a suggestion" do
+    @fill_field.send_keys :up
+
+    assert_no_field "homepage_gem_query", with: "rubo"
+  end
+
+  test "down arrow key should loop" do
+    @fill_field.send_keys :down, :down, :down, :down
+    @form.assert_selector "#{SUGGESTIONS}[aria-activedescendant]"
+
+    assert_equal suggestion_options.last["id"], active_descendant
+  end
+
+  test "up arrow key should loop" do
+    @fill_field.send_keys :up, :up, :up, :up
+    @form.assert_selector "#{SUGGESTIONS}[aria-activedescendant]"
+
+    assert_equal suggestion_options.first["id"], active_descendant
+  end
+
+  test "hovering a suggestion selects it" do
+    @form.first("[role='option']", text: "rubocop").hover
+
+    @form.assert_selector "#{SUGGESTIONS}[aria-activedescendant]"
+  end
+
+  test "clicking a suggestion submits the search" do
+    @form.first("[role='option']", text: "rubocop").click
+
+    assert_current_path search_path, ignore_query: true
+    assert_text "rubocop"
+  end
+
+  private
+
+  SUGGESTIONS = "[data-autocomplete-target='suggestions']"
+
+  def suggestions_list
+    @form.find(SUGGESTIONS)
+  end
+
+  def suggestion_options
+    suggestions_list.all("[role='option']", minimum: 0)
+  end
+
+  def active_descendant
+    suggestions_list["aria-activedescendant"]
+  end
+
+  def assert_single_active_suggestion
+    @form.assert_selector "#{SUGGESTIONS}[aria-activedescendant]"
+
+    assert_equal(1, suggestion_options.count { |option| option["id"] == active_descendant })
   end
 end
