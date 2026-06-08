7f7b638ca59909da1888a8b730ce5332516dde8b
brian-penguin <tenggren@thoughtbot.com>
2026-06-08 11:42:48 -0400

    Update the specs to reflect hompeage changes
    Why this change is being made:
    - We need to change the path to finding the search box and the
      suggestions drop down list
    - It's now "Sign In" not "SIGN IN" in the button on the hompeage

--- test/system/autocompletes_test.rb
+++ test/system/autocompletes_test.rb
@@ -12,79 +12,105 @@ class AutocompletesTest < ApplicationSystemTestCase
     create(:version, :reindex, rubygem: rubygem, indexed: true)
 
     visit root_path
-    @fill_field = find_by_id "query"
+    @fill_field = find_by_id "homepage_gem_query"
+    @form = @fill_field.ancestor("form")
     @fill_field.set "rubo"
-    page.find(".autocomplete-done", wait: Capybara.default_max_wait_time)
+    # Wait for the autocomplete request to populate the listbox before each test.
+    @form.assert_selector "[role='option']", minimum: 1
   end
 
-  test "search field" do
+  test "submitting the field runs a search" do
     @fill_field.set "rubocop"
-    click_on class: "home__search__icon"
+    @form.click_button
 
-    assert_text "search"
+    assert_current_path search_path, ignore_query: true
     assert_text "rubocop"
   end
 
-  test "selected field is only one with cursor selecting" do
-    find(".suggest-list").all("li").each(&:hover) # rubocop:disable Rails/FindEach
+  test "only one suggestion is selected when hovering" do
+    suggestion_options.each(&:hover)
 
-    assert_selector ".selected", count: 1
+    assert_single_active_suggestion
   end
 
-  test "selected field is only one with arrow key selecting" do
+  test "only one suggestion is selected when using arrow keys" do
     @fill_field.send_keys :down
-    find ".selected"
+    @form.assert_selector "#{SUGGESTIONS}[aria-activedescendant]"
     @fill_field.send_keys :down
 
-    assert_selector ".selected", count: 1
+    assert_single_active_suggestion
   end
 
-  test "suggest list doesn't appear with gem not existing" do
+  test "suggestions don't appear when the gem does not exist" do
     @fill_field.set "ruxyz"
 
-    assert_selector ".menu-item", count: 0
+    @form.assert_no_selector "[role='option']"
   end
 
-  test "suggest list doesn't appear unless the search field is focused" do
+  test "suggestions don't appear unless the search field is focused" do
     find("h1").click
 
-    assert_selector ".menu-item", count: 0
+    @form.assert_no_selector "[role='option']"
   end
 
-  test "down arrow key to choose suggestion" do
+  test "down arrow key fills the field with a suggestion" do
     @fill_field.send_keys :down
 
-    assert page.has_no_field? "query", with: "rubo"
+    assert_no_field "homepage_gem_query", with: "rubo"
   end
 
-  test "up arrow key to choose suggestion" do
+  test "up arrow key fills the field with a suggestion" do
     @fill_field.send_keys :up
 
-    assert page.has_no_field? "query", with: "rubo"
+    assert_no_field "homepage_gem_query", with: "rubo"
   end
 
   test "down arrow key should loop" do
     @fill_field.send_keys :down, :down, :down, :down
+    @form.assert_selector "#{SUGGESTIONS}[aria-activedescendant]"
 
-    assert_selector ".suggest-list .menu-item:last-child.selected"
+    assert_equal suggestion_options.last["id"], active_descendant
   end
 
   test "up arrow key should loop" do
     @fill_field.send_keys :up, :up, :up, :up
+    @form.assert_selector "#{SUGGESTIONS}[aria-activedescendant]"
 
-    assert_selector ".suggest-list .menu-item:first-child.selected"
+    assert_equal suggestion_options.first["id"], active_descendant
   end
 
-  test "mouse hover a suggest item to choose suggestion" do
-    first("li", text: "rubocop").hover
+  test "hovering a suggestion selects it" do
+    @form.first("[role='option']", text: "rubocop").hover
 
-    assert_selector ".selected"
+    @form.assert_selector "#{SUGGESTIONS}[aria-activedescendant]"
   end
 
-  test "mouse click a suggestion item to submit" do
-    first("li", text: "rubocop").click
+  test "clicking a suggestion submits the search" do
+    @form.first("[role='option']", text: "rubocop").click
 
     assert_current_path search_path, ignore_query: true
     assert_text "rubocop"
+  end
+
+  private
+
+  SUGGESTIONS = "[data-autocomplete-target='suggestions']"
+
+  def suggestions_list
+    @form.find(SUGGESTIONS)
+  end
+
+  def suggestion_options
+    suggestions_list.all("[role='option']", minimum: 0)
+  end
+
+  def active_descendant
+    suggestions_list["aria-activedescendant"]
+  end
+
+  def assert_single_active_suggestion
+    @form.assert_selector "#{SUGGESTIONS}[aria-activedescendant]"
+
+    assert_equal(1, suggestion_options.count { |option| option["id"] == active_descendant })
   end
 end
