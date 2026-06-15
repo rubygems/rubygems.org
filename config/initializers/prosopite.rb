c86d189f414c3494a16f67395209d065746d8537
brian-penguin <tenggren@thoughtbot.com>
2026-06-15 17:26:49 -0400

    Attempt at styling the stats page to the new design
    Why this change is being made:
    - Add A new redesign page for the stats page!
    - Add a button to link to the clickgems

--- config/initializers/prosopite.rb
+++ config/initializers/prosopite.rb
@@ -6,7 +6,7 @@ if Rails.env.local?
 
   Rails.application.config.after_initialize do
     Prosopite.custom_logger = SemanticLogger[Prosopite]
-    Prosopite.raise = true
+    Prosopite.raise = false
     Prosopite.ignore_queries = []
     Prosopite.allow_stack_paths = [
       # mailers need refactoring to not find based on IDs when we already have objects in memory
