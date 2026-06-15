bb6d6d1ed99e7335f682f8083c5f3a26e1aef889
brian-penguin <tenggren@thoughtbot.com>
2026-06-15 15:17:53 -0400

    Swap the /news and /releases to hammy layout
    Why this change is being made:
    - We want to use our new component everywhere!
    
    What were the changes made to support this:
    - Add new component and list out the gems

--- app/views/components/rubygem_component.rb
+++ app/views/components/rubygem_component.rb
@@ -14,7 +14,8 @@ class RubygemComponent < ApplicationComponent
   def view_template(&)
     link_to rubygem_path(@rubygem.name), class: LINK_CLASSES do
       div(class: "flex flex-row w-full items-center justify-between") do
-        h2(class: "text-b1 font-semibold text-neutral-900 dark:text-white group-hover:text-orange-500 transition-colors") do
+        h2(class: "text-b1 font-semibold text-neutral-900 dark:text-white group-hover:text-orange-500 transition-colors",
+           data: { testid: "rubygem-name" }) do
           plain @rubygem.name
         end
         version = latest_version_number(@rubygem)
