456090a4b8a5f044d378197cb0fd6b46c2486545
brian-penguin <tenggren@thoughtbot.com>
2026-06-26 11:41:49 -0400

    Undo my terrible rebase
    Why this change is being made:
    - Should have taken the updates not the HEAD

--- test/system/autocompletes_test.rb
+++ test/system/autocompletes_test.rb
@@ -21,70 +21,6 @@ class AutocompletesTest < ApplicationSystemTestCase
 
   test "submitting the field runs a search" do
     @fill_field.set "rubocop"
-    click_on class: "home__search__icon"
-
-    assert_text "Search"
-    assert_text "rubocop"
-  end
-
-  test "selected field is only one with cursor selecting" do
-    find(".suggest-list").all("li").each(&:hover) # rubocop:disable Rails/FindEach
-
-    assert_selector ".selected", count: 1
-  end
-
-  test "selected field is only one with arrow key selecting" do
-    @fill_field.send_keys :down
-    find ".selected"
-    @fill_field.send_keys :down
-
-    assert_selector ".selected", count: 1
-  end
-
-  test "suggest list doesn't appear with gem not existing" do
-    @fill_field.set "ruxyz"
-
-    assert_selector ".menu-item", count: 0
-  end
-
-  test "suggest list doesn't appear unless the search field is focused" do
-    find("h1").click
-
-    assert_selector ".menu-item", count: 0
-  end
-
-  test "down arrow key to choose suggestion" do
-    @fill_field.send_keys :down
-
-    assert page.has_no_field? "query", with: "rubo"
-  end
-
-  test "up arrow key to choose suggestion" do
-    @fill_field.send_keys :up
-
-    assert page.has_no_field? "query", with: "rubo"
-  end
-
-  test "down arrow key should loop" do
-    @fill_field.send_keys :down, :down, :down, :down
-
-    assert_selector ".suggest-list .menu-item:last-child.selected"
-  end
-
-  test "up arrow key should loop" do
-    @fill_field.send_keys :up, :up, :up, :up
-
-    assert_selector ".suggest-list .menu-item:first-child.selected"
-  end
-
-  test "mouse hover a suggest item to choose suggestion" do
-    first("li", text: "rubocop").hover
-
-    assert_selector ".selected"
-  end
-
-  test "mouse click a suggestion item to submit" do
-    first("li", text: "rubocop").click
     @form.click_button
 
     assert_current_path search_path, ignore_query: true
@@ -108,13 +44,13 @@ class AutocompletesTest < ApplicationSystemTestCase
   test "suggestions don't appear when the gem does not exist" do
     @fill_field.set "ruxyz"
 
-    @form.assert_no_selector "[role='option']"
+    assert @form.has_no_selector?("[role='option']")
   end
 
   test "suggestions don't appear unless the search field is focused" do
     find("h1").click
 
-    @form.assert_no_selector "[role='option']"
+    assert @form.has_no_selector?("[role='option']")
   end
 
   test "down arrow key fills the field with a suggestion" do
@@ -144,9 +80,12 @@ class AutocompletesTest < ApplicationSystemTestCase
   end
 
   test "hovering a suggestion selects it" do
-    @form.first("[role='option']", text: "rubocop").hover
+    option = @form.first("[role='option']", text: "rubocop")
+    option.hover
 
     @form.assert_selector "#{SUGGESTIONS}[aria-activedescendant]"
+
+    assert_equal option["id"], active_descendant
   end
 
   test "clicking a suggestion submits the search" do
