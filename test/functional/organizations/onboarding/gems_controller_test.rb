c4176c6817c6ca2696c2dbad125da8f0f6d989d8
brian-penguin <tenggren@thoughtbot.com>
2026-07-08 15:33:34 -0400

    Setup the tests with a rubygem instead of relying on the namesake_rubygem
    Why this change is being made:
    - We won't have namesake_rubygems anymore
    - These specs expect that a gem has been selected in order to send the
      invitations
    
    What were the changes made to support this:
    - Cleanup cleanup everybody everywhere

--- test/functional/organizations/onboarding/gems_controller_test.rb
+++ test/functional/organizations/onboarding/gems_controller_test.rb
@@ -5,14 +5,12 @@ require "test_helper"
 class Organizations::Onboarding::GemsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = create(:user, :mfa_enabled)
-    @namesake_rubygem = create(:rubygem, owners: [@user])
     @gem = create(:rubygem, owners: [@user])
     @organization_onboarding = create(
       :organization_onboarding,
       created_by: @user,
-      namesake_rubygem: @namesake_rubygem,
       status: :pending,
-      organization_handle: @namesake_rubygem.name,
+      organization_handle: "existing-name",
       organization_name: "Existing Name"
     )
 
@@ -36,25 +34,28 @@ class Organizations::Onboarding::GemsControllerTest < ActionDispatch::Integratio
       patch organization_onboarding_gems_path(as: @user), params: { organization_onboarding: { rubygems: [@gem.id] } }
 
       assert_redirected_to organization_onboarding_users_path
-      assert_equal [@namesake_rubygem.id, @gem.id], @organization_onboarding.reload.rubygems
+      assert_equal [@gem.id], @organization_onboarding.reload.rubygems
     end
 
+    # TODO: BRIAN - we should prevent submission unless at least one gem is selected
     should "allow selecting no additional gems" do
       patch organization_onboarding_gems_path(as: @user)
 
       assert_redirected_to organization_onboarding_users_path
-      assert_equal [@namesake_rubygem.id], @organization_onboarding.reload.rubygems
+
+      assert_equal [], @organization_onboarding.reload.rubygems
     end
 
+    # TODO: BRIAN - we should prevent submission unless at least one gem is selected
     should "remove non namesake gems if rubygems param is empty" do
       @organization_onboarding.update!(rubygems: [@gem.id])
 
-      assert_equal [@namesake_rubygem.id, @gem.id], @organization_onboarding.reload.rubygems
+      assert_equal [@gem.id], @organization_onboarding.reload.rubygems
 
       patch organization_onboarding_gems_path(as: @user), params: { organization_onboarding: { rubygems: [""] } }
 
       assert_redirected_to organization_onboarding_users_path
-      assert_equal [@namesake_rubygem.id], @organization_onboarding.reload.rubygems
+      assert_equal [], @organization_onboarding.reload.rubygems
     end
 
     should "invalidate unknown gems" do
@@ -62,7 +63,7 @@ class Organizations::Onboarding::GemsControllerTest < ActionDispatch::Integratio
       patch organization_onboarding_gems_path(as: @user), params: { organization_onboarding: { rubygems: [notmygem.id] } }
 
       assert_response :unprocessable_content
-      assert_equal [@namesake_rubygem.id], @organization_onboarding.reload.rubygems
+      assert_equal [], @organization_onboarding.reload.rubygems
     end
   end
 end
