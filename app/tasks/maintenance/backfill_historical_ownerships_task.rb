5a7607772af280e43b935936a3b1cbd279ab41a3
Eric Mueller <nevinera@gmail.com>
2026-07-16 08:03:27 -0700

    Add backfill task for existing HistoricalOwnership records

---
+++ app/tasks/maintenance/backfill_historical_ownerships_task.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Maintenance::BackfillHistoricalOwnershipsTask < MaintenanceTasks::Task
+  attribute :min_ownership_id, :integer
+  attribute :max_ownership_id, :integer
+
+  def collection
+    scope = Ownership.all
+    scope = scope.where(id: min_ownership_id..) if min_ownership_id.present?
+    scope = scope.where(id: ..max_ownership_id) if max_ownership_id.present?
+    scope
+  end
+
+  def process(ownership)
+    HistoricalOwnership.find_or_create_by!(rubygem_id: ownership.rubygem_id, user_id: ownership.user_id, removed_at: nil) do |historical|
+      historical.role = ownership.role
+      historical.first_owned_at = ownership.created_at
+    end
+  end
+end
