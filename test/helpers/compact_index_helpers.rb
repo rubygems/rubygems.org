42a60f0fddeb2d1f390ca737d926346b89ffd5cd
Gira Chawda <gira.chawda@shopify.com>
2026-07-29 12:03:32 -0400

    Remove lingering compact index v1 version object
    Assisted-By: devx/1ff3122c-cbf6-4009-a89f-12ab20861492

--- test/helpers/compact_index_helpers.rb
+++ test/helpers/compact_index_helpers.rb
@@ -1,22 +1,19 @@
 # frozen_string_literal: true
 
 module CompactIndexHelpers
-  def build_version(version: 1, **args)
+  def build_version(**args)
     name = args.fetch(:name, "test_gem")
     number = args.fetch(:number, "1.0")
-    common = [
+
+    CompactIndex::GemVersionV2.new(
       number,
       args[:platform],
       args.fetch(:checksum, "sum+#{name}+#{number}"),
       args.fetch(:info_checksum, "info+#{name}+#{number}"),
       args[:dependencies],
       args[:ruby_version],
-      args[:rubygems_version]
-    ]
-    if version == 2
-      CompactIndex::GemVersionV2.new(*common, args[:created_at])
-    else
-      CompactIndex::GemVersion.new(*common)
-    end
+      args[:rubygems_version],
+      args[:created_at]
+    )
   end
 end
