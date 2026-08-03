7ff226dd5b01cb319baab772dc010e58a77fb328
brian-penguin <tenggren@thoughtbot.com>
2026-08-03 17:37:11 -0400

    Treat Organization Handles as their own type of handles
    Why this change is being made:
    - As we work towards a public rollout we want to reduce the surface
      area for problems in the Organization handle namespaces.
    
    What were the changes made to support this:
    - allow for a handle starting with a number (in this case 37signals)
    - dont allow for organization handles which are taken by User handles
      (I'm not 100% on this one as of currently since I know the use of
      service user accounts is something that happens)
    - Add a BUNCH of reserved words to the organization handle reserved
      list. This includes all our routes path segments, a list of prominent
      ruby/rails companies (minus the ones who have already added
      organizations), a bunch of words that look like they could belong to
      the app or rubygems namespaces, words that are tricky and annoying to
      sort out like "nil", and words that imply "trust" like "admin" or
      "safety"
    - Update the seeds to use gem-org as the local test org
    - Update the lib/patterns to include ORGANIZATION_HANDLE_PATTERN which
      is the same as the base handle pattern but allows for integers leading

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -6,9 +6,10 @@ class Organization < ApplicationRecord
   validates :handle, presence: true,
     uniqueness: { case_sensitive: false },
     length: { within: 2..40 },
-    format: { with: Patterns::HANDLE_PATTERN }
+    format: { with: Patterns::ORGANIZATION_HANDLE_PATTERN }
   validates :name, presence: true, length: { within: 2..255 }
   validate :handle_not_reserved
+  validate :unique_with_user_handle, if: :handle_changed?
 
   has_many :memberships, -> { where.not(confirmed_at: nil) }, dependent: :destroy, inverse_of: :organization
   has_many :unconfirmed_memberships, -> { where(confirmed_at: nil) }, class_name: "Membership", dependent: :destroy, inverse_of: :organization
@@ -57,8 +58,16 @@ class Organization < ApplicationRecord
   def handle_not_reserved
     return if handle.blank?
 
-    return unless Handle.reserved?(handle)
+    return unless Organization::Handle.reserved?(handle)
 
     errors.add(:handle, "is reserved and cannot be used")
+  end
+
+  # Prevent creating a organization if there's a User with the same handle
+  # TODO BRIAN: Is this a worth-while requirement?
+  def unique_with_user_handle
+    return if handle.blank?
+
+    errors.add(:handle, "has already been taken") if User.where("lower(handle) = lower(?)", handle).any?
   end
 end
