eb749d99d00ba5bc87a1e34bdb642dc08649bcb9
anton-bondaronok <anton.bondaronok@rubyroidlabs.com>
2026-08-04 15:21:03 +0200

    Add push notifications for organization members

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -13,7 +13,18 @@ class Membership < ApplicationRecord
 
   scope :with_minimum_role, ->(role) { where(role: Access.flag_for_role(role)...) }
 
+  scope :by_organization_handle, -> { joins(:organization).order("organizations.handle ASC") }
+
   validates :user, uniqueness: { scope: :organization }
+
+  def self.update_notifier(to_enable, to_disable, notifier_attr)
+    where(id: to_enable).update_all(notifier_attr => true) if to_enable.any?
+    where(id: to_disable).update_all(notifier_attr => false) if to_disable.any?
+  end
+
+  def self.update_push_notifier(to_enable_push, to_disable_push)
+    update_notifier(to_enable_push, to_disable_push, "push_notifier")
+  end
 
   before_create :set_invitation_expire_time
 
