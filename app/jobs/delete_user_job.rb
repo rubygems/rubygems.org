b40a01430de89c63cf8bff1447f4da6e30abab57
Colby Swandale <colby@rubygems.org>
2026-08-06 14:51:27 +1000

    Clarify self-service deletion notifications

--- app/jobs/delete_user_job.rb
+++ app/jobs/delete_user_job.rb
@@ -5,15 +5,20 @@ class DeleteUserJob < ApplicationJob
   queue_with_priority PRIORITIES.fetch(:profile_deletion)
 
   def perform(user:, actor:)
-    notify_user = actor == user
     email = user.email
     return if user.discarded?
     user.discard!
-    Mailer.deletion_complete(email).deliver_later if notify_user
+    Mailer.deletion_complete(email).deliver_later if self_service_deletion?(user, actor)
   rescue ActiveRecord::ActiveRecordError, Discard::RecordNotDiscarded => e
     # Catch the exception so we can log it, otherwise using `destroy` would give
     # us no hint as to why the deletion failed.
     Rails.error.report(e, context: { user: user.as_json, email: }, handled: true)
-    Mailer.deletion_failed(email).deliver_later if notify_user
+    Mailer.deletion_failed(email).deliver_later if self_service_deletion?(user, actor)
+  end
+
+  private
+
+  def self_service_deletion?(user, actor)
+    user == actor
   end
 end
