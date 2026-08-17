7ea131d53477d6cc1b398b2b9f488b2e8b7d4fb8
brian-penguin <tenggren@thoughtbot.com>
2026-08-17 11:42:38 -0400

    Validate that we cannot create a new reservation for an existing gem

--- app/models/gem_name_reservation.rb
+++ app/models/gem_name_reservation.rb
@@ -3,6 +3,7 @@
 class GemNameReservation < ApplicationRecord
   validates :name, uniqueness: { case_sensitive: false }, presence: true, length: { maximum: Gemcutter::MAX_FIELD_LENGTH }
   validate :downcase_name_check
+  validate :rubygem_name_available, if: :needs_name_validation?
 
   has_many :audits, as: :auditable, inverse_of: :auditable, dependent: :nullify
 
@@ -11,6 +12,15 @@ class GemNameReservation < ApplicationRecord
   end
 
   private
+
+  def needs_name_validation?
+    new_record? || name_changed?
+  end
+
+  def rubygem_name_available
+    return unless Rubygem.where("lower(name) = ?", name&.downcase).any?
+    errors.add(:name, "rubygem exists with name")
+  end
 
   def downcase_name_check
     return unless name.to_s != name.to_s.downcase
