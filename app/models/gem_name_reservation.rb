64ed9a36e4415c6d256103419ccdcf9cd354caa4
brian-penguin <tenggren@thoughtbot.com>
2026-08-19 15:52:54 -0400

    Set a limit of 25 for the number of org name reservations
    Why this change is being made:
    - This is so that we don't get too carried away. We will also allow
      there to be a feature toggle to override for unlimited
    
    What were the changes made to support this:
    -

--- app/models/gem_name_reservation.rb
+++ app/models/gem_name_reservation.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class GemNameReservation < ApplicationRecord
+  ORGANIZATION_LIMIT = 25
+
   validates :name, uniqueness: { case_sensitive: false }, presence: true, length: { maximum: Gemcutter::MAX_FIELD_LENGTH }
   validate :downcase_name_check
   validate :rubygem_name_available, if: :needs_name_validation?
+  validate :organization_within_limit, if: :needs_limit_validation?
 
   has_many :audits, as: :auditable, inverse_of: :auditable, dependent: :nullify
 
@@ -17,6 +20,17 @@ class GemNameReservation < ApplicationRecord
 
   def needs_name_validation?
     new_record? || name_changed?
+  end
+
+  def needs_limit_validation?
+    organization.present? && (new_record? || organization_id_changed?)
+  end
+
+  def organization_within_limit
+    return if organization.gem_name_reservations_unlimited?
+    return if organization.gem_name_reservations.count < ORGANIZATION_LIMIT
+
+    errors.add(:base, :organization_limit_reached, count: ORGANIZATION_LIMIT)
   end
 
   def rubygem_name_available
