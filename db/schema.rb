656d195e9eeee907e498e08a00ffefd6d06634a8
Colby Swandale <colby@rubygems.org>
2026-09-04 08:17:06 +0000

    Index case-insensitive Rubygem names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_04_071618) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_04_073906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "pg_catalog.plpgsql"
@@ -624,6 +624,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_04_071618) do
     t.string "name"
     t.bigint "organization_id"
     t.datetime "updated_at", precision: nil
+    t.index "lower((name)::text)", name: "index_rubygems_on_lower_name"
     t.index "regexp_replace(upper((name)::text), '[_-]'::text, ''::text, 'g'::text)", name: "dashunderscore_typos_idx"
     t.index "upper((name)::text) varchar_pattern_ops", name: "index_rubygems_upcase"
     t.index ["indexed"], name: "index_rubygems_on_indexed"
