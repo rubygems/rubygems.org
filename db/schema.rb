8aae2aaa751697c5ec79ff988ba8d6b272f375aa
anton-bondaronok <anton.bondaronok@rubyroidlabs.com>
2026-09-07 12:41:11 +0200

    Fix schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_25_080543) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_04_073906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "pg_catalog.plpgsql"
